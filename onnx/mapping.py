57a1b92f042489190330a1203bc2458300ee511a
Junjie Bai <bai@in.tum.de>
2017-10-05 13:10:48 -0700

    Expose varies mapping

--- onnx/mapping.py
+++ onnx/mapping.py
@@ -1,22 +1,50 @@
 from onnx.onnx_pb2 import TensorProto
-from numpy import np
+import numpy as np
 
-
-NP_TO_TENSOR = {
-    np.float32: TensorProto.FLOAT,
-    np.uint8: TensorProto.UINT8,
-    np.int8: TensorProto.INT8,
-    np.uint16: TensorProto.UINT16,
-    np.int16: TensorProto.INT16,
-    np.int32: TensorProto.INT32,
-    np.int64: TensorProto.INT64,
-    np.bool: TensorProto.BOOL,
-    np.float16: TensorProto.FLOAT16,
-    np.float64: TensorProto.DOUBLE,
-    np.complex64: TensorProto.COMPLEX64,
-    np.complex128: TensorProto.COMPLEX128,
-    np.uint32: TensorProto.UINT32,
-    np.uint64: TensorProto.UINT64,
+TENSOR_TYPE_TO_NP_TYPE = {
+    TensorProto.FLOAT: np.dtype('float32'),
+    TensorProto.UINT8: np.dtype('uint8'),
+    TensorProto.INT8: np.dtype('int8'),
+    TensorProto.UINT16: np.dtype('uint16'),
+    TensorProto.INT16: np.dtype('int16'),
+    TensorProto.INT32: np.dtype('int32'),
+    TensorProto.INT64: np.dtype('int64'),
+    TensorProto.BOOL: np.dtype('bool'),
+    TensorProto.FLOAT16: np.dtype('float16'),
+    TensorProto.DOUBLE: np.dtype('float64'),
+    TensorProto.COMPLEX64: np.dtype('complex64'),
+    TensorProto.COMPLEX128: np.dtype('complex128'),
+    TensorProto.UINT32: np.dtype('uint32'),
+    TensorProto.UINT64: np.dtype('uint64'),
 }
 
-TENSOR_TO_NP = {v: k for k, v in NP_TO_TENSOR.items()}
+NP_TYPE_TO_TENSOR_TYPE = {v: k for k, v in TENSOR_TYPE_TO_NP_TYPE.items()}
+
+TENSOR_TYPE_TO_STORAGE_TENSOR_TYPE = {
+    TensorProto.FLOAT: TensorProto.FLOAT,
+    TensorProto.UINT8: TensorProto.INT32,
+    TensorProto.INT8: TensorProto.INT32,
+    TensorProto.UINT16: TensorProto.INT32,
+    TensorProto.INT16: TensorProto.INT32,
+    TensorProto.INT32: TensorProto.INT32,
+    TensorProto.INT64: TensorProto.INT64,
+    TensorProto.BOOL: TensorProto.INT32,
+    TensorProto.FLOAT16: TensorProto.UINT16,
+    TensorProto.DOUBLE: TensorProto.DOUBLE,
+    TensorProto.COMPLEX64: TensorProto.FLOAT,
+    TensorProto.COMPLEX128: TensorProto.FLOAT,
+    TensorProto.UINT32: TensorProto.UINT32,
+    TensorProto.UINT64: TensorProto.UINT64,
+}
+
+STORAGE_TENSOR_TYPE_TO_FIELD = {
+    TensorProto.FLOAT: 'float_data',
+    TensorProto.INT32: 'int32_data',
+    TensorProto.INT64: 'int64_data',
+    TensorProto.UINT16: 'int32_data',
+    TensorProto.DOUBLE: 'double_data',
+    TensorProto.COMPLEX64: 'float_data',
+    TensorProto.COMPLEX128: 'float_data',
+    TensorProto.UINT32: 'uint64_data',
+    TensorProto.UINT64: 'uint64_data',
+}
--- onnx/numpy_helper.py
+++ onnx/numpy_helper.py
@@ -9,11 +9,11 @@ import numpy as np
 from onnx.onnx_pb2 import TensorProto
 from onnx import mapping
 
-
 if sys.byteorder != 'little':
     raise RuntimeError(
         'Numpy helper for tensor/ndarray is not available on big endian '
         'systems yet.')
+
 
 def combine_pairs_to_complex(fa):
     return [complex(fa[i * 2], fa[i * 2 + 1]) for i in range(len(fa)//2)]
@@ -34,8 +34,11 @@ def to_array(tensor):
     if tensor.data_type == TensorProto.STRING:
         raise ValueError("Tensor data type STRING is not supported.")
 
-    data_type = tensor.data_type
-    np_dtype = mapping.TENSOR_TO_NP[tensor_dtype]
+    tensor_dtype = tensor.data_type
+    np_dtype = mapping.TENSOR_TYPE_TO_NP_TYPE[tensor_dtype]
+    storage_type = mapping.TENSOR_TYPE_TO_STORAGE_TENSOR_TYPE[tensor_dtype]
+    storage_np_dtype = mapping.TENSOR_TYPE_TO_NP_TYPE[storage_type]
+    storage_field = mapping.STORAGE_TENSOR_TYPE_TO_FIELD[storage_type]
     dims = tensor.dims
 
     if tensor.HasField("raw_data"):
@@ -44,55 +47,17 @@ def to_array(tensor):
             tensor.raw_data,
             dtype=np_dtype).reshape(dims)
     else:
-        # Conventional fields not using raw bytes.
-        if data_type == TensorProto.FLOAT:
-            return np.asarray(
-                tensor.float_data, dtype=np.float32).reshape(dims)
-        elif data_type == TensorProto.COMPLEX64:
-            return np.asarray(
-                combine_pairs_to_complex(tensor.float_data), dtype=np.complex64).reshape(dims)
-        elif data_type == TensorProto.UINT8:
-            return np.asarray(
-                tensor.int32_data, dtype=np.int32).reshape(dims).astype(np.uint8)
-        elif data_type == TensorProto.INT8:
-            return np.asarray(
-                tensor.int32_data, dtype=np.int32).reshape(dims).astype(np.int8)
-        elif data_type == TensorProto.UINT16:
-            return np.asarray(
-                tensor.int32_data, dtype=np.int32).reshape(dims).astype(np.uint16)
-        elif data_type == TensorProto.INT16:
-            return np.asarray(
-                tensor.int32_data, dtype=np.int32).reshape(dims).astype(np.int16)
-        elif data_type == TensorProto.INT32:
-            return np.asarray(
-                tensor.int32_data, dtype=np.int32).reshape(dims)
-        elif data_type == TensorProto.INT64:
-            return np.asarray(
-                tensor.int64_data, dtype=np.int64).reshape(dims)
-        elif data_type == TensorProto.UINT32:
-            return np.asarray(
-                tensor.uint64_data, dtype=np.uint32).reshape(dims)
-        elif data_type == TensorProto.UINT64:
-            return np.asarray(
-                tensor.uint64_data, dtype=np.uint64).reshape(dims)
-        elif data_type == TensorProto.DOUBLE:
-            return np.asarray(
-                tensor.double_data, dtype=np.float64).reshape(dims)
-        elif data_type == TensorProto.COMPLEX128:
-            return np.asarray(
-                combine_pairs_to_complex(tensor.double_data), dtype=np.complex128).reshape(dims)
-        elif data_type == TensorProto.STRING:
-            raise NotImplementedError("Not implemented.")
-        elif data_type == TensorProto.BOOL:
-            return np.asarray(
-                tensor.int32_data, dtype=np.int32).reshape(dims).astype(np.bool)
-        elif data_type == TensorProto.FLOAT16:
-            return np.asarray(
-                tensor.int32_data, dtype=np.uint16).reshape(dims).view(np.float16)
-        else:
-            # TODO: complete the data type.
-            raise RuntimeError(
-                "Tensor data type not understood yet: {}".format(str(data_type)))
+        data = getattr(tensor, storage_field),
+        if (tensor_dtype == TensorProto.COMPLEX64 or
+            tensor_dtype == TensorProto.COMPLEX128):
+            datat = combine_pairs_to_complex(data)
+        return (
+            np.asarray(
+                data,
+                dtype=storage_np_dtype)
+            .astype(np_dtype)
+            .reshape(dims)
+        )
 
 
 def from_array(arr, name=None):
@@ -114,7 +79,7 @@ def from_array(arr, name=None):
         raise NotImplementedError("Need to properly implement string.")
     # For numerical types, directly use numpy raw bytes.
     try:
-        dtype = mapping.NP_TO_TENSOR[arr.dtype]
+        dtype = mapping.NP_TYPE_TO_TENSOR_TYPE[arr.dtype]
     except KeyError:
         raise RuntimeError(
             "Numpy data type not understood yet: {}".format(str(arr.dtype)))
