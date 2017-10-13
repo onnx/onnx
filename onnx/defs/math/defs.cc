14cd2a0e0e5caa22b8b638a2faa0e9557a7a2979
Lu Fang <lufang@fb.com>
2017-10-12 21:19:00 -0700

    Add LogSoftmax definition

--- onnx/defs/math/defs.cc
+++ onnx/defs/math/defs.cc
@@ -385,6 +385,37 @@ will throw errors.
   .Output(0, "output", "The softmax normalized output values with the same "
           "shape as input tensor.");
 
+OPERATOR_SCHEMA(LogSoftmax)
+  .NumInputs(1)
+  .NumOutputs(1)
+  .SetDoc(R"DOC(
+The operator computes the logsoftmax normalized values for each layer in the batch
+ of the given input. The input is a 2-D tensor (Tensor<float>) of size
+(batch_size x input_feature_dimensions). The output tensor has the same shape
+and contains the logsoftmax normalized values of the corresponding input.
+
+X does not need to explicitly be a 2D vector; rather, it will be
+coerced into one. For an arbitrary n-dimensional tensor
+X \in [a_0, a_1, ..., a_{k-1}, a_k, ..., a_{n-1}] and k is
+the axis provided, then X will be coerced into a 2-dimensional tensor with
+dimensions [a_0 * ... * a_{k-1}, a_k * ... * a_{n-1}]. For the default
+case where axis=1, this means the X tensor will be coerced into a 2D tensor
+of dimensions [a_0, a_1 * ... * a_{n-1}], where a_0 is often the batch size.
+In this situation, we must have a_0 = N and a_1 * ... * a_{n-1} = D.
+Each of these dimensions must be matched correctly, or else the operator
+will throw errors.
+)DOC")
+  .Attr("axis",
+        "(int) default to 1; describes the axis of the inputs when coerced "
+        "to 2D; defaults to one because the 0th axis most likely describes "
+        "the batch_size",
+        AttrType::INT)
+  .Input(0, "input",
+         "The input tensor that's coerced into a 2D matrix of size (NxD) "
+         "as described above.")
+  .Output(0, "output", "The logsoftmax normalized output values with the same "
+          "shape as input tensor.");
+
 OPERATOR_SCHEMA(Gemm)
     .NumInputs(3)
     .NumOutputs(1)
