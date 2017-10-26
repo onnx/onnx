e0981c115182a020e3f49b92390da12ed5bbf8d4
Edward Z. Yang <ezyang@fb.com>
2017-10-26 11:54:11 -0400

    Working ifdef, but build broken.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- onnx/gen_proto.py
+++ onnx/gen_proto.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import os
+import re
 
 autogen_header = """\
 //
@@ -14,14 +15,47 @@ autogen_header = """\
 
 """
 
+IF_ONNX_ML_REGEX = re.compile(r'\s*//\s*#if\s+ONNX-ML\s*$')
+ENDIF_ONNX_ML_REGEX = re.compile(r'\s*//\s*#endif\s*$')
+
+def process_ifs(lines, onnx_ml):
+    in_if = False
+    for line in lines:
+        if IF_ONNX_ML_REGEX.match(line):
+            assert not in_if
+            in_if = True
+        elif ENDIF_ONNX_ML_REGEX.match(line):
+            assert in_if
+            in_if = False
+        else:
+            if not in_if or (in_if and onnx_ml):
+                yield line
+
+def translate(source, proto, onnx_ml):
+    lines = source.splitlines()
+    lines = process_ifs(lines, onnx_ml=onnx_ml)
+    return "\n".join(lines)  # TODO: not Windows friendly
+
 def main():
     onnx_proto_in = os.path.join(os.path.dirname(__file__), "onnx.proto.in")
     onnx_proto = os.path.join(os.path.dirname(__file__), "onnx.proto")
+    onnx_ml_proto = os.path.join(os.path.dirname(__file__), "onnx-ml.proto")
+    onnx_proto3 = os.path.join(os.path.dirname(__file__), "onnx.proto3")
+    onnx_ml_proto3 = os.path.join(os.path.dirname(__file__), "onnx-ml.proto3")
     with open(onnx_proto_in, 'r') as fin:
-        # As of right now, it's a no-op.  Add whatever processing you want here
+        source = fin.read()
         with open(onnx_proto, 'w') as fout:
             fout.write(autogen_header)
-            fout.write(fin.read())
+            fout.write(translate(source, proto=2, onnx_ml=False))
+        with open(onnx_ml_proto, 'w') as fout:
+            fout.write(autogen_header)
+            fout.write(translate(source, proto=2, onnx_ml=True))
+        with open(onnx_proto3, 'w') as fout:
+            fout.write(autogen_header)
+            fout.write(translate(source, proto=3, onnx_ml=False))
+        with open(onnx_ml_proto3, 'w') as fout:
+            fout.write(autogen_header)
+            fout.write(translate(source, proto=3, onnx_ml=True))
 
 if __name__ == '__main__':
     main()
