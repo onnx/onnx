ccd5ee705209a963a7257743de8603dab0a971f2
Junjie Bai <bai@in.tum.de>
2017-11-03 20:27:11 -0700

    Throw ValidationError in case schema check failed

--- onnx/checker.h
+++ onnx/checker.h
@@ -2,6 +2,7 @@
 
 #include <stdexcept>
 #include "onnx/onnx.pb.h"
+#include "onnx/string_utils.h"
 
 namespace onnx {
 namespace checker {
@@ -9,6 +10,9 @@ class ValidationError : public std::runtime_error {
  public:
   using std::runtime_error::runtime_error;
 };
+
+#define fail_check(...) \
+  throw onnx::checker::ValidationError(onnx::MakeString(__VA_ARGS__));
 
 using IR_VERSION_TYPE = decltype(Version::IR_VERSION);
 void check_value_info(const ValueInfoProto& value_info, int ir_version);
