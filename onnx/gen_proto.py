9c11b62fb799186d4f4ce7e71e6cdb838bb5ea66
George Wu <jywutech@gmail.com>
2017-11-10 16:18:44 -0800

    Fix issues causing format difference on windows platform resulting in build failure.

--- onnx/gen_proto.py
+++ onnx/gen_proto.py
@@ -4,6 +4,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import io
 import os
 import re
 
@@ -76,25 +77,25 @@ def convert(stem, do_onnx_ml=False):
     proto3 = qualify("{}.proto3".format(stem))
     print("Processing {}".format(proto_in))
 
-    with open(proto_in, 'r') as fin:
+    with io.open(proto_in, 'r') as fin:
         source = fin.read()
         print("Writing {}".format(proto))
-        with open(proto, 'w') as fout:
+        with io.open(proto, 'w', newline='') as fout:
             fout.write(autogen_header)
             fout.write(translate(source, proto=2, onnx_ml=False))
         print("Writing {}".format(proto3))
-        with open(proto3, 'w') as fout:
+        with io.open(proto3, 'w', newline='') as fout:
             fout.write(autogen_header)
             fout.write(translate(source, proto=3, onnx_ml=False))
         if do_onnx_ml:
             ml_proto = qualify("{}-ml.proto".format(stem))
             ml_proto3 = qualify("{}-ml.proto3".format(stem))
             print("Writing {}".format(ml_proto))
-            with open(ml_proto, 'w') as fout:
+            with io.open(ml_proto, 'w', newline='') as fout:
                 fout.write(autogen_header)
                 fout.write(translate(source, proto=2, onnx_ml=True))
             print("Writing {}".format(ml_proto3))
-            with open(ml_proto3, 'w') as fout:
+            with io.open(ml_proto3, 'w', newline='') as fout:
                 fout.write(autogen_header)
                 fout.write(translate(source, proto=3, onnx_ml=True))
 
