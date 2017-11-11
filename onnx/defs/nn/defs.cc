c8e474d460b1c5f6fec8b9fc633de67c2f99371a
Sherlock Huang <bahuang@microsoft.com>
2017-11-10 21:21:58 -0800

    Add InstanceNorm

--- onnx/defs/nn/defs.cc
+++ onnx/defs/nn/defs.cc
@@ -182,6 +182,8 @@ computes the output.)DOC";
                         "and upper part of the corresponding axis. So `pads` will have two values per axis, "
                         "first value corresponding to the number of pixels added to the begining of the axis "
                         "and the second value corresponding to the number of pixels add at the end of the axis. "
+                        "The order should be axis_0_begin, axis_0_end, axis_1_begin, ..., axis_n_begin, "
+                        "axis_n_end, n is kernel's dimension."
                         "This attribute cannot be used simultaneously with auto_pad attribute.",
                         AttrType::INTS);
             schema.Attr("group",
@@ -305,19 +307,20 @@ namespace onnx {
 } // namespace onnx
 
 OPERATOR_SCHEMA(BatchNormalization)
-.NumInputs(5)
-.NumOutputs({ 1, 5 })
-.EnforceConsumed({ {3, 1}, {4, 2} })
-.SetDoc(R"DOC(
+    .NumInputs(5)
+    .NumOutputs({ 1, 5 })
+    .EnforceConsumed({ {3, 1}, {4, 2} })
+    .SetDoc(R"DOC(
 Carries out batch normalization as described in the paper
 https://arxiv.org/abs/1502.03167. Depending on the mode it is being run,
 there are multiple cases for the number of outputs, which we list below:
 
 Output case #1: Y, mean, var, saved_mean, saved_var (training mode)
 Output case #2: Y (test mode)
-)DOC")
-.Attr("spatial",
-    "Compute the mean and variance across all spatial elements or per feature.",
+    )DOC")
+    .Attr("spatial",
+        "If true, compute the mean and variance across all spatial elements "
+        "If false, compute the mean and variance across per feature.",
     AttrType::INT)
     .Attr("is_test",
         "If set to nonzero, run spatial batch normalization in test mode.",
@@ -365,6 +368,52 @@ Output case #2: Y (test mode)
     .Output(4,
         "saved_var",
         "Saved variance used during training to speed up "
+        "gradient computation. Should not be used for testing.", "T")
+    .TypeConstraint("T", { "tensor(float16)", "tensor(float)", "tensor(double)" },
+        "Constrain input and output types to float tensors.");
+
+OPERATOR_SCHEMA(InstanceNorm)
+    .NumInputs(3)
+    .NumOutputs(1, 3)
+    .AllowConsumed({{0, 0}})
+    .SetDoc(R"DOC(
+Carries out instance normalization as described in the paper
+https://arxiv.org/abs/1607.08022. Depending on the mode it is being run,
+there are multiple cases for the number of outputs, which we list below:
+
+* Output case #1: output
+* Output case #2: output, saved_mean
+  - don't use, doesn't make sense but won't crash
+* Output case #3: output, saved_mean, saved_inv_stdev
+  - Makes sense for training only
+
+For training mode, type 3 is faster in the sense that for the backward
+pass, it is able to reuse the saved mean and inv_stdev in the gradient
+computation.
+)DOC")
+    .Attr("epsilon",
+        "The epsilon value to use to avoid division by zero.",
+        AttrType::FLOAT)
+    .Input(0,
+        "input",
+        "The input 4-dimensional tensor of shape NCHW or NHWC depending "
+        "on the order parameter.", "T")
+    .Input(1,
+        "scale",
+        "The input 1-dimensional scale tensor of size C.", "T")
+    .Input(2,
+        "bias",
+        "The input 1-dimensional bias tensor of size C.", "T")
+    .Output(0,
+        "output",
+        "The output 4-dimensional tensor of the same shape as input.", "T")
+    .Output(1,
+        "saved_mean",
+        "Optional saved mean used during training to speed up gradient "
+        "computation. Should not be used for testing.", "T")
+    .Output(2,
+        "saved_inv_stdev",
+        "Optional saved inverse stdev used during training to speed up "
         "gradient computation. Should not be used for testing.", "T")
     .TypeConstraint("T", { "tensor(float16)", "tensor(float)", "tensor(double)" },
         "Constrain input and output types to float tensors.");
