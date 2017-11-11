0e31a3a587fd0a9764e6efcf4711aaa153dda646
Ke Zhang <kezhan@microsoft.com>
2017-11-10 17:53:18 -0800

    add onnx_ml build in ci.

--- .travis/build.sh
+++ .travis/build.sh
@@ -21,3 +21,20 @@ python onnx/defs/gen_doc.py -o docs/Operators.md
 python onnx/gen_proto.py
 backend-test-tools generate-data
 git diff --exit-code
+
+# install onnx-ml
+cd $onnx_dir
+ccache -z
+pip install . --install-option="--onnxml=1"
+ccache -s
+
+# onnx tests
+cd $onnx_dir
+pip install pytest-cov nbval
+pytest
+
+# check auto-gen files up-to-date
+python onnx/defs/gen_doc.py -o docs/Operators.md
+python onnx/gen_proto.py
+backend-test-tools generate-data
+git diff --exit-code
