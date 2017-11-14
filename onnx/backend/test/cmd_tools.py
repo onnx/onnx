9d8fd39da02baebd93b65f859ce272cd82bc358a
Ke Zhang <kezhan@microsoft.com>
2017-11-14 11:25:29 -0800

    fix format issue.

--- onnx/backend/test/cmd_tools.py
+++ onnx/backend/test/cmd_tools.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import argparse
+import io
 import json
 import os
 import shutil
@@ -28,15 +29,15 @@ def generate_data(args):
     for case in node_cases:
         output_dir = os.path.join(args.output, 'node', case.name)
         prepare_dir(output_dir)
-        with open(os.path.join(output_dir, 'node.pb'), 'wb') as f:
+        with io.open(os.path.join(output_dir, 'node.pb', newline=''), 'wb') as f:
             f.write(case.node.SerializeToString())
         for i, input_np in enumerate(case.inputs):
             tensor = numpy_helper.from_array(input_np, case.node.input[i])
-            with open(os.path.join(output_dir, 'input_{}.pb'.format(i)), 'wb') as f:
+            with io.open(os.path.join(output_dir, 'input_{}.pb'.format(i)), 'wb', newline='') as f:
                 f.write(tensor.SerializeToString())
         for i, output_np in enumerate(case.outputs):
             tensor = numpy_helper.from_array(output_np, case.node.output[i])
-            with open(os.path.join(output_dir, 'output_{}.pb'.format(i)), 'wb') as f:
+            with io.open(os.path.join(output_dir, 'output_{}.pb'.format(i)), 'wb', newline='') as f:
                 f.write(tensor.SerializeToString())
 
     # model tests
@@ -44,7 +45,7 @@ def generate_data(args):
     for case in model_cases:
         output_dir = os.path.join(args.output, 'model', case.name)
         prepare_dir(output_dir)
-        with open(os.path.join(output_dir, 'data.json'), 'w') as f:
+        with io.open(os.path.join(output_dir, 'data.json'), 'w', newline='') as f:
             json.dump({
                 'url': case.url,
                 'model_name': case.model_name,
