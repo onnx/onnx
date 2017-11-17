503b67833f850aee1660732c1a004b02790651b5
Lu Fang <30275821+houseroad@users.noreply.github.com>
2017-11-17 02:58:29 -0800

    Add onnx.load_from_string and related tests (#256)
    * Add onnx.load_from_string and related tests
    
    * Fix decoded expression
    
    * Fix the test for onnx.load
    
    * Move the check to load_from_string
    
    * Update comment

--- onnx/__init__.py
+++ onnx/__init__.py
@@ -16,21 +16,33 @@ import google.protobuf.message
 
 def load(obj):
     '''
-    Loads a binary protobuf that stores onnx graph
+    Loads a binary protobuf that stores onnx model
 
     @params
     Takes a file-like object (has "read" function)
     or a string containing a file name
     @return ONNX ModelProto object
     '''
-    model = ModelProto()
     if hasattr(obj, 'read') and callable(obj.read):
         s = obj.read()
     else:
         with open(obj, 'rb') as f:
             s = f.read()
+    return load_from_string(s)
+
+
+def load_from_string(s):
+    '''
+    Loads a binary string that stores onnx model
+
+    @params
+    Takes a string object containing protobuf
+    @return ONNX ModelProto object
+    '''
+    model = ModelProto()
     decoded = model.ParseFromString(s)
-    if decoded != len(s):
+    # in python implementation ParseFromString returns None
+    if decoded is not None and decoded != len(s):
         raise google.protobuf.message.DecodeError(
             "Protobuf decoding consumed too few bytes: {} out of {}".format(
                 decoded, len(s)))
