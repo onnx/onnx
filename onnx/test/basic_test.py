8127b08ce24a07b0044ac272da363c9e5cb6e5ea
Lu Fang <30275821+houseroad@users.noreply.github.com>
2017-11-17 02:58:29 -0800

    Add onnx.load_from_string and related tests (#256)
    * Add onnx.load_from_string and related tests
    
    * Fix decoded expression
    
    * Fix the test for onnx.load
    
    * Move the check to load_from_string
    
    * Update comment

--- onnx/test/basic_test.py
+++ onnx/test/basic_test.py
@@ -2,11 +2,40 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+
 from onnx.onnx_pb2 import AttributeProto, NodeProto, GraphProto, ModelProto, IR_VERSION
+
+import io
+import onnx
+import os
+import tempfile
 import unittest
 
 
 class TestProtobufExists(unittest.TestCase):
+
+    def test_load(self):
+        # Create a model proto.
+        model = ModelProto()
+        model.ir_version = IR_VERSION
+        model_string = model.SerializeToString()
+
+        # Test if input is string
+        loaded_model = onnx.load_from_string(model_string)
+        self.assertTrue(model == loaded_model)
+
+        # Test if input has a read function
+        f = io.BytesIO(model_string)
+        loaded_model = onnx.load(f)
+        self.assertTrue(model == loaded_model)
+
+        # Test if input is a file name
+        f = tempfile.NamedTemporaryFile(delete=False)
+        f.write(model_string)
+        f.close()
+        loaded_model = onnx.load(f.name)
+        self.assertTrue(model == loaded_model)
+        os.remove(f.name)
 
     def test_existence(self):
         try:
