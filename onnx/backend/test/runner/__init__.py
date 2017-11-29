948d58ecfdda53b8b065749624cee8ca65d2c527
bddppq <bai@in.tum.de>
2017-11-29 11:32:07 -0500

    Fix wrong failed download message in Backend Test (#327)
    * Fix wrong failed download message in Backend Test
    
    * reraise

--- onnx/backend/test/runner/__init__.py
+++ onnx/backend/test/runner/__init__.py
@@ -157,9 +157,11 @@ class Runner(object):
                 print('Done')
                 with tarfile.open(download_file.name) as t:
                     t.extractall(models_dir)
+            except Exception as e:
+                print('Failed to prepare data for model {}: {}'.format(
+                    model_test.model_name, e))
+                raise
             finally:
-                print('Failed to prepare data for model {}'.format(
-                    model_test.model_name))
                 os.remove(download_file.name)
         return model_dir
 
