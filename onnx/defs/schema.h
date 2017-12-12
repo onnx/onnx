163516210187eb37807ed7406174279d607c4ddf
Ke Zhang <linkerzhang@yeah.net>
2017-12-12 13:49:41 -0800

    fix issue so that we could handle many versions of one op (#366)

--- onnx/defs/schema.h
+++ onnx/defs/schema.h
@@ -564,9 +564,11 @@ class OpSchemaRegistry {
   }
 };
 
-#define OPERATOR_SCHEMA(name)                          \
-  static onnx::OpSchemaRegistry::OpSchemaRegisterOnce( \
-      op_schema_register_once##name) = OpSchema(#name, __FILE__, __LINE__)
+#define OPERATOR_SCHEMA(name)   OPERATOR_SCHEMA_UNIQ_HELPER(__COUNTER__, name)
+#define OPERATOR_SCHEMA_UNIQ_HELPER(Counter, name) OPERATOR_SCHEMA_UNIQ(Counter, name)
+#define OPERATOR_SCHEMA_UNIQ(Counter, name)                                         \
+  static onnx::OpSchemaRegistry::OpSchemaRegisterOnce(                              \
+      op_schema_register_once##name##Counter) = OpSchema(#name, __FILE__, __LINE__)
 
 // Helper function
 size_t ReplaceAll(std::string& s, const char* from, const char* to);
