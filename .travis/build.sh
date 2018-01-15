cf2e52b5a21e91f199a83bc3e1234f083c9439a2
Dmytro Dzhulgakov <dzhulgakov@users.noreply.github.com>
2018-01-15 14:20:13 -0800

    add EOL check to travis CI (#420)
    * add EOL check to travis CI
    
    * install dos2unix package in travis
    
    * fix the line ending

--- .travis/build.sh
+++ .travis/build.sh
@@ -16,6 +16,11 @@ cd $onnx_dir
 pip install pytest-cov nbval
 pytest
 
+# check line endings to be UNIX
+find . -type f -regextype posix-extended -regex '.*\.(py|cpp|md|h|cc|proto|proto3|in)' | xargs dos2unix
+git status
+git diff --exit-code
+
 # check auto-gen files up-to-date
 python onnx/defs/gen_doc.py
 python onnx/gen_proto.py
