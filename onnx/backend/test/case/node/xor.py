157d9a4e167bffc0534c0f291fd037c80a9347d3
Yuhong Guo <guoyuhong85@126.com>
2018-01-19 09:27:02 -0800

    Add node test of logical operators. (#431)

---
+++ onnx/backend/test/case/node/xor.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import numpy as np
+
+import onnx
+from .base import Base, expect
+
+
+class Xor(Base):
+
+    @staticmethod
+    def export():
+        node = onnx.helper.make_node(
+            'Xor',
+            inputs=['x', 'y'],
+            outputs=['xor'],
+        )
+
+        x = (np.random.randn(3, 4, 5) > 0).astype(np.bool)
+        y = (np.random.randn(3, 4, 5) > 0).astype(np.bool)
+        z = np.logical_xor(x, y)
+        expect(node, inputs=[x, y], outputs=[z],
+               name='test_xor')
+
+    @staticmethod
+    def export_xor_broadcast():
+        node = onnx.helper.make_node(
+            'Xor',
+            inputs=['x', 'y'],
+            outputs=['xor'],
+            broadcast=1,
+        )
+
+        x = (np.random.randn(3, 4, 5) > 0).astype(np.bool)
+        y = (np.random.randn(5) > 0).astype(np.bool)
+        z = np.logical_xor(x, y)
+        expect(node, inputs=[x, y], outputs=[z],
+               name='test_xor_bcast1d')
+
+        x = (np.random.randn(3, 4, 5) > 0).astype(np.bool)
+        y = (np.random.randn(4, 5) > 0).astype(np.bool)
+        z = np.logical_xor(x, y)
+        expect(node, inputs=[x, y], outputs=[z],
+               name='test_xor_bcast2d')
