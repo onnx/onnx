fe5e9d22da95f0ce94b8feb9698aa38b3e589113
Lu Fang <lufang@fb.com>
2018-01-23 22:55:01 -0800

    Add docs for optimizer

--- onnx/optimizer.py
+++ onnx/optimizer.py
@@ -1,5 +1,6 @@
 """onnx optimizer
 
+This enables users to optimize their models.
 """
 from __future__ import absolute_import
 from __future__ import division
@@ -8,4 +9,15 @@ from __future__ import unicode_literals
 
 import onnx.onnx_cpp2py_export.optimizer as C
 
+"""Apply the optimization on the serialized ModelProto.
+
+Arguments:
+    input (string): serialized ModelProto
+    names (list of string): list of optimization names
+
+Supported pass names:
+    -- eliminate_nop_transpose
+    -- fuse_consecutive_transposes
+    -- fuse_transpose_into_gemm
+"""
 optimize = C.optimize
