07207618ed6ff16bb152b5683ba70c192acb3fb9
bddppq <bai@in.tum.de>
2018-01-24 15:35:51 -0800

    Remove std::move calls to temporary object that prevents compiler copy elision (#460)

--- onnx/optimizer/optimize.h
+++ onnx/optimizer/optimize.h
@@ -31,7 +31,7 @@ struct Optimizer {
 
   std::unique_ptr<onnx::ModelProto> optimize(std::unique_ptr<onnx::ModelProto> mp_in, std::vector<std::string>& names) {
 
-    std::shared_ptr<onnx::Graph> g(std::move(onnx::ImportModelProto(*mp_in)));
+    std::shared_ptr<onnx::Graph> g(onnx::ImportModelProto(*mp_in));
 
     if (g.get() == nullptr) {
       std::cerr << "Warning: onnx optimizer is unable to parse input model. "
