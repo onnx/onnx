498d25e884f1070be0b1b162557c9c24dabb63c0
Tianlei Wu <tlwu@microsoft.com>
2018-01-26 11:10:16 -0800

    update reciprocal test to avoid number close to zero

--- onnx/backend/test/case/node/reciprocal.py
+++ onnx/backend/test/case/node/reciprocal.py
@@ -20,8 +20,7 @@ class Reciprocal(Base):
             outputs=['y'],
         )
 
-        x = np.random.randn(3, 4, 5).astype(np.float32)
-        x[x==0] = 1
+        x = np.random.rand(3, 4, 5).astype(np.float32) + 0.5
         y = np.reciprocal(x)
         expect(node, inputs=[x], outputs=[y],
                name='test_reciprocal')
