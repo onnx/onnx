f5819e1d204003f325b746a7543651493c054e76
Tianlei Wu <tlwu@microsoft.com>
2018-01-26 17:34:45 -0800

    add explicit input tensors as example

--- onnx/backend/test/case/node/sqrt.py
+++ onnx/backend/test/case/node/sqrt.py
@@ -20,6 +20,11 @@ class Sqrt(Base):
             outputs=['y'],
         )
 
+        x = np.array([1, 4, 9]).astype(np.float32)
+        y = np.sqrt(x) #expected output [1., 2., 3.]
+        expect(node, inputs=[x], outputs=[y],
+               name='test_sqrt_example')
+
         x = np.abs(np.random.randn(3, 4, 5).astype(np.float32))
         y = np.sqrt(x)
         expect(node, inputs=[x], outputs=[y],
