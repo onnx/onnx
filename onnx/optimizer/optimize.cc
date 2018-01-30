dab78d3e2d10824bec0f19411f206ef31e088ae0
Junjie Bai <bai@in.tum.de>
2018-01-30 12:45:50 -0800

    Remove unnecessary usage of std::uniqe_ptr

--- onnx/optimizer/optimize.cc
+++ onnx/optimizer/optimize.cc
@@ -48,9 +48,10 @@ void PrepareOutput(const onnx::ModelProto& mp_in, onnx::ModelProto& mp_out) {
 
 static Optimizer _optimizer;
 
-std::unique_ptr<onnx::ModelProto> Optimize(
-    std::unique_ptr<onnx::ModelProto> mp_in, std::vector<std::string>& names) {
-  return _optimizer.optimize(std::move(mp_in), names);
+onnx::ModelProto Optimize(
+    const onnx::ModelProto& mp_in,
+    const std::vector<std::string>& names) {
+  return _optimizer.optimize(mp_in, names);
 }
 
 }}
--- onnx/proto_utils.h
+++ onnx/proto_utils.h
@@ -6,12 +6,12 @@
 namespace onnx {
 
 template <typename Proto>
-bool ParseProtoFromBytes(Proto* proto, const char* buffer, size_t length) {
+bool ParseProtoFromBytes(Proto& proto, const char* buffer, size_t length) {
   // Total bytes hard limit / warning limit are set to 1GB and 512MB
   // respectively.
   ::google::protobuf::io::CodedInputStream coded_stream(
       new google::protobuf::io::ArrayInputStream(buffer, length));
   coded_stream.SetTotalBytesLimit(1024LL << 20, 512LL << 20);
-  return proto->ParseFromCodedStream(&coded_stream);
+  return proto.ParseFromCodedStream(&coded_stream);
 }
 } // namespace onnx
