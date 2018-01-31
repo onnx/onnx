af14b8cee280e6ccc8842d90d8ca23160321b164
James Reed <jamesreed@fb.com>
2018-01-30 19:51:04 -0800

    Unsqueeze op (#497)

---
+++ onnx/backend/test/case/node/squeeze.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import numpy as np
+
+import onnx
+from ..base import Base
+from . import expect
+
+
+class Squeeze(Base):
+
+    @staticmethod
+    def export_squeeze():
+        node = onnx.helper.make_node(
+            'Squeeze',
+            inputs=['x'],
+            outputs=['y'],
+            axes=[0],
+        )
+        x = np.random.randn(1, 3, 4, 5).astype(np.float32)
+        y = np.squeeze(x, axis=0)
+
+        expect(node, inputs=[x], outputs=[y],
+               name='test_squeeze')
