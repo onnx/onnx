b15381c741d32d0f2a4b2421ac7f2557cb43f402
Ke Zhang <linkerzhang@yeah.net>
2018-02-06 12:59:13 -0800

    setup cmake (#469)
    * set up cmake for dev in windows
    
    * add git link
    
    * remove zlib as it's not needed.
    
    * fix most of the comments.
    
    * change cmake version to 3.1
    
    * remove readme
    
    * add pybind11 into cmake
    
    * use local protobuf.
    
    * remove include for protobuf as it's not needed.
    
    * merge all things in Cmakelists.txt in root folder
    
    * update Cmakelists.txt in root folder
    
    * use customized generation function for proto header/code generation.
    
    * adding comments.

--- onnx/onnx_pb.h
+++ onnx/onnx_pb.h
@@ -4,6 +4,45 @@
 #ifndef ONNX_ONNX_PB_H
 #define ONNX_ONNX_PB_H
 
+// Defines ONNX_EXPORT and ONNX_IMPORT. On Windows, this corresponds to
+// different declarations (dllexport and dllimport). On Linux/Mac, it just
+// resolves to the same "default visibility" setting.
+#if defined(_MSC_VER)
+#if defined(ONNX_BUILD_SHARED_LIBS)
+#define ONNX_EXPORT __declspec(dllexport)
+#define ONNX_IMPORT __declspec(dllimport)
+#else
+#define ONNX_EXPORT
+#define ONNX_IMPORT
+#endif
+#else
+#if defined(__GNUC__)
+#if __GNUC_PREREQ(4, 9)
+#define ONNX_EXPORT [[gnu::visibility("default")]]
+#else
+#define ONNX_EXPORT __attribute__((__visibility__("default")))
+#endif
+#else
+#define ONNX_EXPORT
+#endif
+#define ONNX_IMPORT ONNX_EXPORT
+#endif
+
+// ONNX_API is a macro that, depends on whether you are building the
+// main ONNX library or not, resolves to either ONNX_EXPORT or
+// ONNX_IMPORT.
+//
+// This is used in e.g. ONNX's protobuf files: when building the main library,
+// it is defined as ONNX_EXPORT to fix a Windows global-variable-in-dll
+// issue, and for anyone dependent on ONNX it will be defined as
+// ONNX_IMPORT.
+// This is a solution from https://github.com/caffe2/caffe2/blob/4f534fad1af9f77d4f0496ecd37dafb382330223/caffe2/core/common.h
+#ifdef ONNX_BUILD_MAIN_LIB
+#define ONNX_API ONNX_EXPORT
+#else
+#define ONNX_API ONNX_IMPORT
+#endif
+
 #ifdef ONNX_ML
 #include "onnx/onnx-ml.pb.h"
 #else
