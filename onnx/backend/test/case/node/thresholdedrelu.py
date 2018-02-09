581491fc712e684bb683c2a16e922b085e1465f3
Giri Anantharaman <giriman@fb.com>
2018-02-08 17:39:03 -0800

    Fixing a bug with using clip in thresholded relu testcase.

--- onnx/backend/test/case/node/thresholdedrelu.py
+++ onnx/backend/test/case/node/thresholdedrelu.py
@@ -22,14 +22,15 @@ class ThresholdedRelu(Base):
             alpha=alpha
         )
 
-        x = np.array([-1.5, 0., 1.2, 2.2]).astype(np.float32)
-        y = np.clip(x, alpha, np.inf)  # expected output [0., 0., 0., 2.2]
+        x = np.array([-1.5, 0., 1.2, 2.0, 2.2]).astype(np.float32)
+        y = np.clip(x, alpha, np.inf)  # expected output [0., 0., 0., 0., 2.2]
+        y[y == alpha] = 0
         expect(node, inputs=[x], outputs=[y],
                name='test_thresholdedrelu_example')
 
         x = np.random.randn(3, 4, 5).astype(np.float32)
         y = np.clip(x, alpha, np.inf)
-
+        y[y == alpha] = 0
         expect(node, inputs=[x], outputs=[y],
                name='test_thresholdedrelu')
 
@@ -43,6 +44,6 @@ class ThresholdedRelu(Base):
         )
         x = np.random.randn(3, 4, 5).astype(np.float32)
         y = np.clip(x, default_alpha, np.inf)
-
+        y[y == default_alpha] = 0
         expect(node, inputs=[x], outputs=[y],
                name='test_thresholdedrelu_default')
