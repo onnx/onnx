a487129f7b1e4ca6798d43ab4671747e9d940987
Peyman Manikashani <peykash@microsoft.com>
2018-02-16 14:51:25 -0800

    address PR comments

--- onnx/backend/test/case/node/reshape.py
+++ onnx/backend/test/case/node/reshape.py
@@ -16,12 +16,12 @@ class Reshape(Base):
     def export():
         original_shape = [2, 3, 4]
         test_cases = {
-            'reordered_dims':[4,2,3],
-            'reduced_dims':[3,8],
-            'extended_dims':[3,2,2,2],
+            'reordered_dims':[4, 2, 3],
+            'reduced_dims':[3, 8],
+            'extended_dims':[3, 2, 2, 2],
             'one_dim':[24],
-            'negative_dim':[6,-1,2]
-            }
+            'negative_dim':[6, -1, 2]
+        }
         data = np.random.random_sample(original_shape).astype(np.float32)
 
         for test_name,test_shape in test_cases.items():
