237894554499a6fc0b23e7d25a0b1c472c1307c6
Sebastian Meßmer <smessmer@users.noreply.github.com>
2018-02-28 20:17:49 -0800

    Clarify reshape behavior when '0' is passed in (#569)

--- onnx/defs/tensor/defs.cc
+++ onnx/defs/tensor/defs.cc
@@ -41,8 +41,8 @@ It takes a tensor as input and an argument `shape`. It outputs the reshaped tens
 
 At most one dimension of the new shape can be -1. In this case, the value is
 inferred from the size of the tensor and the remaining dimensions. A dimension
-could also be 0, in which case the actual dimension value is going to be copied
-from the shape argument.)DOC")
+could also be 0, in which case the actual dimension value is unchanged (i.e. taken
+from the input tensor).)DOC")
     .Attr("shape", "New shape", AttributeProto::INTS, OPTIONAL)
     .Input(0, "data", "An input tensor.", "T")
     .Output(0, "reshaped", "Reshaped data.", "T")
