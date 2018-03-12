8145347c9a70959d582247cb2e50cdc2d135bebf
Sebastian Messmer <messmer@fb.com>
2018-03-12 12:18:40 -0700

    test

--- .travis/script.sh
+++ .travis/script.sh
@@ -34,7 +34,7 @@ cat onnx/onnx_pb.py
 # Mypy only works with Python 3
 if [ "${PYTHON_VERSION}" != "python2" ]; then
   # Mypy only works with our generated _pb.py files when we install in develop mode, so let's do that
-  pip install -e .
+  time ONNX_NAMESPACE=ONNX_NAMESPACE_FOO_BAR_FOR_CI pip install -e .[mypy]
 
   time mypy .
   # Also test in python2 mode (but this is still in the python 3 CI
