054f49eaca034afed49933d19fcb5c072b41bfa9
Wenhao Hu <fumihwh@gmail.com>
2018-03-21 20:35:58 +0900

    lint pool_op_common.py

--- onnx/backend/test/case/node/pool_op_common.py
+++ onnx/backend/test/case/node/pool_op_common.py
@@ -7,7 +7,7 @@ def get_pad_shape(auto_pad, input_spatial_shape, kernel_spatial_shape, strides_s
     if auto_pad in ('SAME_UPPER', 'SAME_LOWER'):
         for i in range(len(input_spatial_shape)):
             pad_shape[i] = (output_spatial_shape[i] - 1) * strides_spatial[i] + kernel_spatial_shape[i] - \
-                input_spatial_shape[i]
+                           input_spatial_shape[i]
     elif auto_pad == 'VALID':
         pass
     return pad_shape
@@ -26,25 +26,25 @@ def get_output_shape(auto_pad, input_spatial_shape, kernel_spatial_shape, stride
 
 
 def pool(padded, x_shape, kernel_shape, strides_shape, out_shape, pad_shape, pooling_type):
-  spatial_size = len(x_shape) - 2
-  y = np.zeros([x_shape[0], x_shape[1]] + list(out_shape))
+    spatial_size = len(x_shape) - 2
+    y = np.zeros([x_shape[0], x_shape[1]] + list(out_shape))
 
-  for shape in itertools.product(range(x_shape[0]),
-                                 range(x_shape[1]),
-                                 *[range(
-                                   int((x_shape[i + 2] + pad_shape[i] - kernel_shape[i]) / strides_shape[i] + 1))
-                                   for i in range(spatial_size)]):
-    window = padded[shape[0], shape[1]]
-    window_vals = np.array([window[i] for i in list(
-      itertools.product(
-        *[range(strides_shape[i] * shape[i + 2], strides_shape[i] * shape[i + 2] + kernel_shape[i]) for i in
-          range(spatial_size)])
-    )])
-    if pooling_type == 'AVG':
-      f = np.average
-    elif pooling_type == 'MAX':
-      f = np.max
-    else:
-      raise NotImplementedError('Pooling type {} does not support. Should be AVG, MAX'.format(pooling_type))
-    y[shape] = f(window_vals[np.where(~np.isnan(window_vals))])
-  return y.astype(np.float32)
+    for shape in itertools.product(range(x_shape[0]),
+                                   range(x_shape[1]),
+                                   *[range(
+                                       int((x_shape[i + 2] + pad_shape[i] - kernel_shape[i]) / strides_shape[i] + 1))
+                                       for i in range(spatial_size)]):
+        window = padded[shape[0], shape[1]]
+        window_vals = np.array([window[i] for i in list(
+            itertools.product(
+                *[range(strides_shape[i] * shape[i + 2], strides_shape[i] * shape[i + 2] + kernel_shape[i]) for i in
+                  range(spatial_size)])
+        )])
+        if pooling_type == 'AVG':
+            f = np.average
+        elif pooling_type == 'MAX':
+            f = np.max
+        else:
+            raise NotImplementedError('Pooling type {} does not support. Should be AVG, MAX'.format(pooling_type))
+        y[shape] = f(window_vals[np.where(~np.isnan(window_vals))])
+    return y.astype(np.float32)
