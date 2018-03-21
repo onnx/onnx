1401b8befe415d92d16b7fae96b6d4f0c9047bdb
Junjie Bai <bai@in.tum.de>
2018-03-20 19:56:40 -0700

    tweak travis build scripts

--- .travis/script.sh
+++ .travis/script.sh
@@ -3,10 +3,7 @@
 script_path=$(python -c "import os; import sys; print(os.path.realpath(sys.argv[1]))" "${BASH_SOURCE[0]}")
 source "${script_path%/*}/setup.sh"
 
-onnx_dir="$PWD"
-
 # onnx tests
-cd $onnx_dir
 pip install pytest-cov nbval
 pytest
 
