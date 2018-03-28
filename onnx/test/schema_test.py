f03979fbd6fe23198fdcd1c352ec311b005bebc7
Junjie Bai <bai@in.tum.de>
2018-03-27 23:05:35 -0700

    Add test

--- onnx/test/schema_test.py
+++ onnx/test/schema_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from onnx import defs
+from onnx import defs, AttributeProto
 
 
 class TestSchema(unittest.TestCase):
@@ -10,6 +10,11 @@ class TestSchema(unittest.TestCase):
 
     def test_typecheck(self):
         defs.get_schema("Conv")
+
+    def test_attr_default_value(self):
+        v = defs.get_schema("BatchNormalization").attributes['epsilon'].default_value
+        self.assertEqual(type(v), AttributeProto)
+        self.assertEqual(v.type, AttributeProto.FLOAT)
 
 
 if __name__ == '__main__':
