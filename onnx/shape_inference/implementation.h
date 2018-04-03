8df963b9f658b024345cbfdb04e6caaa638bb391
Anders Papitto <anderspapitto@gmail.com>
2018-04-03 14:38:24 -0700

    handling dim_param in shape inference

--- onnx/shape_inference/implementation.h
+++ onnx/shape_inference/implementation.h
@@ -6,6 +6,24 @@
 namespace ONNX_NAMESPACE { namespace shape_inference {
 
 struct InferenceContextImpl : public InferenceContext {
+  InferenceContextImpl(const NodeProto & n,
+                       std::unordered_map<std::string, TypeProto_Tensor *>& valueTypesByName) {
+    for (const auto& attr : n.attribute()) {
+      attributesByName_[attr.name()] = &attr;
+    }
+
+    for (const auto& input : n.input()) {
+      auto iter = valueTypesByName.find(input);
+      if (iter != valueTypesByName.end()) {
+        allInputTypes_.push_back(iter->second);
+      } else {
+        allInputTypes_.push_back(nullptr);
+      }
+    }
+
+    allOutputTypes_.resize(n.output_size());
+  }
+
   const AttributeProto* getAttribute(const std::string& name) const override {
     auto iter = attributesByName_.find(name);
     if (iter == attributesByName_.end()) {
@@ -45,89 +63,71 @@ void InferShapes(ModelProto& m) {
     valueTypesByName[vi.name()] = vi.mutable_type()->mutable_tensor_type();
   }
 
+  std::unordered_map<std::string, int> dimParamValues;
+
   for (const auto& n : g->node()) {
     auto schema = OpSchemaRegistry::Schema(n.op_type());
-    if (schema) {
-      InferenceContextImpl ctx;
+    if (!schema) {
+      continue;
+    }
 
-      {
-        std::unordered_map<std::string, const AttributeProto *> attributesByName;
-        for (const auto& attr : n.attribute()) {
-          attributesByName[attr.name()] = &attr;
-        }
-        ctx.attributesByName_ = std::move(attributesByName);
+    InferenceContextImpl ctx(n, valueTypesByName);
+
+    schema->GetShapeInferenceFunction()(ctx);
+
+    for (int i = 0; i < n.output_size(); i++) {
+      const auto& output = n.output(i);
+      const auto& inferredType = *ctx.getOutputType(i);
+
+      // In this case, we have no new information, so don't bother
+      // to add a contentless ValueInfo.
+      if (inferredType.elem_type() == TensorProto::UNDEFINED &&
+          !inferredType.has_shape()) {
+        continue;
       }
 
-      {
-        std::vector<const TypeProto_Tensor *> allInputTypes;
-
-        for (const auto& input : n.input()) {
-          auto iter = valueTypesByName.find(input);
-          if (iter != valueTypesByName.end()) {
-            allInputTypes.push_back(iter->second);
-          } else {
-            allInputTypes.push_back(nullptr);
-          }
-        }
-
-        ctx.allInputTypes_ = std::move(allInputTypes);
+      // If there is already a ValueInfo associated with this
+      // output, reuse it. Otherwise add a new one.
+      auto iter = valueTypesByName.find(output);
+      TypeProto_Tensor* existingType = nullptr;
+      if (iter != valueTypesByName.end()) {
+        existingType = iter->second;
+      } else {
+        auto vi = g->add_value_info();
+        vi->set_name(output);
+        existingType = vi->mutable_type()->mutable_tensor_type();
       }
 
-      ctx.allOutputTypes_.resize(n.output_size());
-
-      schema->GetShapeInferenceFunction()(ctx);
-
-      for (int i = 0; i < n.output_size(); i++) {
-        const auto& output = n.output(i);
-        const auto& inferredType = *ctx.getOutputType(i);
-
-        // In this case, we have no new information, so don't bother
-        // to add a contentless ValueInfo.
-        if (inferredType.elem_type() == TensorProto::UNDEFINED &&
-            !inferredType.has_shape()) {
-          continue;
-        }
-
-        // If there is already a ValueInfo associated with this
-        // output, reuse it. Otherwise add a new one.
-        auto iter = valueTypesByName.find(output);
-        TypeProto_Tensor* existingType = nullptr;
-        if (iter != valueTypesByName.end()) {
-          existingType = iter->second;
+      // Incorporate the inferred information.
+      if (inferredType.elem_type() != TensorProto::UNDEFINED) {
+        if (existingType->elem_type() != TensorProto::UNDEFINED &&
+            existingType->elem_type() != inferredType.elem_type()) {
+          throw std::runtime_error("inferred type differs from existing type");
         } else {
-          auto vi = g->add_value_info();
-          vi->set_name(output);
-          existingType = vi->mutable_type()->mutable_tensor_type();
+          existingType->set_elem_type(inferredType.elem_type());
         }
-
-        // Incorporate the inferred information.
-        if (inferredType.elem_type() != TensorProto::UNDEFINED) {
-          if (existingType->elem_type() != TensorProto::UNDEFINED &&
-              existingType->elem_type() != inferredType.elem_type()) {
-            throw std::runtime_error("inferred type differs from existing type");
-          } else {
-            existingType->set_elem_type(inferredType.elem_type());
-          }
-        }
-        for (int j = 0; j < inferredType.shape().dim_size(); j++) {
-          const auto& inferredDim = inferredType.shape().dim(j);
-          if (existingType->mutable_shape()->dim_size() <= j) {
-            existingType->mutable_shape()->add_dim();
-          }
-          auto* existingDim = existingType->mutable_shape()->mutable_dim(j);
-          if (inferredDim.has_dim_value()) {
-            auto inferredDimValue = inferredDim.dim_value();
-            if (existingDim->has_dim_value() && existingDim->dim_value() != inferredDimValue) {
-              throw std::runtime_error("inferred dimension differs from existing dimension");
-            } else {
-              *existingDim = inferredDim;
-            }
-          }
-        }
-
-        // Make it available to futher inference.
-        valueTypesByName[output] = existingType;
       }
+
+      for (int j = 0; j < inferredType.shape().dim_size(); j++) {
+        const auto& inferredDim = inferredType.shape().dim(j);
+        if (existingType->mutable_shape()->dim_size() <= j) {
+          existingType->mutable_shape()->add_dim();
+        }
+        auto* existingDim = existingType->mutable_shape()->mutable_dim(j);
+        if (inferredDim.has_dim_value()) {
+          auto inferredDimValue = inferredDim.dim_value();
+          if (existingDim->has_dim_value() && existingDim->dim_value() != inferredDimValue) {
+            throw std::runtime_error("inferred dimension differs from existing dimension");
+          }
+          if (existingDim->has_dim_param()) {
+            dimParamValues[existingDim->dim_param()] = inferredDimValue;
+          }
+          *existingDim = inferredDim;
+        }
+      }
+
+      // Make it available to futher inference.
+      valueTypesByName[output] = existingType;
     }
   }
 }
