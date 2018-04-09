cb8f28fd0ced58953725608c9b3ba05fc569798c
Sebastian Messmer <messmer@fb.com>
2018-04-09 13:17:02 -0700

    fix travis

--- .travis/script.sh
+++ .travis/script.sh
@@ -7,6 +7,22 @@ source "${script_path%/*}/setup.sh"
 pip install pytest-cov nbval
 pytest
 
+# lint python code
+pip install flake8
+flake8
+
+# Mypy only works with Python 3
+if [ "${PYTHON_VERSION}" != "python2" ]; then
+  # Mypy only works with our generated _pb.py files when we install in develop mode, so let's do that
+  pip uninstall -y onnx
+  time ONNX_NAMESPACE=ONNX_NAMESPACE_FOO_BAR_FOR_CI pip install -e .[mypy]
+
+  time mypy .
+  # Also test in python2 mode (but this is still in the python 3 CI
+  # instance, because mypy itself needs python 3)
+  time mypy --py2 .
+fi
+
 # check line endings to be UNIX
 find . -type f -regextype posix-extended -regex '.*\.(py|cpp|md|h|cc|proto|proto3|in)' | xargs dos2unix
 git status
@@ -24,19 +40,3 @@ git diff --exit-code
 # in a private namespace.
 ! grep -R --include='*.cc' --include='*.h' 'namespace onnx' .
 ! grep -R --include='*.cc' --include='*.h' 'onnx::' .
-
-# lint python code
-pip install flake8
-flake8
-
-# Mypy only works with Python 3
-if [ "${PYTHON_VERSION}" != "python2" ]; then
-  # Mypy only works with our generated _pb.py files when we install in develop mode, so let's do that
-  pip uninstall -y onnx
-  time ONNX_NAMESPACE=ONNX_NAMESPACE_FOO_BAR_FOR_CI pip install -e .[mypy]
-
-  time mypy .
-  # Also test in python2 mode (but this is still in the python 3 CI
-  # instance, because mypy itself needs python 3)
-  time mypy --py2 .
-fi
