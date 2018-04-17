ae36959caf0c30babf201937b40f956142680911
Lu Fang <lufang@fb.com>
2018-04-17 14:04:51 -0700

    Add functions to load and save TensorProto easily

--- onnx/__init__.py
+++ onnx/__init__.py
@@ -14,59 +14,110 @@ import onnx.defs  # noqa
 import google.protobuf.message  # type: ignore
 
 
-def load(obj):
-    '''
-    Loads a binary protobuf that stores onnx model
-
-    @params
-    Takes a file-like object (has "read" function)
-    or a string containing a file name
-    @return ONNX ModelProto object
-    '''
+def _load_string(obj):
     if hasattr(obj, 'read') and callable(obj.read):
         s = obj.read()
     else:
         with open(obj, 'rb') as f:
             s = f.read()
-    return load_from_string(s)
+    return s
 
 
-def load_from_string(s):
+def _save_string(str, f):
+    if hasattr(f, 'write') and callable(f.write):
+        f.write(str)
+    else:
+        with open(f, 'wb') as writable:
+            writable.write(str)
+
+
+def serialize(proto):
     '''
-    Loads a binary string that stores onnx model
+    Serialize a in-memory proto to bytes
 
     @params
-    Takes a string object containing protobuf
-    @return ONNX ModelProto object
+    proto is a in-memory proto, such as a ModelProto, TensorProto, etc
+
+    @return
+    Serialized proto in bytes
     '''
-    model = ModelProto()
-    decoded = model.ParseFromString(s)
-    # in python implementation ParseFromString returns None
+    if isinstance(proto, bytes):
+        return proto
+    elif hasattr(proto, 'SerializeToString') and callable(proto.SerializeToString):
+        return proto.SerializeToString()
+    else:
+        raise ValueError('No SerializeToString method is detected. '
+                         'neither obj is a str.\ntype is {}'.format(type(proto)))
+
+
+def deserialize(s, obj):
+    '''
+    Parse bytes into a in-memory proto
+
+    @params
+    s is bytes containing serialized proto
+    obj can be a class object, such as ModelProto, also can be a proto instance to be filled in
+
+    @return
+    The proto instance filled in by s
+    '''
+    if not isinstance(s, bytes):
+        raise ValueError('Parameter s must be bytes, but got type: {}'.format(type(s)))
+
+    if callable(obj):
+        obj = obj()
+    if not (hasattr(obj, 'ParseFromString') and callable(obj.ParseFromString)):
+        raise ValueError('No ParseFromString method is detected. '
+                         '\ntype is {}'.format(type(obj)))
+
+    decoded = obj.ParseFromString(s)
     if decoded is not None and decoded != len(s):
         raise google.protobuf.message.DecodeError(
             "Protobuf decoding consumed too few bytes: {} out of {}".format(
                 decoded, len(s)))
-    return model
+    return obj
 
 
-def _save(model, writable):
-    if isinstance(model, ModelProto):
-        writable.write(model.SerializeToString())
-    elif isinstance(model, str):
-        writable.write(model)
-    else:
-        raise ValueError('Model is neither ModelProto nor str.\n{}'.format(model))
-
-
-def save(model, f):
+def load(obj, cls=None):
     '''
-    Saves the model to the specified path.
+    Loads a binary protobuf into memory
 
     @params
-    Takes an ONNX model (ModelProto or string) and a path (writable or string)
+    obj can be a file-like object (has "read" function) or a string containing a file name
+    cls is a class object, such as ModelProto, TensorProto, etc, if not specified,
+    load as ModelProto
+
+    @return
+    Loaded in-memory proto object
     '''
-    if hasattr(f, 'write') and callable(f.write):
-        _save(model, f)
-    else:
-        with open(f, 'wb') as writable:
-            _save(model, writable)
+    s = _load_string(obj)
+    return load_from_string(s, cls)
+
+
+def load_from_string(s, cls=None):
+    '''
+    Loads a binary string that contains serialized proto
+
+    @params
+    s is a string, which contains serialized proto
+    cls is a class object, such as ModelProto, TensorProto, etc, if not specified,
+    load as ModelProto
+
+    @return
+    Loaded in-memory proto object
+    '''
+    if not cls:
+        cls = ModelProto
+    return deserialize(s, cls)
+
+
+def save(proto, f):
+    '''
+    Saves the proto to the specified path.
+
+    @params
+    proto can be either a in-memory proto (e.g., ModelProto) or serialized proto
+    f can be a file-like object (has "write" function) or a string containing a file name
+    '''
+    s = serialize(proto)
+    _save_string(s, f)
--- onnx/test/basic_test.py
+++ onnx/test/basic_test.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from onnx import AttributeProto, NodeProto, GraphProto, ModelProto, IR_VERSION
+from onnx import AttributeProto, NodeProto, GraphProto, ModelProto, TensorProto, IR_VERSION
 
 import io
 import onnx
@@ -11,56 +11,76 @@ import os
 import tempfile
 import unittest
 
+from onnx import helper
 
-class TestProtobufExists(unittest.TestCase):
+
+class TestBasicFunctions(unittest.TestCase):
 
     def _simple_model(self):
-        # Create a model proto.
+        # Create a ModelProto.
         model = ModelProto()
         model.ir_version = IR_VERSION
         return model
 
-    def test_load(self):
-        model = self._simple_model()
-        model_string = model.SerializeToString()
+    def _simple_tensor(self):
+        # Create a TensorProto.
+        tensor = helper.make_tensor(
+            name='test-tensor',
+            data_type=TensorProto.FLOAT,
+            dims=(2, 3, 4),
+            vals=[x + 0.5 for x in range(24)]
+        )
+        return tensor
 
-        # Test if input is string
-        loaded_model = onnx.load_from_string(model_string)
-        self.assertTrue(model == loaded_model)
+    def test_serialize_and_deserialize(self):
+        opts = {
+            '_simple_model': ModelProto,
+            '_simple_tensor': TensorProto,
+        }
+        for gen in opts:
+            obj = getattr(self, gen)()
+            cls = opts[gen]
+            se_obj = onnx.serialize(obj)
+            de_obj = onnx.deserialize(se_obj, cls)
+            se_obj2 = onnx.serialize(de_obj)
+            de_obj2 = onnx.deserialize(se_obj, cls())
+            self.assertTrue(obj == de_obj)
+            self.assertTrue(se_obj == se_obj2)
+            self.assertTrue(de_obj == de_obj2)
+            self.assertRaises(ValueError, onnx.serialize, object())
+            self.assertRaises(ValueError, onnx.deserialize, se_obj, object())
 
-        # Test if input has a read function
-        f = io.BytesIO(model_string)
-        loaded_model = onnx.load(f)
-        self.assertTrue(model == loaded_model)
+    def test_save_and_load(self):
+        opts = {
+            '_simple_model': ModelProto,
+            '_simple_tensor': TensorProto,
+        }
+        for gen in opts:
+            proto = getattr(self, gen)()
+            cls = opts[gen]
+            proto_string = onnx.serialize(proto)
 
-        # Test if input is a file name
-        try:
-            f = tempfile.NamedTemporaryFile(delete=False)
-            f.write(model_string)
-            f.close()
-            loaded_model = onnx.load(f.name)
-            self.assertTrue(model == loaded_model)
-        finally:
-            os.remove(f.name)
+            # Test if input is string
+            loaded_proto = onnx.load_from_string(proto_string, cls)
+            self.assertTrue(proto == loaded_proto)
 
-    def test_save(self):
-        # Create a model proto.
-        model = self._simple_model()
+            # Test if input has a read function
+            f = io.BytesIO()
+            onnx.save(proto_string, f)
+            f = io.BytesIO(f.getvalue())
+            loaded_proto = onnx.load(f, cls)
+            self.assertTrue(proto == loaded_proto)
 
-        # Test writable parameter
-        try:
-            f = tempfile.NamedTemporaryFile(delete=False)
-            onnx.save(model, f)
-            f.close()
-            loaded_model = onnx.load(f.name)
-            self.assertTrue(model == loaded_model)
+            # Test if input is a file name
+            try:
+                f = tempfile.NamedTemporaryFile(delete=False)
+                onnx.save(proto, f)
+                f.close()
 
-            # Test path string paramter
-            onnx.save(model, f.name)
-            loaded_model = onnx.load(f.name)
-            self.assertTrue(model == loaded_model)
-        finally:
-            os.remove(f.name)
+                loaded_proto = onnx.load(f.name, cls)
+                self.assertTrue(proto == loaded_proto)
+            finally:
+                os.remove(f.name)
 
     def test_existence(self):
         try:
