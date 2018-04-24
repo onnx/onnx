38f96a0aa0067e437a97af7771e7a040886ed111
Wenhao Hu <fumihwh@gmail.com>
2018-04-25 00:20:34 +0900

    add tile test case

--- docs/Operators.md
+++ docs/Operators.md
@@ -7401,6 +7401,38 @@ This version of the operator has been available since version 1 of the default O
 </dl>
 
 
+#### Examples
+
+<details>
+<summary>tile</summary>
+
+```python
+node = onnx.helper.make_node(
+    'Tile',
+    inputs=['x', 'y'],
+    outputs=['z']
+)
+
+x = np.array([
+    [0, 1, 2, 3]
+], dtype=np.float32)
+
+repeats = np.array([2, 2], dtype=np.int64)
+
+z = np.array([
+    [0, 1, 2, 3, 0, 1, 2, 3],
+    [0, 1, 2, 3, 0, 1, 2, 3]
+], dtype=np.float32)
+
+expect(node,
+       inputs=[x, repeats],
+       outputs=[z],
+       name='test_tile')
+```
+
+</details>
+
+
 ### <a name="TopK"></a><a name="topk">**TopK**</a>
 
   Retrieve the top-K elements along a specified axis. Given an input tensor of
---
+++ onnx/backend/test/case/node/tile.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import numpy as np  # type: ignore
+
+import onnx
+from ..base import Base
+from . import expect
+
+
+class Tile(Base):
+
+    @staticmethod
+    def export_tile():
+        node = onnx.helper.make_node(
+            'Tile',
+            inputs=['x', 'y'],
+            outputs=['z']
+        )
+
+        x = np.array([
+            [0, 1, 2, 3]
+        ], dtype=np.float32)
+
+        repeats = np.array([2, 2], dtype=np.int64)
+
+        z = np.array([
+            [0, 1, 2, 3, 0, 1, 2, 3],
+            [0, 1, 2, 3, 0, 1, 2, 3]
+        ], dtype=np.float32)
+
+        expect(node,
+               inputs=[x, repeats],
+               outputs=[z],
+               name='test_tile')
