15ae3cd431dcadf58d0aafb01ccca6925194bd0e
Raymond Yang <zihao.yang@microsoft.com>
2018-05-01 21:08:34 -0700

    Update gtests trigger variable name

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,7 @@
 script_path=$(python -c "import os; import sys; print(os.path.realpath(sys.argv[1]))" "${BASH_SOURCE[0]}")
 source "${script_path%/*}/setup.sh"
 
-export ENABLE_GTESTS=1
+export ONNX_ENABLE_GTESTS=1
 pip install protobuf numpy
 time ONNX_NAMESPACE=ONNX_NAMESPACE_FOO_BAR_FOR_CI python  setup.py bdist_wheel --universal --dist-dir .
 find . -maxdepth 1 -name "*.whl" -ls -exec pip install {} \;
