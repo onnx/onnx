707e2f4102fbfa4e4ac40685bdca551aa7e3e571
Sebastian Meßmer <smessmer@users.noreply.github.com>
2018-05-11 16:18:41 -0700

    [Typing 4/5] Add type hints to onnx/backend (#913)

--- onnx/backend/test/case/node/less.py
+++ onnx/backend/test/case/node/less.py
@@ -13,7 +13,7 @@ from . import expect
 class Less(Base):
 
     @staticmethod
-    def export():
+    def export():  # type: () -> None
         node = onnx.helper.make_node(
             'Less',
             inputs=['x', 'y'],
@@ -27,7 +27,7 @@ class Less(Base):
                name='test_less')
 
     @staticmethod
-    def export_less_broadcast():
+    def export_less_broadcast():  # type: () -> None
         node = onnx.helper.make_node(
             'Less',
             inputs=['x', 'y'],
