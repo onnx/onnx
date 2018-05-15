15ba9f946b79127a28162ea9dc105da41d49713c
Sebastian Messmer <messmer@fb.com>
2018-05-15 10:06:51 -0700

    fix code review comments

--- onnx/test/basic_test.py
+++ onnx/test/basic_test.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from onnx import AttributeProto, NodeProto, GraphProto, ModelProto, TensorProto, IR_VERSION
-from typing import IO
 
 import io
 import onnx
