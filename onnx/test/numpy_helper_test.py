f9973b3aa1775e4069cb794e391eef194b57f30c
Sebastian Messmer <messmer@fb.com>
2018-05-15 09:54:42 -0700

    fix type hint

--- onnx/test/numpy_helper_test.py
+++ onnx/test/numpy_helper_test.py
@@ -13,14 +13,14 @@ import unittest
 
 class TestNumpyHelper(unittest.TestCase):
 
-    def _test_numpy_helper_float_type(self, dtype):  # type: (Text) -> None
+    def _test_numpy_helper_float_type(self, dtype):  # type: (np.number) -> None
         a = np.random.rand(13, 37).astype(dtype)
         tensor_def = numpy_helper.from_array(a, "test")
         self.assertEqual(tensor_def.name, "test")
         a_recover = numpy_helper.to_array(tensor_def)
         np.testing.assert_equal(a, a_recover)
 
-    def _test_numpy_helper_int_type(self, dtype):  # type: (Text) -> None
+    def _test_numpy_helper_int_type(self, dtype):  # type: (np.number) -> None
         a = np.random.randint(
             np.iinfo(dtype).min,
             np.iinfo(dtype).max,
