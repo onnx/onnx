0954fe9c53fb7c30c94bb18e352a76c87cb87fe0
Sebastian Meßmer <smessmer@users.noreply.github.com>
2018-05-22 10:47:10 -0700

    Enforce writing type hints. (#993)
    * Disallow untyped functions
    
    * more type hint catching up

--- onnx/backend/test/case/node/reducelogsumexp.py
+++ onnx/backend/test/case/node/reducelogsumexp.py
@@ -13,7 +13,7 @@ from . import expect
 class ReduceLogSumExp(Base):
 
     @staticmethod
-    def export_do_not_keepdims():
+    def export_do_not_keepdims():  # type: () -> None
         shape = [3, 2, 2]
         axes = [1]
         keepdims = 0
@@ -47,7 +47,7 @@ class ReduceLogSumExp(Base):
             name='test_reduce_log_sum_exp_do_not_keepdims_random')
 
     @staticmethod
-    def export_keepdims():
+    def export_keepdims():  # type: () -> None
         shape = [3, 2, 2]
         axes = [1]
         keepdims = 1
@@ -83,7 +83,7 @@ class ReduceLogSumExp(Base):
               name='test_reduce_log_sum_exp_keepdims_random')
 
     @staticmethod
-    def export_default_axes_keepdims():
+    def export_default_axes_keepdims():  # type: () -> None
         shape = [3, 2, 2]
         axes = None
         keepdims = 1
