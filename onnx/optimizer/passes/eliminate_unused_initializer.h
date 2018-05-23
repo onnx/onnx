68d88807fec83932030bcc31db4adad089b54e5f
Wenhao Hu <fumihwh@gmail.com>
2018-05-23 18:50:47 +0900

    DescendOnGraphAttributes

--- onnx/optimizer/passes/eliminate_unused_initializer.h
+++ onnx/optimizer/passes/eliminate_unused_initializer.h
@@ -5,55 +5,39 @@
 
 #include "onnx/optimizer/passes/optimize_pass.h"
 
-namespace ONNX_NAMESPACE { namespace optimization {
+namespace ONNX_NAMESPACE {
+namespace optimization {
 
 struct EliminateUnusedInitializer final : public OptimizePass {
   explicit EliminateUnusedInitializer()
-    : OptimizePass("eliminate_unused_initializer", API_TYPE::IR) {
+      : OptimizePass("eliminate_unused_initializer", API_TYPE::IR) {}
+
+  void erase_used_initializers(
+      Graph& g,
+      std::unordered_set<std::string> * initializer_names) {
+    for (auto output : g.outputs()) {
+      initializer_names->erase(output->uniqueName());
+    }
+    for (auto it = g.begin(); it != g.end(); ++it) {
+      auto* n = *it;
+      DescendOnGraphAttributes(
+          n,
+          [this, initializer_names](Graph& g) {
+            erase_used_initializers(g, initializer_names);
+          });
+      for (auto* input : n->inputs()) {
+        initializer_names->erase(input->uniqueName());
+      }
+    }
   }
 
   void eliminate_unused_initializer(Graph& graph) {
-    std::unordered_set<std::string> used_initializer_names;
-    // get outputs unique names
-    std::vector<std::string> outputs_unique_names;
-    std::transform(
-        graph.outputs().begin(),
-        graph.outputs().end(),
-        std::back_inserter(outputs_unique_names),
-        [](Value* output) { return output->uniqueName(); });
-    for (auto it = graph.begin(); it != graph.end(); ++it) {
-      auto* n = *it;
-      DescendOnGraphAttributes(n, [this](Graph& g){eliminate_unused_initializer(g);});
-      // put all initializers used as input of any node
-      for (auto* input : n->inputs()) {
-        if (std::find(
-                graph.initializer_names().begin(),
-                graph.initializer_names().end(),
-                input->uniqueName()) != graph.initializer_names().end()) {
-          used_initializer_names.insert(input->uniqueName());
-        }
-      }
-    }
-
-    std::vector<std::string> removed_initializer_names;
-
-    // get initializer names should be removed
-    // if it is not used and not an output
-    std::copy_if(
-        graph.initializer_names().begin(),
-        graph.initializer_names().end(),
-        std::back_inserter(removed_initializer_names),
-        [&used_initializer_names, &outputs_unique_names](std::string name) {
-          return used_initializer_names.find(name) ==
-              used_initializer_names.end() &&
-              std::find(
-                  outputs_unique_names.begin(),
-                  outputs_unique_names.end(),
-                  name) == outputs_unique_names.end();
-        });
+    std::unordered_set<std::string> initializer_names(
+        graph.initializer_names().begin(), graph.initializer_names().end());
+    erase_used_initializers(graph, &initializer_names);
 
     // remove initializer and input if need
-    for (std::string name : removed_initializer_names) {
+    for (std::string name : initializer_names) {
       graph.eraseInitializer(name);
       auto iter = std::find_if(
           graph.inputs().begin(), graph.inputs().end(), [&name](Value* input) {
@@ -70,4 +54,5 @@ struct EliminateUnusedInitializer final : public OptimizePass {
   }
 };
 
-}} // namespace ONNX_NAMESPACE::optimization
+} // namespace optimization
+} // namespace ONNX_NAMESPACE
