2dce0ea51a7a5247cd0b9f80d0a94504f8aebbb4
Junjie Bai <bai@in.tum.de>
2018-05-23 16:09:25 -0700

    Remove test cases for the old attribute "axis" and update test data

--- onnx/backend/test/case/node/prelu.py
+++ onnx/backend/test/case/node/prelu.py
@@ -42,20 +42,3 @@ class PRelu(Base):
 
         expect(node, inputs=[x, slope], outputs=[y],
                name='test_prelu_broadcast')
-
-    @staticmethod
-    def export_prelu_axis():  # type: () -> None
-        node = onnx.helper.make_node(
-            'PRelu',
-            inputs=['x', 'slope'],
-            outputs=['y'],
-            broadcast=1,
-            axis=1,
-        )
-
-        x = np.random.randn(3, 4, 5).astype(np.float32)
-        slope = np.random.randn(4).astype(np.float32)
-        y = np.clip(x, 0, np.inf) + np.clip(x, -np.inf, 0) * slope[:, np.newaxis]
-
-        expect(node, inputs=[x, slope], outputs=[y],
-               name='test_prelu_axis')
