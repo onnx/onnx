7d4af293ddc01fb3cf28d06200f9372dd4f1fe5b
Jeff <38966965+jeffbloo@users.noreply.github.com>
2018-05-24 08:31:19 -0700

    Enable non-static schema registration (#894)
    * Allow non-static schema registration
    
    * Fix raw string causing GCC failure
    
    * re-gen doc
    
    * nit: Revert unncessary change in cpp2py_export.cc
    
    * Add missing include guard
    
    * nit: fix incorrect version number in variable name
    
    * nit: remove unused include
    
    * clang-format
    
    * Revert "nit: remove unused include"
    
    This reverts commit 4c8519df68b09af02737b1d0048b1b6a0f47ff68.
    
    * clang-format

--- onnx/common/ir_pb_converter.h
+++ onnx/common/ir_pb_converter.h
@@ -8,7 +8,33 @@
 
 namespace ONNX_NAMESPACE {
 
+class ConvertError final : public std::runtime_error {
+ public:
+  using std::runtime_error::runtime_error;
+
+  ConvertError(const std::string& message) : std::runtime_error(message) {}
+
+  const char* what() const noexcept override {
+    if (!expanded_message_.empty()) {
+      return expanded_message_.c_str();
+    }
+    return std::runtime_error::what();
+  }
+
+  void AppendContext(const std::string& context) {
+    expanded_message_ = ONNX_NAMESPACE::MakeString(
+        std::runtime_error::what(), "\n\n==> Context: ", context);
+  }
+
+ private:
+  std::string expanded_message_;
+};
+
+#define fail_convert(...) \
+  throw ONNX_NAMESPACE::ConvertError(ONNX_NAMESPACE::MakeString(__VA_ARGS__));
+
 void ExportModelProto(ONNX_NAMESPACE::ModelProto* p_m, const std::shared_ptr<Graph>& g);
+
 std::unique_ptr<Graph> ImportModelProto(const ONNX_NAMESPACE::ModelProto& mp);
 
 } // namespace ONNX_NAMESPACE
