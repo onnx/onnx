a3541f78feec774722de1f85b2bd0963bfe0c19f
Anirudh Acharya <aanirud@8c85904b0bf4.ant.amazon.com>
2018-05-25 17:54:29 -0700

    change window size

--- onnx/backend/test/case/node/lrn.py
+++ onnx/backend/test/case/node/lrn.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 import numpy as np  # type: ignore
 
 import onnx
+import math
 from ..base import Base
 from . import expect
 
@@ -14,31 +15,51 @@ class LRN(Base):
 
     @staticmethod
     def export():
+        alpha = 0.0002
+        beta = 0.5
+        bias = 2.0
+        nsize = 3
         node = onnx.helper.make_node(
             'LRN',
             inputs=['x'],
             outputs=['y'],
-            alpha=0.0002,
-            beta=0.5,
-            bias=2.0,
-            size=1
+            alpha=alpha,
+            beta=beta,
+            bias=bias,
+            size=nsize
         )
-        x = np.random.ranf([5, 5, 5, 5]).astype(np.float32) * 1000
-        y = [i/((2 + 0.0002 * i ** 2) ** 0.5) for i in x][0]
+        x = np.random.rand(5, 5, 5, 5).astype(np.float32) * 1000
+        square_sum = np.zeros((5,5,5,5))
+        for n,c,h,w in np.ndindex(x.shape):
+            square_sum[n,c,h,w] = sum(x[n,
+                                        max(0, c - int(math.floor((nsize - 1) / 2)))
+                                        :min(4, c + int(math.ceil((nsize - 1) / 2)) + 1),
+                                        h,
+                                        w] ** 2)
+        y = x / ((bias + (alpha / nsize) * square_sum) ** beta)
         expect(node, inputs=[x], outputs=[y],
                name='test_lrn')
 
     @staticmethod
     def export_default():
+        alpha = 0.0001
+        beta = 0.75
+        bias = 1.0
+        nsize = 3
         node = onnx.helper.make_node(
             'LRN',
             inputs=['x'],
             outputs=['y'],
-            alpha=0.0002,
-            beta=0.5,
-            size=1
+            size=3
         )
-        x = np.random.ranf([5, 5, 5, 5]).astype(np.float32) * 1000
-        y = [i/((1 + 0.0002 * i ** 2) ** 0.5) for i in x][0]
+        x = np.random.rand(5, 5, 5, 5).astype(np.float32) * 1000
+        square_sum = np.zeros((5,5,5,5))
+        for n,c,h,w in np.ndindex(x.shape):
+            square_sum[n,c,h,w] = sum(x[n,
+                                        max(0, c - int(math.floor((nsize - 1) / 2)))
+                                        :min(4, c + int(math.ceil((nsize - 1) / 2)) + 1),
+                                        h,
+                                        w] ** 2)
+        y = x / ((bias + (alpha / nsize) * square_sum) ** beta)
         expect(node, inputs=[x], outputs=[y],
                name='test_lrn_default')
