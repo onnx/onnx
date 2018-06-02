ce3415959cae0b52d2ae2c15689896a472b10bd2
zrphercule <zrphercule@gmail.com>
2018-06-01 21:29:22 -0700

    Extract constant to initializer (#1050)
    * extract_constant_to_initializer
    
    * new optimizer pass with a testcase
    
    * finalized testcase, tensor shape & elem type corrected
    
    * format adjustment for OCD patients
    
    * const auto name
    
    * int64_t type allowed in class Dimension
    
    * remove useless blank line in optimizer_test.py
    
    * annotation of new test case is added
    
    * modified
    
    * clang formatted
    
    * removed use of int in Dimension; explaination of new pass has been added
    
    * safe move(t)

---
+++ onnx/optimizer/passes/extract_constant_to_initializer.h
@@ -0,0 +1,54 @@
+// ATTENTION: The code in this file is highly EXPERIMENTAL.
+// Adventurous users should note that the APIs will probably change.
+
+#pragma once
+
+// Before:
+//	 A = Constant()
+// After:
+//	 A is in the initializer list
+//
+//	 this pass can handle the case satisfy all following conditions:
+//	   condition 1: A is the output of a Constant node
+
+#include "onnx/optimizer/passes/optimize_pass.h"
+
+namespace ONNX_NAMESPACE {
+namespace optimization {
+
+struct ExtractConstantToInitializer final : public OptimizePass {
+  explicit ExtractConstantToInitializer()
+      : OptimizePass("extract_constant_to_initializer", API_TYPE::IR) {}
+
+  void extract_constant_to_initializer(Graph& graph) {
+    for (auto it = graph.begin(); it != graph.end(); ++it) {
+      auto* n = *it;
+      DescendOnGraphAttributes(
+          n, [this](Graph& g) { extract_constant_to_initializer(g); });
+      if (n->kind() == kConstant) {
+        const auto name = n->output()->uniqueName();
+        Tensor t = n->t(kvalue);
+        t.setName(name);
+        std::vector<Dimension> tsizes;
+        for (auto v : t.sizes()) {
+          tsizes.push_back(v);
+        }
+        Node* param = graph.create(kParam, 1);
+        param->output()->setUniqueName(name);
+        param->output()->setSizes(tsizes);
+        param->output()->setElemType(t.elem_type());
+        graph.addInitializer(std::move(t), name);
+        graph.addInput()->copyMetadata(param->output());
+        n->replaceAllUsesWith(param);
+        it.destroyCurrent();
+      }
+    }
+  }
+
+  void optimize(Graph& graph) override {
+    extract_constant_to_initializer(graph);
+  }
+};
+
+} // namespace optimization
+} // namespace ONNX_NAMESPACE
