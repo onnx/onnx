0c8c37562345c265439f18f5710bcb8296f2bac0
Varun Jain <jainv@fb.com>
2018-06-05 16:15:24 -0700

    continuing optimization pass

--- onnx/optimizer/passes/fuse_bn_into_conv.h
+++ onnx/optimizer/passes/fuse_bn_into_conv.h
@@ -14,24 +14,40 @@ struct FuseBNIntoConv final : public OptimizePass {
     : OptimizePass("fuse_consecutive_transposes", API_TYPE::IR) {
   }
 
+  bool check_initializers(Node* conv, Node* bn) {
+    auto bn_inputs = bn->inputs();
+    auto conv_inputs = conv->inputs();
+    for (int i = 1; i <= 4; i++)  {
+      if (get_initializer_index(bn_inputs[i]) == -1)  {
+        return false;
+      }
+    }
+    if (get_initializer_index(conv_inputs[1]) == -1)  {
+      return false;
+    }
+    if (conv_inputs.size() == 3 && get_initializer_index(conv_inputs[1]) == -1) {
+      return false;
+    }
+    return true;
+  }
+
   void modify_conv(Node* conv, Node* bn)  {
-    auto bn_inputs = bn.inputs();
-    auto conv_inputs = conv.inputs();
-    auto s = bn_inputs[1];
-    auto bbn = bn_inputs[2];
-    auto m = bn_inputs[3];
-    auto var = bn_inputs[4];
+    auto bn_inputs = bn->inputs();
+    auto conv_inputs = conv->inputs();
+    auto bn_shape = bn->sizes();
+    auto conv_shape = conv->sizes();
+    auto s = initializers()[get_initializer_index(bn_inputs[1])];
+    auto bbn = initializers()[get_initializer_index(bn_inputs[2])];
+    auto m = initializers()[get_initializer_index(bn_inputs[3])];
+    auto var = initializers()[get_initializer_index(bn_inputs[4])];
     auto epsilon = bn->f(kepsilon);
     auto frac = s / sqrt(var + epsilon);
-    conv_inputs[1] *= frac;
-    Value* bc;
+    conv_inputs[1].scale(frac);
     if (conv_inputs.size() == 2)  {
-      bc = 0;
+      Value* bc = new Value(, 2);
       conv_inputs.addInput(bc);
-    } else {
-      bc = conv_inputs[2];
     }
-    conv_inputs[2] = (bc - m) * frac + bbn;
+    (conv_inputs[2].add(m.scale(-1))).scale(frac)).add(bbn);
   }
 
   void fuse_bn_into_conv(Graph& graph) {
@@ -40,7 +56,9 @@ struct FuseBNIntoConv final : public OptimizePass {
       DescendOnGraphAttributes(n, [this](Graph& g){fuse_bn_into_conv(g);});
       if (n->kind() == kBatchNormalization && n->input()->node()->kind() == kConv) {
         auto origInput = n->input();
-        if (origInput->uses().size() > 1 || n->outputs().size() > 1) {
+        if (origInput->uses().size() > 1 ||
+            n->outputs().size() > 1 ||
+            !check_initializers(n->input()->node(), n)) {
           continue;
         }
         n->output().replaceAllUsesWith(origInput);
