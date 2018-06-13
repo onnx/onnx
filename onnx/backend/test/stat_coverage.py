2090eac03f711306b868e9503deb0d3b09cad18b
Lu Fang <lufang@fb.com>
2018-06-13 08:14:33 +0800

    lint

--- onnx/backend/test/stat_coverage.py
+++ onnx/backend/test/stat_coverage.py
@@ -1,4 +1,9 @@
-import io
+#!/usr/bin/env python
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import os
 
 from onnx import defs
