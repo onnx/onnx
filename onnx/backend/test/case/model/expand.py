ddf156e46537d588121f09927eae5559138384fb
zrphercule <zrphercule@gmail.com>
2018-06-18 14:40:48 -0700

    formatted

--- onnx/backend/test/case/model/expand.py
+++ onnx/backend/test/case/model/expand.py
@@ -12,27 +12,26 @@ from . import expect
 
 class ExpandDynamicShape(Base):
 
-
     @staticmethod
     def export():  # type -> None
 
         def make_graph(node, input_shape, shape_shape, output_shape):
             graph = onnx.helper.make_graph(
-                nodes = [node],
-                name = 'Expand',
-                inputs = [onnx.helper.make_tensor_value_info('X',
+                nodes=[node],
+                name='Expand',
+                inputs=[onnx.helper.make_tensor_value_info('X',
                                                              onnx.TensorProto.FLOAT,
                                                              input_shape),
                           onnx.helper.make_tensor_value_info('shape',
                                                              onnx.TensorProto.INT64,
                                                              shape_shape)],
-                outputs = [onnx.helper.make_tensor_value_info('Y',
+                outputs=[onnx.helper.make_tensor_value_info('Y',
                                                               onnx.TensorProto.FLOAT,
                                                               output_shape)])
             return graph
 
         node = onnx.helper.make_node(
-            'Expand', ['X', 'shape'], ['Y'], name = 'test')
+            'Expand', ['X', 'shape'], ['Y'], name='test')
         input_shape = [1, 3, 1]
         x = np.ones(input_shape)
 
@@ -41,29 +40,25 @@ class ExpandDynamicShape(Base):
         y = x * np.ones(shape)
         graph = make_graph(node, input_shape, shape.shape, y.shape)
         model = onnx.helper.make_model(graph, producer_name='backend-test')
-        expect(model, inputs = [x, shape], outputs = [y], name =
-                "test_expand_shape_model1")
+        expect(model, inputs=[x, shape], outputs=[y], name="test_expand_shape_model1")
 
         #2nd testcase
         shape = np.array([1, 3])
         y = x * np.ones(shape)
         graph = make_graph(node, input_shape, shape.shape, y.shape)
         model = onnx.helper.make_model(graph, producer_name='backend-test')
-        expect(model, inputs = [x, shape], outputs = [y], name =
-               "test_expand_shape_model2")
+        expect(model, inputs=[x, shape], outputs=[y], name="test_expand_shape_model2")
 
         #3rd testcase
         shape = np.array([3, 1, 3])
         y = x * np.ones(shape)
         graph = make_graph(node, input_shape, shape.shape, y.shape)
         model = onnx.helper.make_model(graph, producer_name='backend-test')
-        expect(model, inputs = [x, shape], outputs = [y], name =
-               "test_expand_shape_model3")
+        expect(model, inputs=[x, shape], outputs=[y], name="test_expand_shape_model3")
 
         #4th testcase
         shape = np.array([3, 3, 1, 3])
         y = x * np.ones(shape)
         graph = make_graph(node, input_shape, shape.shape, y.shape)
         model = onnx.helper.make_model(graph, producer_name='backend-test')
-        expect(model, inputs = [x, shape], outputs = [y], name =
-               "test_expand_shape_model4")
+        expect(model, inputs=[x, shape], outputs=[y], name="test_expand_shape_model4")
