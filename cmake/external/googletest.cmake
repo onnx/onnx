5b6f555139cb248d054b2b68a407aad94f1e206c
peter <peter_jiachen@163.com>
2018-06-26 23:15:55 +0800

    More reverts

--- cmake/external/googletest.cmake
+++ cmake/external/googletest.cmake
@@ -6,31 +6,13 @@ set(googletest_BUILD ${CMAKE_CURRENT_BINARY_DIR}/googletest/)
 set(googletest_TAG 0fe96607d85cf3a25ac40da369db62bbee2939a5)
 #718fd88d8f145c63b8cc134cf8fed92743cc112f
 
-if(MSVC)
-    if("${CMAKE_GENERATOR}" MATCHES "Ninja")
-        set(googletest_STATIC_LIBRARIES
-            ${CMAKE_CURRENT_BINARY_DIR}/googletest/src/googletest/googletest/gtest.lib)
-    else()
-        set(googletest_STATIC_LIBRARIES
-            ${CMAKE_CURRENT_BINARY_DIR}/googletest/src/googletest/googletest/Release/gtest.lib)
-    endif()
-    set(ADDITIONAL_C_FLAGS "/wd4996")
-    set(ADDITIONAL_CXX_FLAGS "/wd4996")
-    IF("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        if (PY_ARCH)
-            set(ADDITIONAL_C_FLAGS "${ADDITIONAL_C_FLAGS} -m${PY_ARCH}")
-            set(ADDITIONAL_CXX_FLAGS "${ADDITIONAL_CXX_FLAGS} -m${PY_ARCH}")
-        endif(PY_ARCH)
-    endif()
+if(WIN32)
+  set(googletest_STATIC_LIBRARIES
+      ${CMAKE_CURRENT_BINARY_DIR}/googletest/src/googletest/googletest/Release/gtest.lib)
 else()
   set(googletest_STATIC_LIBRARIES
       ${CMAKE_CURRENT_BINARY_DIR}/googletest/src/googletest/googletest/libgtest.a)
-  set(ADDITIONAL_C_FLAGS "")
-  set(ADDITIONAL_CXX_FLAGS "")
 endif()
-
-MESSAGE(STATUS "ADDITIONAL_C_FLAGS: ${ADDITIONAL_C_FLAGS}")
-MESSAGE(STATUS "ADDITIONAL_CXX_FLAGS: ${ADDITIONAL_CXX_FLAGS}")
 
 ExternalProject_Add(googletest
     PREFIX googletest
@@ -40,10 +22,6 @@ ExternalProject_Add(googletest
     BUILD_IN_SOURCE 1
     BUILD_COMMAND ${CMAKE_COMMAND} --build . --config Release --target gtest
     INSTALL_COMMAND ""
-    BUILD_BYPRODUCTS "${googletest_STATIC_LIBRARIES}"
-    CMAKE_ARGS
-        -DCMAKE_C_FLAGS=${ADDITIONAL_C_FLAGS}
-        -DCMAKE_CXX_FLAGS=${ADDITIONAL_CXX_FLAGS}
     CMAKE_CACHE_ARGS
         -DCMAKE_BUILD_TYPE:STRING=Release
         -DBUILD_GMOCK:BOOL=OFF
