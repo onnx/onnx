e3010fa21583819eb33b5194c595c12c92b16b6f
Raymond Yang <zihao.yang@microsoft.com>
2018-07-02 14:42:16 -0700

    Update MVN testcase to caculate expected output on the fly

--- onnx/backend/test/case/node/mvn.py
+++ onnx/backend/test/case/node/mvn.py
@@ -21,7 +21,7 @@ class MVN(Base):
             axes=[0, 2, 3]
         )
 
-        x = np.array([[[[0.8439683], [0.5665144], [0.05836735]],
+        input_data = np.array([[[[0.8439683], [0.5665144], [0.05836735]],
             [[0.02916367], [0.12964272], [0.5060197]],
             [[0.79538304], [0.9411346], [0.9546573]]],
             [[[0.17730942], [0.46192095], [0.26480448]],
@@ -31,16 +31,13 @@ class MVN(Base):
             [[0.81929934], [0.7862604], [0.11799799]],
             [[0.69248444], [0.54119414], [0.07513223]]]], dtype=np.float32)
 
-        # Hard-coded pre calculated data for avoid the overhead in
-        # calculating test data (with across_channels=False)
-        y = np.array([[[[1.3546424], [0.330535], [-1.5450816]],
-            [[-1.2106763], [-0.89259505], [0.29888144]],
-            [[0.38083088], [0.81808794], [0.85865635]]],
-            [[[-1.1060556], [-0.05552877], [-0.78310347]],
-            [[0.8328137], [-1.250282], [0.6746787]],
-            [[0.7669372], [0.9113869], [-1.6463585]]],
-            [[[-0.23402767], [1.6092132], [0.429406]],
-            [[1.290614], [1.1860244], [-0.92945826]],
-            [[0.0721334], [-0.38174], [-1.7799333]]]], dtype=np.float32)
-        expect(node, inputs=[x], outputs=[y],
+        # Calculate expected output data
+        data_mean = np.mean(input_data, axis=(0, 2, 3), keepdims=1)
+        data_mean_squared = np.power(data_mean, 2)
+        data_squared = np.power(input_data, 2)
+        data_squared_mean = np.mean(data_squared, axis=(0, 2, 3), keepdims=1)
+        std = np.sqrt(data_squared_mean - data_mean_squared)
+        expected_output = (input_data - data_mean) / std
+
+        expect(node, inputs=[input_data], outputs=[expected_output],
                name='test_mvn')
