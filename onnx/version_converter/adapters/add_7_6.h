3ce7201966a8cf92d279f3c26800f9e61a96a6ef
Akshay Chalana <akshayc@fb.com>
2018-07-11 10:22:52 -0700

    Slight changes

--- onnx/version_converter/adapters/add_7_6.h
+++ onnx/version_converter/adapters/add_7_6.h
@@ -14,6 +14,7 @@ struct Add_7_6 final : public Adapter {
   void adapt_add_7_6(Graph& graph, Node& node) {
     // Verify that broadcasts are allowed in limited spec of opset version 6
     // Multidirectional broadcasting, as defined in Broadcasting.md
+<<<<<<< HEAD
     // MathDocGenerator provides differences
     // Main change: encode broadcasting commands as explicit attribute
     ArrayRef<Value*> inputs = node.inputs();
@@ -64,6 +65,8 @@ struct Add_7_6 final : public Adapter {
       // Add broadcast attribute
       node.i_(kbroadcast, 1);
     }
+=======
+>>>>>>> Slight changes
   }
 
   void adapt(Graph& graph, Node& node) override {
