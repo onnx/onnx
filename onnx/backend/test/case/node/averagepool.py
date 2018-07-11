5c71785828c470758722b873dc0cd139c53662cd
Lu Fang <lufang@fb.com>
2018-07-11 15:29:57 -0700

    Add the removed cases back

--- onnx/backend/test/case/node/averagepool.py
+++ onnx/backend/test/case/node/averagepool.py
@@ -99,6 +99,34 @@ class AveragePool(Base):
         expect(node, inputs=[x], outputs=[y], name='test_averagepool_2d_precomputed_strides')
 
     @staticmethod
+    def export_averagepool_2d_precomputed_same_upper():  # type: () -> None
+        """
+        input_shape: [1, 1, 5, 5]
+        output_shape: [1, 1, 3, 3]
+        pad_shape: [2, 2] -> [1, 1, 1, 1] by axis
+        """
+        node = onnx.helper.make_node(
+            'AveragePool',
+            inputs=['x'],
+            outputs=['y'],
+            kernel_shape=[3, 3],
+            strides=[2, 2],
+            auto_pad='SAME_UPPER'
+        )
+        x = np.array([[[
+            [1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 10],
+            [11, 12, 13, 14, 15],
+            [16, 17, 18, 19, 20],
+            [21, 22, 23, 24, 25],
+        ]]]).astype(np.float32)
+        y = np.array([[[[4, 5.5, 7],
+                        [11.5, 13, 14.5],
+                        [19, 20.5, 22]]]]).astype(np.float32)
+
+        expect(node, inputs=[x], outputs=[y], name='test_averagepool_2d_precomputed_same_upper')
+
+    @staticmethod
     def export_averagepool_1d_default():  # type: () -> None
         """
         input_shape: [1, 3, 32]
@@ -195,37 +223,6 @@ class AveragePool(Base):
         expect(node, inputs=[x], outputs=[y], name='test_averagepool_2d_same_upper')
 
     @staticmethod
-    def export_averagepool_2d_same_upper_count_include_pad():  # type: () -> None
-        """
-        iutput_shape: [1, 3, 32, 32]
-        output_shape: [1, 3, 32, 32]
-        pad_shape: [1, 1] -> [0, 1, 0, 1] by axis
-        """
-        node = onnx.helper.make_node(
-            'AveragePool',
-            inputs=['x'],
-            outputs=['y'],
-            kernel_shape=[2, 2],
-            auto_pad='SAME_UPPER',
-            count_include_pad=1
-        )
-        x = np.random.randn(1, 3, 32, 32).astype(np.float32)
-        x_shape = np.shape(x)
-        kernel_shape = (2, 2)
-        strides = (1, 1)
-        out_shape = get_output_shape('SAME_UPPER', x_shape[2:], kernel_shape, strides)
-        pad_shape = get_pad_shape('SAME_UPPER', x_shape[2:], kernel_shape, strides, out_shape)
-        pad_top = pad_shape[0] // 2
-        pad_bottom = pad_shape[0] - pad_top
-        pad_left = pad_shape[1] // 2
-        pad_right = pad_shape[1] - pad_left
-        padded = np.pad(x, ((0, 0), (0, 0), (pad_top, pad_bottom), (pad_left, pad_right)), mode='constant',
-                        constant_values=0)
-        y = pool(padded, x_shape, kernel_shape, strides, out_shape, pad_shape, 'AVG', count_include_pad=1)
-
-        expect(node, inputs=[x], outputs=[y], name='test_averagepool_2d_same_upper_count_include_pad')
-
-    @staticmethod
     def export_averagepool_2d_same_lower():  # type: () -> None
         """
         iutput_shape: [1, 3, 32, 32]
@@ -254,37 +251,6 @@ class AveragePool(Base):
         y = pool(padded, x_shape, kernel_shape, strides, out_shape, pad_shape, 'AVG')
 
         expect(node, inputs=[x], outputs=[y], name='test_averagepool_2d_same_lower')
-
-    @staticmethod
-    def export_averagepool_2d_same_lower_count_include_pad():  # type: () -> None
-        """
-        iutput_shape: [1, 3, 32, 32]
-        output_shape: [1, 3, 32, 32]
-        pad_shape: [1, 1] -> [1, 0, 1, 0] by axis
-        """
-        node = onnx.helper.make_node(
-            'AveragePool',
-            inputs=['x'],
-            outputs=['y'],
-            kernel_shape=[2, 2],
-            auto_pad='SAME_LOWER',
-            count_include_pad=1,
-        )
-        x = np.random.randn(1, 3, 32, 32).astype(np.float32)
-        x_shape = np.shape(x)
-        kernel_shape = (2, 2)
-        strides = (1, 1)
-        out_shape = get_output_shape('SAME_LOWER', x_shape[2:], kernel_shape, strides)
-        pad_shape = get_pad_shape('SAME_LOWER', x_shape[2:], kernel_shape, strides, out_shape)
-        pad_bottom = pad_shape[0] // 2
-        pad_top = pad_shape[0] - pad_bottom
-        pad_right = pad_shape[1] // 2
-        pad_left = pad_shape[1] - pad_right
-        padded = np.pad(x, ((0, 0), (0, 0), (pad_top, pad_bottom), (pad_left, pad_right)), mode='constant',
-                        constant_values=0)
-        y = pool(padded, x_shape, kernel_shape, strides, out_shape, pad_shape, 'AVG', count_include_pad=1)
-
-        expect(node, inputs=[x], outputs=[y], name='test_averagepool_2d_same_lower_count_include_pad')
 
     @staticmethod
     def export_averagepool_2d_pads():  # type: () -> None
