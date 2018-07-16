f081fd610fc3b5d017725265cf96a3096018def2
Raymond Yang <zihao.yang@microsoft.com>
2018-07-16 16:08:37 -0700

    Minor fix in c++ tests; Generated function docs for ML domain

--- onnx/test/c++/function_get_test.cc
+++ onnx/test/c++/function_get_test.cc
@@ -8,7 +8,8 @@ TEST(FunctionAPITest, Get_All_Functions) {
   std::multimap<std::string, std::unique_ptr<FunctionProto>> temp_map;
   FunctionBuilderRegistry& function_registry =
       FunctionBuilderRegistry::OnnxInstance();
-  Common::Status status = function_registry.GetFunctions("", &temp_map);
+  Common::Status status =
+      function_registry.GetFunctions(ONNX_DOMAIN, &temp_map);
   size_t input_size = temp_map.size();
   EXPECT_EQ(input_size, 1);
   EXPECT_EQ(temp_map.count("FuncMeanVarianceNormalization"), 1);
