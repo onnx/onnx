cfe5a5a33a93b457379bd90f71dd5163ef139b03
Anders Papitto <anderspapitto@gmail.com>
2018-07-24 13:01:42 -0700

    enable most optimizer passes by default

--- onnx/optimizer.py
+++ onnx/optimizer.py
@@ -19,28 +19,54 @@ from typing import Text, Sequence
 
 Arguments:
     input (ModelProto): model
-    names (list of string): list of optimization names
+    names (list of string): list of optimization names.
 
 Return:
     return (ModelProto) optimized model
 
 Supported pass names:
-    -- nop
     -- eliminate_identity
     -- eliminate_nop_transpose
     -- eliminate_unused_initializer
+    -- extract_constant_to_initializer
+    -- fuse_add_bias_into_conv
+    -- fuse_bn_into_conv
     -- fuse_consecutive_squeezes
     -- fuse_consecutive_transposes
-    -- fuse_add_bias_into_conv
     -- fuse_transpose_into_gemm
-"""
+    -- lift_lexical_references
+    -- split
 
+Most passes are on by default.  See `default_enabled_passes` in
+`optimizer.py` for the full list.
+
+A few passes are off by default, because they have some unusual
+behavior.  These are
+
+    -- split. This optimization splits a single graph into two.
+
+A note on motivations: the intent here is to behave somewhat
+analogously to `gcc -O2`.  You don't have to invoke optimization, and
+you are free to invoke optimization with a carefully selected set of
+passes, but the default is to apply all reasonable passes, which may
+grow/change over time.
+
+"""
+default_enabled_passes = [
+    'eliminate_identity',
+    'eliminate_nop_transpose',
+    'eliminate_unused_initializer',
+    'extract_constant_to_initializer',
+    'fuse_add_bias_into_conv',
+    'fuse_bn_into_conv',
+    'fuse_consecutive_squeezes',
+    'fuse_consecutive_transposes',
+    'fuse_transpose_into_gemm',
+    'lift_lexical_references'
+]
 
 def optimize(model, passes=[]):  # type: (ModelProto, Sequence[Text]) -> ModelProto
-    if len(passes) == 0:
-        passes = ['eliminate_nop_transpose',
-                  'fuse_consecutive_transposes',
-                  'fuse_transpose_into_gemm']
+    passes = passes or default_enabled_passes
     if not isinstance(model, ModelProto):
         raise ValueError('Optimizer only accepts ModelProto, incorrect type: {}'.format(type(model)))
 
