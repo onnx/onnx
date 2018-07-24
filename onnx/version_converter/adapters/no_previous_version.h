0f522f0a82680b060c9a9278a1bab163aa9b5bf0
Akshay Chalana <akshayc@fb.com>
2018-07-24 14:55:37 -0700

    No Previous Version rename

--- onnx/version_converter/adapters/no_previous_version.h
+++ onnx/version_converter/adapters/no_previous_version.h
@@ -8,8 +8,8 @@
 namespace ONNX_NAMESPACE { namespace version_conversion {
 
 struct NoPreviousVersionAdapter final : public Adapter {
-  explicit NoPreviousVersionAdapter(const std::string& op_name, const OpSetID
-    initial, const OpSetID target): Adapter(op_name,
+  explicit NoPreviousVersionAdapter(const std::string op_name, const OpSetID
+    initial, const OpSetID target): Adapter(std::move(op_name),
     std::move(initial), std::move(target)) {}
 
   void adapt(std::shared_ptr<Graph> graph, Node* node) const override {
