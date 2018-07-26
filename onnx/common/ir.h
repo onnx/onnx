8511d35cf3f567ecdcec6eed2fde245219128129
Akshay Chalana <akshayc@fb.com>
2018-07-26 16:35:25 -0700

    Finished addressing Lu comments

--- onnx/common/ir.h
+++ onnx/common/ir.h
@@ -786,7 +786,7 @@ class OpSetID final {
       :domain_(""), version_(version) {}
 
     explicit OpSetID(const std::string& domain, int64_t version)
-      :domain_(std::move(domain)), version_(version) {}
+      :domain_(domain), version_(version) {}
 
     // target must be in the form "<domain>&<version>"
     std::string toString() const {
