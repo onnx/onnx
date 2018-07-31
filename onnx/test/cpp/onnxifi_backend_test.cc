2e7753cba14a622f7c8f31b7cc7cb8097b57ef09
zrphercule <zrphercule@gmail.com>
2018-07-31 15:00:07 -0700

    move macro ONNXIFI_DUMMY_LIBRARY to onnxifi.h

--- onnx/test/cpp/onnxifi_backend_test.cc
+++ onnx/test/cpp/onnxifi_backend_test.cc
@@ -4,15 +4,6 @@
 #include "onnx/onnxifi_loader.h"
 #include "onnx/onnxifi.h"
 
-#if defined(__APPLE__)
-#define ONNXIFI_DUMMY_LIBRARY "libonnxifi_dummy.dylib"
-#elif defined(_WIN32)
-#define ONNXIFI_DUMMY_LIBRARY L"onnxifi_dummy.dll"
-#else
-#define ONNXIFI_DUMMY_LIBRARY "libonnxifi_dummy.so"
-#endif
-
-
 namespace ONNX_NAMESPACE
 {
 	namespace Test
