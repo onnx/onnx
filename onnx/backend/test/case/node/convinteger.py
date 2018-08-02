66849549b3f438c40998a7674578ffda0e6fb090
Ke Zhang <kezhan@microsoft.com>
2018-08-01 17:43:23 -0700

    update a test case.

--- onnx/backend/test/case/node/convinteger.py
+++ onnx/backend/test/case/node/convinteger.py
@@ -20,8 +20,8 @@ class ConvInteger(Base):
                         [10, 11, 12, 13, 14],
                         [15, 16, 17, 18, 19],
                         [20, 21, 22, 23, 24]]]]).astype(np.uint8)
-        W = np.array([[[[1, 1, 1],  # (1, 1, 3, 3) tensor for convolution weights
-                        [1, 1, 1],
+        W = np.array([[[[0, 1, 1],  # (1, 1, 3, 3) tensor for convolution weights
+                        [1, 1, 0],
                         [1, 1, 1]]]]).astype(np.uint8)
         Z = np.array([1]).astype(np.uint8)
         # Convolution with padding
@@ -32,11 +32,11 @@ class ConvInteger(Base):
             # Default values for other attributes: strides=[1, 1], dilations=[1, 1], groups=1
             pads=[1, 1, 1, 1],
         )
-        y_with_padding = np.array([[[[17, 24, 30, 36, 29],  # (1, 1, 5, 5) output tensor
-                                     [36, 54, 63, 72, 54],
-                                     [66, 99, 108, 117, 84],
-                                     [96, 144, 153, 162, 114],
-                                     [77, 114, 120, 126, 89]]]]).astype(np.uint32)
+        y_with_padding = np.array([[[[15, 21, 26, 31, 27],  # (1, 1, 5, 5) output tensor
+                                     [29, 47, 54, 61, 50],
+                                     [54, 82, 89, 96, 75],
+                                     [79, 117, 114, 131, 100],
+                                     [55, 77, 81, 85, 70]]]]).astype(np.uint32)
         expect(node_with_padding, inputs=[x, W, Z], outputs=[y_with_padding],
                name='test_basic_convinteger_with_padding')
 
@@ -48,9 +48,9 @@ class ConvInteger(Base):
             # Default values for other attributes: strides=[1, 1], dilations=[1, 1], groups=1
             pads=[0, 0, 0, 0],
         )
-        y_without_padding = np.array([[[[54, 63, 72],  # (1, 1, 3, 3) output tensor
-                                        [99, 108, 117],
-                                        [144, 153, 162]]]]).astype(np.uint32)
+        y_without_padding = np.array([[[[47, 54, 61],  # (1, 1, 3, 3) output tensor
+                                        [82, 89, 96],
+                                        [117, 124, 131]]]]).astype(np.uint32)
         expect(node_without_padding, inputs=[x, W, Z], outputs=[y_without_padding],
                name='test_basic_convinteger_without_padding')
 
