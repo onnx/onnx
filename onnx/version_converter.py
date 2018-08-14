30d3b0810b73c53b627d43755a94f4c951215f80
Akshay Chalana <akshayc@fb.com>
2018-08-14 15:44:43 -0700

    Supported adapters

--- onnx/version_converter.py
+++ onnx/version_converter.py
@@ -44,10 +44,10 @@ Supported adapters:
     --BatchNorm from Opset 6 to Opset 7
     --BatchNorm from Opset 6 to Opset 5
     --BatchNorm from Opset 5 to Opset 6
-
-Unsupported adapters:
     --Concat from Opset 4 to Opset 3
     --Concat from Opset 3 to Opset 4
+
+Unsupported adapters:
     --MaxPool from Opset 8 to Opset 7
     --MaxPool from Opset 7 to Opset 8
     --Reshape from Opset 5 to Opset 4
