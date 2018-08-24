0cc9cb7e88f3e3e7bf17639465a2eddfdc060bd9
Yuming Chang <yuming@iclink.tw>
2018-08-24 14:05:45 +0800

    Fix flake8 format errors.

--- onnx/test/shape_inference_test.py
+++ onnx/test/shape_inference_test.py
@@ -217,7 +217,7 @@ class TestShapeInference(unittest.TestCase):
     def test_upsample(self):  # type: () -> None
         graph = self._make_graph(
             [('x', TensorProto.INT32, (2, 4, 3, 5))],
-            [make_node("Upsample", ['x'], ['y'], scales = [1.0, 1.1, 1.3, 1.9])],
+            [make_node("Upsample", ['x'], ['y'], scales=[1.0, 1.1, 1.3, 1.9])],
             [])
         self._assert_inferred(graph, [make_tensor_value_info('y', TensorProto.INT32, (2, 4, 3, 9))])
 
