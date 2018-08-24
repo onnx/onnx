d9fd74c04f58a63cc7ead8f35af2340f01a12f77
Akshay Chalana <akshayc@fb.com>
2018-08-24 13:38:56 -0700

    Squashing all commits into one

--- onnx/test/version_converter_test.py
+++ onnx/test/version_converter_test.py
@@ -283,6 +283,38 @@ class TestVersionConverter(unittest.TestCase):
         assert converted_model.graph.node[0].op_type == "Concat"
         assert converted_model.opset_import[0].version == 3
 
+    # Test Reshape Adapter: 6 -> 4
+    def test_reshape_6_4(self):  # type: () -> None
+        nodes = [helper.make_node('Constant', [], ["shape"],
+                    value=helper.make_tensor("", TensorProto.INT64, [1],
+                        [5])),
+                    helper.make_node('Reshape', ["X", "shape"], ["Y"])]
+        graph = helper.make_graph(
+            nodes,
+            "test",
+            [helper.make_tensor_value_info("X", TensorProto.FLOAT, (5,))],
+            [helper.make_tensor_value_info("Y", TensorProto.FLOAT, (5,))])
+        converted_model = self._converted(graph, helper.make_operatorsetid(
+            "", 6), 4)
+        # Assert equality of graph and converted_model
+        # TODO: Switch back to node[0]
+        assert converted_model.graph.node[1].op_type == "Reshape"
+        assert converted_model.opset_import[0].version == 4
+
+    # Test Reshape Adapter: 4 -> 6
+    def test_reshape_4_6(self):  # type: () -> None
+        nodes = [helper.make_node('Reshape', ["X"], ["Y"], shape=[5])]
+        graph = helper.make_graph(
+            nodes,
+            "test",
+            [helper.make_tensor_value_info("X", TensorProto.FLOAT, (5,))],
+            [helper.make_tensor_value_info("Y", TensorProto.FLOAT, (5,))])
+        converted_model = self._converted(graph, helper.make_operatorsetid(
+            "", 4), 6)
+        # Assert equality of graph and converted_model
+        assert converted_model.graph.node[0].op_type == "Reshape"
+        assert converted_model.opset_import[0].version == 6
+
 
 if __name__ == '__main__':
     unittest.main()
