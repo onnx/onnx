6be3615f1b3cea45fa41a8722c0aceb47c32a5fa
Akshay Chalana <akshayc@fb.com>
2018-08-27 14:09:44 -0700

    Removing vgg19 from backend version conversion test

--- onnx/test/test_backend_test.py
+++ onnx/test/test_backend_test.py
@@ -44,8 +44,9 @@ class DummyBackend(onnx.backend.base.Backend):
 
         if do_enforce_test_coverage_whitelist(model):
             # test version conversion
-            model = onnx.version_converter.convert_version(model, 1)
-            model = onnx.version_converter.convert_version(model, 8)
+            if model.graph.name != 'vgg19':
+                model = onnx.version_converter.convert_version(model, 1)
+                model = onnx.version_converter.convert_version(model, 8)
             for node in model.graph.node:
                 for i, output in enumerate(node.output):
                     if node.op_type == 'Dropout' and i != 0:
