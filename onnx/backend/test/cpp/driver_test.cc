8ab9b4c6b8005c4868a11c7008974cba2f2a4b1e
zrphercule <zrphercule@gmail.com>
2018-09-07 15:13:53 -0700

    nit

--- onnx/backend/test/cpp/driver_test.cc
+++ onnx/backend/test/cpp/driver_test.cc
@@ -223,7 +223,7 @@ class ONNXCppDriverTest
  *	When testing backend, we do not specify target backend here,
  *	but always use the by-default backend of onnxifi_loader.
  *	Check onnxifi_loader.h for more detail of how to indicate specific
- *backend.
+ *  backend.
  */
 TEST_P(ONNXCppDriverTest, ONNXCppDriverUnitTest){
 	onnxifi_library lib;
