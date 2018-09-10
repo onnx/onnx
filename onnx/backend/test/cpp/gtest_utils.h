df14e74aa31d8a860185f864499e7b561bf63b5a
zrphercule <zrphercule@gmail.com>
2018-09-10 10:17:31 -0700

    Change onnxifi test driver classname (#1396)
    * change name
    
    * documents

--- onnx/backend/test/cpp/gtest_utils.h
+++ onnx/backend/test/cpp/gtest_utils.h
@@ -1,3 +1,3 @@
 #include "gtest/gtest.h"
 #include "onnx/backend/test/cpp/driver/test_driver.h"
-std::vector<ONNX_NAMESPACE::testing::ProtoTestCase>& GetTestCases();
+std::vector<ONNX_NAMESPACE::testing::ResolvedTestCase>& GetTestCases();
