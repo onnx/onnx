463a70624ba4f8605f9403a3ab7ab4d33db95713
qinwen <suexu1025@gmail.com>
2018-09-21 11:23:24 -0700

    add format for merge.py

--- onnx/backend/test/case/node/merge.py
+++ onnx/backend/test/case/node/merge.py
@@ -2,18 +2,23 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+
 import numpy as np  # type: ignore
+
 import onnx
 from ..base import Base
 from . import expect
+
+
 class Merge(Base):
+
     @staticmethod
     def export():  # type: () -> None
         shape = (4, 4, 1)
         node = onnx.helper.make_node(
             'Merge',
-            inputs=['x'],
-            outputs=['y'],
+            inputs =['x'],
+            outputs =['y'],
             axis = 1
         )
         new_shape = [-1]
@@ -21,3 +26,4 @@ class Merge(Base):
         x = np.random.randint(0, 100, size=shape, dtype=np.int32)
         y = np.reshape(x, new_shape)
         expect(node, inputs=[x], outputs=[y], name='test_merge')
+
