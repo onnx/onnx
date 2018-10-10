9bb4cc690ccb0e4e60bdcf1e0d9c6965be675097
bddppq <bai@in.tum.de>
2018-10-09 22:12:04 -0700

    Add CircleCI configuration files

---
+++ .circleci/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+# setup onnx as the submodule of pytorch
+export ONNX_DIR="$PYTORCH_DIR/third_party/onnx"
+git clone --recursive --quiet https://github.com/pytorch/pytorch.git "$PYTORCH_DIR"
+rm -rf "$ONNX_DIR"
+cp -r "$PWD" "$ONNX_DIR"
+
+pip install ninja
+
+# install pytorch
+cd $PYTORCH_DIR
+pip install -r requirements.txt
+python setup.py build develop
+# install onnx
+cd $ONNX_DIR
+python setup.py develop
