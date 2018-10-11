a8ea5bcc9315615d8f0d9f1d66a942902608eef1
Junjie Bai <bai@in.tum.de>
2018-10-11 11:09:44 -0700

    .

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # setup onnx as the submodule of pytorch
-export ONNX_DIR="$PYTORCH_DIR/third_party/onnx"
+ONNX_DIR="$PYTORCH_DIR/third_party/onnx"
 git clone --recursive --quiet https://github.com/pytorch/pytorch.git "$PYTORCH_DIR"
 rm -rf "$ONNX_DIR"
 cp -r "$PWD" "$ONNX_DIR"
