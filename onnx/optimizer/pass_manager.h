3e219f6c087568e091a70dc8febd548841a64759
Armen <armen.ag@live.com>
2018-10-17 11:50:05 -0700

    ONNX Optimization Rewrite (#1452)
    * new optimization skeleton
    
    * add skeletong of manager/registry
    
    * rewrite all optimizations through for new framework
    
    * rewrite all optimizations for new framework
    
    * fixed bug in destroyCurrent() when called twice
    
    * add comments
    
    * fix optimizer_onnx example + std imports
    
    * fix undefined types in msvc
    
    * static singleton optimizer+small fixes
    
    * code cleanup + fixed point opt + deadend elimination
    
    * fix linting issue
    
    * maintain consistency with caffe2
    
    * Trigger Commit
    
    * use smart pointers when interfacing with std containers
    
    * move as much of the rewrite to smart pointers as possible
    
    * Trigger CI
    
    * implement smarter way to use it.destroyCurrent()
    
    * remove deadend elimination as default pass
    
    * Trigger CI
    
    * Trigger CI
    
    * minor changes + remove need of RTTI
    
    * add nop tests back
    
    * Trigger CI

---
+++ onnx/optimizer/pass_manager.h
@@ -0,0 +1,48 @@
+#pragma once
+// ATTENTION: The code in this file is highly EXPERIMENTAL.
+// Adventurous users should note that the APIs will probably change.
+
+#include <set>
+#include "onnx/optimizer/pass.h"
+#include "onnx/optimizer/passes/eliminate_deadend.h"
+
+namespace ONNX_NAMESPACE {
+namespace optimization {
+
+// An analysis returned from the run done by a manager
+struct PassManagerAnalysis {};
+struct EmptyPassManagerAnalysis : PassManagerAnalysis {};
+
+// Base class of all PassManager's. The class should be able to add new passes
+// as well as run the passes given a graph.
+class PassManager {
+ public:
+  PassManager();
+  virtual ~PassManager();
+
+  virtual void add(std::shared_ptr<Pass> P) = 0;
+  virtual std::shared_ptr<PassManagerAnalysis> run(Graph& graph) = 0;
+};
+
+// The GeneralPassManager has no restriction on type of Pass and runs the passes
+// once in a linear fashion.
+class GeneralPassManager : public PassManager {
+ public:
+  GeneralPassManager() {}
+  ~GeneralPassManager() override;
+
+  void add(std::shared_ptr<Pass> pass) override;
+  std::shared_ptr<PassManagerAnalysis> run(Graph& graph) override;
+
+ protected:
+  std::set<std::shared_ptr<Pass>> passes;
+};
+
+// Exhibits the same behavior as GeneralPassManager but will instead check
+// whether or not fixed point optimization is needed.
+class FixedPointPassManager : public GeneralPassManager {
+  std::shared_ptr<PassManagerAnalysis> run(Graph& graph) override;
+};
+
+} // namespace optimization
+} // namespace ONNX_NAMESPACE
