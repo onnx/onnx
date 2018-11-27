809a5d17cb7f5cd3bc7afceba761ffde79ca8c60
Ganesan Ramalingam <grama@microsoft.com>
2018-11-27 14:27:52 -0800

    update scan unit tests

--- onnx/test/shape_inference_test.py
+++ onnx/test/shape_inference_test.py
@@ -1035,6 +1035,42 @@ class TestShapeInference(unittest.TestCase):
              make_tensor_value_info('scan_output', TensorProto.FLOAT, (seq_len, input_size))],
             opset_imports=[helper.make_opsetid("", 9)])
 
+    def test_scan_opset9_axes(self):    # type: () -> None
+        axis_0_len = 'axis0'
+        seq_len = 'sequence'
+        input_size = 2
+        loop_state_size = 3
+
+        # can't use self._make_graph for the subgraph as it add more inputs for the Reshape operations it inserts.
+        # this breaks the subgraph inferencing as it expects the number of inputs passed from Scan to match
+        # the GraphProto, but Scan knows nothing about the additional inputs.
+        input_value_infos = [make_tensor_value_info('loop_state_in', TensorProto.UNDEFINED, None),
+                             make_tensor_value_info('input', TensorProto.UNDEFINED, None)]
+        output_value_infos = [make_tensor_value_info('loop_state_out', TensorProto.UNDEFINED, None),
+                              make_tensor_value_info('output', TensorProto.UNDEFINED, None)]
+
+        subgraph = helper.make_graph(
+            [make_node('Identity', ['loop_state_in'], ['loop_state_out']),
+             make_node('Identity', ['input'], ['output'])],
+            "subgraph",
+            input_value_infos,
+            output_value_infos
+        )
+
+        graph = self._make_graph(
+            [('loop_state_orig', TensorProto.FLOAT, (loop_state_size,)),
+             ('scan_input', TensorProto.FLOAT, (axis_0_len, seq_len, input_size))],
+            [make_node('Scan', ['loop_state_orig', 'scan_input'], ['loop_state_final', 'scan_output'],
+                       num_scan_inputs=1, body=subgraph, axes=[1])],
+            []
+        )
+
+        self._assert_inferred(
+            graph,
+            [make_tensor_value_info('loop_state_final', TensorProto.FLOAT, (loop_state_size,)),
+             make_tensor_value_info('scan_output', TensorProto.FLOAT, (seq_len, axis_0_len, input_size))],
+            opset_imports=[helper.make_opsetid("", 9)])
+
     def test_if(self):  # type: () -> None
 
         # Create a simple If node where the 'then' subgraph adds to the current value, and the 'else' subgraph
