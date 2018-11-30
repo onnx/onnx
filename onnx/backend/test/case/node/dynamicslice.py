6fd891560a23792e2b33d1dce5479a77787f3b9c
Ganesan Ramalingam <grama@microsoft.com>
2018-11-30 11:34:30 -0800

    Replace np.long by np.int64

--- onnx/backend/test/case/node/dynamicslice.py
+++ onnx/backend/test/case/node/dynamicslice.py
@@ -22,9 +22,9 @@ class DynamicSlice(Base):
 
         x = np.random.randn(20, 10, 5).astype(np.float32)
         y = x[0:3, 0:10]
-        starts = np.array([0, 0], dtype=np.long)
-        ends = np.array([3, 10], dtype=np.long)
-        axes = np.array([0, 1], dtype=np.long)
+        starts = np.array([0, 0], dtype=np.int64)
+        ends = np.array([3, 10], dtype=np.int64)
+        axes = np.array([0, 1], dtype=np.int64)
 
         expect(node, inputs=[x, starts, ends, axes], outputs=[y],
                name='test_dynamic_slice')
@@ -38,9 +38,9 @@ class DynamicSlice(Base):
         )
 
         x = np.random.randn(20, 10, 5).astype(np.float32)
-        starts = np.array([0], dtype=np.long)
-        ends = np.array([-1], dtype=np.long)
-        axes = np.array([1], dtype=np.long)
+        starts = np.array([0], dtype=np.int64)
+        ends = np.array([-1], dtype=np.int64)
+        axes = np.array([1], dtype=np.int64)
         y = x[:, 0:-1]
 
         expect(node, inputs=[x, starts, ends, axes], outputs=[y],
@@ -55,9 +55,9 @@ class DynamicSlice(Base):
         )
 
         x = np.random.randn(20, 10, 5).astype(np.float32)
-        starts = np.array([1000], dtype=np.long)
-        ends = np.array([1000], dtype=np.long)
-        axes = np.array([1], dtype=np.long)
+        starts = np.array([1000], dtype=np.int64)
+        ends = np.array([1000], dtype=np.int64)
+        axes = np.array([1], dtype=np.int64)
         y = x[:, 1000:1000]
 
         expect(node, inputs=[x, starts, ends, axes], outputs=[y],
@@ -72,9 +72,9 @@ class DynamicSlice(Base):
         )
 
         x = np.random.randn(20, 10, 5).astype(np.float32)
-        starts = np.array([1], dtype=np.long)
-        ends = np.array([1000], dtype=np.long)
-        axes = np.array([1], dtype=np.long)
+        starts = np.array([1], dtype=np.int64)
+        ends = np.array([1000], dtype=np.int64)
+        axes = np.array([1], dtype=np.int64)
         y = x[:, 1:1000]
 
         expect(node, inputs=[x, starts, ends, axes], outputs=[y],
@@ -89,8 +89,8 @@ class DynamicSlice(Base):
         )
 
         x = np.random.randn(20, 10, 5).astype(np.float32)
-        starts = np.array([0, 0, 3], dtype=np.long)
-        ends = np.array([20, 10, 4], dtype=np.long)
+        starts = np.array([0, 0, 3], dtype=np.int64)
+        ends = np.array([20, 10, 4], dtype=np.int64)
         y = x[:, :, 3:4]
 
         expect(node, inputs=[x, starts, ends], outputs=[y],
