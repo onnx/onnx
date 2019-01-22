9a486e8b386878b8b7c946f5b559abe81252bebd
G. Ramalingam <grama@microsoft.com>
2019-01-22 10:14:44 -0800

    Relax constraint that the initializers must be a subset of graph inputs (#1718)
    * Relax constraint that the initializers must be a subset of graph inputs
    
    * generate proto files
    
    * Update documentation about initializers and graph inputs
    
    * Update IR VERSION
    
    * update checker to use ir-version
    
    * fix version in example
    
    * update checker_test to test both IR_VERSIONs
    
    * address flake warning
    
    * address flake8 warning

--- onnx/test/checker_test.py
+++ onnx/test/checker_test.py
@@ -7,7 +7,9 @@ import unittest
 import numpy as np  # type: ignore
 
 from onnx import checker, helper
-from onnx import TensorProto
+from onnx import TensorProto, GraphProto
+import onnx.onnx_cpp2py_export.checker as C
+import onnx.defs
 
 
 class TestChecker(unittest.TestCase):
@@ -42,6 +44,32 @@ class TestChecker(unittest.TestCase):
             "Relu", [""], ["Y"], name="test")
         self.assertRaises(checker.ValidationError, checker.check_node, node)
 
+    def test_check_graph_ir_version_3(self):  # type: () -> None
+        ctx = C.CheckerContext()
+        ctx.ir_version = 3
+        ctx.opset_imports = {'': onnx.defs.onnx_opset_version()}
+
+        def check_ir_version_3(g):   # type: (GraphProto) -> None
+            checker.check_graph(g, ctx)
+
+        node = helper.make_node(
+            "Relu", ["X"], ["Y"], name="test")
+        graph = helper.make_graph(
+            [node],
+            "test",
+            [helper.make_tensor_value_info("X", TensorProto.FLOAT, [1, 2])],
+            [helper.make_tensor_value_info("Y", TensorProto.FLOAT, [1, 2])])
+        check_ir_version_3(graph)
+
+        graph.initializer.extend([self._sample_float_tensor])
+
+        graph.initializer[0].name = 'no-exist'
+
+        self.assertRaises(checker.ValidationError, check_ir_version_3, graph)
+
+        graph.initializer[0].name = 'X'
+        check_ir_version_3(graph)
+
     def test_check_graph(self):  # type: () -> None
         node = helper.make_node(
             "Relu", ["X"], ["Y"], name="test")
@@ -55,7 +83,7 @@ class TestChecker(unittest.TestCase):
         graph.initializer.extend([self._sample_float_tensor])
 
         graph.initializer[0].name = 'no-exist'
-        self.assertRaises(checker.ValidationError, checker.check_graph, graph)
+        checker.check_graph(graph)
 
         graph.initializer[0].name = 'X'
         checker.check_graph(graph)
