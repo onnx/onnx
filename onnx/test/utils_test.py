4e9c241ee2c6722fcf2109a447a3fd22b501bbcc
BowenBao <semisqg@gmail.com>
2019-02-01 15:33:22 -0800

    Add helper function for updating model input/output dimensions.

--- onnx/test/utils_test.py
+++ onnx/test/utils_test.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 import unittest
 import onnx.utils
 from onnx import helper, TensorProto
-
+import numpy as np
 
 class TestUtilityFunctions(unittest.TestCase):
     def test_polish_model(self):  # type: () -> None
@@ -23,6 +23,28 @@ class TestUtilityFunctions(unittest.TestCase):
         self.assertEqual(len(polished_def.graph.node), 1)
         self.assertFalse(polished_def.graph.node[0].HasField('doc_string'))
 
+    def test_update_inputs_outputs_dim(self):  # type: () -> None
+        node_def = helper.make_node(
+            "Conv",
+            inputs=['x', 'W'],
+            outputs=['y'],
+            kernel_shape=[3, 3],
+            strides=[2, 2],
+        )
+        graph_def = helper.make_graph(
+            [node_def],
+            'test',
+            [helper.make_tensor_value_info('x', TensorProto.FLOAT, [1, 1, 5, 5]),
+             helper.make_tensor_value_info('W', TensorProto.FLOAT, [1, 1, 3, 3])],
+            [helper.make_tensor_value_info('y', TensorProto.FLOAT, [1, 1, 2, 2])]
+        )
+        model_def = helper.make_model(graph_def, producer_name='test')
+        updated_def = onnx.utils.update_inputs_outputs_dims(model_def, [[1, 1, 'x1', -1], [1, 1, 3, 3]], [[1, 1, -1, -1]])
+        onnx.checker.check_model(updated_def)
+        self.assertEqual(updated_def.graph.input[0].type.tensor_type.shape.dim[2].dim_param, 'x1')
+        self.assertEqual(updated_def.graph.input[0].type.tensor_type.shape.dim[3].dim_param, 'in_0_3')
+        self.assertEqual(updated_def.graph.output[0].type.tensor_type.shape.dim[2].dim_param, 'out_0_2')
+        self.assertEqual(updated_def.graph.output[0].type.tensor_type.shape.dim[3].dim_param, 'out_0_3')
 
 if __name__ == '__main__':
     unittest.main()
--- onnx/utils.py
+++ onnx/utils.py
@@ -21,3 +21,32 @@ def polish_model(model):  # type: (ModelProto) -> ModelProto
     model = onnx.optimizer.optimize(model)
     onnx.checker.check_model(model)
     return model
+
+def update_inputs_outputs_dims(model, input_dims, output_dims):
+    """
+        This function updates the sizes of dimensions of the model's inputs and outputs to the values
+        provided in input_dims and output_dims. if the dim value provided is negative, a unique dim_param
+        will be set for that dimension.
+    """
+    def update_dim(tensor, dim, i, j, dim_param_prefix):
+        dim_proto = tensor.type.tensor_type.shape.dim[j]
+        if isinstance(dim, int):
+            if dim >= 0:
+                dim_proto.dim_value = dim
+            else:
+                dim_proto.dim_param = dim_param_prefix + str(i) + '_' + str(j)
+        elif isinstance(dim, str):
+            dim_proto.dim_param = dim
+        else:
+            raise ValueError('Only int or str is accepted as dimension value, incorrect type: {}'.format(type(dim)))
+
+    for i, input_dim_arr in enumerate(input_dims):
+        for j, dim in enumerate(input_dim_arr):
+            update_dim(model.graph.input[i], dim, i, j, 'in_')
+
+    for i, output_dim_arr in enumerate(output_dims):
+        for j, dim in enumerate(output_dim_arr):
+            update_dim(model.graph.output[i], dim, i, j, 'out_')
+
+    onnx.checker.check_model(model)
+    return model
