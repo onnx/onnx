af13148bc2e89a37391d918310905203dd2f03b4
linkerzhang <kezhan@microsoft.com>
2019-02-13 11:00:03 -0800

    Revert "Implement Op Annotation's for ONNX (#1648)"
    This reverts commit 0628582506ede89f94faa80977d86e3f7e19739c.

--- onnx/defs/op_annotation.cc
+++
@@ -1,7 +0,0 @@
-#include "onnx/defs/op_annotation.h"
-
-namespace ONNX_NAMESPACE {
-std::shared_ptr<OpAnnotationRegistry> OpAnnotationRegistry::instance_ =
-    std::shared_ptr<OpAnnotationRegistry>(new OpAnnotationRegistry());
-
-} // namespace ONNX_NAMESPACE
