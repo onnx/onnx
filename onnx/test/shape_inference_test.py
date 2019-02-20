ca6933f7980aadb34088aa8d75466ae6aef5743b
linkerzhang <kezhan@microsoft.com>
2019-02-20 10:48:51 -0800

    add test for resize

--- onnx/test/shape_inference_test.py
+++ onnx/test/shape_inference_test.py
@@ -245,6 +245,17 @@ class TestShapeInference(unittest.TestCase):
             graph,
             [make_tensor_value_info('y', TensorProto.INT32, (2, 4, 3, 9))],
             opset_imports=[helper.make_opsetid("", 9)])
+            
+    def test_resize(self):  # type: () -> None
+        graph = self._make_graph(
+            [('x', TensorProto.INT32, (2, 4, 3, 5)),
+             ('scales', TensorProto.FLOAT, (4,))],
+            [make_node("Resize", ['x', 'scales'], ['y'])],
+            [],
+            initializer=[make_tensor('scales', TensorProto.FLOAT, (4,), (1.0, 1.1, 1.3, 1.9))])
+        self._assert_inferred(
+            graph,
+            [make_tensor_value_info('y', TensorProto.INT32, (2, 4, 3, 9))])
 
     def test_shape(self):  # type: () -> None
         graph = self._make_graph(
