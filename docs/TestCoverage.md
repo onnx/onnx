9a7c8bac1774c4b98b6979125e9c0c60f90eb962
Raymond Yang <zihao.yang@microsoft.com>
2019-03-12 18:08:27 -0700

    Update testcase script

--- docs/TestCoverage.md
+++ docs/TestCoverage.md
@@ -7,7 +7,7 @@
 ## Summary
 Node tests have covered 112/119 (94.12%, 5 generators excluded) common operators.
 
-Node tests have covered 2/7 (28.57%, 0 generators excluded) experimental operators.
+Node tests have covered 2/11 (18.18%, 0 generators excluded) experimental operators.
 
 * [Covered Common Operators](#covered-common-operators)
 * [No Cover Common Operators](#no-cover-common-operators)
@@ -6959,10 +6959,22 @@ expect(node, inputs=[x], outputs=[y],
 ### ATen (call for test cases)
 
 
+### Affine (call for test cases)
+
+
+### Crop (call for test cases)
+
+
 ### GRUUnit (call for test cases)
 
 
 ### GivenTensorFill (call for test cases)
+
+
+### ImageScaler (call for test cases)
+
+
+### ParametricSoftplus (call for test cases)
 
 
 ### Scale (call for test cases)
--- onnx/backend/test/case/node/__init__.py
+++ onnx/backend/test/case/node/__init__.py
@@ -15,8 +15,69 @@ import onnx.mapping
 from ..utils import import_recursive
 from ..test_case import TestCase
 
+
 _NodeTestCases = []
 
+from onnx.onnx_pb import NodeProto, AttributeProto
+
+
+def function_expand_helper(node, function_proto, op_prefix):
+    node_list = []
+    input_names_map = dict()
+    output_names_map = dict()
+    attribute_map = node.attribute
+
+    for idx in range(len(node.input)):
+        if idx not in range(len(function_proto.input)):
+            continue
+        input_names_map[function_proto.input[idx]] = node.input[idx]
+
+    for idx in range(len(node.output)):
+        if idx not in range(len(function_proto.output)):
+            continue
+        output_names_map[function_proto.output[idx]] = node.output[idx]
+
+    for internal_node in function_proto.node:
+        new_node = NodeProto()
+        new_node.CopyFrom(internal_node)
+        new_node.ClearField("input")
+        new_node.ClearField("output")
+        new_node.ClearField("attribute")
+        for internal_name in internal_node.input:
+            if internal_name in input_names_map:
+                new_node.input.extend(input_names_map[internal_name])
+            else:
+                new_node.input.extend(op_prefix + internal_name)
+        for internal_name in internal_node.output:
+            if internal_name in output_names_map:
+                new_node.output.extend(output_names_map[internal_name])
+            else:
+                new_node.output.extend(op_prefix + internal_name)
+        for attr in internal_node.attribute:
+            if attr.HasField("ref_attr_name"):
+                if attr.ref_attr_name in attribute_map:
+                    new_attr = AttributeProto()
+                    new_attr.CopyFrom(attribute_map[attr.ref_attr_name])
+                    new_node.attribute.extend([new_attr])
+            else:
+                new_attr = AttributeProto()
+                new_attr.CopyFrom(attr)
+                new_node.attribute.extend([new_attr])
+        node_list.append(new_node)
+    return node_list
+
+
+def function_testcase_helper(node, name):
+    test_op = node.op_type
+    op_prefix = test_op + "_" + name + "_expanded_function"
+    schema = onnx.defs.get_schema(test_op)
+    if not schema.has_function:
+        return []
+    function_proto = schema.function_body
+
+    # function_proto.attributes
+    node_list = function_expand_helper(node, function_proto, op_prefix)
+    return node_list
 
 def _extract_value_info(arr, name):  # type: (np.ndarray, Text) -> onnx.ValueInfoProto
     return onnx.helper.make_tensor_value_info(
@@ -56,6 +117,29 @@ def expect(node,  # type: onnx.NodeProto
         rtol=1e-3,
         atol=1e-7,
     ))
+
+    expanded_function_nodes = function_testcase_helper(node, name)
+    if expanded_function_nodes:
+        function_test_name = name + '_expanded'
+        graph = onnx.helper.make_graph(
+            nodes=expanded_function_nodes,
+            name=function_test_name,
+            inputs=inputs_vi,
+            outputs=outputs_vi)
+        kwargs[str('producer_name')] = 'backend-test'
+        model = onnx.helper.make_model(graph, **kwargs)
+        _NodeTestCases.append(TestCase(
+            name=function_test_name,
+            model_name=function_test_name,
+            url=None,
+            model_dir=None,
+            model=model,
+            data_sets=[(inputs, outputs)],
+            kind='node',
+            rtol=1e-3,
+            atol=1e-7,
+        ))
+
 
 
 def collect_testcases():  # type: () -> List[TestCase]
