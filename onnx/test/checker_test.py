8bc4753e99d3594eee7ce258ae0215f87a15a5a8
Lu Fang <30275821+houseroad@users.noreply.github.com>
2019-03-20 14:31:54 -0700

    Skip the schema check on ops in non-standard domain (#1876)
    * Skip the schema check on ops in non-standard domain
    
    * remove the warning and add a test

--- onnx/test/checker_test.py
+++ onnx/test/checker_test.py
@@ -236,6 +236,19 @@ class TestChecker(unittest.TestCase):
             "ConstantFill", [], ["Y"], name="test", shape=[1, 2])
         checker.check_node(node)
 
+    def test_skip_schema_check_on_non_standard_domain(self):  # type: () -> None
+        node = helper.make_node(
+            "NonExistOp", ["X"], ["Y"], name="test", domain="test.domain")
+        graph = helper.make_graph(
+            [node],
+            "test",
+            [helper.make_tensor_value_info("X", TensorProto.FLOAT, [1, 2])],
+            [helper.make_tensor_value_info("Y", TensorProto.FLOAT, [1, 2])])
+        onnx_id = helper.make_opsetid("test.domain", 1)
+        model = helper.make_model(graph, producer_name='test',
+                                  opset_imports=[onnx_id])
+        checker.check_model(model)
+
 
 if __name__ == '__main__':
     unittest.main()
