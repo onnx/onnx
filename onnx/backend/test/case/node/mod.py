f9e8469d26566211dff9763cf90c8e4905e64b77
Jeff Saremi <jeffsaremi@hotmail.com>
2019-03-28 08:01:53 -0700

    formatting fix

--- onnx/backend/test/case/node/mod.py
+++ onnx/backend/test/case/node/mod.py
@@ -25,6 +25,7 @@ class Mod(Base):
         z = np.mod(x, y)  # expected output [2., -3.,  5., -2.,  3.,  3.]
         expect(node, inputs=[x, y], outputs=[z],
                name='test_mod_float_mixed_sign_example')
+
     @staticmethod
     def export_fmod_mixed_sign():  # type: () -> None
         node = onnx.helper.make_node(
@@ -39,6 +40,7 @@ class Mod(Base):
         z = np.fmod(x, y)  # expected output [-0.1,  0.4,  5. ,  0.1, -0.4,  3.]
         expect(node, inputs=[x, y], outputs=[z],
                name='test_mod_fmod_mixed_sign_example')
+
     @staticmethod
     def export_int64_mixed_sign():  # type: () -> None
         node = onnx.helper.make_node(
@@ -52,6 +54,7 @@ class Mod(Base):
         z = np.mod(x, y)  # expected output [ 0, -2,  5,  0,  2,  3]
         expect(node, inputs=[x, y], outputs=[z],
                name='test_mod_int64_mixed_sign_example')
+
     @staticmethod
     def export_mul_broadcast():  # type: () -> None
         node = onnx.helper.make_node(
@@ -60,7 +63,7 @@ class Mod(Base):
             outputs=['z'],
         )
 
-        x = np.arange(0,30).reshape([3,2,5])
+        x = np.arange(0, 30).reshape([3, 2, 5])
         y = np.array([7])
         z = np.mod(x, y)
         expect(node, inputs=[x, y], outputs=[z],
