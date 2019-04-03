f0fc361dac340ad88afa148b0ae5be32f87b9886
Wei-Sheng Chin <wschin@outlook.com>
2019-04-03 09:21:15 -0700

    Format code

--- onnx/backend/test/case/node/isinf.py
+++ onnx/backend/test/case/node/isinf.py
@@ -13,45 +13,39 @@ from . import expect
 class IsInf(Base):
 
     @staticmethod
-    def export_infinity(): # type: () -> None
+    def export_infinity():  # type: () -> None
         node = onnx.helper.make_node('IsInf',
-                inputs = ['x'],
-                outputs = ['y'],
-        )
+                                     inputs=['x'],
+                                     outputs=['y'],
+                                     )
 
         x = np.array([-1.2, np.nan, np.inf, 2.8, np.NINF, np.inf],
-                dtype=np.float32)
+                     dtype=np.float32)
         y = np.isinf(x)
-
         expect(node, inputs=[x], outputs=[y], name='test_isinf')
 
-
     @staticmethod
-    def export_positive_infinity_only(): # type: () -> None
+    def export_positive_infinity_only():  # type: () -> None
         node = onnx.helper.make_node('IsInf',
-                inputs = ['x'],
-                outputs = ['y'],
-                detect_negative=0
-        )
+                                     inputs=['x'],
+                                     outputs=['y'],
+                                     detect_negative=0
+                                     )
 
         x = np.array([-1.7, np.nan, np.inf, 3.6, np.NINF, np.inf],
-                dtype=np.float32)
-
+                     dtype=np.float32)
         y = np.isposinf(x)
-
         expect(node, inputs=[x], outputs=[y], name='test_isinf_positive')
 
     @staticmethod
-    def export_negative_infinity_only(): # type: () -> None
+    def export_negative_infinity_only():  # type: () -> None
         node = onnx.helper.make_node('IsInf',
-                inputs = ['x'],
-                outputs = ['y'],
-                detect_positive=0
-        )
+                                     inputs=['x'],
+                                     outputs=['y'],
+                                     detect_positive=0
+                                     )
 
         x = np.array([-1.7, np.nan, np.inf, -3.6, np.NINF, np.inf],
-                dtype=np.float32)
-
+                     dtype=np.float32)
         y = np.isneginf(x)
-
         expect(node, inputs=[x], outputs=[y], name='test_isinf_negative')
