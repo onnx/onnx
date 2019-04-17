a3cb84fc7effb3af0fed7ac3262b170a189181fb
hariharans29 <shariharan91@gmail.com>
2019-04-16 18:25:04 -0700

    Make pads and value attributes inputs in Pad operator

--- onnx/backend/test/case/node/pad.py
+++ onnx/backend/test/case/node/pad.py
@@ -13,7 +13,7 @@ from . import expect
 class Pad(Base):
 
     @staticmethod
-    def export_constant_pad():  # type: () -> None
+    def export_constant_pad_with_1D_pads():  # type: () -> None
         node = onnx.helper.make_node(
             'Pad',
             inputs=['x', 'pads', 'value'],
@@ -21,7 +21,7 @@ class Pad(Base):
             mode='constant'
         )
         x = np.random.randn(1, 3, 4, 5).astype(np.float32)
-        pads = np.array([[0, 0], [0, 0], [1, 2], [3, 4]]).astype(np.int64)
+        pads = np.array([0, 0, 1, 3, 0, 0, 2, 4]).astype(np.int64)
         value = np.array([1.2]).astype(np.float32)        
         y = np.pad(
             x,
@@ -31,10 +31,31 @@ class Pad(Base):
         )
 
         expect(node, inputs=[x, pads, value], outputs=[y],
-               name='test_constant_pad')
+               name='constant_pad_with_1D_pads')
 
     @staticmethod
-    def export_reflection_and_edge_pad():  # type: () -> None
+    def export_constant_pad_with_2D_pads():  # type: () -> None
+        node = onnx.helper.make_node(
+            'Pad',
+            inputs=['x', 'pads', 'value'],
+            outputs=['y'],
+            mode='constant'
+        )
+        x = np.random.randn(1, 3, 4, 5).astype(np.float32)
+        pads = np.array([[0, 0, 1, 3, 0, 0, 2, 4]]).astype(np.int64)
+        value = np.array([1.2]).astype(np.float32)        
+        y = np.pad(
+            x,
+            pad_width=((0, 0), (0, 0), (1, 2), (3, 4)),
+            mode='constant',
+            constant_values=1.2,
+        )
+
+        expect(node, inputs=[x, pads, value], outputs=[y],
+               name='constant_pad_with_2D_pads')
+               
+    @staticmethod
+    def export_reflection_and_edge_pad_with_1D_pads():  # type: () -> None
         for mode in ['edge', 'reflect']:
             node = onnx.helper.make_node(
                 'Pad',
@@ -43,7 +64,7 @@ class Pad(Base):
                 mode=mode
             )
             x = np.random.randn(1, 3, 4, 5).astype(np.float32)
-            pads = np.array([[0, 0], [0, 0], [1, 1], [1, 1]]).astype(np.int64)
+            pads = np.array([0, 0, 1, 1, 0, 0, 1, 1]).astype(np.int64)
             y = np.pad(
                 x,
                 pad_width=((0, 0), (0, 0), (1, 1), (1, 1)),
@@ -51,4 +72,4 @@ class Pad(Base):
             )
 
             expect(node, inputs=[x, pads], outputs=[y],
-                   name='test_{}_pad'.format(mode))
+                   name='test_{}_pad_with_1D_pads'.format(mode))
--- onnx/optimizer/passes/eliminate_nop_pad.h
+++ onnx/optimizer/passes/eliminate_nop_pad.h
@@ -18,20 +18,51 @@ struct EliminateNopPad final : public PredicateBasedPass {
   std::string getPassName() const override {
     return "eliminate_nop_pad";
   }
-  static bool is_nop_pad(const std::vector<int64_t>& pads) {
-    for (size_t i = 0; i < pads.size(); i++)
-      if (pads[i] > 0)
-        return false;
+  static bool is_nop_pad(Node* node, Graph& graph) {
+    const auto pads_name = node->inputs()[1]->uniqueName();
+    const auto pads_initializer = graph.getInitializer(pads_name);
+    // 'pad' node has the 'pads' input which has not been initialized -
+    // can't proceed with elimination
+    if (pads_initializer == graph.initializers().end())
+      return false;
+
+    // validate values within 'pads'
+    std::vector<int64_t> pads;
+    if (pads_initializer->elem_type() == TensorProto::INT64 &&
+        pads_initializer->is_raw_data()) {
+      const auto& raw_data = pads_initializer->raw();
+      const size_t num_elements = static_cast<size_t>(raw_data.size() / 8);
+      pads.resize(num_elements);
+      const int64_t* int64_data =
+          reinterpret_cast<const int64_t*>(raw_data.c_str());
+      for (size_t i = 0; i < num_elements; ++i) {
+        if (int64_data[i] > 0)
+          return false;
+      }
+      return true;
+    } else if (pads_initializer->elem_type() == TensorProto::INT64) {
+      pads = pads_initializer->int64s();
+      for (const auto& val : pads) {
+        if (val > 0)
+          return false;
+      }
+    }
+    // not relevant data type for this input -
+    // can't proceed with elimination
+    else {
+      return false;
+    }
     return true;
   }
 
   bool patternMatchPredicate(Node* node) override {
-    return (node->kind() == kPad && node->hasAttribute(kpads)) &&
-        is_nop_pad(node->is(kpads));
+    return node->kind() == kPad;
   }
-  bool runTransform(Node* node, Graph&, NodeDestroyType& destroy_current)
+  bool runTransform(Node* node, Graph& graph, NodeDestroyType& destroy_current)
       override {
-    node->output()->replaceAllUsesWith(node->input());
+    if (!is_nop_pad(node, graph))
+      return false;
+    node->output()->replaceAllUsesWith(node->inputs()[0]);
     destroy_current = NodeDestroyType::DestroyOne;
     return true;
   }
