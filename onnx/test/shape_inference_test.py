5d068fb81647e4e7e46a31aca7a2b646389e9001
hariharans29 <shariharan91@gmail.com>
2019-05-06 13:39:37 -0700

    More changes

--- onnx/test/shape_inference_test.py
+++ onnx/test/shape_inference_test.py
@@ -1734,8 +1734,7 @@ class TestShapeInference(unittest.TestCase):
             [('x', TensorProto.FLOAT, (6,))],
             [make_node('Unique', ['x'], ['y', 'idx'])],
             [])
-        self._assert_inferred(graph, [make_tensor_value_info('y', TensorProto.FLOAT, (None,)),
-                                      make_tensor_value_info('idx', TensorProto.INT64, (6,))])
+        self._assert_inferred(graph, [make_tensor_value_info('idx', TensorProto.INT64, (6,))])
 
 
 if __name__ == '__main__':
