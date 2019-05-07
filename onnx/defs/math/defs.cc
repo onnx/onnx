d8568e8481dd1bc0af9aceddd5fb6fbb3fd83785
hariharans29 <shariharan91@gmail.com>
2019-05-07 13:17:14 -0700

    Initial commit

--- onnx/defs/math/defs.cc
+++ onnx/defs/math/defs.cc
@@ -3,6 +3,7 @@
 
 #include <functional>
 #include "onnx/defs/schema.h"
+#include "onnx/defs/tensor_proto_util.h"
 
 namespace ONNX_NAMESPACE {
 
@@ -944,7 +945,6 @@ ONNX_OPERATOR_SET_SCHEMA(
           // Type inference:
           propagateElemTypeFromInputToOutput(ctx, 0, 0);
           updateOutputElemType(ctx, 1, TensorProto::INT64);
-
           // Shape inference:
           if (!hasInputShape(ctx, 0))
             return;
@@ -955,30 +955,49 @@ ONNX_OPERATOR_SET_SCHEMA(
             axis += rank;
           if (axis < 0 || axis >= rank)
             fail_shape_inference("Invalid value for attribute axis");
-          // TODO: unclear what results should be if axis has less than k
-          // elements.
-          // Infer output shape if 'K' is available
+
+	      const auto& axis_dim = input_shape.dim(static_cast<int>(axis));
           const auto* k = ctx.getInputData(1);
-          if (nullptr != k) {
-            if (k->dims_size() != 1 || k->int64_data_size() != 1 ||
-                k->data_type() != TensorProto::INT64)
-              fail_shape_inference(
-                  "K input must be a one-dimensional tensor of size 1 and of type int64.");
+
+ 		  // Infer output shape if: 
+		  // (1) 'K' is available
+		  // (2) axis_dim has dim value
+          // Othewise cannot reliably compute output shape as axis dim value is unknown
+          // and hence cannot determine if axis dim value >= k (which should be enforced)
+          if (nullptr != k && axis_dim.has_dim_value()) {
+            int64_t k_value = 0;  
+            if (k->dims_size() != 1 || k->dims(0) != 1)
+                fail_shape_inference("K input must be a one-dimensional tensor of size 1.");
+            if (k->has_raw_data() && 
+				k->data_type() == TensorProto::INT64)
+                k_value = ParseRawData<int64_t>(k)[0];
+            else if (k->data_type() == TensorProto::INT64)
+                k_value = k->int64_data(0);             
+            else 
+              fail_shape_inference("K input must be of type int64.");
+
+            if (axis_dim.dim_value() < k_value)
+              fail_shape_inference("Axis has less than the requested k elements.");
+
             TensorShapeProto result_shape = input_shape;
             result_shape.mutable_dim(static_cast<int>(axis))
-                ->set_dim_value(k->int64_data(0));
-            updateOutputShape(ctx, 0, result_shape);
+                ->set_dim_value(k_value);
+
+			updateOutputShape(ctx, 0, result_shape);
             updateOutputShape(ctx, 1, result_shape);
-          } else {
-            // Infer output shapes' rank in any case
-            auto* output_shape_0 = getOutputShape(ctx, 0);
-            auto* output_shape_1 = getOutputShape(ctx, 1);
-            for (int i = 0; i < input_shape.dim_size(); ++i) {
-              output_shape_0->add_dim();
-              output_shape_1->add_dim();
-            }
+
+			return;
           }
-          return;
+              
+		  // Infer output shapes' rank in any case
+          auto* output_shape_0 = getOutputShape(ctx, 0);
+          auto* output_shape_1 = getOutputShape(ctx, 1);
+          for (int i = 0; i < input_shape.dim_size(); ++i) {
+            output_shape_0->add_dim();
+            output_shape_1->add_dim();
+          }
+
+		  return;
         }));
 
 static const char* Sin_ver7_doc = R"DOC(
