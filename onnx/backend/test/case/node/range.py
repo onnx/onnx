25c42b70eb2a142cf668f3ff66fb14b16d71a331
hariharans29 <shariharan91@gmail.com>
2019-06-07 16:02:03 -0700

    Fix build failure

--- onnx/backend/test/case/node/range.py
+++ onnx/backend/test/case/node/range.py
@@ -22,11 +22,11 @@ class Range(Base):
 
         start = np.array([1.0], dtype=np.float32)
         limit = np.array([5.0], dtype=np.float32)
-        delta = np.array([2.0], dtype=np.float32)        
+        delta = np.array([2.0], dtype=np.float32)
         output = np.arange(start[0], limit[0], delta[0], dtype=np.float32)  # expected output [1.0, 3.0]
         expect(node, inputs=[start, limit, delta], outputs=[output],
                name='test_range_positive_delta')
-               
+
     @staticmethod
     def export_range_negative_delta():  # type: () -> None
         node = onnx.helper.make_node(
@@ -37,7 +37,7 @@ class Range(Base):
 
         start = np.array([10.0], dtype=np.float32)
         limit = np.array([6.0], dtype=np.float32)
-        delta = np.array([-3.0], dtype=np.float32)        
+        delta = np.array([-3.0], dtype=np.float32)
         output = np.arange(start[0], limit[0], delta[0], dtype=np.float32)  # expected output [10.0, 7.0]
         expect(node, inputs=[start, limit, delta], outputs=[output],
                name='test_range_negative_delta')
