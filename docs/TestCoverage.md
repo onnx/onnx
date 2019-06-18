3cb14de1986394dc7168d9d15610ce0fd8bc9b61
James Allingham <james.allingham@gmail.com>
2019-06-18 13:11:58 +0200

    Revamped test cases for reshape
    * Increased the number of test cases from 5 to 7.
    
        * The new tests cover the 0 dim case
    
    * Changed the existing cases so that, where possible, the 'batch' dim is unchanged.
    
        * Some frameworks do not allow for the first dim to change, so this makes these tests more generally applicable.
    
    * Generated the new test data, updated the operators doc, and updated the test coverage doc.

--- docs/TestCoverage.md
+++ docs/TestCoverage.md
@@ -6225,11 +6225,13 @@ There are 1 test cases, listed as following:
 ```python
 original_shape = [2, 3, 4]
 test_cases = {
-    'reordered_dims': np.array([4, 2, 3], dtype=np.int64),
-    'reduced_dims': np.array([3, 8], dtype=np.int64),
-    'extended_dims': np.array([3, 2, 2, 2], dtype=np.int64),
+    'reordered_dims': np.array([2, 4, 3], dtype=np.int64),
+    'reduced_dims': np.array([2, 12], dtype=np.int64),
+    'extended_dims': np.array([2, 3, 2, 2], dtype=np.int64),
     'one_dim': np.array([24], dtype=np.int64),
-    'negative_dim': np.array([6, -1, 2], dtype=np.int64),
+    'negative_dim': np.array([2, -1, 2], dtype=np.int64),
+    'zero_dim': np.array([2, 0, 4, 1], dtype=np.int64),
+    'zero_and_negative_dim': np.array([2, 0, 1, -1], dtype=np.int64),
 }
 data = np.random.random_sample(original_shape).astype(np.float32)
 
@@ -6240,7 +6242,11 @@ for test_name, shape in test_cases.items():
         outputs=['reshaped'],
     )
 
-    reshaped = np.reshape(data, shape)
+    new_shape = np.copy(shape)
+    zero_index = np.where(shape == 0)
+    new_shape[zero_index] = np.array(original_shape)[zero_index]
+
+    reshaped = np.reshape(data, new_shape)
     expect(node, inputs=[data, shape], outputs=[reshaped],
            name='test_reshape_' + test_name)
 ```
--- onnx/backend/test/case/node/reshape.py
+++ onnx/backend/test/case/node/reshape.py
@@ -16,11 +16,13 @@ class Reshape(Base):
     def export():  # type: () -> None
         original_shape = [2, 3, 4]
         test_cases = {
-            'reordered_dims': np.array([4, 2, 3], dtype=np.int64),
-            'reduced_dims': np.array([3, 8], dtype=np.int64),
-            'extended_dims': np.array([3, 2, 2, 2], dtype=np.int64),
+            'reordered_dims': np.array([2, 4, 3], dtype=np.int64),
+            'reduced_dims': np.array([2, 12], dtype=np.int64),
+            'extended_dims': np.array([2, 3, 2, 2], dtype=np.int64),
             'one_dim': np.array([24], dtype=np.int64),
-            'negative_dim': np.array([6, -1, 2], dtype=np.int64),
+            'negative_dim': np.array([2, -1, 2], dtype=np.int64),
+            'zero_dim': np.array([2, 0, 4, 1], dtype=np.int64),
+            'zero_and_negative_dim': np.array([2, 0, 1, -1], dtype=np.int64),
         }
         data = np.random.random_sample(original_shape).astype(np.float32)
 
@@ -31,6 +33,12 @@ class Reshape(Base):
                 outputs=['reshaped'],
             )
 
-            reshaped = np.reshape(data, shape)
+            # replace zeros with corresponding dim size
+            # we need to do this because np.reshape doesn't support 0
+            new_shape = np.copy(shape)
+            zero_index = np.where(shape == 0)
+            new_shape[zero_index] = np.array(original_shape)[zero_index]
+
+            reshaped = np.reshape(data, new_shape)
             expect(node, inputs=[data, shape], outputs=[reshaped],
                    name='test_reshape_' + test_name)
