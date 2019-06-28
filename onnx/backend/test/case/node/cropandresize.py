c044ddbd0270dfd622130381cff118be9d2ddda4
David Fan <jiafa@microsoft.com>
2019-06-27 17:34:55 -0700

    Add CropAndResize

---
+++ onnx/backend/test/case/node/cropandresize.py
@@ -0,0 +1,92 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import numpy as np  # type: ignore
+
+import onnx
+from ..base import Base
+from . import expect
+
+
+class CropAndResize(Base):
+    @staticmethod
+    def export_crop_and_resize():  # type: () -> None
+        node = onnx.helper.make_node(
+            "CropAndResize",
+            inputs=["X", "rois", "batch_indices", "crop_size"],
+            outputs=["Y"],
+            extrapolation_value=0,
+        )
+
+        X = np.array(
+            [
+                [
+                    [
+                        [
+                            1.1,
+                            2.2,
+                        ],
+                        [
+                            3.3,
+                            4.4,
+
+                        ],
+                    ],
+                    [
+                        [
+                            5.5,
+                            6.6,
+                        ],
+                        [
+                            7.7,
+                            8.8,
+
+                        ],
+                    ],
+                ],
+            ],
+            dtype=np.float32,
+        )
+        batch_indices = np.array([0, 0, 0], dtype=np.int64)
+        rois = np.array([[0.0, 0.0, 1.0, 1.0], [0.0, 0.0, 0.5, 0.5], [0.0, 0.0, 0.5, 1.0]], dtype=np.float32)
+        crop_size = np.array([2, 2], dtype=np.int64)
+        # (num_rois, C, output_height, output_width)
+        Y = np.array(
+            [
+                [
+                    [
+                        [1.1, 2.2],
+                        [3.3, 4.4],
+                    ],
+                    [
+                        [5.5, 6.6],
+                        [7.7, 8.8],
+                    ],
+                ],
+                [
+                    [
+                        [1.1, 1.65],
+                        [2.2, 2.75],
+                    ],
+                    [
+                        [5.5, 6.05],
+                        [6.6, 7.15],
+                    ],
+                ],
+                [
+                    [
+                        [1.1, 2.2],
+                        [2.2, 3.3],
+                    ],
+                    [
+                        [5.5, 6.6],
+                        [6.6, 7.7],
+                    ],
+                ],
+            ],
+            dtype=np.float32,
+        )
+
+        expect(node, inputs=[X, rois, batch_indices, crop_size], outputs=[Y], name="test_crop_and_resize")
--- onnx/defs/object_detection/defs.cc
+++ onnx/defs/object_detection/defs.cc
@@ -190,4 +190,115 @@ ONNX_OPERATOR_SET_SCHEMA(
             selected_indices_type->set_elem_type(::ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_INT64);
         }));
 
+static const char* CropAndResize_doc = R"DOC(
+Extracts crops from the input image tensor and resizes them using bilinear sampling or nearest neighbor sampling
+(possibly with aspect ratio change) to a common output size specified by crop_height and crop_width.
+Returns a tensor with crops from the input image at positions defined at the bounding box locations in boxes.
+The cropped boxes are all resized (with bilinear or nearest neighbor interpolation) to
+a fixed size = [crop_height, crop_width]. The result is a 4-D tensor [num_boxes, crop_height, crop_width, depth].
+The resizing is corner aligned.)DOC";
+
+ONNX_OPERATOR_SET_SCHEMA(
+    CropAndResize,
+    11,
+    OpSchema()
+      .Attr(
+          "mode",
+          "The pooling method. Two modes are supported: 'bilinear' and 'nearest'. "
+          "Default is 'bilinear'.",
+          AttributeProto::STRING,
+          std::string("bilinear"))
+      .Attr(
+          "extrapolation_value",
+          "Value used for extrapolation, when applicable. "
+          "Default is 0.0f. ",
+          AttributeProto::FLOAT,
+          0.f)
+      .Input(
+          0,
+          "X",
+          "Input data tensor from the previous operator; "
+          "4-D feature map of shape (N, C, H, W), "
+          "where N is the batch size, C is the number of channels, "
+          "and H and W are the height and the width of the data.",
+          "T1")
+      .Input(
+          1,
+          "rois",
+          "RoIs (Regions of Interest) to pool over; rois is "
+          "2-D input of shape (num_rois, 4) given as "
+          "[[y1, x1, y2, x2], ...]. "
+          "The RoIs' coordinates are normalized in the coordinate system of the input image. "
+          "Each coordinate set has a 1:1 correspondence with the 'batch_indices' input.",
+          "T1")
+      .Input(
+          2,
+          "batch_indices",
+          "1-D tensor of shape (num_rois,) with each element denoting "
+          "the index of the corresponding image in the batch.",
+          "T2")
+      .Input(
+          3,
+          "crop_size",
+          "1-D tensor of 2 elements: [crop_height, crop_width]. "
+          "All cropped image patches are resized to this size. Both crop_height and crop_width need to be positive.",
+          "T2")
+      .Output(
+          0,
+          "Y",
+          "RoI pooled output, 4-D tensor of shape "
+          "(num_rois, C, crop_height, crop_width). The r-th batch element Y[r-1] "
+          "is a pooled feature map corresponding to the r-th RoI X[r-1].",
+          "T1")
+      .TypeConstraint(
+          "T1",
+          {"tensor(float16)", "tensor(float)", "tensor(double)"},
+          "Constrain types to float tensors.")
+      .TypeConstraint(
+          "T2",
+          {"tensor(int32)"},
+          "Constrain types to int tensors.")
+      .SetDoc(CropAndResize_doc)
+      .TypeAndShapeInferenceFunction([](InferenceContext& ctx) {
+        if (!hasNInputShapes(ctx, 4)) {
+          return;
+        }
+        propagateElemTypeFromInputToOutput(ctx, 0, 0);
+
+        auto& input_shape = getInputShape(ctx, 0);
+        auto& rois_shape = getInputShape(ctx, 1);
+        auto& batch_index_shape = getInputShape(ctx, 2);
+        auto& crop_size_shape = getInputShape(ctx, 3);
+
+        if (input_shape.dim_size() != 4) {
+          fail_shape_inference("first input tensor has wrong dimension");
+        }
+        if (rois_shape.dim_size() != 2) {
+          fail_shape_inference("rois input tensor has wrong dimension");
+        }
+        if (batch_index_shape.dim_size() != 1) {
+          fail_shape_inference("batch_indices shape input tensor has wrong dimension");
+        }
+        if (crop_size_shape.dim_size() != 1) {
+          fail_shape_inference("crop_size shape input tensor has wrong dimension");
+        }
+
+        output_shape->clear_dim();
+        for (auto i = 0; i < 2; i++) {
+          if （rois_shape.dim(i).has_dim_value()) {
+            output_shape->add_dim()->set_dim_value(static_cast<int64_t>(
+              rois_shape.dim(i).dim_value()));
+          }
+          else if (rois_shape.dim(i).has_dim_param()) {
+            output_shape->add_dim()->set_dim_param(rois_shape.dim(i).dim_param());
+          }
+          else {
+            output_shape->add_dim();
+          }
+        }
+
+        output_shape->add_dim();
+        output_shape->add_dim();
+      }));
+
 } // namespace ONNX_NAMESPACE
