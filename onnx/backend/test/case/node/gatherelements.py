7d76550f4a05239d2c00ba8422c2077b8bdcba17
Lara <lahaidar@microsoft.com>
2019-07-12 12:47:48 -0700

    update tests with numpy implementation

--- onnx/backend/test/case/node/gatherelements.py
+++ onnx/backend/test/case/node/gatherelements.py
@@ -14,37 +14,45 @@ class GatherElements(Base):
 
     @staticmethod
     def export_gather_elements_0():  # type: () -> None
+        axis = 1
         node = onnx.helper.make_node(
             'GatherElements',
             inputs=['data', 'indices'],
             outputs=['y'],
-            axis=1,
+            axis=axis,
         )
         data = np.array([[1, 2],
                          [3, 4]], dtype=np.float32)
         indices = np.array([[0, 0],
                             [1, 0]], dtype=np.int64)
-        y = np.array([[1, 1],
-                      [4, 3]], dtype=np.float32)
+        # The below GatherElements' numpy implementation is from
+        # https://stackoverflow.com/questions/46868056/how-to-gather-elements-of-specific-indices-in-numpy/51529812#51529812
+        data_swaped = np.swapaxes(data, 0, axis)
+        index_swaped = np.swapaxes(indices, 0, axis)
+        gathered = np.choose(index_swaped, data_swaped)
+        y = np.swapaxes(gathered, 0, axis)
 
         expect(node, inputs=[data, indices.astype(np.int64)], outputs=[y],
                name='test_gather_elements_0')
 
     @staticmethod
     def export_gather_elements_1():  # type: () -> None
+        axis = 0
         node = onnx.helper.make_node(
             'GatherElements',
             inputs=['data', 'indices'],
             outputs=['y'],
-            axis=0,
+            axis=axis,
         )
         data = np.array([[1, 2, 3],
                          [4, 5, 6],
                          [7, 8, 9]], dtype=np.float32)
         indices = np.array([[1, 2, 0],
                             [2, 0, 0]], dtype=np.int64)
-        y = np.array([[4, 8, 3],
-                      [7, 2, 3]], dtype=np.float32)
+        data_swaped = np.swapaxes(data, 0, axis)
+        index_swaped = np.swapaxes(indices, 0, axis)
+        gathered = np.choose(index_swaped, data_swaped)
+        y = np.swapaxes(gathered, 0, axis)
 
         expect(node, inputs=[data, indices.astype(np.int64)], outputs=[y],
                name='test_gather_elements_1')
