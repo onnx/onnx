99dd06a724e55a39f2e3e292be2a72d99cec72cf
daquexian <daquexian566@gmail.com>
2019-08-07 14:25:09 +0800

    Multi-line docs

--- onnx/defs/tensor/defs.cc
+++ onnx/defs/tensor/defs.cc
@@ -1427,20 +1427,26 @@ ONNX_OPERATOR_SET_SCHEMA(
             AttributeProto::INT,
             static_cast<int64_t>(0))
         .Attr("coordinate_transformation_mode",
-            "The scaler used in interpolation. "
-            "For each dimension, denote x_resized as the coordinate in the resized tensor, x_original as the corresponding coordinate in the original tensor, length_original as the length of the original tensor in the specific dimension, length_resized as the length of the resized tensor in the specific dimension, roi_x = (start, end) of the corresponding dimension in input \"roi\", scale = length_resized / length_original"
-            "if scaler is \"half_pixel\", "
-            "x_original = (x_resized + 0.5) / scale - 0.5, "
-            "if scaler is \"pytorch_half_pixel\", "
-            "x_original = length_resize > 1 ? (x_resized + 0.5) / scale - 0.5 : 0, "
-            "if scaler is \"align_corners\", "
-            "x_original = x_resized * (length_original - 1) / (length_resized - 1), "
-            "if scaler is \"asymmetric\", "
-            "x_original = x_resized / scale, "
-            "if scaler is \"tf_half_pixel_for_nn\", "
-            "x_original = (x_resized + 0.5) / scale, "
-            "if scaler is \"tf_crop_and_resize\", "
-            "x_original = length_resized > 1 ? roi_x[0] * (length_original - 1) + x_resized * (roi_x[1] - roi_x[0]) * (length_original - 1) / (length_resized - 1) : 0.5 * (roi_x[0] + roi_x[1]) * (length_original - 1).",
+            R"DOC(The scaler used in interpolation.
+            For each dimension, denote x_resized as the coordinate in the resized tensor, x_original as the corresponding coordinate in the original tensor, length_original as the length of the original tensor in the specific dimension, length_resized as the length of the resized tensor in the specific dimension, roi_x = (start, end) of the corresponding dimension in input "roi", scale = length_resized / length_original, <br/>
+
+            if scaler is "half_pixel", <br/>
+            x_original = (x_resized + 0.5) / scale - 0.5, <br/>
+
+            if scaler is "pytorch_half_pixel", <br/>
+            x_original = length_resize > 1 ? (x_resized + 0.5) / scale - 0.5 : 0, <br/>
+
+            if scaler is "align_corners", <br/>
+            x_original = x_resized * (length_original - 1) / (length_resized - 1), <br/>
+            
+            if scaler is "asymmetric", <br/>
+            x_original = x_resized / scale, <br/>
+
+            if scaler is "tf_half_pixel_for_nn", <br/>
+            x_original = (x_resized + 0.5) / scale, <br/>
+
+            if scaler is "tf_crop_and_resize", <br/>
+            x_original = length_resized > 1 ? roi_x[0] * (length_original - 1) + x_resized * (roi_x[1] - roi_x[0]) * (length_original - 1) / (length_resized - 1) : 0.5 * (roi_x[0] + roi_x[1]) * (length_original - 1).)DOC",
             AttributeProto::STRING,
             std::string("half_pixel"))
         .Attr("nearest_mode",
