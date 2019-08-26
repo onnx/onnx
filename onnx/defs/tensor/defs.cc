290db8df5892599d2b7e6907d6f1336da5605bcd
daquexian <daquexian566@gmail.com>
2019-08-26 13:39:10 +0800

    Update the description of nearest_mode of resize op (#2257)
    * update resize nearest mode description
    
    * Re-generate the docs

--- onnx/defs/tensor/defs.cc
+++ onnx/defs/tensor/defs.cc
@@ -1662,7 +1662,7 @@ ONNX_OPERATOR_SET_SCHEMA(
             AttributeProto::STRING,
             std::string("half_pixel"))
         .Attr("nearest_mode",
-            "Four modes: round_prefer_floor (default), round_prefer_ceil, floor, ceil. Only used by nearest interpolation. It indicates how to get \"nearest\" pixel in input tensor from x_original, so this attribute is valid only if \"mode\" is \"nearest\".",
+            "Four modes: round_prefer_floor (default, as known as round half down), round_prefer_ceil (as known as round half up), floor, ceil. Only used by nearest interpolation. It indicates how to get \"nearest\" pixel in input tensor from x_original, so this attribute is valid only if \"mode\" is \"nearest\".",
             AttributeProto::STRING,
             std::string("round_prefer_floor"))
         .Attr("extrapolation_value",
