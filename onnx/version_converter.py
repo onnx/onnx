011b5446461faa674c56dda36a6bd3286b7e54ff
autoih <ihjhuo@ibm.com>
2019-09-18 19:42:34 -0700

    modify incorrect type error

--- onnx/version_converter.py
+++ onnx/version_converter.py
@@ -159,9 +159,9 @@ Unsupported adapters:
 
 def convert_version(model, target_version):  # type: (ModelProto, int) -> ModelProto
     if not isinstance(model, ModelProto):
-        raise ValueError('VersionConverter only accepts ModelProto as model, incorrect type: {}'.format(type(model)))
+        raise TypeError('VersionConverter only accepts ModelProto as model, incorrect type: {}'.format(type(model)))
     if not isinstance(target_version, int):
-        raise ValueError('VersionConverter only accepts int as target_version, incorrect type: {}'.format(type(target_version)))
+        raise TypeError('VersionConverter only accepts int as target_version, incorrect type: {}'.format(type(target_version)))
     model_str = model.SerializeToString()
     converted_model_str = C.convert_version(model_str, target_version)
     return onnx.load_from_string(converted_model_str)
