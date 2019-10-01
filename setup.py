0334e73d76ecfbbd0beeb68a9435d38d3bfa5eff
Aiken Cairncross <acairncross@gmail.com>
2019-10-01 16:34:01 +0100

    Remove typing packages from test requirements (#2375)
    The requirement is already part of the install_requires so this is
    redundant. (It should only be required for python_version < 3.5 in any
    case.)

--- setup.py
+++ setup.py
@@ -301,8 +301,6 @@ setup_requires.append('pytest-runner')
 tests_require.append('pytest')
 tests_require.append('nbval')
 tests_require.append('tabulate')
-tests_require.append('typing')
-tests_require.append('typing-extensions')
 
 if sys.version_info[0] == 3:
     # Mypy doesn't work with Python 2
