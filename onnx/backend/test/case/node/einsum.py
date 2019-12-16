ed7eec68c29922fc2f422d44bf87fbf7ce4eb151
Negin Raoof <neginmr@utexas.edu>
2019-12-16 14:21:07 -0800

    Update einsum.py

--- onnx/backend/test/case/node/einsum.py
+++ onnx/backend/test/case/node/einsum.py
@@ -9,10 +9,10 @@ import numpy as np  # type: ignore
 import onnx
 from ..base import Base
 from . import expect
-from typing import List
+from typing import Tuple
 
 
-def einsum_reference_implementation(Eqn, Operands):  # type: (np.ndarray, List[np.ndarray]) -> np.ndarray
+def einsum_reference_implementation(Eqn, Operands):  # type: (np.ndarray, Tuple[np.ndarray]) -> np.ndarray
     Z = np.einsum(Eqn[0], *Operands)
     return Z
 
