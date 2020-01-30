27907bc02d8415934a3e1a50a02a92564d7e0e1e
Wei-Sheng Chin <wechi@microsoft.com>
2020-01-30 15:08:44 -0800

    Update onnx/checker.cc
    Co-Authored-By: Jonny Shipton <tmvector@gmail.com>

--- onnx/checker.cc
+++ onnx/checker.cc
@@ -534,7 +534,7 @@ void check_node(
       node.op_type(), domain_version, node.domain());
   if (!schema) {
     if (node.domain() == ONNX_DOMAIN || node.domain() == AI_ONNX_ML_DOMAIN ||
-        node.domain() == "ai.onnx" || node.domain() == "ai.onnx.training") {
+        node.domain() == "ai.onnx" || node.domain() == AI_ONNX_TRAINING_DOMAIN) {
       // fail the checker if op in built-in domains has no schema
       fail_check(
           "No Op registered for " + node.op_type() +
