Fix parentesis in formula

onnx/backend/test/case/node/celu.py
@@ -34,7 +34,7 @@ class Celu(Base):
 
         # Calculate expected output data
         positive_input = np.max(0, input_data)
-        negative_input = np.min(0, alpha * np.exp(input_data / alpha) - 1)
+        negative_input = np.min(0, alpha * (np.exp(input_data / alpha) - 1))
         expected_output = positive_input + negative_input
 
         expect(node, inputs=[input_data], outputs=[expected_output],