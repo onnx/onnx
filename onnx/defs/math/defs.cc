a50af6bac8f5b1b8a858612ddbc9cb6f126f9e69
Liqun Fu <liqfu@microsoft.com>
2020-02-06 17:14:01 -0800

    fix a few typos

--- onnx/defs/math/defs.cc
+++ onnx/defs/math/defs.cc
@@ -1671,7 +1671,7 @@ static const char* NegativeLogLikelihoodLoss_ver12_doc = R"DOC(
 A NegativeLogLikelihoodLoss operator computes (weighted) negative log likelihood loss.
 Its "input" tensor has the shape of (N, C, d1, d2, ..., dk) where k >= 0.
 The "input" tensor contains log-probabilities for input[n, :, d_1, d_2,..., d_k] being in a class of [0, C).
-The operator's "target" input tensor has the shape of (N, d1, d2, ..., dk). It encode class labels (one of C classes) for N x d1 x d2 x ... x dk samples.
+The operator's "target" input tensor has the shape of (N, d1, d2, ..., dk). It encodes class labels (one of C classes) for N x d1 x d2 x ... x dk samples.
 The loss value for input[n, :, d_1, d_2,...d_k] being classified as class c = target[n][d_1][d_2]...[d_k] is computed as:
 
     loss[n][d_1][d_2]...[d_k] = -input[n][c][d_1][d_2]...[d_k].
@@ -1832,7 +1832,7 @@ ONNX_OPERATOR_SET_SCHEMA(
         .Output(0, "loss", "The negative log likelihood loss", "T")
         .Attr(
             "reduction",
-            "Type of reduction to apply to loss: none, sum, mean(default). "
+            "Type of reduction to apply to loss: none, sum, mean (default). "
             "'none': the output is the loss for each sample."
             "'sum': the output will be summed. "
             "'mean': the sum of the output will be divided by the sum of applied weights.",
