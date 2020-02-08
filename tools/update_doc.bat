958788295d916147894687ed53328d87fce8b61c
Wei-Sheng Chin <wschin@outlook.com>
2020-02-07 17:26:08 -0800

    Polish the helper script

--- tools/update_doc.bat
+++ tools/update_doc.bat
@@ -1,4 +1,12 @@
+:: Run this script from ONNX root directory under Anaconda.
+set CMAKE_ARGS="-DONNX_USE_PROTOBUF_SHARED_LIBS=ON"
 set ONNX_ML=1
+
+cd onnx
+python gen_proto.py -l
+python gen_proto.py -l --ml
+cd ..
+
 python setup.py develop
 
 python onnx\backend\test\cmd_tools.py generate-data
