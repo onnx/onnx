0c19c43479b89d4fe08a475d2ffb1c1b998cfe22
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2020-02-26 23:49:35 +0200

    More tests for FFT

--- onnx/backend/test/case/node/fft.py
+++ onnx/backend/test/case/node/fft.py
@@ -13,14 +13,14 @@ from . import expect
 class FFT(Base):
 
     @staticmethod
-    def export_dim_one():  # type: () -> None
+    def export_dim_1d():  # type: () -> None
         node = onnx.helper.make_node(
             'FFT',
             inputs=['X'],
             outputs=['Y'],
         )
 
-        input_data = np.array([[[1., 0], [0, 0], [0, 0]]], dtype=np.float64)
+        input_data = np.array([[[1., 0.2], [0, 3], [1, 0]]], dtype=np.float64)
 
         # Convert to complex
         input_data_complex = input_data.view(dtype=np.complex128)[:,:,0]
@@ -28,4 +28,40 @@ class FFT(Base):
         expected_output = np.stack([fft_result.real, fft_result.imag], axis=2)
 
         expect(node, inputs=[input_data], outputs=[expected_output],
-               name='test_fft_dim_one')
+               name='test_fft_1d')
+
+    @staticmethod
+    def export_dim_2d():  # type: () -> None
+        node = onnx.helper.make_node(
+            'FFT',
+            inputs=['X'],
+            outputs=['Y'],
+        )
+
+        input_data = np.array([[[[1., 2.], [0, 0.5]], [[0, 0.7], [1.3, 0.4]]]], dtype=np.float64)
+
+        # Convert to complex
+        input_data_complex = input_data.view(dtype=np.complex128)[:,:,:,0]
+        fft_result = np.fft.fft2(input_data_complex)
+        expected_output = np.stack([fft_result.real, fft_result.imag], axis=3)
+
+        expect(node, inputs=[input_data], outputs=[expected_output],
+               name='test_fft_2d')
+
+    @staticmethod
+    def export_dim_3d():  # type: () -> None
+        node = onnx.helper.make_node(
+            'FFT',
+            inputs=['X'],
+            outputs=['Y'],
+        )
+
+        input_data = np.random.randn(1,3,4,5,2).astype(np.float64)
+
+        # Convert to complex
+        input_data_complex = input_data.view(dtype=np.complex128)[:,:,:,:,0]
+        fft_result = np.fft.fftn(input_data_complex, (1, 2, 3))
+        expected_output = np.stack([fft_result.real, fft_result.imag], axis=4)
+
+        expect(node, inputs=[input_data], outputs=[expected_output],
+               name='test_fft_3d')
