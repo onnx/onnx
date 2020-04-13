f89f38793fd564eba7eaa0d3519619e841e27b4c
Wei-Sheng Chin <wschin@outlook.com>
2020-04-12 21:56:48 -0700

    [Training] Adam Optimizer (#1970)
    * Adam draft
    
    Update docs
    
    * Empty shape inference
    
    * Add shape inference
    
    * Add tests
    
    * Fix a test
    
    * Sync with recent master changes
    
    * Sync with master
    
    * Make flake8 happy
    
    * Sync docs
    
    * sync doc
    
    * Revert side-effect changes
    
    * sync with master
    
    * Add 1 back
    
    * Polish doc
    
    * Finish the design of Adam spec
    
    * formatting

--- onnx/defs/training/defs.cc
+++ onnx/defs/training/defs.cc
@@ -611,4 +611,181 @@ ONNX_TRAINING_OPERATOR_SET_SCHEMA(
             }
         }));
 
+static const char* Adam_ver1_doc = R"DOC(
+    Compute one iteration of Adam, a stochastic gradient based optimization
+    algorithm. This operator can conduct the optimization of multiple tensor variables.
+
+    Let's define the behavior of this operator. First of all, Adam requires
+    some parameters:
+     
+     - The learning-rate "R".
+     - The update count "T". That is, the number of training iterations conducted.
+     - A L2-norm regularization coefficient "norm_coefficient".
+     - A small constant "epsilon" to avoid dividing-by-zero. 
+     - Two coefficients, "alpha" and "beta".
+
+    At each Adam iteration, the optimized tensors are moved along a direction
+    computed based on their exponentially-averaged historical gradient and
+    exponentially-averaged historical squared gradient. Assume that only a tensor
+    "X" is being optimized. The rest of required information is
+    
+     - the value of "X",
+     - "X"'s gradient (denoted by "G"),
+     - "X"'s exponentially-averaged historical gradient (denoted by "V"), and
+     - "X"'s exponentially-averaged historical squared gradient (denoted by "H").
+
+    Some of those parameters are passed into this operator as input tensors and others
+    are stored as this operator's attributes. Specifically, this operator's input tensor
+    list is ["R", "T", "X", "G", "V", "H"]. That is, "R" is the first input, "T" is
+    the second input, and so on. Other parameters are given as attributes because they
+    are constants. Moreover, the corresponding output tensors are 
+    
+     - the new value of "X" (called "X_new"),
+     - the new exponentially-averaged historical gradient (denoted by "V_new"), and
+     - the new exponentially-averaged historical squared gradient (denoted by "H_new").
+
+    Those outputs are computed following the pseudo code below.
+
+    Let "+", "-", "*", and "/" are all element-wise arithmetic operations with
+    numpy-style broadcasting support. The pseudo code to compute those outputs is:
+
+      // Add gradient of 0.5 * norm_coefficient * ||X||_2^2, where ||X||_2 is the 2-norm.
+      G_regularized = norm_coefficient * X + G
+
+      // Update exponentially-averaged historical gradient.
+      V_new = alpha * V + (1 - alpha) * G_regularized
+
+      // Update exponentially-averaged historical squared gradient.
+      H_new = beta * H + (1 - beta) * G_regularized * G_regularized
+
+      // Compute the element-wise square-root of H_new. V_new will be element-wisely
+      // divided by H_sqrt for a better update direction.
+      H_sqrt = Sqrt(H_new) + epsilon
+
+      // Compute learning-rate. Note that "alpha**T"/"beta**T" is alpha's/beta's T-th power.
+      R_adjusted = T > 0 ? R * Sqrt(1 - beta**T) / (1 - alpha**T) : R
+
+      // Compute new value of "X".
+      X_new = X - R_adjusted * V_new / H_sqrt
+
+      // Post-update regularization.
+      X_final = (1 - norm_coefficient_post) * X_new 
+
+    If there are multiple inputs to be optimized, the pseudo code will be applied
+    independently to each of them.
+)DOC";
+
+ONNX_TRAINING_OPERATOR_SET_SCHEMA(
+    Adam,
+    1,
+    OpSchema()
+        .SetDoc(Adam_ver1_doc)
+        .Input(0, "R", "The initial learning rate.", "T1")
+        .Input(1, "T", "The update count of \"X\". It should be a scalar.", "T2")
+        .Input(
+            2,
+            "inputs",
+            "The tensors to be optimized, followed by their respective gradients, "
+            "followed by their respective accumulated gradients (aka momentum), "
+            "followed by their respective accumulated squared gradients. For example, "
+            "to optimize tensors \"X_1\" and \"X_2,\", the input list would be "
+            "[\"X_1\", \"X_2\", "
+            "gradient of \"X_1\", gradient of \"X_2\", "
+            "accumulated gradient of \"X_1\", accumulated gradient of \"X_2\", "
+            "accumulated squared gradient of \"X_1\", accumulated squared gradient of \"X_2\"].",
+            "T3",
+            OpSchema::Variadic,
+            false)
+        .Output(
+            0,
+            "outputs",
+            "New values of optimized tensors, "
+            "followed by their respective new accumulated gradients, "
+            "followed by their respective new accumulated squared gradients. "
+            "For example, if two tensors \"X_1\" and \"X_2\" are optimized, "
+            "the outputs list would be "
+            "[new value of \"X_1\", new value of \"X_2\", "
+            "new accumulated gradient of \"X_1\", "
+            "new accumulated gradient of \"X_2\", "
+            "new accumulated squared gradient of \"X_1\", "
+            "new accumulated squared gradient of \"X_2\"].",
+            "T3",
+            OpSchema::Variadic,
+            false)
+        .Attr(
+            "alpha",
+            "Coefficient of previously accumulated gradient in running average. Default to 0.9.",
+            AttributeProto::FLOAT,
+            0.9f)
+        .Attr(
+            "beta",
+            "Coefficient of previously accumulated squared-gradient in running average. Default to 0.999.",
+            AttributeProto::FLOAT,
+            0.999f)
+        .Attr(
+            "norm_coefficient",
+            "Regularization coefficient of 0.5 * norm_coefficient * ||X||_2^2. Default to 0, "
+            "which means no regularization.",
+            AttributeProto::FLOAT,
+            0.0f)
+        .Attr(
+            "norm_coefficient_post",
+            "Regularization coefficient of 0.5 * norm_coefficient * ||X||_2^2. Default to 0, "
+            "which means no regularization.",
+            AttributeProto::FLOAT,
+            0.0f)
+        .Attr(
+            "epsilon",
+            "Small scalar to avoid dividing by zero.",
+            AttributeProto::FLOAT,
+            1e-6f)
+        .TypeConstraint(
+            "T1",
+            {"tensor(float)", "tensor(double)"},
+            "Constrain input types to float scalars.")
+        .TypeConstraint(
+            "T2",
+            {"tensor(int64)"},
+            "Constrain input types to 64-bit integer scalars.")
+        .TypeConstraint(
+            "T3",
+            {"tensor(float)", "tensor(double)"},
+            "Constrain input and output types to float tensors.")
+        .TypeAndShapeInferenceFunction([](InferenceContext &ctx) {
+            // Assume that the input list is [R, T, X1, X2, G1, G2, V1, V2, H1, H2] and
+            // output list is [X1_new, X2_new, V1_new, V2_new, H1_new, H2_new] for explaining
+            // the code below in a simpler way.
+
+            // The count of input tensors excluding "R" and "T".
+            auto num_adjustable_tensors = ctx.getNumInputs() - 2;
+
+            // Check number of (optimized tensor, gradient, momentum) tuples.
+            if (num_adjustable_tensors % 4 != 0)
+              fail_shape_inference(
+                  "The sum of optimized tensor count, gradient tensor count, momentum tensor count, ",
+                  "accumulated squared-gradient tensor count should be a multiple of 4 in the ",
+                  "\"inputs\" of Adam operator.");
+
+            // The count of "X1" and "X2".
+            auto num_optimized_tensors = num_adjustable_tensors / 4;
+            for (size_t i = 0; i < num_optimized_tensors; ++i){
+              // Pass X1's/X2's shapes to X1_new/X2_new.
+              size_t i_in = 2 + i;
+              size_t i_out = i;
+              propagateElemTypeFromInputToOutput(ctx, i_in, i_out);
+              propagateShapeFromInputToOutput(ctx, i_in, i_out);
+
+              // Pass V1's/V2's shapes to V1_new/V2_new.
+              i_in = 2 + 2 * num_optimized_tensors + i;
+              i_out = num_optimized_tensors + i;
+              propagateElemTypeFromInputToOutput(ctx, i_in, i_out);
+              propagateShapeFromInputToOutput(ctx, i_in, i_out);
+
+              // Pass H1's/H2's shapes to H1_new/H2_new.
+              i_in = 2 + 3 * num_optimized_tensors + i;
+              i_out = 2 * num_optimized_tensors + i;
+              propagateElemTypeFromInputToOutput(ctx, i_in, i_out);
+              propagateShapeFromInputToOutput(ctx, i_in, i_out);
+        }}));
+
 } // namespace ONNX_NAMESPACE
--- onnx/test/shape_inference_test.py
+++ onnx/test/shape_inference_test.py
@@ -2953,6 +2953,58 @@ class TestShapeInference(unittest.TestCase):
              make_tensor_value_info('V2_new', TensorProto.FLOAT, (3, 4))],
             opset_imports=[helper.make_opsetid('', 12), helper.make_opsetid('ai.onnx.training', 1)])
 
+    def test_adam(self):  # type: () -> None
+        graph = self._make_graph(
+            [('R', TensorProto.FLOAT, ()),  # scalar's shape is ()
+             ('T', TensorProto.INT64, ()),  # scalar's shape is ()
+             ('X', TensorProto.FLOAT, (1, 2)),
+             ('G', TensorProto.FLOAT, (1, 2)),
+             ('V', TensorProto.FLOAT, (1, 2)),
+             ('H', TensorProto.FLOAT, (1, 2))],
+            [make_node('Adam', ['R', 'T', 'X', 'G', 'V', 'H'], ['X_new', 'V_new', 'H_new'],
+             domain='ai.onnx.training',
+             alpha=0.9, beta=1.0, norm_coefficient=0.02)],
+            [])
+
+        infos = [make_tensor_value_info('X_new', TensorProto.FLOAT, (1, 2)),
+                 make_tensor_value_info('V_new', TensorProto.FLOAT, (1, 2)),
+                 make_tensor_value_info('H_new', TensorProto.FLOAT, (1, 2))]
+
+        self._assert_inferred(
+            graph,
+            infos,
+            opset_imports=[make_opsetid('ai.onnx.training', 1), make_opsetid('', 12)])
+
+    def test_adam_multiple(self):  # type: () -> None
+        graph = self._make_graph(
+            [('R', TensorProto.FLOAT, ()),  # scalar's shape is ()
+             ('T', TensorProto.INT64, ()),  # scalar's shape is ()
+             ('X1', TensorProto.FLOAT, (1, 2)),
+             ('X2', TensorProto.FLOAT, (3, 4)),
+             ('G1', TensorProto.FLOAT, (1, 2)),
+             ('G2', TensorProto.FLOAT, (3, 4)),
+             ('V1', TensorProto.FLOAT, (1, 2)),
+             ('V2', TensorProto.FLOAT, (3, 4)),
+             ('H1', TensorProto.FLOAT, (1, 2)),
+             ('H2', TensorProto.FLOAT, (3, 4))],
+            [make_node('Adam', ['R', 'T', 'X1', 'X2', 'G1', 'G2', 'V1', 'V2', 'H1', 'H2'],
+             ['X1_new', 'X2_new', 'V1_new', 'V2_new', 'H1_new', 'H2_new'],
+             domain='ai.onnx.training',
+             alpha=0.9, beta=1.0, norm_coefficient=0.02)],
+            [])
+
+        infos = [make_tensor_value_info('X1_new', TensorProto.FLOAT, (1, 2)),
+                 make_tensor_value_info('X2_new', TensorProto.FLOAT, (3, 4)),
+                 make_tensor_value_info('V1_new', TensorProto.FLOAT, (1, 2)),
+                 make_tensor_value_info('V2_new', TensorProto.FLOAT, (3, 4)),
+                 make_tensor_value_info('H1_new', TensorProto.FLOAT, (1, 2)),
+                 make_tensor_value_info('H2_new', TensorProto.FLOAT, (3, 4))]
+
+        self._assert_inferred(
+            graph,
+            infos,
+            opset_imports=[make_opsetid('ai.onnx.training', 1), make_opsetid('', 12)])
+
     def test_pad_opset10(self):  # type: () -> None
         graph = self._make_graph(
             [('x', TensorProto.FLOAT, (1, None, 2))],
