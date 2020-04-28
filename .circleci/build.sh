f0fc30c08ce1175da04bcfa96d036690215fc893
neginraoof <neginmr@utexas.edu>
2020-04-28 09:06:08 -0700

    Remove unused packages

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -28,23 +28,6 @@ $PYTHON -m virtualenv "$VENV_DIR"
 source "$VENV_DIR/bin/activate"
 pip install -U pip==19 setuptools
 
-# setup onnx as the submodule of pytorch
-PYTORCH_DIR=/tmp/pytorch
-ONNX_DIR="$PYTORCH_DIR/third_party/onnx"
-export ONNX_ML=0
-git clone --recursive --quiet https://github.com/pytorch/pytorch.git "$PYTORCH_DIR"
-rm -rf "$ONNX_DIR"
-cp -r "$PWD" "$ONNX_DIR"
-
-# install ninja to speedup the build
-pip install ninja
-
-# install pytorch
-cd $PYTORCH_DIR
-./scripts/onnx/install-develop.sh
-# install onnxruntime
-pip install -i https://test.pypi.org/simple/ ort-nightly==1.1.0.dev1228
-
 # report sccache hit/miss stats
 if hash sccache 2>/dev/null; then
     sccache --show-stats
