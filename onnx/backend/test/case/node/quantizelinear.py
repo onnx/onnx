25e36691b1d362bb6f39297a2769acc2bdd9adb0
Kevin Chen <kevinch@nvidia.com>
2020-05-22 14:37:51 -0700

    Fix whitespace

--- onnx/backend/test/case/node/quantizelinear.py
+++ onnx/backend/test/case/node/quantizelinear.py
@@ -14,8 +14,8 @@ class QuantizeLinear(Base):
     @staticmethod
     def export():  # type: () -> None
         node = onnx.helper.make_node('QuantizeLinear',
-            inputs=['x', 'y_scale', 'y_zero_point'],
-            outputs=['y'],)
+                                     inputs=['x', 'y_scale', 'y_zero_point'],
+                                     outputs=['y'],)
 
         x = np.array([0, 2, 3, 1000, -254, -1000]).astype(np.float32)
         y_scale = np.float32(2)
@@ -24,37 +24,37 @@ class QuantizeLinear(Base):
 
         expect(node, inputs=[x, y_scale, y_zero_point], outputs=[y],
                name='test_quantizelinear')
-    
+
     @staticmethod
     def export_channels():  # type: () -> None
         node = onnx.helper.make_node('QuantizeLinear',
-            inputs=['x', 'y_scale', 'y_zero_point'],
-            outputs=['y'],)
+                                     inputs=['x', 'y_scale', 'y_zero_point'],
+                                     outputs=['y'],)
 
-        x = np.array([[[[-162,   10],
-                        [-100,  232],
-                        [ -20,  -50]],
+        x = np.array([[[[-162, 10],
+                        [-100, 232],
+                        [-20, -50]],
 
-                       [[ -76,    0],
-                        [   0,  252],
-                        [  32,  -44]],
+                       [[-76, 0],
+                        [0, 252],
+                        [32, -44]],
 
-                       [[ 245, -485],
+                       [[245, -485],
                         [-960, -270],
-                        [-375, -470]],],], dtype=np.float32)
+                        [-375, -470]], ], ], dtype=np.float32)
         y_scale = np.array([2, 4, 5], dtype=np.float32)
         y_zero_point = np.array([84, 24, 196], dtype=np.uint8)
-        y = np.array([[[[  3,  89],
-                        [ 34, 200],
-                        [ 74,  59]],
+        y = np.array([[[[3, 89],
+                        [34, 200],
+                        [74, 59]],
 
-                       [[  5,  24],
-                        [ 24,  87],
-                        [ 32,  13]],
+                       [[5, 24],
+                        [24, 87],
+                        [32, 13]],
 
-                       [[245,  99],
-                        [  4, 142],
-                        [121, 102]],],], dtype=np.uint8)
+                       [[245, 99],
+                        [4, 142],
+                        [121, 102]], ], ], dtype=np.uint8)
 
         expect(node, inputs=[x, y_scale, y_zero_point], outputs=[y],
-                name='test_quantizelinear')
+               name='test_quantizelinear')
