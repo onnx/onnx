23261534b1796f59087a083a8bbd3fbc05f5d75d
Chun-Wei Chen <jacky82226@gmail.com>
2020-07-02 16:27:09 -0700

    revert setup.py for additional flag

--- setup.py
+++ setup.py
@@ -48,7 +48,6 @@ ONNX_ML = not bool(os.getenv('ONNX_ML') == '0')
 ONNX_VERIFY_PROTO3 = bool(os.getenv('ONNX_VERIFY_PROTO3') == '1')
 ONNX_NAMESPACE = os.getenv('ONNX_NAMESPACE', 'onnx')
 ONNX_BUILD_TESTS = bool(os.getenv('ONNX_BUILD_TESTS') == '1')
-ONNX_EXCLUDE_TESTS = bool(os.getenv('ONNX_EXCLUDE_TESTS') == '1')
 
 DEBUG = bool(os.getenv('DEBUG'))
 COVERAGE = bool(os.getenv('COVERAGE'))
@@ -291,13 +290,8 @@ ext_modules = [
 # Packages
 ################################################################################
 
-# Exclude test data while building ONNX
-if ONNX_EXCLUDE_TESTS:
-    packages = setuptools.find_packages(exclude='onnx.backend.test*')
 # no need to do fancy stuff so far
-else:
-    packages = setuptools.find_packages()
-
+packages = setuptools.find_packages()
 
 install_requires.extend([
     'protobuf',
