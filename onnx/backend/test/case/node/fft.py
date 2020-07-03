5ed1a2eb08557e3972f0d937adebd9a3dd1766f9
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2020-07-03 21:08:18 +0300

    Add space after comas

--- onnx/backend/test/case/node/fft.py
+++ onnx/backend/test/case/node/fft.py
@@ -23,7 +23,7 @@ class FFT(Base):
         input_data = np.array([[[1., 0.2], [0, 3], [1, 0]]], dtype=np.float64)
 
         # Convert to complex
-        input_data_complex = input_data.view(dtype=np.complex128)[:,:,0]
+        input_data_complex = input_data.view(dtype=np.complex128)[:, :, 0]
         fft_result = np.fft.fft(input_data_complex)
         expected_output = np.stack([fft_result.real, fft_result.imag], axis=2)
 
@@ -41,7 +41,7 @@ class FFT(Base):
         input_data = np.array([[[[1., 2.], [0, 0.5]], [[0, 0.7], [1.3, 0.4]]]], dtype=np.float64)
 
         # Convert to complex
-        input_data_complex = input_data.view(dtype=np.complex128)[:,:,:,0]
+        input_data_complex = input_data.view(dtype=np.complex128)[:, :, :, 0]
         fft_result = np.fft.fft2(input_data_complex)
         expected_output = np.stack([fft_result.real, fft_result.imag], axis=3)
 
@@ -59,7 +59,7 @@ class FFT(Base):
         input_data = np.random.randn(1,3,4,5,2).astype(np.float64)
 
         # Convert to complex
-        input_data_complex = input_data.view(dtype=np.complex128)[:,:,:,:,0]
+        input_data_complex = input_data.view(dtype=np.complex128)[:, :, :, :, 0]
         fft_result = np.fft.fftn(input_data_complex, (1, 2, 3))
         expected_output = np.stack([fft_result.real, fft_result.imag], axis=4)
 
