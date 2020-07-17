3a6fcb3f9c206d066ac2f443fb15fcc1bad91c51
Chun-Wei Chen <jacky82226@gmail.com>
2020-07-17 09:07:24 -0700

    use older and stable vision

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -4,10 +4,9 @@ set -ex
 
 source /tmp/venv/bin/activate
 
-pip install -q git+https://github.com/pytorch/pytorch.git
 # install torchvision from master
 # the one on pypi requires cuda
-pip install -q git+https://github.com/pytorch/vision.git
+pip install -q git+https://github.com/pytorch/vision.git@86b6c3e22e9d7d8b0fa25d08704e6a31a364973b
 
 cd /tmp/pytorch
 CI=1 exec "scripts/onnx/test.sh"
