155ca3b4287d516b1a909ff22d452fe95c4182a9
Chun-Wei Chen <jacky82226@gmail.com>
2020-07-17 11:04:19 -0700

    print environment message for debug

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -4,8 +4,13 @@ set -ex
 
 source /tmp/venv/bin/activate
 
+wget https://raw.githubusercontent.com/pytorch/pytorch/master/torch/utils/collect_env.py
+python collect_env.py
+
 # install torchvision from master
 # the one on pypi requires cuda
+pip install numpy
+pip install --pre torch -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html
 pip install -q git+https://github.com/pytorch/vision.git
 
 cd /tmp/pytorch
