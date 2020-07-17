476c9f9223a944755207aa15b9663c0878a9394c
Chun-Wei Chen <jacky82226@gmail.com>
2020-07-17 15:11:12 -0700

    print envioronment and install develop

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -4,12 +4,18 @@ set -ex
 
 source /tmp/venv/bin/activate
 
+apt list --installed | grep -E "lib(png|jpeg)"
+
 # install torchvision from master
 # the one on pypi requires cuda
 # Currently the master branch of pytorch/vision is not stable
 # Should set it back to the master if the issue from master branch has been resovled
 # Therefore use certain commit on 07/06/2020 for
-pip install -vvv git+https://github.com/pytorch/vision.git@86b6c3e22e9d7d8b0fa25d08704e6a31a364973
+git clone https://github.com/pytorch/vision.git
+cd vision
+git submodule update --init --recursive
+python setup.py develop
+cd ..
 
 cd /tmp/pytorch
 CI=1 exec "scripts/onnx/test.sh"
