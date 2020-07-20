9a10657ad242e2c3a0f04fd465edfe96db812289
Chun-Wei Chen <jacky82226@gmail.com>
2020-07-20 06:52:20 -0700

    use -vvv to pront details

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -14,7 +14,7 @@ apt list --installed | grep -E "lib(png|jpeg)"
 # Currently the master branch of pytorch/vision is not stable
 # Should set it back to the master if the issue from master branch has been resovled
 
-pip install -q git+https://github.com/pytorch/vision.git
+pip install -vvv git+https://github.com/pytorch/vision.git
 
 cd /tmp/pytorch
 CI=1 exec "scripts/onnx/test.sh"
