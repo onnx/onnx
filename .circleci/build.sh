941d22d34d1fb53d5390dc44e4d8e0e33812badc
Chun-Wei Chen <jacky82226@gmail.com>
2020-07-21 13:47:42 -0700

    remove from build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,9 +4,6 @@ set -ex
 
 export MAX_JOBS=8
 
-# update libpng for pytorch/vision
-sudo apt install -y libturbojpeg-dev
-
 # setup sccache wrappers
 if hash sccache 2>/dev/null; then
     SCCACHE_BIN_DIR="/tmp/sccache"
