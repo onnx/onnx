860de97e8126126eda5f1b0626b062844720985c
Chun-Wei Chen <jacky82226@gmail.com>
2020-07-24 10:38:49 -0700

    remove useless comments

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -10,10 +10,7 @@ sudo apt install ./libturbojpeg0_2.0.5-1_amd64.deb -y
 
 # install torchvision from master
 # the one on pypi requires cuda
-# Currently the master branch of pytorch/vision is not stable
-# Should set it back to the master if the issue from master branch has been resovled
-
-pip install -vvv git+https://github.com/pytorch/vision.git
+pip install -q git+https://github.com/pytorch/vision.git
 
 cd /tmp/pytorch
 CI=1 exec "scripts/onnx/test.sh"
