e719aa030f8f2f4af24c031bf6bd18cf0ed08f8c
Matteo Salvarezza <matteo.salvarezza@gmail.com>
2020-07-27 21:34:18 +0200

    Simplified clip adapter

--- onnx/version_converter/adapters/clip_10_11.h
+++ onnx/version_converter/adapters/clip_10_11.h
@@ -16,48 +16,29 @@ class Clip_10_11 final : public Adapter {
 
       // Turn min/max attributes into tensor (if present) and add value as input
       if(has_min) {
-        addMinInit(graph, node);
+        attrToInput(graph, node, "min", node->f(kmin));
+        node->removeAttribute(kmin);
       }
       if(has_max) {
-        if(!has_min) {addMinDefault(graph, node);}
-        addMaxInit(graph, node);
+        if(!has_min) {attrToInput(graph, node, "min", std::numeric_limits<float>::lowest())}
+        attrToInput(graph, node, "max", node->f(kmax));
+        node->removeAttribute(kmax);
       }
     }
 
-    void addMinInit(std::shared_ptr<Graph> graph, Node *node) const {
-      Tensor t_min;
-      t_min.elem_type() = TensorProto_DataType_FLOAT;
-      auto& data_min = t_min.floats();
-      data_min.emplace_back(node->f(kmin));
-      Value* v_min;
-      v_min = graph->addInitializerAndInput(t_min, "min");
-      node->addInput(v_min);
-      node->removeAttribute(kmin);
-    }
-
-    void addMaxInit(std::shared_ptr<Graph> graph, Node *node) const {
-      Tensor t_max;
-      t_max.elem_type() = TensorProto_DataType_FLOAT;
-      auto& data_max = t_max.floats();
-      data_max.emplace_back(node->f(kmax));
-      Value* v_max;
-      v_max = graph->addInitializerAndInput(t_max, "max");
-      node->addInput(v_max);
-      node->removeAttribute(kmax);
-    }
-
-    void addMinDefault(std::shared_ptr<Graph> graph, Node *node) const {
-      Tensor t_min;
-      t_min.elem_type() = TensorProto_DataType_FLOAT;
-      auto& data_min = t_min.floats();
-      data_min.emplace_back(std::numeric_limits<float>::lowest());
-      Value* v_min;
-      v_min = graph->addInitializerAndInput(t_min, "min");
-      node->addInput(v_min);
+    void attrToInput(std::shared_ptr<Graph> graph, Node *node, const std::string name, 
+      float val) {
+      Tensor t;
+      t.elem_type() = TensorProto_DataType_FLOAT;
+      auto& data = t.floats();
+      data.emplace_back(val);
+      Value* v;
+      v = graph->addInitializerAndInput(t, name);
+      node->addInput(v);      
     }
 
     void adapt(std::shared_ptr<Graph> graph, Node* node) const override {
-	    adapt_clip_10_11(graph, node);
+      adapt_clip_10_11(graph, node);
     }
 };
 
