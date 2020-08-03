ce5ae9d6ce0682291000c43ce2612f3d03cd6f04
Chun-Wei Chen <jacky82226@gmail.com>
2020-08-03 14:54:40 -0700

    correct filename

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -6,7 +6,7 @@ source /tmp/venv/bin/activate
 
 # install new libturbojpeg for pytorch/vision
 wget http://ftp.br.debian.org/debian/pool/main/libj/libjpeg-turbo/libturbojpeg0_1.5.2-2+b1_amd64.deb
-sudo apt install ./libturbojpeg0_2.0.5-1_amd64.deb -y
+sudo apt install ./libturbojpeg0_1.5.2-2+b1_amd64.deb -y
 
 # install torchvision from master
 # the one on pypi requires cuda
