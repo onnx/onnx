a4941b73b1772f3142a2803aaecc8453114e1bdc
Chun-Wei Chen <jacky82226@gmail.com>
2020-10-14 15:29:58 -0700

    use PYTHON_EXECUTABLE from CMake
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- tools/protoc-gen-mypy.sh
+++ tools/protoc-gen-mypy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python -u  "$PWD"/../tools/protoc-gen-mypy.py
+# use PYTHON_EXECUTABLE from CMake to get the right python to execute
+PYTHON_EXECUTABLE=$(grep PYTHON_EXECUTABLE:FILEPATH CMakeCache.txt | cut -d "=" -f2)
+$PYTHON_EXECUTABLE -u "$PWD"/../tools/protoc-gen-mypy.py
