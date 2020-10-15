73cb8a9cfc8f9c3cf2c763363a8b9675ff04338b
Chun-Wei Chen <jacky82226@gmail.com>
2020-10-15 14:03:37 -0700

    use configure_file to get PYTHON_EXEC
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- tools/protoc-gen-mypy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# use PYTHON_EXECUTABLE from CMake to get the right python to execute
-PYTHON_EXECUTABLE=$(grep PYTHON_EXECUTABLE:FILEPATH CMakeCache.txt | cut -d "=" -f2)
-$PYTHON_EXECUTABLE -u "$PWD"/../tools/protoc-gen-mypy.py
