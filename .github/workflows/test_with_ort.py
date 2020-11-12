0e5ee80601fdc0e40ee18c4e86e7b4d582629c3f
Chun-Wei Chen <jacky82226@gmail.com>
2020-11-11 19:59:56 -0800

    fix flake8
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- .github/workflows/test_with_ort.py
+++ .github/workflows/test_with_ort.py
@@ -3,7 +3,7 @@ import numpy
 import onnxruntime as rt
 from onnxruntime.datasets import get_example
 import numpy.random
- 
+
 example1 = get_example("sigmoid.onnx")
 model = onnx.load(example1)
 onnx.checker.check_model(model)
@@ -13,25 +13,25 @@ inferred_model = onnx.shape_inference.infer_shapes(model, True)
 temp_filename = "temp.onnx"
 onnx.save(inferred_model, temp_filename)
 print(onnx.__version__)
- 
+
 print(rt.__version__)
 sess = rt.InferenceSession(temp_filename)
- 
+
 input_name = sess.get_inputs()[0].name
 print("input name", input_name)
 input_shape = sess.get_inputs()[0].shape
 print("input shape", input_shape)
 input_type = sess.get_inputs()[0].type
 print("input type", input_type)
- 
+
 output_name = sess.get_outputs()[0].name
 print("output name", output_name)
 output_shape = sess.get_outputs()[0].shape
 print("output shape", output_shape)
 output_type = sess.get_outputs()[0].type
 print("output type", output_type)
- 
-x = numpy.random.random((3,4,5))
+
+x = numpy.random.random((3, 4, 5))
 x = x.astype(numpy.float32)
 res = sess.run([output_name], {input_name: x})
 print(res)
