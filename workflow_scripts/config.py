a09c92cb59b1c6c7816a36e06fa056c210cbe669
Chun-Wei Chen <jacky82226@gmail.com>
2020-12-01 16:37:18 -0800

    use x64
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- workflow_scripts/config.py
+++ workflow_scripts/config.py
@@ -10,9 +10,5 @@ SKIP_CHECKER_MODELS = ['vision/classification/alexnet/model/bvlcalexnet-3.onnx',
                        'vision/classification/resnet/model/resnet50-caffe2-v1-3.onnx',  # opset1 typeinference function missing
                        'vision/classification/shufflenet/model/shufflenet-3.onnx',  # opset1 typeinference function missing
                        'vision/classification/squeezenet/model/squeezenet1.0-3.onnx',  # opset1 typeinference function missing
-                       'vision/classification/vgg/model/vgg16-7.onnx',  # only failed on Windows-CI
-                       'vision/classification/vgg/model/vgg19-7.onnx',  # only failed on Windows-CI
-                       'vision/classification/vgg/model/vgg16-bn-7.onnx',  # only failed on Windows-CI
-                       'vision/classification/vgg/model/vgg19-bn-7.onnx',  # only failed on Windows-CI
                        'vision/classification/vgg/model/vgg19-caffe2-3.onnx',  # opset1 typeinference function missing
                        'vision/classification/zfnet-512/model/zfnet512-3.onnx']  # opset1 typeinference function missing
