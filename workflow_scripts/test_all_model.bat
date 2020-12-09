38fac38391172706ec8fd611a651f9455b23686a
Chun-Wei Chen <jacky82226@gmail.com>
2020-12-08 21:50:23 -0800

    get python exit code
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- workflow_scripts/test_all_model.bat
+++ workflow_scripts/test_all_model.bat
@@ -1,8 +1,7 @@
 python ../onnx/workflow_scripts/get_model_path.py > models.txt
 @echo off
 for /F %%f in (models.txt) do ( 
-    python ../onnx/workflow_scripts/test_single_model.py --test_model %%f
-    if [[ $? = 0 ]]; then
+    if python ../onnx/workflow_scripts/test_single_model.py --test_model %%f; then
         echo "%%f success"
     else
         echo "%%f fail"
