6c767b521ad961be0adc5eee279abac4c553b56f
adeykin <adeykin90@gmail.com>
2020-12-18 01:26:51 +0300

    remove unused parameter
    Signed-off-by: adeykin <adeykin90@gmail.com>

--- onnx/version_converter/adapters/upsample_6_7.h
+++ onnx/version_converter/adapters/upsample_6_7.h
@@ -9,7 +9,7 @@ namespace ONNX_NAMESPACE { namespace version_conversion {
 struct Upsample_6_7 final: public Adapter {
   explicit Upsample_6_7() : Adapter("Upsample", OpSetID(6), OpSetID(7)) {}
 
-  void adapt_upsample_6_7(std::shared_ptr<Graph> graph, Node* node) const {
+  void adapt_upsample_6_7(std::shared_ptr<Graph>, Node* node) const {
 
     Symbol width_scale_symbol = Symbol("width_scale");
     Symbol height_scale_symbol = Symbol("height_scale");
