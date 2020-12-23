3e30880e75a5a8bd45b9d9f78a3639ecc69a2171
Chun-Wei Chen <jacky82226@gmail.com>
2020-12-22 17:24:11 -0800

    install protobuf from source and use python3
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

---
+++ workflow_scripts/install_linux_proto.sh
@@ -0,0 +1,13 @@
+export NUMCORES=`grep -c ^processor /proc/cpuinfo`
+
+git clone https://github.com/protocolbuffers/protobuf.git
+cd protobuf
+git checkout 3.11.x
+git submodule update --init --recursive
+./autogen.sh --disable-shared and enable PIC.
+
+CFLAGS="-fPIC -g -O2" CXXFLAGS="-fPIC -g -O2" ./configure --disable-shared
+make -j${NUMCORES}
+make check
+make install
+ldconfig
