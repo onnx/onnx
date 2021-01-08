0353a99f62dc8a9e8670afe84b3fa0dbeda8c24f
Chun-Wei Chen <jacky82226@gmail.com>
2021-01-08 14:04:02 -0800

    build without python devel
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- workflow_scripts/install_linux_proto.sh
+++ workflow_scripts/install_linux_proto.sh
@@ -1,4 +1,4 @@
-export NUMCORES=`grep -c ^processor /proc/cpuinfo`
+export NUM_PROCESSOR=`grep -c ^processor /proc/cpuinfo`
 
 git clone https://github.com/protocolbuffers/protobuf.git
 cd protobuf
@@ -7,7 +7,7 @@ git submodule update --init --recursive
 ./autogen.sh --disable-shared and enable PIC.
 
 CFLAGS="-fPIC -g -O2" CXXFLAGS="-fPIC -g -O2" ./configure --disable-shared
-make -j${NUMCORES}
+make -j${NUM_PROCESSOR}
 make check
 make install
 ldconfig
