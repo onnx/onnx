4fd82921bfde54d2f36026e860e61c1206eaa6f7
Chun-Wei Chen <jacky82226@gmail.com>
2021-01-08 13:18:38 -0800

    Revert "update ir for subgraph and optimizer, add some const methods (#3131)" (#3203)
    This reverts commit 8e99dcff98f32ab30fa31ec1fdbdc5eb0df11f04.
    
    Signed-off-by: jcwchen <jacky82226@gmail.com>

--- onnx/test/optimizer_test.py
+++ onnx/test/optimizer_test.py
@@ -15,7 +15,6 @@ import onnx.optimizer
 import unittest
 
 
-@unittest.skip("ONNX optimizer is maintained in onnx/optimizer repo. Tests here are deprecated")
 class TestOptimizer(unittest.TestCase):
 
     def _optimized(self, graph, opts, fixed_point=False, **kwargs):  # type: (GraphProto, Sequence[Text], bool, **Any) -> ModelProto
