558590fa0ebe1c7cd0aceb4a25d37b4359df070b
Anna Jung (VMware) <antheaj@vmware.com>
2021-01-12 14:17:24 -0600

    Update mypy to the latest version
    Signed-off-by: Anna Jung (VMware) <antheaj@vmware.com>

--- onnx/helper.py
+++ onnx/helper.py
@@ -5,7 +5,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import collections
+from collections.abc import Iterable
 import numbers
 from six import text_type, integer_types, binary_type
 
@@ -338,7 +338,7 @@ def make_attribute(
     if doc_string:
         attr.doc_string = doc_string
 
-    is_iterable = isinstance(value, collections.Iterable)
+    is_iterable = isinstance(value, Iterable)
     bytes_or_false = _to_bytes_or_false(value)
     # First, singular cases
     # float
--- setup.py
+++ setup.py
@@ -311,10 +311,7 @@ setup_requires.append('pytest-runner')
 tests_require.append('pytest')
 tests_require.append('nbval')
 tests_require.append('tabulate')
-
-if sys.version_info[0] == 3:
-    # Mypy doesn't work with Python 2
-    extras_require['mypy'] = ['mypy==0.600']
+extras_require['mypy'] = ['mypy==0.790']
 
 ################################################################################
 # Final
