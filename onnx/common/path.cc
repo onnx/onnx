6057dbfbd642c52d164f692e7161e81df4370d9d
Chun-Wei Chen <jacky82226@gmail.com>
2021-01-14 13:37:19 -0800

    improve path join and introduce path utils
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

---
+++ onnx/common/path.cc
@@ -0,0 +1,25 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Copyright (c) ONNX Project Contributors.
+// Licensed under the MIT license.
+
+#pragma once
+
+#include "onnx/common/path.h"
+#include "onnx/string_utils.h"
+#include <iostream>
+
+namespace ONNX_NAMESPACE {
+
+const std::string path_join(const std::string& origin, const std::string& append) {
+    std::string new_path = origin;
+    if (new_path.length() >= k_preferred_path_separator.length() && 
+        new_path.substr(new_path.length () - k_preferred_path_separator.length(), k_preferred_path_separator.length()) != k_preferred_path_separator) {
+        new_path += k_preferred_path_separator;
+    }
+    return new_path + append;
+}
+
+} // namespace ONNX_NAMESPACE
