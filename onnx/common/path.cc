40de8ffd8a17ad8cd183f9b23e85f115fef4493a
Chun-Wei Chen <jacky82226@gmail.com>
2021-01-28 14:55:43 -0800

    add &
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/common/path.cc
+++ onnx/common/path.cc
@@ -9,7 +9,7 @@
 
 namespace ONNX_NAMESPACE {
 
-std::string path_join(const std::string origin, const std::string append) {
+std::string path_join(const std::string& origin, const std::string& append) {
     if (origin.find_last_of(k_preferred_path_separator) != origin.length() - k_preferred_path_separator.length()) {
         return origin + k_preferred_path_separator + append;
     }
