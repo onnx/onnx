466250d41056fce172e3daff14c23db1cad285dc
Chun-Wei Chen <jacky82226@gmail.com>
2021-01-27 18:35:55 -0800

    use name instead of module_op_name first
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/backend/test/case/utils.py
+++ onnx/backend/test/case/utils.py
@@ -29,9 +29,8 @@ def import_recursive(package, op_type=None):  # type: (ModuleType) -> None
     for (_module_loader, name, ispkg) in pkgutil.iter_modules(pkg_dir):
         module_name = "{}.{}".format(module_location, name)  # Module/package
         # module_name is onnx.backend.test.case.node.op_type
-        # only import if op_type is matched
-        module_op_name = module_name[module_name.rindex('.')+1:]
-        if op_type and op_type in module_op_name:
+        # only import if op_type is in python test filename
+        if op_type and op_type in name:
             module = importlib.import_module(module_name)
         if ispkg:
             import_recursive(module)
