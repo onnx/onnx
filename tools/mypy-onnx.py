de38592a6cbb5984d0affed531233902c9200977
Chun-Wei Chen <jacky82226@gmail.com>
2021-02-01 17:31:10 -0800

    remove --no-site-packages
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- tools/mypy-onnx.py
+++ tools/mypy-onnx.py
@@ -12,8 +12,8 @@ def main():  # type: () -> None
         root_folder = os.path.realpath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
         os.chdir(root_folder)
         # Use silent to prevent mypy catching other typecheck errors which are not related to ONNX itself
-        subprocess.check_call(["mypy", ".", "--no-site-packages", "--follow-imports", "silent"])
-        subprocess.check_call(["mypy", "--py2", ".", "--no-site-packages", "--follow-imports", "silent"])
+        subprocess.check_call(["mypy", ".", "--follow-imports", "silent"])
+        subprocess.check_call(["mypy", "--py2", ".", "--follow-imports", "silent"])
 
         exit(0)
     except subprocess.CalledProcessError:
