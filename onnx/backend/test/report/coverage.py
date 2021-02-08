b4d69b49f4216ebbe8857bd76e7c1a77665ee44f
Ewa Tusień <ewa.tusien@intel.com>
2021-02-08 10:42:34 +0100

    [Bugfix] Use domain explicitly when obtaining schema for a given operator (#3256)
    Signed-off-by: Ewa21 <ewa.tusien@intel.com>
    
    Co-authored-by: Ashwini Khade <askhade@microsoft.com>

--- onnx/backend/test/report/coverage.py
+++ onnx/backend/test/report/coverage.py
@@ -47,7 +47,7 @@ class NodeCoverage(object):
         if self.op_type is None:
             self.op_type = node.op_type
             assert self.op_type is not None
-            self.schema = defs.get_schema(self.op_type)
+            self.schema = defs.get_schema(self.op_type, node.domain)
 
         for attr in node.attribute:
             self.attr_coverages[attr.name].add(attr)
