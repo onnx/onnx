ea10e50eb950cecb275958d4cdcb4f90b5a367b1
Chun-Wei Chen <jacky82226@gmail.com>
2021-02-12 09:53:43 -0800

    fix flake8
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- workflow_scripts/test_generated_backend.py
+++ workflow_scripts/test_generated_backend.py
@@ -1,31 +1,33 @@
 import onnx
 import os
 import pytest
+import sys
+
 
 def main():
-    count = 0
     script_dir = os.path.dirname(os.path.abspath(__file__))
     directory = os.path.join(script_dir, '../onnx/backend/test/data/node')
-    count = 0
-    fail_count = 0
+    count = failed_count = 0
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith('.onnx'):
-                test_dir_name = os.path.basename(os.path.normpath(root))
                 try:
                     onnx_model_path = os.path.join(root, file)
                     model = onnx.load(onnx_model_path)
+                    # check model
                     onnx.checker.check_model(model)
                     inferred_model = onnx.shape_inference.infer_shapes(model)
                     onnx.checker.check_model(inferred_model)
                 except Exception as e:
-                    fail_count += 1
-                    print("{} fail: {}".format(test_dir_name, e))
+                    failed_count += 1
+                    test_dir_name = os.path.basename(os.path.normpath(root))
+                    print("{} failed: {}".format(test_dir_name, e))
                 count += 1
-    if fail_count == 0:
+    print('-----------------------------')
+    if failed_count == 0:
         print("{} backend models passed.".format(count))
     else:
-        print("{} failed in {} backend models.".format(fail_count, count))
+        print("{} failed in {} backend models.".format(failed_count, count))
         sys.exit(1)
 
 
