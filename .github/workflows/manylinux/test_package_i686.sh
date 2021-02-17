51dbe56943eb5aa9c84da173b92ff4a700997edd
Chun-Wei Chen <jacky82226@gmail.com>
2021-02-17 14:31:40 -0800

    test backend test data in all release CIs
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- .github/workflows/manylinux/test_package_i686.sh
+++ .github/workflows/manylinux/test_package_i686.sh
@@ -12,12 +12,21 @@ PY_VER=${python_map[$PY_VERSION]}
 yum install -y protobuf-devel cmake3
 
 PIP_COMMAND="/opt/python/${PY_VER}/bin/pip install --no-cache-dir"
+PYTHON_COMAND="/opt/python/"${PY_VER}"/bin/python"
 
 $PIP_COMMAND --upgrade pip
 $PIP_COMMAND numpy protobuf==3.11.3
 $PIP_COMMAND dist/*-manylinux2010_i686.whl
 
+# pytest with the built wheel
 $PIP_COMMAND pytest==5.4.3 nbval
 /opt/python/${PY_VER}/bin/pytest
+
+# Test backend test data
+# onnx.checker all existing backend data
+$PYTHON_COMAND workflow_scripts/test_generated_backend.py
+# onnx.checker all generated backend data
+$PYTHON_COMAND onnx/backend/test/cmd_tools.py generate-data
+$PYTHON_COMAND workflow_scripts/test_generated_backend.py
 
 echo "Succesfully test the wheel"
