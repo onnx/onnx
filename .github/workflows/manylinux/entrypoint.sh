096ed33b2d2c8adeae7f7c2a4ef5e7d2cd347658
Chun-Wei Chen <jacky82226@gmail.com>
2021-03-17 13:58:21 -0700

    try safer dir name instead of build
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- .github/workflows/manylinux/entrypoint.sh
+++ .github/workflows/manylinux/entrypoint.sh
@@ -23,7 +23,7 @@ git clone https://github.com/protocolbuffers/protobuf.git
 cd protobuf
 git checkout 3.11.x
 git submodule update --init --recursive
-mkdir build && cd build
+mkdir build_source && cd build_source
 
 cmake ../cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_SYSCONFDIR=/etc -DCMAKE_POSITION_INDEPENDENT_CODE=ON -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
 make -j${NUM_PROCESSOR}
