35e789c000c0153e6241756463f4f41f16551a47
impactaky <impactaky@users.noreply.github.com>
2021-03-30 13:39:29 +0900

    Add type annotation
    Signed-off-by: impactaky <impactaky@users.noreply.github.com>

--- onnx/test/shape_inference_test.py
+++ onnx/test/shape_inference_test.py
@@ -3530,19 +3530,19 @@ class TestShapeInference(unittest.TestCase):
                                       make_tensor_value_info('saved_var', TensorProto.FLOAT, ('C',)),  # type: ignore
                                       ])
 
-    def test_nonzero(self):
+    def test_nonzero(self):  # type: () -> None
         graph = self._make_graph(
             [('x', TensorProto.FLOAT, (None,))],
             [make_node('NonZero', ['x'], ['out'])],
             [])
-        self._assert_inferred(graph, [make_tensor_value_info('out', TensorProto.INT64, (1, None))])
+        self._assert_inferred(graph, [make_tensor_value_info('out', TensorProto.INT64, (1, None))])  # type: ignore
 
-    def test_nonzero_no_shape(self):
+    def test_nonzero_no_shape(self):  # type: () -> None
         graph = self._make_graph(
             [('x', TensorProto.FLOAT, None)],
             [make_node('NonZero', ['x'], ['out'])],
             [])
-        self._assert_inferred(graph, [make_tensor_value_info('out', TensorProto.INT64, (None, None))])
+        self._assert_inferred(graph, [make_tensor_value_info('out', TensorProto.INT64, (None, None))])  # type: ignore
 
 
 if __name__ == '__main__':
