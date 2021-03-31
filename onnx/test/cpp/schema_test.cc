0ef16f1f39018c9872f0759343857c11e2cf8773
Chun-Wei Chen <jacky82226@gmail.com>
2021-03-30 18:20:57 -0700

    try simple test
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/test/cpp/schema_test.cc
+++ onnx/test/cpp/schema_test.cc
@@ -12,12 +12,16 @@ using namespace ONNX_NAMESPACE;
 namespace ONNX_NAMESPACE {
 namespace Test {
 
-TEST(SchemaTest, RegisterAllOpsetSchema) {
+TEST(SchemaTest, RegisterCertainOpsetSchema) {
 #ifdef __ONNX_DISABLE_STATIC_REGISTRATION
     EXPECT_TRUE(OpSchemaRegistry::Instance()->GetLoadedSchemaVersion() == -1);
-    RegisterOnnxOperatorSetSchema(12);
-    EXPECT_TRUE(OpSchemaRegistry::Instance()->GetLoadedSchemaVersion() == 12);
-    //EXPECT_TRUE(OpSchemaRegistry::GetRegisteredSchemaCount() == ONNX_DBG_GET_COUNT_IN_OPSETS());
+    RegisterOnnxOperatorSetSchema(13);
+    EXPECT_TRUE(OpSchemaRegistry::Instance()->GetLoadedSchemaVersion() == 13);
+
+    auto opSchema = OpSchemaRegistry::Schema("Add");
+    EXPECT_NE(nullptr != opSchema);
+    EXPECT_EQ(opSchema->SinceVersion, 13);
+
 #endif
 }
 } // namespace Test
