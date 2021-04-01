a4c4eb4d320e1a17bc06212e853a426791329fcd
Michal Karzynski <michal.karzynski@intel.com>
2021-04-01 14:02:17 +0200

    Bump version number for testing 1.9.x release
    Signed-off-by: Michal Karzynski <michal.karzynski@intel.com>

--- onnx/test/helper_test.py
+++ onnx/test/helper_test.py
@@ -293,6 +293,7 @@ class TestHelperNodeFunctions(unittest.TestCase):
         test([("", 11)], 6)
         test([("", 12)], 7)
         test([("", 13)], 7)
+        test([("", 14)], 7)
         # standard opset can be referred to using empty-string or "ai.onnx"
         test([("ai.onnx", 9)], 4)
         test([("ai.onnx.ml", 2)], 6)
