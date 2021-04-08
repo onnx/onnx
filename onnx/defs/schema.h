6adc7ec1c542cc494f8aa8430383aafb1d67e9e0
Ian Bearman <ianb@microsoft.com>
2021-04-08 12:27:54 -0700

    Add bfloat16 to Pow 2nd argument and bump to v15
    Signed-off-by: Ian Bearman <ianb@microsoft.com>

--- onnx/defs/schema.h
+++ onnx/defs/schema.h
@@ -889,7 +889,7 @@ class OpSchemaRegistry final : public ISchemaRegistry {
       // Increase the highest version when you make BC-breaking changes to the
       // operator schema on specific domain. Update the lowest version when it's
       // determined to remove too old version history.
-      map_[ONNX_DOMAIN] = std::make_pair(1, 14);
+      map_[ONNX_DOMAIN] = std::make_pair(1, 15);
       map_[AI_ONNX_ML_DOMAIN] = std::make_pair(1, 2);
       map_[AI_ONNX_TRAINING_DOMAIN] = std::make_pair(1, 1);
       // ONNX's preview domain contains operators subject to change, so
