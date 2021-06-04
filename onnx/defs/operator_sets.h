e48ecbb329c89bf03ae212166966bd535a04d644
Chun-Wei Chen <jacky82226@gmail.com>
2021-06-04 13:37:49 -0400

    set status in the end
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/defs/operator_sets.h
+++ onnx/defs/operator_sets.h
@@ -977,8 +977,6 @@ class OpSet_Onnx_ver15 {
 };
 
 inline void RegisterOnnxOperatorSetSchema() {
-  // 0 means all versions of ONNX schema have been loaded
-  OpSchemaRegistry::Instance()->SetLoadedSchemaVersion(0);
   RegisterOpSetSchema<OpSet_Onnx_ver1>();
   RegisterOpSetSchema<OpSet_Onnx_ver2>();
   RegisterOpSetSchema<OpSet_Onnx_ver3>();
@@ -994,11 +992,11 @@ inline void RegisterOnnxOperatorSetSchema() {
   RegisterOpSetSchema<OpSet_Onnx_ver13>();
   RegisterOpSetSchema<OpSet_Onnx_ver14>();
   RegisterOpSetSchema<OpSet_Onnx_ver15>();
+  // 0 means all versions of ONNX schema have been loaded
+  OpSchemaRegistry::Instance()->SetLoadedSchemaVersion(0);
 }
 
 inline void RegisterOnnxOperatorSetSchema(int target_version) {
-  // Sets to record the loaded version and prevent the full operator check in Debug mode
-  OpSchemaRegistry::Instance()->SetLoadedSchemaVersion(target_version);
   // Update here if opset_version bumps 
   // These calls for schema registration here are required to be in descending order for this to work correctly
   RegisterOpSetSchema<OpSet_Onnx_ver14>(target_version);
@@ -1015,6 +1013,8 @@ inline void RegisterOnnxOperatorSetSchema(int target_version) {
   RegisterOpSetSchema<OpSet_Onnx_ver3>(target_version);
   RegisterOpSetSchema<OpSet_Onnx_ver2>(target_version);
   RegisterOpSetSchema<OpSet_Onnx_ver1>(target_version);
+  // Sets to record the loaded version and prevent the full operator check in Debug mode
+  OpSchemaRegistry::Instance()->SetLoadedSchemaVersion(target_version);
 }
 
 } // namespace ONNX_NAMESPACE
