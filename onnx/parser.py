162082894e0029689d7a2ebd1cf8ef99dabbe811
Ganesan Ramalingam <grama@microsoft.com>
2021-06-29 12:43:07 -0700

    Address lgtm-bot warning
    Signed-off-by: Ganesan Ramalingam <grama@microsoft.com>

--- onnx/parser.py
+++ onnx/parser.py
@@ -1,8 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import onnx
-import onnx.onnx_cpp2py_export.parser as C
-from onnx import GraphProto, ModelProto
+from onnx import GraphProto, ModelProto, onnx_cpp2py_export
 from typing import Text
 
 
@@ -11,7 +10,7 @@ class ParseError(Exception):
 
 
 def parse_model(model_text):  # type: (Text) -> ModelProto
-    (success, msg, model_proto_str) = C.parse_model(model_text)
+    (success, msg, model_proto_str) = onnx_cpp2py_export.parser.parse_model(model_text)
     if success:
         return onnx.load_from_string(model_proto_str)
     else:
@@ -19,7 +18,7 @@ def parse_model(model_text):  # type: (Text) -> ModelProto
 
 
 def parse_graph(graph_text):  # type: (Text) -> GraphProto
-    (success, msg, graph_proto_str) = C.parse_graph(graph_text)
+    (success, msg, graph_proto_str) = onnx_cpp2py_export.parser.parse_graph(graph_text)
     if success:
         G = onnx.GraphProto()
         G.ParseFromString(graph_proto_str)
