31f0fd6103e231664d1cfd3b25264f672f5de960
Ashwini Khade <askhade@microsoft.com>
2021-07-02 14:24:44 -0700

    update protobuf build options and build instructions (#3550)
    * update protobuf build options and build instructions
    
    Signed-off-by: Ashwini Khade <askhade@microsoft.com>
    
    * update readme
    
    Signed-off-by: Ashwini Khade <askhade@microsoft.com>
    
    * plus updates per review
    
    Signed-off-by: Ashwini Khade <askhade@microsoft.com>
    
    * plus a few more
    
    Signed-off-by: Ashwini Khade <askhade@microsoft.com>
    
    * fix typo
    
    Signed-off-by: Ashwini Khade <askhade@microsoft.com>
    
    Co-authored-by: G. Ramalingam <grama@microsoft.com>

--- .github/workflows/manylinux/entrypoint.sh
+++ .github/workflows/manylinux/entrypoint.sh
@@ -24,7 +24,7 @@ git checkout v3.11.3
 git submodule update --init --recursive
 mkdir build_source && cd build_source
 
-cmake ../cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_SYSCONFDIR=/etc -DCMAKE_POSITION_INDEPENDENT_CODE=ON -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
+cmake ../cmake -Dprotobuf_BUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_SYSCONFDIR=/etc -DCMAKE_POSITION_INDEPENDENT_CODE=ON -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
 make install
 cd $ONNX_PATH
