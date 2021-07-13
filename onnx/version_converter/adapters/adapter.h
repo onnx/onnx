1cdd8fabee3d4ca46f65db052525747a9e977395
Matteo Salvarezza <matteo.salvarezza@gmail.com>
2021-07-13 13:29:19 +0200

    Added comment
    Signed-off-by: Matteo Salvarezza <matteo.salvarezza@gmail.com>

--- onnx/version_converter/adapters/adapter.h
+++ onnx/version_converter/adapters/adapter.h
@@ -26,6 +26,10 @@ class Adapter {
         target_version_(target_version) {
     }
 
+    // This will almost always return its own node argument after modifying it in place.
+    // The only exception are adapters for deprecated operators: in this case the input
+    // node must be destroyed and a new one must be created and returned. See e.g.
+    // upsample_9_10.h
     virtual Node* adapt(std::shared_ptr<Graph> /*graph*/, Node* node) const = 0;
 
     const std::string& name() const {
