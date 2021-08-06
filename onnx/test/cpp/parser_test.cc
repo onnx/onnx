9ee304f90d0ffbec61c710cf332cbb039e32095f
Ganesan Ramalingam <grama@microsoft.com>
2021-08-06 16:54:54 -0700

    Extend test
    Signed-off-by: Ganesan Ramalingam <grama@microsoft.com>

--- onnx/test/cpp/parser_test.cc
+++ onnx/test/cpp/parser_test.cc
@@ -384,10 +384,11 @@ foo (x, w, b) => (c) {
 }
 )ONNX";
 
-  ModelProto model;
-  Parse(model, code);
+  // ModelProto model;
+  // Parse(model, code);
 
-  EXPECT_EQ(model.functions_size(), 1);
+  // EXPECT_EQ(model.functions_size(), 1);
+  CheckModel(code);
 }
 
 } // namespace Test
