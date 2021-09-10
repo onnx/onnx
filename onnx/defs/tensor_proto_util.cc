5687823a822ceaa7c330b68e6d7d77347aea82f6
Chun-Wei Chen <jacky82226@gmail.com>
2021-09-09 21:03:25 -0400

    correct wrong test written by me
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/defs/tensor_proto_util.cc
+++ onnx/defs/tensor_proto_util.cc
@@ -50,8 +50,8 @@ namespace ONNX_NAMESPACE {
         expected_size *= tensor_proto->dims(i);                                \
       }                                                                        \
       if (tensor_proto->dims_size() != 0 && data.size() != expected_size) {    \
-        fail_shape_inference("The data size of Tensor: ", tensor_proto->name(),\
-        " was expected to have (calculated by dims)", expected_size,           \
+        fail_shape_inference("The data size of tensor: ", tensor_proto->name(),\
+        " was expected to have (calculated by dims) ", expected_size,          \
         " but the actual data size was ", data.size());                        \
       }                                                                        \
       res.insert(res.end(), data.begin(), data.end());                         \
