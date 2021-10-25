da889e6b95750350726d149bf447bf0cd1245964
Chun-Wei Chen <jacky82226@gmail.com>
2021-10-25 08:52:09 -0700

    version bump (#3795)
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/helper.py
+++ onnx/helper.py
@@ -41,7 +41,8 @@ VERSION_TABLE = [
     ('1.8.1', 7, 13, 2, 1),
     ('1.9.0', 7, 14, 2, 1),
     ('1.10.0', 8, 15, 2, 1),
-    ('1.10.1', 8, 15, 2, 1)
+    ('1.10.1', 8, 15, 2, 1),
+    ('1.10.2', 8, 15, 2, 1)
 ]  # type: VersionTableType
 
 VersionMapType = Dict[Tuple[Text, int], int]
