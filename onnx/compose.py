b4171b4ceb6b46d354c6929601b514947ef1c95e
Joaquin Anton <janton@nvidia.com>
2021-11-02 10:39:17 +0100

    Add add_prefix and expand_out_dim
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- onnx/compose.py
+++ onnx/compose.py
@@ -5,8 +5,10 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import copy
 from typing import List, Tuple, Text, Optional
-from onnx import GraphProto
+from onnx import GraphProto, helper
+from onnx import TensorProto as tp
 
 def merge(
         g1,  # type: GraphProto
@@ -41,8 +43,7 @@ def merge(
 
     # Connecting outputs of the first graph with the inputs of the second
     for io_pair in io_map:
-        g1_out_name = io_pair[0]
-        g2_in_name = io_pair[1]
+        g1_out_name, g2_in_name = io_pair
         found = False
         for node in g.node:
             for index, name in enumerate(node.input):
@@ -78,4 +79,97 @@ def merge(
         g.doc_string = f"Graph combining {g1.name} and {g2.name}\n" + \
             g1.name + "\n\n" + g1.doc_string + "\n\n" + g2.name + "\n\n" + g2.doc_string
 
+    return g
+
+
+def add_prefix(
+        g,  # type: GraphProto
+        prefix,  # type: Text
+        rename_nodes=True,  # type: Optional[bool]
+        rename_edges=True,  # type: Optional[bool]
+        rename_inputs=True,  # type: Optional[bool]
+        rename_outputs=True,  # type: Optional[bool]
+):  # type: (...) -> GraphProto
+    """Adds a prefix to names of elements in a graph: Nodes, Edges, Inputs, Outputs
+
+    It can be used as a utility before merging graphs that have overlapping names.
+    Empty names are not prefixed.
+
+    Arguments:
+        g (GraphProto): Graph
+        prefix (Text): Prefix to be added to each name in the graph
+        rename_nodes (bool): Whether to prefix node names
+        rename_edges (bool): Whether to prefix node edge names
+        rename_inputs (bool): Whether to prefix input names
+        rename_outputs (bool): Whether to prefix output names
+    """
+    g = copy.deepcopy(g)
+
+    def prefixed(prefix, name):
+        return prefix + name if len(name) > 0 else name
+
+    if rename_nodes:
+        for n in g.node:
+            n.name = prefixed(prefix, n.name)
+
+    if rename_edges:
+        def clear(lst):
+            for _ in range(len(lst)):
+                lst.pop()
+
+        for n in g.node:
+            out_names = [prefixed(prefix, name) for name in n.output]
+            clear(n.output)
+            n.output.extend(out_names)
+
+            in_names = [prefixed(prefix, name) for name in n.input]
+            clear(n.input)
+            n.input.extend(in_names)
+
+    if rename_inputs:
+        for i in g.input:
+            i.name = prefixed(prefix, i.name)
+
+    if rename_outputs:
+        for o in g.output:
+            o.name = prefixed(prefix, o.name)
+
+    return g
+
+
+def expand_out_dim(
+        g,  # type: GraphProto
+        dim_idx,  # type: int
+):  # type: (...) -> GraphProto
+    """Inserts an extra dimension with extent 1 to each output in the graph.
+
+    Inserts and Unsqueeze node for each output. It can be used as a utility before merging graphs,
+    for example when the second one expects a batch dimension.
+
+    Arguments:
+        g (GraphProto): Graph
+        dim_idx (int): Index of the dimension to be inserted.
+                       A negative value means counting dimensions from the back.
+    """
+    g = copy.deepcopy(g)
+
+    expand_dim_k = g.name + "_expand_dim_idx"
+    g.node.append(
+        helper.make_node(
+            'Constant', inputs=[], outputs=[expand_dim_k], name=f"{expand_dim_k}-constant",
+            value=helper.make_tensor(name="{expand_dim_k}-value", data_type=tp.INT64,
+                                     dims=[1,], vals=[dim_idx,]))
+    )
+
+    for _ in range(len(g.output)):
+        o = g.output.pop(0)
+        new_name = o.name + '_expanded'
+        g.node.append(
+            helper.make_node('Unsqueeze', inputs=[o.name, expand_dim_k],
+                             outputs=[new_name], name=f'unsqueeze-{o.name}')
+        )
+        new_shape = [d.dim_value for d in o.type.tensor_type.shape.dim]
+        new_shape.insert(dim_idx, 1)
+        g.output.append(
+            helper.make_tensor_value_info(new_name, o.type.tensor_type.elem_type, new_shape))
     return g
--- onnx/test/compose_test.py
+++ onnx/test/compose_test.py
@@ -17,6 +17,7 @@ class TestComposeFunctions(unittest.TestCase):
     def test_merge(self):  # type: () -> None
         def create_tensor(name):  # type: ignore
             return helper.make_tensor_value_info(name, TensorProto.FLOAT, [1, 2])
+
         A0 = create_tensor("A0")
         A1 = create_tensor("A1")
         B00 = create_tensor("B00")
@@ -75,6 +76,85 @@ class TestComposeFunctions(unittest.TestCase):
 
         m4 = helper.make_model(g4, producer_name='test')
         onnx.checker.check_model(m4)
+
+    def test_add_prefix(self):  # type: () -> None
+        def create_tensor(name):  # type: ignore
+            return helper.make_tensor_value_info(name, TensorProto.FLOAT, [1, 2])
+
+        A0 = create_tensor("A0")
+        A1 = create_tensor("A1")
+        B00 = create_tensor("B00")
+        B10 = create_tensor("B10")
+        B20 = create_tensor("B20")
+
+        L0_0 = helper.make_node("Add", ["A0", "A1"], ["B00"], "L0_0")
+        L0_1 = helper.make_node("Sub", ["A0", "A1"], ["B10"], "L0_1")
+        L0_2 = helper.make_node("Mul", ["A0", "A1"], ["B20"], "L0_2")
+
+        g0 = helper.make_graph(
+            [L0_0, L0_1, L0_2],
+            "test1",
+            [A0, A1],
+            [B00, B10, B20])
+
+        g1 = onnx.compose.add_prefix(g0, 'g0/')
+
+        for e1, e0 in zip(g1.input, g0.input):
+            self.assertEqual(e1.name, 'g0/' + e0.name if len(e0.name) > 0 else e0.name)
+
+        for e1, e0 in zip(g1.output, g0.output):
+            self.assertEqual(e1.name, 'g0/' + e0.name if len(e0.name) > 0 else e0.name)
+
+        for n1, n0 in zip(g1.node, g0.node):
+            self.assertEqual(n1.name, 'g0/' + n0.name if len(n0.name) > 0 else n0.name)
+
+            for e1, e0 in zip(n1.input, n0.input):
+                self.assertEqual(e1, 'g0/' + e0 if len(e0) > 0 else e0)
+
+            for e1, e0 in zip(n1.output, n0.output):
+                self.assertEqual(e1, 'g0/' + e0 if len(e0) > 0 else e0)
+
+        m = helper.make_model(g1, producer_name='test')
+        onnx.checker.check_model(m)
+
+
+    def test_expand_out_dim(self):  # type: () -> None
+        def create_tensor(name, shape=[1, 2]):  # type: ignore
+            return helper.make_tensor_value_info(name, TensorProto.FLOAT, shape)
+
+        A0 = create_tensor("A0", shape=[5, 4])
+        A1 = create_tensor("A1", shape=[5, 4])
+        B0 = create_tensor("B0", shape=[3, 2])
+        B1 = create_tensor("B1", shape=[3, 2])
+        C0 = create_tensor("C0", shape=[5, 4])
+        C1 = create_tensor("C1", shape=[3, 2])
+
+
+        L0 = helper.make_node("Add", ["A0", "A1"], ["C0"], "L0")
+        L1 = helper.make_node("Sub", ["B0", "B1"], ["C1"], "L1")
+
+        g0 = helper.make_graph(
+            [L0, L1],
+            "test1",
+            [A0, A1, B0, B1],
+            [C0, C1])
+
+        def out_shape(out):
+            return [out.type.tensor_type.shape.dim[d].dim_value \
+                for d in range(len(out.type.tensor_type.shape.dim))]
+
+        for dim_idx in [0]:
+            g1 = onnx.compose.expand_out_dim(g0, dim_idx)
+
+            for out0, out1 in zip(g0.output, g1.output):
+                self.assertEqual(out1.name, out0.name + '_expanded')
+                self.assertEqual(out1.type.tensor_type.elem_type, out0.type.tensor_type.elem_type)
+                expected_out_shape = out_shape(out0)
+                expected_out_shape.insert(dim_idx, 1)
+                self.assertEqual(out_shape(out1), expected_out_shape)
+
+            m = helper.make_model(g1, producer_name='test')
+            onnx.checker.check_model(m)
 
 
 if __name__ == '__main__':
