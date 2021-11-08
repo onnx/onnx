8636c636d9afc3ab30e9e1955c7ed31508b84686
Joe <joe@preferred.jp>
2021-11-08 04:52:04 +0000

    Append dim even both dim value and param are not set
    Signed-off-by: Joe <joe@preferred.jp>

--- onnx/defs/data_propagators.h
+++ onnx/defs/data_propagators.h
@@ -11,6 +11,8 @@ inline void appendDimToTensorShapeProto(TensorShapeProto& tsp, const TensorShape
     tsp.mutable_dim()->Add()->set_dim_value(dim.dim_value());
   } else if (dim.has_dim_param()) {
     tsp.mutable_dim()->Add()->set_dim_param(dim.dim_param());
+  } else {
+    tsp.mutable_dim()->Add();
   }
 }
 
