c74381e450a3f8c43ed2236f77445d7fc9c5072a
Chun-Wei Chen <jacky82226@gmail.com>
2022-01-05 08:35:14 -0800

    6be8011a073feeca08d256ac64335a19fc8eee4c6312668b6781ace71db0de20
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- .github/workflows/manylinux/test_package_i686.sh
+++ .github/workflows/manylinux/test_package_i686.sh
@@ -20,11 +20,7 @@ PYTEST_COMMAND="${PYTHON_BIN}pytest"
 $PIP_INSTALL_COMMAND --upgrade pip
 
 # pip install -r requirements-release will bump into issue in i686 due to pip install cryptography failure
-if [ "PY_VERSION" == "3.8" ] || [ "PY_VERSION" == "3.9" ]; then
-    $PIP_INSTALL_COMMAND numpy==1.21.4 protobuf==3.16.0 pytest==5.4.3 nbval ipython==7.16.1 || { echo "Installing Python requirements failed."; exit 1; }
-else
-    $PIP_INSTALL_COMMAND numpy==1.16.6 protobuf==3.16.0 pytest==5.4.3 nbval ipython==7.16.1 || { echo "Installing Python requirements failed."; exit 1; }
-fi
+$PIP_INSTALL_COMMAND numpy protobuf==3.16.0 pytest==5.4.3 nbval ipython==7.16.1 || { echo "Installing Python requirements failed."; exit 1; }
 $PIP_INSTALL_COMMAND dist/*manylinux2010_i686.whl
 
 # pytest with the built wheel
@@ -38,11 +34,7 @@ $PYTHON_COMAND onnx/backend/test/cmd_tools.py generate-data
 $PYTHON_COMAND workflow_scripts/test_generated_backend.py
 
 # Verify ONNX with the latest numpy
-if [ "PY_VERSION" == "3.8" ] || [ "PY_VERSION" == "3.9" ]; then
-    $PIP_UNINSTALL_COMMAND numpy==1.21.4 onnx && $PIP_INSTALL_COMMAND numpy
-else
-    $PIP_UNINSTALL_COMMAND numpy onnx && $PIP_INSTALL_COMMAND numpy
-fi
+$PIP_UNINSTALL_COMMAND numpy onnx && $PIP_INSTALL_COMMAND numpy
 $PIP_INSTALL_COMMAND dist/*manylinux2010_i686.whl
 $PYTEST_COMMAND
 
