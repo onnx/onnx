793ddeda9e2de8b71bc2d1e6d957c4e6d1274e5c
Xavier Dupré <xadupre@users.noreply.github.com>
2022-01-18 22:50:48 +0100

    Update TreeEnsembleClassifier and TreeEnsembleRegressor to support tensor as attributes (#3897)
    * update TreeEnsembleClassifier and TreeEnsembleRegressor
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>
    
    * change the type of another attribute
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>
    
    * add missing file
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>
    
    * update documentation
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>
    
    * eol
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>
    
    * add field with _as_tensor
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>
    
    * fix error messages
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>
    
    * fix error message
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>
    
    * fix missing change
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>
    
    Co-authored-by: xavier dupré <xavier.dupre@gmail.com>
    Co-authored-by: G. Ramalingam <grama@microsoft.com>

--- onnx/defs/schema.h
+++ onnx/defs/schema.h
@@ -937,7 +937,7 @@ class OpSchemaRegistry final : public ISchemaRegistry {
       // operator schema on specific domain. Update the lowest version when it's
       // determined to remove too old version history.
       map_[ONNX_DOMAIN] = std::make_pair(1, 16);
-      map_[AI_ONNX_ML_DOMAIN] = std::make_pair(1, 2);
+      map_[AI_ONNX_ML_DOMAIN] = std::make_pair(1, 3);
       map_[AI_ONNX_TRAINING_DOMAIN] = std::make_pair(1, 1);
       // ONNX's preview domain contains operators subject to change, so
       // versining is not meaningful and that domain should have only one
@@ -947,7 +947,7 @@ class OpSchemaRegistry final : public ISchemaRegistry {
       // the max version above in a *release* version of ONNX. But in other
       // versions, the max version may be ahead of the last-release-version.
       last_release_version_map_[ONNX_DOMAIN] = 15;
-      last_release_version_map_[AI_ONNX_ML_DOMAIN] = 2;
+      last_release_version_map_[AI_ONNX_ML_DOMAIN] = 3;
       last_release_version_map_[AI_ONNX_TRAINING_DOMAIN] = 1;
       last_release_version_map_[AI_ONNX_PREVIEW_TRAINING_DOMAIN] = 1;
     }
