7e26ccba7b1452db47fa97b9a8b463dc98b06e70
Chris Hua <chua@squareup.com>
2022-01-19 15:59:51 -0800

    mypy: update to 0.760 and remove vendored protobuf stubs (#3939)
    * mypy: update to 0.760, remove protobuf stubs
    
    - We can remove the stubs in favor of types-protobuf package
    - mypy==0.600 doesn't install properly for me, updating to a somewhat newer version that does install but doesn't change much logic otherwise
    
    Signed-off-by: Chris Hua <hua.christopher@gmail.com>
    
    * mypy: use setattr
    
    per https://github.com/python/mypy/issues/2427
    
    Signed-off-by: Chris Hua <hua.christopher@gmail.com>
    
    * mypy: fix type signatures
    
    Signed-off-by: Christopher Hua <chua@squareup.com>
    Signed-off-by: Liqun Fu <liqfu@microsoft.com>

--- onnx/backend/base.py
+++ onnx/backend/base.py
@@ -46,7 +46,7 @@ def namedtupledict(typename, field_names, *args, **kwargs):  # type: (Text, Sequ
         if isinstance(key, str):
             key = field_names_map[key]
         return super(type(self), self).__getitem__(key)  # type: ignore
-    data.__getitem__ = getitem
+    setattr(data, "__getitem__", getitem)
     return data
 
 
