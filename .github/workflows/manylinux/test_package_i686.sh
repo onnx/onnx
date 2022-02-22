d07dadb2f0826a9c798b3d970f81c5447589e678
jacky82226 <jacky82226@gmail.com>
2022-02-22 11:00:10 -0800

    skip Python 3.10 testing for ort-nightly
    Signed-off-by: jacky82226 <jacky82226@gmail.com>

--- .github/workflows/manylinux/test_package_i686.sh
+++ .github/workflows/manylinux/test_package_i686.sh
@@ -40,10 +40,10 @@ $PYTHON_COMAND onnx/backend/test/cmd_tools.py generate-data
 $PYTHON_COMAND workflow_scripts/test_generated_backend.py
 
 # Verify ONNX with the latest numpy
-if [ "$PY_VERSION" == "3.8" ] || [ "$PY_VERSION" == "3.9" ]; then
-    $PIP_UNINSTALL_COMMAND numpy onnx && $PIP_INSTALL_COMMAND numpy==1.21.5
-else
+if [ "$PY_VERSION" == "3.7" ] ; then
     $PIP_UNINSTALL_COMMAND numpy onnx && $PIP_INSTALL_COMMAND numpy
+else
+    $PIP_UNINSTALL_COMMAND numpy onnx && $PIP_INSTALL_COMMAND numpy==1.21.5
 fi
 $PIP_INSTALL_COMMAND dist/*manylinux2014_i686.whl
 $PYTEST_COMMAND
