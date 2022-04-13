ebeef16ce7f31ab5a0d834876fd5d543f667ff32
Chun-Wei Chen <jacky82226@gmail.com>
2022-04-13 15:46:50 -0700

    demo for bumping opset version
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/defs/operator_sets.h
+++ onnx/defs/operator_sets.h
@@ -1025,6 +1025,14 @@ class OpSet_Onnx_ver16 {
     fn(GetOpSchema<ONNX_OPERATOR_SET_SCHEMA_CLASS_NAME(Onnx, 16, PRelu)>());
   }
 };
+
+// Iterate over schema from ai.onnx version 17
+class OpSet_Onnx_ver17 {
+ public:
+  static void ForEachSchema(std::function<void(OpSchema&&)> fn) {
+  }
+};
+
 inline void RegisterOnnxOperatorSetSchema() {
   RegisterOpSetSchema<OpSet_Onnx_ver1>();
   RegisterOpSetSchema<OpSet_Onnx_ver2>();
@@ -1042,6 +1050,7 @@ inline void RegisterOnnxOperatorSetSchema() {
   RegisterOpSetSchema<OpSet_Onnx_ver14>();
   RegisterOpSetSchema<OpSet_Onnx_ver15>();
   RegisterOpSetSchema<OpSet_Onnx_ver16>();
+  RegisterOpSetSchema<OpSet_Onnx_ver17>();
   // 0 means all versions of ONNX schema have been loaded
   OpSchemaRegistry::Instance()->SetLoadedSchemaVersion(0);
 }
@@ -1049,6 +1058,7 @@ inline void RegisterOnnxOperatorSetSchema() {
 inline void RegisterOnnxOperatorSetSchema(int target_version) {
   // Update here if opset_version bumps 
   // These calls for schema registration here are required to be in descending order for this to work correctly
+  RegisterOpSetSchema<OpSet_Onnx_ver17>(target_version);
   RegisterOpSetSchema<OpSet_Onnx_ver16>(target_version);
   RegisterOpSetSchema<OpSet_Onnx_ver15>(target_version);
   RegisterOpSetSchema<OpSet_Onnx_ver14>(target_version);
