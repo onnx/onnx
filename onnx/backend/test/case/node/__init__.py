2d815f7bc278ac8c821ef76cb192ed7a9328971d
Chun-Wei Chen <jacky82226@gmail.com>
2022-04-16 06:42:02 -0700

    if unknown newer opset_version, use default ir_version
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/backend/test/case/node/__init__.py
+++ onnx/backend/test/case/node/__init__.py
@@ -183,7 +183,8 @@ def expect(node: onnx.NodeProto,
                 onnx_opset_version = produce_opset_version
         kwargs[str('opset_imports')] = max_inclusive_versions
         if onnx_opset_version is not None:
-            kwargs[str('ir_version')] = _OpsetVersionToIRVersionMap[onnx_opset_version]
+            if onnx_opset_version in _OpsetVersionToIRVersionMap:
+                kwargs[str('ir_version')] = _OpsetVersionToIRVersionMap[onnx_opset_version]
 
     model = onnx.helper.make_model(graph, **kwargs)
 
