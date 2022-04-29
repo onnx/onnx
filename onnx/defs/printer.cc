b49bd5e0b42af2bac8b94fe02bb233122eae0183
Ganesan Ramalingam <grama@microsoft.com>
2022-04-29 12:42:31 -0700

    Extend printer for types and tensorproto
    Signed-off-by: Ganesan Ramalingam <grama@microsoft.com>

--- onnx/defs/printer.cc
+++ onnx/defs/printer.cc
@@ -44,9 +44,40 @@ std::ostream& operator<<(std::ostream& os, const TypeProto_Tensor& tensortype) {
   return os;
 }
 
+std::ostream& operator<<(std::ostream& os, const TypeProto_Sequence& seqType) {
+  return os << "seq(" << seqType.elem_type() << ")";
+}
+
+std::ostream& operator<<(std::ostream& os, const TypeProto_Map& mapType) {
+  return os << "map(" << PrimitiveTypeNameMap::ToString(mapType.key_type()) ", " << seqType.value_type() << ")";
+}
+
+std::ostream& operator<<(std::ostream& os, const TypeProto_Optional& optType) {
+  return os << "optional(" << optType.elem_type() << ")";
+}
+
+std::ostream& operator<<(std::ostream& os, const TypeProto_SparseTensor sparseType) {
+  os << "sparse_tensor(" << PrimitiveTypeNameMap::ToString(tensortype.elem_type());
+  if (tensortype.has_shape()) {
+    if (tensortype.shape().dim_size() > 0)
+      os << tensortype.shape();
+  } else
+    os << "[...]";
+
+  return os << ")";
+}
+
 std::ostream& operator<<(std::ostream& os, const TypeProto& type) {
   if (type.has_tensor_type())
     os << type.tensor_type();
+  else if (type.has_sequence_type())
+    os << type.sequence_type();
+  else if (type.has_map_type())
+    os << type.map_type();
+  else if (type.has_optional_type())
+    os << type.optional_type();
+  else if (type.has_sparse_tensor_type())
+    os << type.sparse_tensor_type();
   return os;
 }
 
@@ -54,41 +85,60 @@ std::ostream& operator<<(std::ostream& os, const TensorProto& tensor) {
   os << PrimitiveTypeNameMap::ToString(tensor.data_type());
   print(os, "[", ",", "]", tensor.dims());
 
-  // TODO: does not yet handle raw_data or FLOAT16 or externally stored data.
   // TODO: does not yet handle name of tensor.
-  switch (static_cast<TensorProto::DataType>(tensor.data_type())) {
-    case TensorProto::DataType::TensorProto_DataType_INT8:
-    case TensorProto::DataType::TensorProto_DataType_INT16:
-    case TensorProto::DataType::TensorProto_DataType_INT32:
-    case TensorProto::DataType::TensorProto_DataType_UINT8:
-    case TensorProto::DataType::TensorProto_DataType_UINT16:
-    case TensorProto::DataType::TensorProto_DataType_BOOL:
-      print(os, " {", ",", "}", tensor.int32_data());
-      break;
-    case TensorProto::DataType::TensorProto_DataType_INT64:
-      print(os, " {", ",", "}", tensor.int64_data());
-      break;
-    case TensorProto::DataType::TensorProto_DataType_UINT32:
-    case TensorProto::DataType::TensorProto_DataType_UINT64:
-      print(os, " {", ",", "}", tensor.uint64_data());
-      break;
-    case TensorProto::DataType::TensorProto_DataType_FLOAT:
-      print(os, " {", ",", "}", tensor.float_data());
-      break;
-    case TensorProto::DataType::TensorProto_DataType_DOUBLE:
-      print(os, " {", ",", "}", tensor.double_data());
-      break;
-    case TensorProto::DataType::TensorProto_DataType_STRING: {
-      const char* sep = "{";
-      for (auto& elt : tensor.string_data()) {
-        os << sep << "\"" << elt << "\"";
-        sep = ", ";
-      }
-      os << "}";
-      break;
+  if (tensor.has_raw_data()) {
+    switch (static_cast<TensorProto::DataType>(tensor.data_type())) {
+      case TensorProto::DataType::TensorProto_DataType_INT32:
+        print(os, "[", ",", "]", ParseData<int32_t>(&tensor));
+        break;
+      case TensorProto::DataType::TensorProto_DataType_INT64:
+        print(os, "[", ",", "]", ParseData<int64_t>(&tensor));
+        break;
+      case TensorProto::DataType::TensorProto_DataType_FLOAT:
+        print(os, "[", ",", "]", ParseData<float>(&tensor));
+        break;
+      case TensorProto::DataType::TensorProto_DataType_DOUBLE:
+        print(os, "[", ",", "]", ParseData<double>(&tensor));
+        break;
+      default:
+        os << "..."; // ParseData not instantiated for other types.
+        break;
     }
-    default:
-      break;
+  } else {
+    switch (static_cast<TensorProto::DataType>(tensor.data_type())) {
+      case TensorProto::DataType::TensorProto_DataType_INT8:
+      case TensorProto::DataType::TensorProto_DataType_INT16:
+      case TensorProto::DataType::TensorProto_DataType_INT32:
+      case TensorProto::DataType::TensorProto_DataType_UINT8:
+      case TensorProto::DataType::TensorProto_DataType_UINT16:
+      case TensorProto::DataType::TensorProto_DataType_BOOL:
+        print(os, " {", ",", "}", tensor.int32_data());
+        break;
+      case TensorProto::DataType::TensorProto_DataType_INT64:
+        print(os, " {", ",", "}", tensor.int64_data());
+        break;
+      case TensorProto::DataType::TensorProto_DataType_UINT32:
+      case TensorProto::DataType::TensorProto_DataType_UINT64:
+        print(os, " {", ",", "}", tensor.uint64_data());
+        break;
+      case TensorProto::DataType::TensorProto_DataType_FLOAT:
+        print(os, " {", ",", "}", tensor.float_data());
+        break;
+      case TensorProto::DataType::TensorProto_DataType_DOUBLE:
+        print(os, " {", ",", "}", tensor.double_data());
+        break;
+      case TensorProto::DataType::TensorProto_DataType_STRING: {
+        const char* sep = "{";
+        for (auto& elt : tensor.string_data()) {
+          os << sep << "\"" << elt << "\"";
+          sep = ", ";
+        }
+        os << "}";
+        break;
+      }
+      default:
+        break;
+    }
   }
   return os;
 }
