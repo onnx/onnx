010c191c58284a8d20990f7bb495bf5638cca5fe
Ettore Tiotto <etiotto@ca.ibm.com>
2022-05-19 17:16:35 -0400

    bump version to 1.12.0
    Signed-off-by: Ettore Tiotto <etiotto@ca.ibm.com>

--- onnx/common/version.h
+++ onnx/common/version.h
@@ -2,12 +2,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
 #pragma once
 
 namespace ONNX_NAMESPACE {
 
 // Represents the most recent release version. Updated with every release.
-constexpr const char* LAST_RELEASE_VERSION = "1.11.0";
+constexpr const char* LAST_RELEASE_VERSION = "1.12.0";
 
-}
+} // namespace ONNX_NAMESPACE
