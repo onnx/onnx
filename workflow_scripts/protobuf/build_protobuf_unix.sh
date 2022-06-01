f313bf821c6c44717cfe39ba6c2d2edd386078c9
Chun-Wei Chen <jacky82226@gmail.com>
2022-06-01 15:50:59 -0700

    upgrade Protobuf to 3.19.0 to accept Protobuf 4.21
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- workflow_scripts/protobuf/build_protobuf_unix.sh
+++ workflow_scripts/protobuf/build_protobuf_unix.sh
@@ -16,9 +16,9 @@ fi
 # Build protobuf from source with -fPIC on Unix-like system
 ORIGINAL_PATH=$(pwd)
 cd ..
-wget https://github.com/protocolbuffers/protobuf/releases/download/v3.16.0/protobuf-cpp-3.16.0.tar.gz
-tar -xvf protobuf-cpp-3.16.0.tar.gz
-cd protobuf-3.16.0
+wget https://github.com/protocolbuffers/protobuf/releases/download/v3.19.0/protobuf-cpp-3.19.0.tar.gz
+tar -xvf protobuf-cpp-3.19.0.tar.gz
+cd protobuf-3.19.0
 mkdir build_source && cd build_source
 cmake ../cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_PROTOBUF_PATH -DCMAKE_INSTALL_SYSCONFDIR=/etc -DCMAKE_POSITION_INDEPENDENT_CODE=ON -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
 make -j$CORE_NUMBER
