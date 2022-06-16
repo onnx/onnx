b73091c19196370afe46dd8b8ed15f6c383910d1
Teodora Sechkova <tsechkova@vmware.com>
2022-06-16 17:10:46 +0300

    Split shape_inference_test.py
    Move testing shape inference with data propagation in a separate
    file 'data_propagation_test.py'.
    
    Signed-off-by: Teodora Sechkova <tsechkova@vmware.com>

--- onnx/test/shape_inference_test.py
+++ onnx/test/shape_inference_test.py
@@ -369,18 +369,6 @@ class TestShapeInference(unittest.TestCase):
             graph,
             [make_tensor_value_info('y', TensorProto.INT32, (3, 4))])
 
-    def test_expand_symbolic_input(self) -> None:
-        graph = self._make_graph(
-            [('x', TensorProto.INT32, (3, 1, 2)),
-             ('y', TensorProto.INT32, (1, 4, 2))],
-            [make_node("Shape", ['y'], ['shape']),
-             make_node("Expand", ['x', 'shape'], ['z'])],
-            [])
-        self._assert_inferred(graph, [
-            make_tensor_value_info('shape', TensorProto.INT64, (3,)),
-            make_tensor_value_info('z', TensorProto.INT32, (3, 4, 2))],
-            data_prop=True)
-
     def test_expand_dynamic_shape(self) -> None:
         graph = self._make_graph(
             [('x', TensorProto.INT32, (1, 2, None)),
@@ -2259,16 +2247,6 @@ class TestShapeInference(unittest.TestCase):
             [])
         self._assert_inferred(graph,
             [make_tensor_value_info('y', TensorProto.UINT8, (None, None, None))])  # type: ignore
-
-    def test_constantofshape_with_symbolic_shape(self) -> None:
-        graph = self._make_graph(
-            [('x', TensorProto.FLOAT, (3, 4, 5))],
-            [make_node("Shape", ['x'], ['shape']),
-             make_node("ConstantOfShape", ['shape'], ['y'], value=make_tensor('value', TensorProto.INT32, (1, ), (2, )))],
-            [])
-        self._assert_inferred(graph,
-            [make_tensor_value_info('shape', TensorProto.INT64, (3,)),
-             make_tensor_value_info('y', TensorProto.INT32, (3, 4, 5))], data_prop=True)  # type: ignore
 
     def test_constantofshape_without_input_shape_scalar(self) -> None:
         graph = self._make_graph([('shape', TensorProto.INT64, (0, ))],
