09183524edc237ecce9be1ed4b0a009ea2f00e61
Justin Chu <justinchu@microsoft.com>
2022-06-21 09:38:45 -0700

    Update style
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- tools/style.sh
+++ tools/style.sh
@@ -3,20 +3,26 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-set -o errexit
+set +o errexit
 set -o nounset
 
 
 cd "$(git rev-parse --show-toplevel)"
 
-echo -e "===> check flake8"
+err=0
+trap 'err=1' ERR
+
+echo -e "\n::group:: Check flake8..."
 flake8 onnx tools workflow_scripts
+echo -e "::endgroup::"
 
-echo -e "\n===> check mypy"
+echo -e "\n::group:: Check mypy"
 mypy . --no-site-packages
+echo -e "::endgroup::"
 
-echo -e "\n===> run clang-format"
+echo -e "\n::group:: Run clang-format"
 git ls-files --exclude-standard -- '*/*.cc' '*/*.h' | \
     xargs ${CLANG_FORMAT_BIN:-clang-format} -i
+echo -e "::endgroup::"
 
-git diff --exit-code
+test $err = 0 # Return non-zero if any command failed
