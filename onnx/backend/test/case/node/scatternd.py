7731826366e4b33ecc99ecc6440efa05c537897c
Philip Lassen <philiplassen+git@gmail.com>
2022-06-21 17:16:39 -0700

    Add scatterND min and max test cases
    Signed-off-by: Philip Lassen <philiplassen+git@gmail.com>

--- onnx/backend/test/case/node/scatternd.py
+++ onnx/backend/test/case/node/scatternd.py
@@ -25,7 +25,7 @@ def scatter_nd_impl(data, indices, updates, reduction="none"):  # type: ignore
         elif reduction == 'max':
             output[indices[i]] = np.max(output[indices[i]], updates[i])
         elif reduction == 'min':
-            output[indices[i]] = np.max(output[indices[i]], updates[i])
+            output[indices[i]] = np.min(output[indices[i]], updates[i])
         else:
             output[indices[i]] = updates[i]
     return output
@@ -144,3 +144,55 @@ class ScatterND(Base):
             outputs=[output],
             name="test_scatternd_multiply",
         )
+
+    @staticmethod
+    def export_scatternd_max() -> None:
+        node = onnx.helper.make_node(
+            'ScatterND',
+            inputs=['data', 'indices', 'updates'],
+            outputs=['y'],
+            reduction='max',
+        )
+        data = np.array(
+            [[[1, 2, 3, 4], [5, 6, 7, 8], [8, 7, 6, 5], [4, 3, 2, 1]],
+                [[1, 2, 3, 4], [5, 6, 7, 8], [8, 7, 6, 5], [4, 3, 2, 1]],
+                [[8, 7, 6, 5], [4, 3, 2, 1], [1, 2, 3, 4], [5, 6, 7, 8]],
+                [[8, 7, 6, 5], [4, 3, 2, 1], [1, 2, 3, 4], [5, 6, 7, 8]]], dtype=np.float32)
+        indices = np.array([[0], [0]], dtype=np.int64)
+        updates = np.array(
+            [[[5, 5, 5, 5], [6, 6, 6, 6], [7, 7, 7, 7], [8, 8, 8, 8]],
+                [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]], dtype=np.float32)
+        # Expecting output as np.array(
+        #    [[[5, 5, 5, 5], [6, 6, 7, 8], [8, 7, 7, 7], [8, 8 ,8, 8]],
+        #     [[1, 2, 3, 4], [5, 6, 7, 8], [8, 7, 6, 5], [4, 3, 2, 1]],
+        #     [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]],
+        #     [[8, 7, 6, 5], [4, 3, 2, 1], [1, 2, 3, 4], [5, 6, 7, 8]]], dtype=np.float32)
+        output = scatter_nd_impl(data, indices, updates, reduction='max')
+        expect(node, inputs=[data, indices, updates], outputs=[output],
+               name='test_scatternd_max')
+
+    @staticmethod
+    def export_scatternd_min() -> None:
+        node = onnx.helper.make_node(
+            'ScatterND',
+            inputs=['data', 'indices', 'updates'],
+            outputs=['y'],
+            reduction='min',
+        )
+        data = np.array(
+            [[[1, 2, 3, 4], [5, 6, 7, 8], [8, 7, 6, 5], [4, 3, 2, 1]],
+                [[1, 2, 3, 4], [5, 6, 7, 8], [8, 7, 6, 5], [4, 3, 2, 1]],
+                [[8, 7, 6, 5], [4, 3, 2, 1], [1, 2, 3, 4], [5, 6, 7, 8]],
+                [[8, 7, 6, 5], [4, 3, 2, 1], [1, 2, 3, 4], [5, 6, 7, 8]]], dtype=np.float32)
+        indices = np.array([[0], [0]], dtype=np.int64)
+        updates = np.array(
+            [[[5, 5, 5, 5], [6, 6, 6, 6], [7, 7, 7, 7], [8, 8, 8, 8]],
+                [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]], dtype=np.float32)
+        # Expecting output as np.array(
+        #    [[[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 3, 2, 1]],
+        #     [[1, 2, 3, 4], [5, 6, 7, 8], [8, 7, 6, 5], [4, 3, 2, 1]],
+        #     [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]],
+        #     [[8, 7, 6, 5], [4, 3, 2, 1], [1, 2, 3, 4], [5, 6, 7, 8]]], dtype=np.float32)
+        output = scatter_nd_impl(data, indices, updates, reduction='min')
+        expect(node, inputs=[data, indices, updates], outputs=[output],
+               name='test_scatternd_min')
