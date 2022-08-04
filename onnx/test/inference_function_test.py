7f60f647ac580b059b4c85517e63cb7362c3c9da
KubinGH <kbachurski@gmail.com>
2022-08-04 12:25:35 +0200

    Use unittest
    Signed-off-by: KubinGH <kbachurski@gmail.com>

--- onnx/test/inference_function_test.py
+++ onnx/test/inference_function_test.py
@@ -1,6 +1,5 @@
+import unittest
 from typing import Iterable, Optional, List
-
-import pytest
 
 import onnx
 import onnx.shape_inference
@@ -29,32 +28,30 @@ def call_infer_types(
     return [onnx.TypeProto.FromString(output) for output in outputs]
 
 
-@pytest.fixture
-def add_schema():
-    return max(
-        (s for s in onnx.defs.get_all_schemas_with_history() if s.name == "Add"),
-        key=lambda s: s.since_version
-    )
+ADD_SCHEMA = max(
+    (s for s in onnx.defs.get_all_schemas_with_history() if s.name == "Add"),
+    key=lambda s: s.since_version
+)
 
 
-def test_add_inference(add_schema):
-    assert call_infer_types(add_schema, [
-        onnx.helper.make_tensor_type_proto(1, ()),
-        onnx.helper.make_tensor_type_proto(1, ())
-    ], 1) == [onnx.helper.make_tensor_type_proto(1, ())]
-    assert call_infer_types(add_schema, [
-        onnx.helper.make_tensor_type_proto(1, (None, 2)),
-        onnx.helper.make_tensor_type_proto(1, (2,))
-    ], 1) == [onnx.helper.make_tensor_type_proto(1, (None, 2))]
-    assert call_infer_types(add_schema, [
-        onnx.helper.make_tensor_type_proto(1, (None, 2)),
-        onnx.helper.make_tensor_type_proto(1, (1, 2))
-    ], 1) == [onnx.helper.make_tensor_type_proto(1, (None, 2))]
+class TestInferenceFunctionCall(unittest.TestCase):
+    def test_add_inference(self) -> None:
+        assert call_infer_types(ADD_SCHEMA, [
+            onnx.helper.make_tensor_type_proto(1, ()),
+            onnx.helper.make_tensor_type_proto(1, ())
+        ], 1) == [onnx.helper.make_tensor_type_proto(1, ())]
+        assert call_infer_types(ADD_SCHEMA, [
+            onnx.helper.make_tensor_type_proto(1, (None, 2)),
+            onnx.helper.make_tensor_type_proto(1, (2,))
+        ], 1) == [onnx.helper.make_tensor_type_proto(1, (None, 2))]
+        assert call_infer_types(ADD_SCHEMA, [
+            onnx.helper.make_tensor_type_proto(1, (None, 2)),
+            onnx.helper.make_tensor_type_proto(1, (1, 2))
+        ], 1) == [onnx.helper.make_tensor_type_proto(1, (None, 2))]
 
-
-def test_add_inference_raises_errors(add_schema):
-    with pytest.raises(onnx.shape_inference.InferenceError):
-        call_infer_types(add_schema, [
-            onnx.helper.make_tensor_type_proto(1, (2,)),
-            onnx.helper.make_tensor_type_proto(1, (3,))
-        ], 1)
+    def test_add_inference_raises_errors(self) -> None:
+        with self.assertRaises(onnx.shape_inference.InferenceError):
+            call_infer_types(ADD_SCHEMA, [
+                onnx.helper.make_tensor_type_proto(1, (2,)),
+                onnx.helper.make_tensor_type_proto(1, (3,))
+            ], 1)
