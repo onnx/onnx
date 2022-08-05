43ee368f62604e409398b84778b317ef7796deef
Chun-Wei Chen <jacky82226@gmail.com>
2022-08-04 17:20:53 -0700

    Add an assert to make sure the quantity is reasonable for vsnprintf
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/common/assertions.cc
+++ onnx/common/assertions.cc
@@ -5,21 +5,24 @@
 // ATTENTION: The code in this file is highly EXPERIMENTAL.
 // Adventurous users should note that the APIs will probably change.
 
-#include "onnx/common/assertions.h"
+#include <assert.h>
 #include <cstdarg>
 #include <cstdio>
+#include <cstring>
+#include "onnx/common/assertions.h"
 #include "onnx/common/common.h"
+#define BUFFER_MAX_SIZE 2048
 
 namespace ONNX_NAMESPACE {
 
 std::string barf(const char* fmt, ...) {
-  char msg[2048];
+  char msg[BUFFER_MAX_SIZE];
   va_list args;
-
+  // To resolve potential vulnerability issue for vsnprintf, add an assert to make sure the quantity is reasonable.
+  assert(strlen(fmt) <= BUFFER_MAX_SIZE && "The string length for vsnprintf is larger than the buffer size 2048.");
   va_start(args, fmt);
-  vsnprintf(msg, 2048, fmt, args);
+  vsnprintf(msg, BUFFER_MAX_SIZE, fmt, args);
   va_end(args);
-
   return std::string(msg);
 }
 
