7b6c1e3935c400bf794832d567d8e7ba65ed0014
Chun-Wei Chen <jacky82226@gmail.com>
2022-08-24 07:23:09 -0700

    fix flake8
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- setup.py
+++ setup.py
@@ -305,15 +305,18 @@ ext_modules = [setuptools.Extension(name="onnx.onnx_cpp2py_export", sources=[])]
 # Packages
 ################################################################################
 
-include_dirs = ["onnx.backend.test.data.*",
- "onnx.backend.test.cpp*",
- "onnx.common",
- "onnx.defs.*",
- "onnx.examples*",
- "onnx.shape_inference",
- "onnx.test.cpp",
- "onnx.version_converter*",
+# Add package directories here if you want to package them with the source
+include_dirs = [
+    "onnx.backend.test.cpp*",
+    "onnx.backend.test.data.*",
+    "onnx.common",
+    "onnx.defs.*",
+    "onnx.examples*",
+    "onnx.shape_inference",
+    "onnx.test.cpp",
+    "onnx.version_converter*",
 ]
+
 packages = list(set(setuptools.find_packages() + setuptools.find_namespace_packages(include=include_dirs)))
 
 requirements_file = "requirements.txt"
