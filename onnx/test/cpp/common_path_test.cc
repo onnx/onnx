ccc83a245dee472a9fe46d9cb2de78dba38354ec
Chun-Wei Chen <jacky82226@gmail.com>
2022-08-31 11:08:57 -0700

    add tests for wstring
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/test/cpp/common_path_test.cc
+++ onnx/test/cpp/common_path_test.cc
@@ -13,57 +13,72 @@ using namespace ONNX_NAMESPACE;
 namespace ONNX_NAMESPACE {
 namespace Test {
 namespace {
-std::string fix_sep(std::string path) {
-  std::string out = path;
+template <typename STRING>
+STRING fix_sep(const STRING& path) {
+  STRING out = path;
   normalize_separator(out);
   return out;
+}
+
+template <typename STRING>
+void expect(const STRING& actual, const STRING& expected, const std::string& error_message) {
+  // Test clean_relative_path and normalize_separator
+  EXPECT_EQ(clean_relative_path(actual), fix_sep(expected)) << error_message;
+}
+void expect_string_and_wstring(const std::string& actual, const std::string& expected) {
+  // Test string
+  expect(actual, expected, "string path mismatch.");
+#ifdef _WIN32
+  // Test wstring with utf8str_to_wstring
+  expect(utf8str_to_wstring(actual), utf8str_to_wstring(expected), "wstring path mismatch.");
+#endif _WIN32
 }
 } // namespace
 
 TEST(PathTest, CleanRelativePathTest) {
   // Already normal.
-  EXPECT_EQ(clean_relative_path("abc"), fix_sep("abc"));
-  EXPECT_EQ(clean_relative_path("abc/def"), fix_sep("abc/def"));
-  EXPECT_EQ(clean_relative_path("a/b/c"), fix_sep("a/b/c"));
-  EXPECT_EQ(clean_relative_path("."), fix_sep("."));
-  EXPECT_EQ(clean_relative_path(".."), fix_sep(".."));
-  EXPECT_EQ(clean_relative_path("../.."), fix_sep("../.."));
-  EXPECT_EQ(clean_relative_path("../../abc"), fix_sep("../../abc"));
+  expect_string_and_wstring("abc", "abc");
+  expect_string_and_wstring("abc/def", "abc/def");
+  expect_string_and_wstring("a/b/c", "a/b/c");
+  expect_string_and_wstring(".", ".");
+  expect_string_and_wstring("..", "..");
+  expect_string_and_wstring("../..", "../..");
+  expect_string_and_wstring("../../abc", "../../abc");
   // Remove leading slash
-  EXPECT_EQ(clean_relative_path("/abc"), fix_sep("abc"));
-  EXPECT_EQ(clean_relative_path("/"), fix_sep("."));
+  expect_string_and_wstring("/abc", "abc");
+  expect_string_and_wstring("/", ".");
   // Remove trailing slash
-  EXPECT_EQ(clean_relative_path("abc/"), fix_sep("abc"));
-  EXPECT_EQ(clean_relative_path("abc/def/"), fix_sep("abc/def"));
-  EXPECT_EQ(clean_relative_path("a/b/c/"), fix_sep("a/b/c"));
-  EXPECT_EQ(clean_relative_path("./"), fix_sep("."));
-  EXPECT_EQ(clean_relative_path("../"), fix_sep(".."));
-  EXPECT_EQ(clean_relative_path("../../"), fix_sep("../.."));
-  EXPECT_EQ(clean_relative_path("/abc/"), fix_sep("abc"));
+  expect_string_and_wstring("abc/", "abc");
+  expect_string_and_wstring("abc/def/", "abc/def");
+  expect_string_and_wstring("a/b/c/", "a/b/c");
+  expect_string_and_wstring("./", ".");
+  expect_string_and_wstring("../", "..");
+  expect_string_and_wstring("../../", "../..");
+  expect_string_and_wstring("/abc/", "abc");
   // Remove doubled slash
-  EXPECT_EQ(clean_relative_path("abc//def//ghi"), fix_sep("abc/def/ghi"));
-  EXPECT_EQ(clean_relative_path("//abc"), fix_sep("abc"));
-  EXPECT_EQ(clean_relative_path("///abc"), fix_sep("abc"));
-  EXPECT_EQ(clean_relative_path("//abc//"), fix_sep("abc"));
-  EXPECT_EQ(clean_relative_path("abc//"), fix_sep("abc"));
+  expect_string_and_wstring("abc//def//ghi", "abc/def/ghi");
+  expect_string_and_wstring("//abc", "abc");
+  expect_string_and_wstring("///abc", "abc");
+  expect_string_and_wstring("//abc//", "abc");
+  expect_string_and_wstring("abc//", "abc");
   // Remove . elements
-  EXPECT_EQ(clean_relative_path("abc/./def"), fix_sep("abc/def"));
-  EXPECT_EQ(clean_relative_path("/./abc/def"), fix_sep("abc/def"));
-  EXPECT_EQ(clean_relative_path("abc/."), fix_sep("abc"));
+  expect_string_and_wstring("abc/./def", "abc/def");
+  expect_string_and_wstring("/./abc/def", "abc/def");
+  expect_string_and_wstring("abc/.", "abc");
   // Remove .. elements
-  EXPECT_EQ(clean_relative_path("abc/def/ghi/../jkl"), fix_sep("abc/def/jkl"));
-  EXPECT_EQ(clean_relative_path("abc/def/../ghi/../jkl"), fix_sep("abc/jkl"));
-  EXPECT_EQ(clean_relative_path("abc/def/.."), fix_sep("abc"));
-  EXPECT_EQ(clean_relative_path("abc/def/../.."), fix_sep("."));
-  EXPECT_EQ(clean_relative_path("/abc/def/../.."), fix_sep("."));
-  EXPECT_EQ(clean_relative_path("abc/def/../../.."), fix_sep(".."));
-  EXPECT_EQ(clean_relative_path("/abc/def/../../.."), fix_sep(".."));
-  EXPECT_EQ(clean_relative_path("abc/def/../../../ghi/jkl/../../../mno"), fix_sep("../../mno"));
-  EXPECT_EQ(clean_relative_path("/../abc"), fix_sep("../abc"));
+  expect_string_and_wstring("abc/def/ghi/../jkl", "abc/def/jkl");
+  expect_string_and_wstring("abc/def/../ghi/../jkl", "abc/jkl");
+  expect_string_and_wstring("abc/def/..", "abc");
+  expect_string_and_wstring("abc/def/../..", ".");
+  expect_string_and_wstring("/abc/def/../..", ".");
+  expect_string_and_wstring("abc/def/../../..", "..");
+  expect_string_and_wstring("/abc/def/../../..", "..");
+  expect_string_and_wstring("abc/def/../../../ghi/jkl/../../../mno", "../../mno");
+  expect_string_and_wstring("/../abc", "../abc");
   // Combinations
-  EXPECT_EQ(clean_relative_path("abc/./../def"), fix_sep("def"));
-  EXPECT_EQ(clean_relative_path("abc//./../def"), fix_sep("def"));
-  EXPECT_EQ(clean_relative_path("abc/../../././../def"), fix_sep("../../def"));
+  expect_string_and_wstring("abc/./../def", "def");
+  expect_string_and_wstring("abc//./../def", "def");
+  expect_string_and_wstring("abc/../../././../def", "../../def");
 }
 
 } // namespace Test
