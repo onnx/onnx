acd69edb29dd6b44bbfe211d9948b75375795502
xadupre <xadupre@microsoft.com>
2022-09-03 23:37:33 +0200

    remove scipy
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- onnx/runtime/aionnx/_op_numpy_helper.py
+++ onnx/runtime/aionnx/_op_numpy_helper.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import numpy as np  # type: ignore
-from scipy.sparse import coo_matrix  # type: ignore
 
 
 def numpy_matmul(a: np.ndarray, b: np.ndarray) -> np.ndarray:
@@ -10,8 +9,6 @@ def numpy_matmul(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     Handles sparse matrices.
     """
     try:
-        if isinstance(a, coo_matrix) or isinstance(b, coo_matrix):
-            return np.dot(a, b)
         if len(a.shape) <= 2 and len(b.shape) <= 2:
             return np.dot(a, b)
         return np.matmul(a, b)
