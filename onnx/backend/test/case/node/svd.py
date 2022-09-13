a9254f0b92333b087ee973f64300148d932680c6
Will Berman <WLBberman@gmail.com>
2022-09-13 10:32:10 -0700

    use same dict for node and numpy function
    Signed-off-by: Will Berman <WLBberman@gmail.com>

--- onnx/backend/test/case/node/svd.py
+++ onnx/backend/test/case/node/svd.py
@@ -20,35 +20,34 @@ class Svd(Base):
                 for (dims, dims_name) in dimensions:
                     A = np.random.randn(*dims)
 
-                    node_args: Any = {"inputs": ["A"]}
-                    numpy_args = {}
+                    args: Any = {}
                     name = f"test_svd_{dims_name}"
 
                     if full_matrices is not None:
-                        node_args["full_matrices"] = full_matrices
-                        numpy_args["full_matrices"] = bool(full_matrices)
+                        args["full_matrices"] = full_matrices
                         if full_matrices:
                             name += "_full_manually_set"
                         else:
                             name += "_partial"
 
                     if compute_uv is not None:
-                        node_args["compute_uv"] = compute_uv
-                        numpy_args["compute_uv"] = bool(compute_uv)
+                        args["compute_uv"] = compute_uv
                         if compute_uv:
                             name += "_compute_uv_manually_set"
                         else:
                             name += "_compute_only_singular_vals"
 
                     if compute_uv == 0:
-                        S = np.linalg.svd(A, **numpy_args)
+                        S = np.linalg.svd(A, **args)
                         outputs = [S]
-                        node_args["outputs"] = ["S"]
+                        output_args = ["S"]
                     else:
-                        U, S, Vh = np.linalg.svd(A, **numpy_args)
+                        U, S, Vh = np.linalg.svd(A, **args)
                         # Flip output ordering to account for ONNX putting S first
                         outputs = [S, U, Vh]
-                        node_args["outputs"] = ["S", "U", "Vh"]
+                        output_args = ["S", "U", "Vh"]
 
-                    node = onnx.helper.make_node("SVD", **node_args)
+                    node = onnx.helper.make_node(
+                        "SVD", inputs=["A"], outputs=output_args, **args
+                    )
                     expect(node, inputs=[A], outputs=outputs, name=name)
