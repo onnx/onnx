5b827920f37298c0ad34c72d12d96e06f6256633
Chun-Wei Chen <jacky82226@gmail.com>
2022-09-25 17:20:16 -0700

    3.20.2
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- workflow_scripts/protobuf/build_protobuf_win.ps1
+++ workflow_scripts/protobuf/build_protobuf_win.ps1
@@ -6,13 +6,13 @@ param(
 )
 
 echo "Build protobuf from source on Windows."
-Invoke-WebRequest -Uri https://github.com/protocolbuffers/protobuf/releases/download/v3.19.5/protobuf-cpp-3.19.5.tar.gz -OutFile protobuf.tar.gz -Verbose
+Invoke-WebRequest -Uri https://github.com/protocolbuffers/protobuf/releases/download/v3.20.2/protobuf-cpp-3.20.2.tar.gz -OutFile protobuf.tar.gz -Verbose
 tar -xvf protobuf.tar.gz
-cd protobuf-3.19.5
+cd protobuf-3.20.2
 $protobuf_root_dir = Get-Location
 mkdir protobuf_install
 cd cmake
-cmake -G "Visual Studio 17 2022" -A $arch -DCMAKE_INSTALL_PREFIX="../protobuf_install" -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -Dprotobuf_BUILD_SHARED_LIBS=OFF -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_BUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=Release .
+cmake -G "Visual Studio 17 2022" -A $arch -DCMAKE_INSTALL_PREFIX="../protobuf_install" -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -Dprotobuf_BUILD_SHARED_LIBS=OFF -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_BUILD_EXAMPLES=OFF .
 msbuild protobuf.sln /m /p:Configuration=Release
 msbuild INSTALL.vcxproj /p:Configuration=Release
 echo "Protobuf installation complete."
