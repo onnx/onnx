e23b9d0e648958020063dba43222f1b332e16790
shubhambhokare1 <shubhambhokare@gmail.com>
2022-11-14 23:26:17 +0000

    Adding map model
    Signed-off-by: shubhambhokare1 <shubhambhokare@gmail.com>

--- onnx/backend/test/case/model/map.py
+++ onnx/backend/test/case/model/map.py
@@ -54,8 +54,8 @@ def MapDeletePairImpl(
 def MapHasKeyImpl(
     mapping: Dict[Union[int, str], np.ndarray],
     key: Union[int, str],
-) -> bool: 
-    return key in list(mapping.keys())
+) -> np.ndarray: 
+    return np.array(key in list(mapping.keys()))
 
 def MapGetValueImpl(
     mapping: Dict[Union[int, str], np.ndarray],
@@ -96,9 +96,11 @@ class Map(Base):
             return graph
 
         # 1st testcase - create map, insert pair and delete pair
+        # After deletion, check if that key exists in the map
         # 1. MapConstruct:                -> {k1: v1, k2: v2}
         # 2. MapInsertPair((k3, v3)):     -> {k1: v1, k2: v2, k3: v3} 
         # 3. MapDeletePair(k2):           -> {k1: v3, k2: v3}
+        # 4. MapHasKey(k2)                -> False
         seq_construct_node = onnx.helper.make_node(
             "SequenceConstruct", ["V1", "V2"], ["Values"]
         )
@@ -109,13 +111,16 @@ class Map(Base):
             "MapInsertPair", ["Map_1", "K3", "V3"], ["Map_2"]
         )
         map_delete_pair_node = onnx.helper.make_node(
-            "MapDeletePair", ["Map_2", "K2"], ["out"]
+            "MapDeletePair", ["Map_2", "K2"], ["Map_3"]
+        )
+        map_has_key_node = onnx.helper.make_node(
+            "MapHasKey", ["Map_3", "K2"], ["out"]
         )
 
         x_shape = [2, 3, 4]
         y_shape = [1, 3, 4]
         z_shape = [3, 3, 4]
-        out_shape = [None, 3, 4]
+        out_shape = [1,]
 
         x = np.ones(x_shape, dtype=np.float32)
         y = np.zeros(y_shape, dtype=np.float32)
@@ -126,8 +131,10 @@ class Map(Base):
 
         out = SequenceConstructImpl(x, y)
         out = MapConstructImpl(keys=keys, values=out)
-        out = MapInsertPairImpl(out, 30, z)
-        out = MapDeletePairImpl(out, 20)
+        out = MapInsertPairImpl(out, key_insert, z)
+        out = MapDeletePairImpl(out, key_delete)
+        out = MapHasKeyImpl(out, key_delete)
+        assert np.array_equal(out, np.array(False))
 
         key_insert = onnx.helper.make_tensor("K3", TensorProto.INT32, (), (key_insert,))
         key_delete = onnx.helper.make_tensor("K2", TensorProto.INT32, (), (key_delete,))
@@ -138,18 +145,18 @@ class Map(Base):
                 map_construct_node,
                 map_insert_pair_node,
                 map_delete_pair_node,
+                map_has_key_node,
             ],
             [x_shape, y_shape, [2,], [], z_shape, []],  # type: ignore
             [out_shape],  # type: ignore
             ["V1", "V2", "Keys", "K3", "V3", "K2"],
             ["out"],
             [onnx.TensorProto.FLOAT] * 2 + [onnx.TensorProto.INT32] * 2 + [onnx.TensorProto.INT64, onnx.TensorProto.INT64],  # type: ignore
-            [onnx.TensorProto.FLOAT],
+            [onnx.TensorProto.BOOL],
             [key_insert, key_delete],
         )
-        #model = onnx.helper.make_model_gen_version(
-        #    graph,
-        #    producer_name="backend-test",
-        #    opset_imports=[onnx.helper.make_opsetid("", 18)],
-        #)
-        #expect(model, inputs=[x, y, z], outputs=[out], name="test_map_model1")
+        model = onnx.helper.make_model_gen_version(
+            graph,
+            producer_name="backend-test",
+        )
+        expect(model, inputs=[x, y, z], outputs=[out], name="test_map_model1")
