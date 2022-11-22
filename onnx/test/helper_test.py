3fd41d249bb8006935aa0031a332dd945e61b7e5
Przemyslaw Wysocki <przemyslaw.wysocki@intel.com>
2022-11-22 20:45:09 +0100

    Bump version (#4666)
    Signed-off-by: p-wysocki <przemyslaw.wysocki@intel.com>
    
    Signed-off-by: p-wysocki <przemyslaw.wysocki@intel.com>
    Co-authored-by: G. Ramalingam <grama@microsoft.com>

--- onnx/test/helper_test.py
+++ onnx/test/helper_test.py
@@ -386,6 +386,7 @@ class TestHelperNodeFunctions(unittest.TestCase):
         test([("", 15)], 8)
         test([("", 16)], 8)
         test([("", 17)], 8)
+        test([("", 18)], 8)
         # standard opset can be referred to using empty-string or "ai.onnx"
         test([("ai.onnx", 9)], 4)
         test([("ai.onnx.ml", 2)], 6)
