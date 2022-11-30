9b7bca2a723ff94edcd007d93b5d0cf1838591dc
Liqun Fu <liqfu@microsoft.com>
2022-11-29 17:00:36 -0800

    fix shape infer of ArrayFeatureExtractor
    Signed-off-by: Liqun Fu <liqfu@microsoft.com>

--- onnx/defs/traditionalml/defs.cc
+++ onnx/defs/traditionalml/defs.cc
@@ -28,21 +28,43 @@ ONNX_ML_OPERATOR_SET_SCHEMA(
           const auto input_ndim = input_shape.dim_size();
 
           auto output_shape = ctx.getOutputType(0)->mutable_tensor_type()->mutable_shape();
-          // This operator only applies to the last dimension; thus -1
-          for (int i = 0; i < input_ndim - 1; ++i) {
-            *output_shape->add_dim() = input_shape.dim(i);
+          if (input_ndim == 1) {
+            output_shape->add_dim()->set_dim_value(1);
+          } else {
+            // This operator only applies to the last dimension; thus -1
+            for (int i = 0; i < input_ndim - 1; ++i) {
+              *output_shape->add_dim() = input_shape.dim(i);
+            }
           }
-          // The length of second input is the length of the last dimension of the output
+          // value of the output's last dimension is the total amount of indices
+          // set Unknown length for the last dimension if it cannot be calculated
+          auto last_dim = output_shape->add_dim();
           if (hasInputShape(ctx, 1)) {
             const auto& indices_shape = getInputShape(ctx, 1);
             if (indices_shape.dim_size() > 0) {
-              auto dim = indices_shape.dim(0);
-              *output_shape->add_dim() = dim;
-              return;
+              int64_t num_indices = 1;
+              std::string single_symbolic_dim;
+              for (int i = 0; i < indices_shape.dim_size(); i++) {
+                if (indices_shape.dim(i).has_dim_value()) {
+                  num_indices *= indices_shape.dim(i).dim_value();
+                } else if (indices_shape.dim(i).has_dim_param()) {
+                  if (single_symbolic_dim.empty()) {
+                    // it is possible to set symbolic dimension param if the rest dim values are all value 1
+                    single_symbolic_dim = indices_shape.dim(i).dim_param();
+                  } else {
+                    return;
+                  }
+                } else {
+                  return;
+                }
+              }
+              if (single_symbolic_dim.empty()) {
+                last_dim->set_dim_value(num_indices);
+              } else if (num_indices == 1) {
+                last_dim->set_dim_param(single_symbolic_dim);
+              }
             }
           }
-          // Unknown length of the last dimension
-          output_shape->add_dim();
         })
         .TypeConstraint(
             "T",
