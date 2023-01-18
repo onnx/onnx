2d51e8481bff24734d81e370fb533129762643db
Xavier Dupré <xadupre@users.noreply.github.com>
2023-01-18 23:15:38 +0100

    Add missing class Split_18 in reference implementation (#4743)
    * Add missing class Split_18 in reference implementation
    
    Signed-off-by: xadupre <xadupre@microsoft.com>
    
    * remove unused variable
    
    Signed-off-by: xadupre <xadupre@microsoft.com>
    
    * add one more test
    
    Signed-off-by: xadupre <xadupre@microsoft.com>
    
    Signed-off-by: xadupre <xadupre@microsoft.com>
    Co-authored-by: G. Ramalingam <grama@microsoft.com>

--- onnx/test/reference_evaluator_test.py
+++ onnx/test/reference_evaluator_test.py
@@ -2213,6 +2213,96 @@ class TestReferenceEvaluator(unittest.TestCase):
         got1 = ref1.run(None, feeds)
         assert_allclose(expected, got1[0])
 
+    def test_split(self):
+        X = make_tensor_value_info("X", TensorProto.FLOAT, [None])
+        Y1 = make_tensor_value_info("Y1", TensorProto.FLOAT, [None])
+        Y2 = make_tensor_value_info("Y2", TensorProto.FLOAT, [None])
+        Y3 = make_tensor_value_info("Y3", TensorProto.FLOAT, [None])
+        Y4 = make_tensor_value_info("Y4", TensorProto.FLOAT, [None])
+
+        node = make_node("Split", ["X"], ["Y1", "Y2", "Y3", "Y4"], num_outputs=4)
+        graph = make_graph([node], "g", [X], [Y1, Y2, Y3, Y4])
+        onnx_model = make_model(graph, opset_imports=[make_opsetid("", 18)])
+        feeds = {"X": np.arange(10).astype(np.float32)}
+
+        expected = [
+            np.array([0, 1, 2], dtype=np.float32),
+            np.array([3, 4, 5], dtype=np.float32),
+            np.array([6, 7, 8], dtype=np.float32),
+            np.array([9], dtype=np.float32),
+        ]
+
+        ref1 = ReferenceEvaluator(onnx_model)
+        got1 = ref1.run(None, feeds)
+        for i in range(4):
+            assert_allclose(expected[i], got1[i])
+
+    def test_split_2(self):
+        X = make_tensor_value_info("X", TensorProto.FLOAT, [None])
+        Y1 = make_tensor_value_info("Y1", TensorProto.FLOAT, [None])
+        Y2 = make_tensor_value_info("Y2", TensorProto.FLOAT, [None])
+        Y3 = make_tensor_value_info("Y3", TensorProto.FLOAT, [None])
+        Y4 = make_tensor_value_info("Y4", TensorProto.FLOAT, [None])
+
+        node = make_node("Split", ["X", "split"], ["Y1", "Y2", "Y3", "Y4"])
+        graph = make_graph([node], "g", [X], [Y1, Y2, Y3, Y4])
+        onnx_model = make_model(graph, opset_imports=[make_opsetid("", 18)])
+        feeds = {
+            "X": np.arange(10).astype(np.float32),
+            "split": np.array([3, 3, 2, 2], dtype=np.int64),
+        }
+
+        expected = [
+            np.array([0, 1, 2], dtype=np.float32),
+            np.array([3, 4, 5], dtype=np.float32),
+            np.array([6, 7], dtype=np.float32),
+            np.array([8, 9], dtype=np.float32),
+        ]
+
+        ref1 = ReferenceEvaluator(onnx_model)
+        got1 = ref1.run(None, feeds)
+        for i in range(4):
+            assert_allclose(expected[i], got1[i])
+
+    def test_split_num_outputs_4(self):
+        X = make_tensor_value_info("X", TensorProto.FLOAT, [None])
+        Y1 = make_tensor_value_info("Y1", TensorProto.FLOAT, [None])
+        Y2 = make_tensor_value_info("Y2", TensorProto.FLOAT, [None])
+        Y3 = make_tensor_value_info("Y3", TensorProto.FLOAT, [None])
+        Y4 = make_tensor_value_info("Y4", TensorProto.FLOAT, [None])
+
+        node = make_node("Split", ["X"], ["Y1", "Y2", "Y3", "Y4"], num_outputs=4)
+        graph = make_graph([node], "g", [X], [Y1, Y2, Y3, Y4])
+        onnx_model = make_model(graph, opset_imports=[make_opsetid("", 18)])
+
+        # case 1
+        feeds = {"X": np.arange(10).astype(np.float32)}
+        expected = [
+            np.array([0, 1, 2], dtype=np.float32),
+            np.array([3, 4, 5], dtype=np.float32),
+            np.array([6, 7, 8], dtype=np.float32),
+            np.array([9], dtype=np.float32),
+        ]
+
+        ref1 = ReferenceEvaluator(onnx_model)
+        got1 = ref1.run(None, feeds)
+        for i in range(4):
+            assert_allclose(expected[i], got1[i])
+
+        # case 2
+        feeds = {"X": np.arange(9).astype(np.float32)}
+        expected = [
+            np.array([0, 1, 2], dtype=np.float32),
+            np.array([3, 4, 5], dtype=np.float32),
+            np.array([6, 7, 8], dtype=np.float32),
+            np.array([], dtype=np.float32),
+        ]
+
+        ref1 = ReferenceEvaluator(onnx_model)
+        got1 = ref1.run(None, feeds)
+        for i in range(4):
+            assert_allclose(expected[i], got1[i])
+
     def test_argmin(self):
         X = make_tensor_value_info("X", TensorProto.FLOAT, [None, None])
         Y = make_tensor_value_info("Y", TensorProto.INT64, [None])
