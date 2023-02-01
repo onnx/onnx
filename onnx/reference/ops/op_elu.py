2174649b126423917ff67a70648398c861ca8b72
xadupre <xadupre@microsoft.com>
2023-02-01 17:53:28 +0100

    fix elu output type
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- onnx/reference/ops/op_elu.py
+++ onnx/reference/ops/op_elu.py
@@ -9,4 +9,4 @@ from ._op import OpRunUnaryNum
 class Elu(OpRunUnaryNum):
     def _run(self, x, alpha=None):  # type: ignore
         alpha = alpha or self.alpha  # type: ignore
-        return (np.where(x > 0, x, alpha * (np.exp(x) - 1)),)
+        return (np.where(x > 0, x, alpha * (np.exp(x) - 1)).astype(x.dtype),)
