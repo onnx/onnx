7b9dfee410969ff39af3bfa3f8e41b92db9720f2
Justin Chu <justinchu@microsoft.com>
2023-02-04 09:23:26 -0800

    try this
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,7 @@ import pathlib
 
 import nox
 
-DEFAULT_ENV_VARS = {
+ONNX_BUILD_ENV_VARS = {
     "CMAKE_ARGS": os.environ.get(
         "CMAKE_ARGS", "-DONNX_WERROR=ON -DONNX_USE_PROTOBUF_SHARED_LIBS=ON"
     ),
@@ -18,6 +18,7 @@ DEFAULT_ENV_VARS = {
     "DEBUG": os.environ.get("DEBUG", "0"),
     "ONNX_ML": os.environ.get("ONNX_ML", "0"),
 }
+
 
 PROTOBUFF_INSTALL_DIR = os.environ.get("PROTOBUFF_INSTALL_DIR", None)
 PROTOBUFF_BIN_PATHS: list[str] | None = (
@@ -30,12 +31,18 @@ PROTOBUFF_BIN_PATHS: list[str] | None = (
     else None
 )
 
+if PROTOBUFF_BIN_PATHS:
+    if "PATH" in os.environ:
+        ONNX_BUILD_ENV_VARS["PATH"] += ":" + ":".join(PROTOBUFF_BIN_PATHS)
+    else:
+        ONNX_BUILD_ENV_VARS["PATH"] = ":".join(PROTOBUFF_BIN_PATHS)
+
 
 @nox.session()
 def test_onnx(session: nox.Session):
     """Build and run ONNX python tests."""
     session.run("pip", "install", "-r", "requirements-release.txt", silent=True)
-    session.run("pip", "install", ".", env=DEFAULT_ENV_VARS)
+    session.run("pip", "install", ".", env=ONNX_BUILD_ENV_VARS)
     session.run("pytest", *session.posargs)
 
 
@@ -47,7 +54,7 @@ def test_cpp(session: nox.Session):
         "python",
         "setup.py",
         "install",
-        env={**DEFAULT_ENV_VARS, "ONNX_BUILD_TESTS": "1"},
+        env={**ONNX_BUILD_ENV_VARS, "ONNX_BUILD_TESTS": "1"},
         silent=True,
     )
     session.run(
@@ -61,6 +68,6 @@ def test_cpp(session: nox.Session):
 def test_backend_test_generation(session: nox.Session):
     """Test backend test data."""
     session.run("pip", "install", "-r", "requirements-release.txt", silent=True)
-    session.run("pip", "install", ".", env=DEFAULT_ENV_VARS)
+    session.run("pip", "install", ".", env=ONNX_BUILD_ENV_VARS)
     session.run("python", "onnx/backend/test/cmd_tools.py", "generate-data", "--clean")
     session.run("bash", "tools/check_generated_backend_test_data.sh", external=True)
