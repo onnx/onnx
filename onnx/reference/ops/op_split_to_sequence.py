35ee2c24523b32c4e2435cdc8fb43e537b193aad
xadupre <xadupre@microsoft.com>
2023-02-06 09:32:57 +0100

    lint
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- onnx/reference/ops/op_split_to_sequence.py
+++ onnx/reference/ops/op_split_to_sequence.py
@@ -13,21 +13,23 @@ class SplitToSequence(OpRun):
         self, mat: np.ndarray, split: np.ndarray, axis: int
     ) -> List[np.ndarray]:
         if split is None:
-            split = [1 for _ in range(mat.shape[axis])]
+            split_length = [1 for _ in range(mat.shape[axis])]
         elif len(split.shape) == 0:
             # A scalar
             dim = mat.shape[axis]
             length = int(split)
             n = dim // int(length)
-            split = [length] * n
+            split_length = [length] * n
             left = dim - length * n
             if left > 0:
-                split.append(left)
+                split_length.append(left)
+        else:
+            split_length = list(split)
 
         sli = [slice(0, s) for s in mat.shape]
         res = []
         pos = 0
-        for spl in split:
+        for spl in split_length:
             sli[axis] = slice(pos, pos + spl)  # type: ignore
             pos += spl
             res.append(mat[tuple(sli)])
