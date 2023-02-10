9b41c88cf896ef62a016bf58c66b4b3a2cd25654
xadupre <xadupre@microsoft.com>
2023-02-10 15:34:36 +0100

    add example for array feature extractor
    Signed-off-by: xadupre <xadupre@microsoft.com>

---
+++ onnx/backend/test/case/node/aionnxml_array_feature_extractor.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: Apache-2.0
+
+import numpy as np
+
+import onnx
+
+from ..base import Base
+from . import expect
+
+
+class ArrayFeatureExtractor(Base):
+    @staticmethod
+    def export() -> None:
+        node = onnx.helper.make_node(
+            "ArrayFeatureExtractor",
+            inputs=["x", "y"],
+            outputs=["z"],
+            domain="ai.onnx.ml"
+        )
+
+        x = np.arange(12).reshape((3, 4)).astype(np.float32)
+        y = np.array([0, 1], dtype=np.int64)
+        z = np.array([[0, 4, 8]], dtype=np.float32).T
+        expect(node, inputs=[x, y], outputs=[z], name="test_array_feature_extractor")
+
