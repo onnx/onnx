4d824452a6f4f8aa66bb654b8958c85c4bb43925
Justin Chu <justinchu@microsoft.com>
2023-02-09 18:17:21 -0800

    naming fix
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- onnx/defs/__init__.py
+++ onnx/defs/__init__.py
@@ -2,7 +2,7 @@
 
 from typing import List
 
-import onnx.onnx_cpp2py_export.defs as C
+import onnx.onnx_cpp2py_export.defs as C  # noqa: N812
 from onnx import AttributeProto, FunctionProto
 
 ONNX_DOMAIN = ""
