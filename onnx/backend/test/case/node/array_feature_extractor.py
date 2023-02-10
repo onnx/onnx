c400d3790060f90c497509ff976aa68c997fc80c
xadupre <xadupre@microsoft.com>
2023-02-10 15:42:12 +0100

    update expected otuput
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- onnx/backend/test/case/node/array_feature_extractor.py
+++ onnx/backend/test/case/node/array_feature_extractor.py
@@ -20,5 +20,5 @@ class ArrayFeatureExtractor(Base):
 
         x = np.arange(12).reshape((3, 4)).astype(np.float32)
         y = np.array([0, 1], dtype=np.int64)
-        z = np.array([[0, 4, 8]], dtype=np.float32).T
+        z = np.array([[0, 4, 8], [1, 5, 9]], dtype=np.float32).T
         expect(node, inputs=[x, y], outputs=[z], name="test_array_feature_extractor")
