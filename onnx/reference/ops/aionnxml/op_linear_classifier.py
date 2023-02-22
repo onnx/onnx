10ac2aff27c99ab10560790fb6478a524032a64c
Justin Chu <justinchuby@users.noreply.github.com>
2023-02-22 15:54:29 -0800

    Disallow and fix relative imports (#4924)
    * Disallow relative imports
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>
    
    * bump ruff version
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>
    
    * Fix
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>
    
    * fix ruff version
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>
    
    * Fix format
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>
    
    * Fix mypy
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>
    
    * format
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>
    
    * black
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>
    
    * doc
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>
    
    * docs again
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>
    
    ---------
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- onnx/reference/ops/aionnxml/op_linear_classifier.py
+++ onnx/reference/ops/aionnxml/op_linear_classifier.py
@@ -3,8 +3,12 @@
 
 import numpy as np
 
-from ._common_classifier import compute_probit, compute_softmax_zero, expit
-from ._op_run_aionnxml import OpRunAiOnnxMl
+from onnx.reference.ops.aionnxml._common_classifier import (
+    compute_probit,
+    compute_softmax_zero,
+    expit,
+)
+from onnx.reference.ops.aionnxml._op_run_aionnxml import OpRunAiOnnxMl
 
 
 class LinearClassifier(OpRunAiOnnxMl):
