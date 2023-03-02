fdf9bc77ec0219748f9ec92a76d399da4d64663b
Chun-Wei Chen <jacky82226@gmail.com>
2023-03-02 08:30:11 -0800

    use astype(np.int64)
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/reference/ops/op_non_zero.py
+++ onnx/reference/ops/op_non_zero.py
@@ -9,5 +9,5 @@ from onnx.reference.op_run import OpRun
 class NonZero(OpRun):
     def _run(self, x):  # type: ignore
         # Specify np.int64 for Windows x86 machines
-        res = np.array(np.vstack(np.nonzero(x)), dtype=np.int64)
+        res = np.vstack(np.nonzero(x)).astype(np.int64)
         return (res,)
