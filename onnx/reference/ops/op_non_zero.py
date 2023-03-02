34d535741c6c975e4b368012a3890bb1ba87d632
Chun-Wei Chen <jacky82226@gmail.com>
2023-03-02 08:03:52 -0800

    Fix Windows x86 pipeline failure: handle NumPy behavior in onnx.reference
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/reference/ops/op_non_zero.py
+++ onnx/reference/ops/op_non_zero.py
@@ -8,5 +8,6 @@ from onnx.reference.op_run import OpRun
 
 class NonZero(OpRun):
     def _run(self, x):  # type: ignore
-        res = np.vstack(np.nonzero(x))
+        # Specify np.int64 for Windows x86 machines
+        res = np.array(np.vstack(np.nonzero(x)), dtype=np.int64)
         return (res,)
