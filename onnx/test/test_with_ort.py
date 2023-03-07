f9edbd5d343c8875421a6e7fae6a5d029890f48a
Chun-Wei Chen <jacky82226@gmail.com>
2023-03-06 16:21:24 -0800

    unittest test_with_ort
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- onnx/test/test_with_ort.py
+++ onnx/test/test_with_ort.py
@@ -2,44 +2,45 @@
 # This file is for testing ONNX with ONNXRuntime during ONNX Release
 # Create a general scenario to use ONNXRuntime with ONNX
 # pylint: disable=C0415
+import unittest
 
 
-def test_with_ort_example() -> None:
-    try:
-        import onnxruntime  # pylint: disable=W0611
+class TestONNXRuntime(unittest.TestCase):
+    def test_with_ort_example(self) -> None:
+        try:
+            import onnxruntime  # pylint: disable=W0611
 
-        del onnxruntime
-    except ImportError:
-        import unittest
-        raise unittest.SkipTest("onnxruntime not installed") from None
+            del onnxruntime
+        except ImportError:
+            raise unittest.SkipTest("onnxruntime not installed") from None
 
-    import numpy
-    import numpy.random
-    import onnxruntime as rt
-    from onnxruntime.datasets import get_example
+        import numpy
+        import numpy.random
+        import onnxruntime as rt
+        from onnxruntime.datasets import get_example
 
-    import onnx
+        import onnx
 
-    # get certain example model from ORT
-    example1 = get_example("sigmoid.onnx")
+        # get certain example model from ORT
+        example1 = get_example("sigmoid.onnx")
 
-    # test ONNX functions
-    model = onnx.load(example1)
-    onnx.checker.check_model(model)
-    onnx.checker.check_model(model, True)
-    inferred_model = onnx.shape_inference.infer_shapes(model, True)
-    temp_filename = "temp.onnx"
-    onnx.save(inferred_model, temp_filename)
+        # test ONNX functions
+        model = onnx.load(example1)
+        onnx.checker.check_model(model)
+        onnx.checker.check_model(model, True)
+        inferred_model = onnx.shape_inference.infer_shapes(model, True)
+        temp_filename = "temp.onnx"
+        onnx.save(inferred_model, temp_filename)
 
-    # test ONNXRuntime functions
-    sess = rt.InferenceSession(temp_filename)
-    input_name = sess.get_inputs()[0].name
-    output_name = sess.get_outputs()[0].name
-    x = numpy.random.random((3, 4, 5))
-    x = x.astype(numpy.float32)
+        # test ONNXRuntime functions
+        sess = rt.InferenceSession(temp_filename)
+        input_name = sess.get_inputs()[0].name
+        output_name = sess.get_outputs()[0].name
+        x = numpy.random.random((3, 4, 5))
+        x = x.astype(numpy.float32)
 
-    sess.run([output_name], {input_name: x})
+        sess.run([output_name], {input_name: x})
 
 
 if __name__ == "__main__":
-    example_test_with_ort()
+    unittest.main()
