6e009f22e3680e0a7c9b3380b6c722dd0f3e3d7b
Liqun Fu <liqfu@microsoft.com>
2023-03-06 16:18:11 -0800

    fix typos
    Signed-off-by: Liqun Fu <liqfu@microsoft.com>

--- onnx/shape_inference.py
+++ onnx/shape_inference.py
@@ -41,15 +41,16 @@ def infer_shapes(
             model_str, check_type, strict_mode, data_prop
         )
         return onnx.load_from_string(inferred_model_str)
-    if isinstance(model, str):
+    elif isinstance(model, str):
         raise TypeError(
             "infer_shapes only accepts ModelProto or bytes,"
             "you can use infer_shapes_path for the model path (String)."
         )
 
-    raise TypeError(
-        f"infer_shapes only accepts ModelProto or bytes, incorrect type: {type(model)}"
-    )
+    else:
+        raise TypeError(
+            f"infer_shapes only accepts ModelProto or bytes, incorrect type: {type(model)}"
+        )
 
 
 def infer_shapes_path(
@@ -68,17 +69,17 @@ def infer_shapes_path(
             "infer_shapes_path only accepts model Path (String),"
             "you can use infer_shapes for the ModelProto."
         )
-    # Directly output the inferred model into the specified path, return nothing
-    if isinstance(model_path, str):
+        # Directly output the inferred model into the specified path, return nothing
+    elif isinstance(model_path, str):
         # If output_path is not defined, default output_path would be the original model path
         if output_path == "":
             output_path = model_path
         C.infer_shapes_path(model_path, output_path, check_type, strict_mode, data_prop)
-
-    raise TypeError(
-        "infer_shapes_path only accepts model path (String), "
-        f"incorrect type: {type(model_path)}"
-    )
+    else:
+        raise TypeError(
+            "infer_shapes_path only accepts model path (String), "
+            f"incorrect type: {type(model_path)}"
+        )
 
 
 def infer_node_outputs(
