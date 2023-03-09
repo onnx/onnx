3cc77ccdd4a99ceb917fbdfe14cec767be89ad7f
Liqun Fu <liqfu@microsoft.com>
2023-03-09 15:00:35 -0800

    redo case gen
    Signed-off-by: Liqun Fu <liqfu@microsoft.com>

--- onnx/backend/test/case/utils.py
+++ onnx/backend/test/case/utils.py
@@ -33,10 +33,10 @@ def import_recursive(package: ModuleType) -> None:
     module_location = package.__name__
     for _module_loader, name, ispkg in pkgutil.iter_modules(pkg_dir):
         module_name = f"{module_location}.{name}"  # Module/package
-        # if not ONNX_ML and module_name.startswith(
-        #     "onnx.backend.test.case.node.ai_onnx_ml"
-        # ):
-        #     continue
+        if not ONNX_ML and module_name.startswith(
+            "onnx.backend.test.case.node.ai_onnx_ml"
+        ):
+            continue
 
         module = importlib.import_module(module_name)
         if ispkg:
