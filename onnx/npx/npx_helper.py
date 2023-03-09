0be3dd0ec6919a2555d75d7a3cc424b5bc8c4c73
xadupre <xadupre@microsoft.com>
2023-03-09 17:17:57 +0100

    lint
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- onnx/npx/npx_helper.py
+++ onnx/npx/npx_helper.py
@@ -74,7 +74,7 @@ def rename_in_onnx_graph(
     new_inputs = []
     for inp in graph.input:
         if inp.name in replacements:
-            new = make_value_info(replacements.get(inp.name, inp.name), inp.t)
+            new = make_value_info(replacements.get(inp.name, inp.name), inp.t)  # type: ignore[attr-defined]
             new_inputs.append(new)  # type: ignore[arg-type]
             continue
         new_inputs.append(inp)  # type: ignore[arg-type]
