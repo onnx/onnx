246065d1b6fa86f6f4b7eae7472de7b41b5f922a
xadupre <xadupre@microsoft.com>
2023-03-16 11:19:58 +0100

    fix import issue
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- onnx/backend/test/case/node/ai_onnx_ml/array_feature_extractor.py
+++ onnx/backend/test/case/node/ai_onnx_ml/array_feature_extractor.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import onnx
 from onnx.backend.test.case.base import Base
-from onnx.backend.test.case.node.node import expect
+from onnx.backend.test.case.node import expect
 
 
 class ArrayFeatureExtractor(Base):
