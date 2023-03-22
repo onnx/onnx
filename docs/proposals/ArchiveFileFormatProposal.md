292813207232132fa6eb99ae47811c4c99619228
Andreas Fehlner <fehlner@arcor.de>
2023-03-22 22:56:26 +0100

    add copyright
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>

--- docs/proposals/ArchiveFileFormatProposal.md
+++ docs/proposals/ArchiveFileFormatProposal.md
@@ -1,3 +1,7 @@
+<!--
+Copyright (c) ONNX Project Contributors
+-->
+
 <!--- SPDX-License-Identifier: Apache-2.0 -->
 
 # ONNX File Format Proposal
--- onnx/reference/ops/aionnx_preview_training/__init__.py
+++ onnx/reference/ops/aionnx_preview_training/__init__.py
@@ -1,3 +1,5 @@
+# Copyright (c) ONNX Project Contributors
+
 # SPDX-License-Identifier: Apache-2.0
 
 from onnx.reference.ops.aionnx_preview_training._op_list import load_op
