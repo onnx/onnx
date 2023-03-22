38bd1afab93486e3e1572bc4e6c48399715cb528
Justin Chu <justinchu@microsoft.com>
2023-03-22 04:44:41 +0000

    Tests
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- onnx/test/schema_test.py
+++ onnx/test/schema_test.py
@@ -24,6 +24,34 @@ class TestOpSchema(unittest.TestCase):
     def test_init_successful(self) -> None:
         defs.OpSchema()
 
+    def test_name_can_be_set(self) -> None:
+        schema = defs.OpSchema()
+        schema.name = "TestOp"
+        self.assertEqual(schema.name, "TestOp")
+
+    def test_doc_can_be_set(self) -> None:
+        schema = defs.OpSchema()
+        schema.doc = "Test doc"
+        self.assertEqual(schema.doc, "Test doc")
+
+    def test_domain_can_be_set(self) -> None:
+        schema = defs.OpSchema()
+        schema.domain = "TestDomain"
+        self.assertEqual(schema.domain, "TestDomain")
+
+    def test_add_attribute_adds_one_attribute(self) -> None:
+        schema = defs.OpSchema()
+        schema.add_attribute("TestAttr", "TestAttr doc", defs.OpSchema.AttrType.INT, required=True)
+        self.assertEqual(len(schema.attributes), 1)
+        self.assertEqual(schema.attributes["TestAttr"].name, "TestAttr")
+        self.assertEqual(schema.attributes["TestAttr"].description, "TestAttr doc")
+        self.assertEqual(schema.attributes["TestAttr"].type, defs.OpSchema.AttrType.INT)
+        self.assertEqual(schema.attributes["TestAttr"].default_value.i, 0)
+
+    def test_add_attribute_can_specify_default_value(self) -> None:
+        # TODO(justinchuby)
+        ...
+
 
 if __name__ == "__main__":
     unittest.main()
