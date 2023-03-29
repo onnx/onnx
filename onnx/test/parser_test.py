c602d427eef878cc6e06aa2354a425b7b3d185c5
Ganesan Ramalingam <grama@microsoft.com>
2023-03-29 11:43:18 -0700

    Add python unit test
    Signed-off-by: Ganesan Ramalingam <grama@microsoft.com>

--- onnx/test/parser_test.py
+++ onnx/test/parser_test.py
@@ -6,7 +6,7 @@ import unittest
 from parameterized import parameterized
 
 import onnx
-from onnx import GraphProto, OperatorSetIdProto, checker
+from onnx import GraphProto, NodeProto, OperatorSetIdProto, checker
 
 
 class TestBasicFunctions(unittest.TestCase):
@@ -206,6 +206,16 @@ class TestBasicFunctions(unittest.TestCase):
 
         expect_model_function_attribute(model)
         expect_custom_node_attribute(model.graph.node[0], expected_attribute)
+
+    def test_parse_node(self):
+        node = onnx.parser.parse_node("out1, out2 = SomeDomain.SomeOp <attr1 = 1> (in1, in2)")
+        self.assertEqual(list(node.input), ["in1", "in2"])
+        self.assertEqual(list(node.output), ["out1", "out2"])
+        self.assertEqual(len(node.attribute), 1)
+        attr_val = onnx.helper.get_node_attr_value(node, "attr1")
+        self.assertEqual(attr_val, 1)
+        self.assertEqual(node.domain, "SomeDomain")
+        self.assertEqual(node.op_type, "SomeOp")
 
 
 if __name__ == "__main__":
