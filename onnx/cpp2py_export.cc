98bd736184965bccedc51acf5a9579cb008839ae
Andreas Fehlner <fehlner@arcor.de>
2023-04-02 11:14:09 +0200

    Revert "update"
    This reverts commit d80d896b7047e303df8e335b242d70866b30911a.
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>

--- onnx/cpp2py_export.cc
+++ onnx/cpp2py_export.cc
@@ -1,6 +1,6 @@
-// Copyright (c) ONNX Project Contributors
-//
-// SPDX-License-Identifier: Apache-2.0
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
--- onnx/frontend/__init__.py
+++ onnx/frontend/__init__.py
@@ -1,3 +1,1 @@
-# Copyright (c) ONNX Project Contributors
-#
 # SPDX-License-Identifier: Apache-2.0
