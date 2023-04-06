d1d93079fe704479f1db87175e8c8cca1bf3fc71
Chun-Wei Chen <jacky82226@gmail.com>
2023-04-05 17:53:07 -0700

    -Q
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- docs/docsgen/make.bat
+++ docs/docsgen/make.bat
@@ -29,7 +29,7 @@ if errorlevel 9009 (
 
 if "%1" == "" goto help
 
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O% -Q
 goto end
 
 :help
