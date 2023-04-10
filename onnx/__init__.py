e8a487018d90b207c40d419b81c076efe1b65ea3
Justin Chu <justinchu@microsoft.com>
2023-04-10 19:20:33 +0000

    Support textproto as a serialization format
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- onnx/__init__.py
+++ onnx/__init__.py
@@ -3,11 +3,15 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # isort:skip_file
+from __future__ import annotations
+
 import os
 import typing
-from typing import Union, IO, Optional, TypeVar, Any
+from typing import Union, IO, Optional, TypeVar
 
 import google.protobuf.message
+import google.protobuf.text_format
+from typing_extensions import Literal
 
 from onnx.onnx_cpp2py_export import ONNX_ML
 from onnx.external_data_helper import (
@@ -65,6 +69,9 @@ from onnx import (
     version_converter,
 )
 
+# Supported model formats that can be loaded from and saved to
+_SupportedFormat = Union[Literal["protobuf"], Literal["textproto"]]
+
 
 def _load_bytes(f: Union[IO[bytes], str]) -> bytes:
     if hasattr(f, "read") and callable(typing.cast(IO[bytes], f).read):
@@ -91,17 +98,22 @@ def _get_file_path(f: Union[IO[bytes], str]) -> Optional[str]:
     return None
 
 
-def _serialize(proto: Union[bytes, google.protobuf.message.Message]) -> bytes:
+def _serialize(proto: Union[bytes, google.protobuf.message.Message], format_: _SupportedFormat) -> bytes:
     """Serialize a in-memory proto to bytes.
 
     Args:
         proto: An in-memory proto, such as a ModelProto, TensorProto, etc
+        format: The serialization format. Can be one of :py:const:`_SupportedFormat`.
 
     Returns:
         Serialized proto in bytes.
     """
     if isinstance(proto, bytes):
         return proto
+    if format_ == "textformat":
+        return google.protobuf.text_format.MessageToString(proto).encode("utf-8")
+
+    assert format_ == "protobuf"
     if hasattr(proto, "SerializeToString") and callable(proto.SerializeToString):
         try:
             result = proto.SerializeToString()
@@ -121,7 +133,7 @@ def _serialize(proto: Union[bytes, google.protobuf.message.Message]) -> bytes:
 _Proto = TypeVar("_Proto", bound=google.protobuf.message.Message)
 
 
-def _deserialize(s: bytes, proto: _Proto) -> _Proto:
+def _deserialize(s: bytes, proto: _Proto, format_: _SupportedFormat) -> _Proto:
     """Parse bytes into a in-memory proto.
 
     Args:
@@ -140,24 +152,28 @@ def _deserialize(s: bytes, proto: _Proto) -> _Proto:
     if not (hasattr(proto, "ParseFromString") and callable(proto.ParseFromString)):
         raise TypeError(f"No ParseFromString method is detected. Type is {type(proto)}")
 
-    decoded = typing.cast(Optional[int], proto.ParseFromString(s))
-    if decoded is not None and decoded != len(s):
-        raise google.protobuf.message.DecodeError(
-            f"Protobuf decoding consumed too few bytes: {decoded} out of {len(s)}"
-        )
+    if format_ == "textproto":
+        google.protobuf.text_format.Parse(s.decode("utf-8"), proto)
+    else:
+        assert format_ == "protobuf"
+        decoded = typing.cast(Optional[int], proto.ParseFromString(s))
+        if decoded is not None and decoded != len(s):
+            raise google.protobuf.message.DecodeError(
+                f"Protobuf decoding consumed too few bytes: {decoded} out of {len(s)}"
+            )
     return proto
 
 
 def load_model(
     f: Union[IO[bytes], str],
-    format: Optional[Any] = None,  # pylint: disable=redefined-builtin
+    format: _SupportedFormat = "protobuf",  # pylint: disable=redefined-builtin
     load_external_data: bool = True,
 ) -> ModelProto:
     """Loads a serialized ModelProto into memory.
 
     Args:
         f: can be a file-like object (has "read" function) or a string containing a file name
-        format: for future use
+        format: The serialization format. Can be one of :py:const:`_SupportedFormat`. Default is "protobuf".
         load_external_data: Whether to load the external data.
             Set to True if the data is under the same directory of the model.
             If not, users need to call :func:`load_external_data_for_model`
@@ -166,8 +182,7 @@ def load_model(
     Returns:
         Loaded in-memory ModelProto.
     """
-    s = _load_bytes(f)
-    model = load_model_from_string(s, format=format)
+    model = load_model_from_string(_load_bytes(f), format=format)
 
     if load_external_data:
         model_filepath = _get_file_path(f)
@@ -180,59 +195,56 @@ def load_model(
 
 def load_tensor(
     f: Union[IO[bytes], str],
-    format: Optional[Any] = None,  # pylint: disable=redefined-builtin
+    format: _SupportedFormat = "protobuf",  # pylint: disable=redefined-builtin
 ) -> TensorProto:
     """Loads a serialized TensorProto into memory.
 
     Args:
         f: can be a file-like object (has "read" function) or a string containing a file name
-        format: for future use
+        format: The serialization format. Can be one of :py:const:`_SupportedFormat`. Default is "protobuf".
 
     Returns:
         Loaded in-memory TensorProto.
     """
-    s = _load_bytes(f)
-    return load_tensor_from_string(s, format=format)
+    return load_tensor_from_string(_load_bytes(f), format)
 
 
 def load_model_from_string(
     s: bytes,
-    format: Optional[Any] = None,  # pylint: disable=redefined-builtin
+    format: _SupportedFormat = "protobuf",  # pylint: disable=redefined-builtin
 ) -> ModelProto:
     """Loads a binary string (bytes) that contains serialized ModelProto.
 
     Args:
         s: a string, which contains serialized ModelProto
-        format: for future use
+        format: The serialization format. Can be one of :py:const:`_SupportedFormat`. Default is "protobuf".
 
     Returns:
         Loaded in-memory ModelProto.
     """
-    del format  # Unused
-    return _deserialize(s, ModelProto())
+    return _deserialize(s, ModelProto(), format)
 
 
 def load_tensor_from_string(
     s: bytes,
-    format: Optional[Any] = None,  # pylint: disable=redefined-builtin
+    format: _SupportedFormat = "protobuf",  # pylint: disable=redefined-builtin
 ) -> TensorProto:
     """Loads a binary string (bytes) that contains serialized TensorProto.
 
     Args:
         s: a string, which contains serialized TensorProto
-        format: for future use
+        format: The serialization format. Can be one of :py:const:`_SupportedFormat`. Default is "protobuf".
 
     Returns:
         Loaded in-memory TensorProto.
     """
-    del format  # Unused
-    return _deserialize(s, TensorProto())
+    return _deserialize(s, TensorProto(), format)
 
 
 def save_model(
     proto: Union[ModelProto, bytes],
     f: Union[IO[bytes], str],
-    format: Optional[Any] = None,  # pylint: disable=redefined-builtin
+    format: _SupportedFormat = "protobuf",  # pylint: disable=redefined-builtin
     save_as_external_data: bool = False,
     all_tensors_to_one_file: bool = True,
     location: Optional[str] = None,
@@ -244,7 +256,8 @@ def save_model(
 
     Args:
         proto: should be a in-memory ModelProto
-        f: can be a file-like object (has "write" function) or a string containing a file name format for future use
+        f: can be a file-like object (has "write" function) or a string containing a file name
+        format: The serialization format. Can be one of :py:const:`_SupportedFormat`. Default is "protobuf".
         save_as_external_data: If true, save tensors to external file(s).
         all_tensors_to_one_file: Effective only if save_as_external_data is True.
             If true, save all tensors to one external file specified by location.
@@ -259,10 +272,8 @@ def save_model(
             If true, convert all tensors to external data
             If false, convert only non-attribute tensors to external data
     """
-    del format  # Unused
-
     if isinstance(proto, bytes):
-        proto = _deserialize(proto, ModelProto())
+        proto = _deserialize(proto, ModelProto(), format_="protobuf")
 
     if save_as_external_data:
         convert_model_to_external_data(
@@ -274,7 +285,7 @@ def save_model(
         basepath = os.path.dirname(model_filepath)
         proto = write_external_data_tensors(proto, basepath)
 
-    serialized = _serialize(proto)
+    serialized = _serialize(proto, format)
     _save_bytes(serialized, f)
 
 
@@ -285,7 +296,7 @@ def save_tensor(proto: TensorProto, f: Union[IO[bytes], str]) -> None:
     Args:
         proto: should be a in-memory TensorProto
         f: can be a file-like object (has "write" function) or a string containing a file name
-        format: for future use
+        format: The serialization format. Can be one of :py:const:`_SupportedFormat`. Default is "protobuf".
     """
     serialized = _serialize(proto)
     _save_bytes(serialized, f)
