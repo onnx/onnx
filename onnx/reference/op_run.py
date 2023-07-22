24c446697e5fcbe3f93b57d1512df3386bb89a90
Xavier Dupre <xadupre@microsoft.com>
2023-07-22 09:39:32 +0200

    remove list
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>

--- onnx/reference/op_run.py
+++ onnx/reference/op_run.py
@@ -160,7 +160,7 @@ def to_array_extended(tensor: TensorProto) -> np.ndarray:
         }
 
         if tensor.HasField("raw_data"):
-            data = list(tensor.raw_data)
+            data = tensor.raw_data
         else:
             data = tensor.int32_data
         shape = tuple(tensor.dims)
