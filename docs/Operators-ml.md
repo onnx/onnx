f6aaa1cba83a7318d86d800beda3061019f966d6
Aditya Goel <agoel4512@gmail.com>
2023-07-31 18:41:00 +0100

    Adds LabelEncoder tests
    Signed-off-by: Aditya Goel <agoel4512@gmail.com>

--- docs/Operators-ml.md
+++ docs/Operators-ml.md
@@ -474,6 +474,62 @@ Other versions of this operator: <a href="Changelog-ml.md#ai.onnx.ml.LabelEncode
 </dl>
 
 
+#### Examples
+
+<details>
+<summary>string_int_label_encoder</summary>
+
+```python
+node = onnx.helper.make_node(
+    "LabelEncoder",
+    inputs=["X"],
+    outputs=["Y"],
+    domain="ai.onnx.ml",
+    keys_strings=["a", "b", "c"],
+    values_int64s=[0, 1, 2],
+    default_int64=42,
+)
+x = np.array(["a", "b", "d", "c", "g"]).astype(object)
+y = np.array([0, 1, 42, 2, 42]).astype(np.int64)
+expect(node, inputs=[x], outputs=[y], name="test_ai_onnx_ml_label_encoder_string_int")
+
+node = onnx.helper.make_node(
+    "LabelEncoder",
+    inputs=["X"],
+    outputs=["Y"],
+    domain="ai.onnx.ml",
+    keys_strings=["a", "b", "c"],
+    values_int64s=[0, 1, 2],
+)
+x = np.array(["a", "b", "d", "c", "g"]).astype(object)
+y = np.array([0, 1, -1, 2, -1]).astype(np.int64)
+expect(node, inputs=[x], outputs=[y], name="test_ai_onnx_ml_label_encoder_string_int_no_default")
+```
+
+</details>
+
+
+<details>
+<summary>tensor_based_label_encoder</summary>
+
+```python
+node = onnx.helper.make_node(
+    "LabelEncoder",
+    inputs=["X"],
+    outputs=["Y"],
+    domain="ai.onnx.ml",
+    keys_as_tensor=make_tensor("keys_as_tensor", onnx.TensorProto.STRING, (3,), ["a", "b", "c"]),
+    values_as_tensor=make_tensor("values_as_tensor", onnx.TensorProto.INT16, (3,), [0, 1, 2]),
+    default_as_tensor=make_tensor("default_as_tensor", onnx.TensorProto.INT16, (1,), [42]),
+)
+x = np.array(["a", "b", "d", "c", "g"]).astype(object)
+y = np.array([0, 1, 42, 2, 42]).astype(np.int16)
+expect(node, inputs=[x], outputs=[y], name="test_ai_onnx_ml_label_encoder_tensor_mapping")
+```
+
+</details>
+
+
 ### <a name="ai.onnx.ml.LinearClassifier"></a><a name="ai.onnx.ml.linearclassifier">**ai.onnx.ml.LinearClassifier**</a>
 
   Linear classifier
---
+++ onnx/backend/test/case/node/ai_onnx_ml/label_encoder.py
@@ -0,0 +1,54 @@
+# Copyright (c) ONNX Project Contributors
+
+# SPDX-License-Identifier: Apache-2.0
+
+import numpy as np
+
+import onnx
+from onnx.backend.test.case.base import Base
+from onnx.backend.test.case.node import expect
+from onnx.helper import make_tensor
+
+
+class LabelEncoder(Base):
+    @staticmethod
+    def export_string_int_label_encoder() -> None:
+        node = onnx.helper.make_node(
+            "LabelEncoder",
+            inputs=["X"],
+            outputs=["Y"],
+            domain="ai.onnx.ml",
+            keys_strings=["a", "b", "c"],
+            values_int64s=[0, 1, 2],
+            default_int64=42,
+        )
+        x = np.array(["a", "b", "d", "c", "g"]).astype(object)
+        y = np.array([0, 1, 42, 2, 42]).astype(np.int64)
+        expect(node, inputs=[x], outputs=[y], name="test_ai_onnx_ml_label_encoder_string_int")
+
+        node = onnx.helper.make_node(
+            "LabelEncoder",
+            inputs=["X"],
+            outputs=["Y"],
+            domain="ai.onnx.ml",
+            keys_strings=["a", "b", "c"],
+            values_int64s=[0, 1, 2],
+        )
+        x = np.array(["a", "b", "d", "c", "g"]).astype(object)
+        y = np.array([0, 1, -1, 2, -1]).astype(np.int64)
+        expect(node, inputs=[x], outputs=[y], name="test_ai_onnx_ml_label_encoder_string_int_no_default")
+
+    @staticmethod
+    def export_tensor_based_label_encoder() -> None:
+        node = onnx.helper.make_node(
+            "LabelEncoder",
+            inputs=["X"],
+            outputs=["Y"],
+            domain="ai.onnx.ml",
+            keys_as_tensor=make_tensor("keys_as_tensor", onnx.TensorProto.STRING, (3,), ["a", "b", "c"]),
+            values_as_tensor=make_tensor("values_as_tensor", onnx.TensorProto.INT16, (3,), [0, 1, 2]),
+            default_as_tensor=make_tensor("default_as_tensor", onnx.TensorProto.INT16, (1,), [42]),
+        )
+        x = np.array(["a", "b", "d", "c", "g"]).astype(object)
+        y = np.array([0, 1, 42, 2, 42]).astype(np.int16)
+        expect(node, inputs=[x], outputs=[y], name="test_ai_onnx_ml_label_encoder_tensor_mapping")
