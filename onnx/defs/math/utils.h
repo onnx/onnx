efbc9fc2b89bcf479347c00184d0344c462b9d8a
Xavier Dupre <xadupre@microsoft.com>
2023-08-04 16:23:29 +0200

    refactoring
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>

--- onnx/defs/math/utils.h
+++ onnx/defs/math/utils.h
@@ -10,6 +10,8 @@ namespace ONNX_NAMESPACE {
 std::function<void(OpSchema&)>
 SoftmaxFamilyDocGenerator(const char* name, const char* description, const char* equation);
 
+void matmulShapeInference(ONNX_NAMESPACE::InferenceContext& ctx, int input1Idx, int input2Idx);
+
 void qlinear_matmul_shape_inference(ONNX_NAMESPACE::InferenceContext& ctx);
 
 const char* qlinear_matmul_doc();
