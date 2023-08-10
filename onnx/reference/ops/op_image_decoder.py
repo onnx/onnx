4ad6d3126a9c1ab9b21a8b5c421fe31880d53d20
Joaquin Anton <janton@nvidia.com>
2023-08-10 11:18:09 +0200

    Add ImageDecoder operator (#5294)
    ### Description
    <!-- - Describe your changes. -->
    - Adds a new ImageDecoder operator, to be used in preprocessing models.
    - Tests use generated images and opencv as a reference.
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve? -->
    Currently, image decoding happens outside of ONNX, bring problems of
    portability and deployment.
    <!-- - If it fixes an open issue, please link to the issue here. -->
    
    ---------
    
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

---
+++ onnx/reference/ops/op_image_decoder.py
@@ -0,0 +1,29 @@
+# Copyright (c) ONNX Project Contributors
+
+# SPDX-License-Identifier: Apache-2.0
+# pylint: disable=C0123,C3001,R0912,R0913,R0914,R1730,W0221,W0613
+
+import cv2
+import numpy as np
+
+from onnx.reference.op_run import OpRun
+
+
+class ImageDecoder(OpRun):
+    def _run(  # type: ignore
+        self,
+        encoded,
+        pixel_format="RGB",
+    ):
+        decoded = None
+        if pixel_format == "BGR":
+            decoded = cv2.imdecode(encoded, cv2.IMREAD_COLOR)
+        elif pixel_format == "RGB":
+            decoded = cv2.imdecode(encoded, cv2.IMREAD_COLOR)
+            decoded = cv2.cvtColor(decoded, cv2.COLOR_BGR2RGB)
+        elif pixel_format == "Grayscale":
+            decoded = cv2.imdecode(encoded, cv2.IMREAD_GRAYSCALE)
+            decoded = np.expand_dims(decoded, axis=2)  # (H, W) to (H, W, 1)
+        else:
+            raise RuntimeError(f"pixel_format={pixel_format!r} is not supported.")
+        return (decoded,)
