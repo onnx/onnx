2a7d8e99caf878f5933661cb6e7dc4fe2189a799
Justin Chu <justinchuby@users.noreply.github.com>
2023-08-24 11:48:07 -0700

    Support JSON as a serialization target (#5523)
    ### Description
    
    Create serializers to the `json` format and register the `json` and
    `onnxjson` extensions to it.
    
    ```python
    import onnx
    model = onnx.load_model("onnx/backend/test/data/light/light_resnet50.onnx")
    onnx.save_model(model, "resnet50.json")
    ```
    
    yields
    
    ```json
    {
      "ir_version": "3",
      "producer_name": "onnx-caffe2",
      "producer_version": "",
      "domain": "",
      "model_version": "0",
      "doc_string": "",
      "graph": {
        "node": [
          {
            "input": [
              "gpu_0/conv1_w_0__SHAPE"
            ],
            "output": [
              "gpu_0/conv1_w_0"
            ],
            "op_type": "ConstantOfShape",
            "attribute": [
              {
                "name": "value",
                "t": {
                  "dims": [
                    "1"
                  ],
                  "data_type": 1,
                  "float_data": [
                    0.02
                  ],
                  "name": ""
                },
                "type": "TENSOR"
              }
            ]
          },
          {
            "input": [
              "gpu_0/pred_b_0__SHAPE"
            ],
            "output": [
              "gpu_0/pred_b_0"
            ],
            "op_type": "ConstantOfShape",
            "attribute": [
              {
                "name": "value",
                "t": {
                  "dims": [
                    "1"
                  ],
                  "data_type": 1,
                  "float_data": [
                    0.02
                  ],
                  "name": ""
                },
                "type": "TENSOR"
              }
            ]
          },
          {
            "input": [
              "gpu_0/pred_w_0__SHAPE"
            ],
            "output": [
              "gpu_0/pred_w_0"
            ],
            "op_type": "ConstantOfShape",
            "attribute": [
              {
                "name": "value",
                "t": {
                  "dims": [
                    "1"
                  ],
                  "data_type": 1,
                  "float_data": [
                    0.02
                  ],
                  "name": ""
                },
                "type": "TENSOR"
              }
            ]
          },
    ...
    ```
    
    ### Motivation and Context
    
    A more familiar text representation for free from protobuf using
    https://googleapis.dev/python/protobuf/latest/google/protobuf/json_format.html
    
    ---------
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnx/serialization.py
+++ onnx/serialization.py
@@ -11,6 +11,7 @@ __all__ = [
 import typing
 from typing import Any, Collection, Optional, Protocol, TypeVar
 
+import google.protobuf.json_format
 import google.protobuf.message
 import google.protobuf.text_format
 
@@ -141,7 +142,31 @@ class _TextProtoSerializer(ProtoSerializer):
         return google.protobuf.text_format.Parse(serialized, proto)
 
 
+class _JsonSerializer(ProtoSerializer):
+    """Serialize and deserialize JSON."""
+
+    supported_format = "json"
+    file_extensions = frozenset({".json", ".onnxjson"})
+
+    def serialize_proto(self, proto: _Proto) -> bytes:
+        json_message = google.protobuf.json_format.MessageToJson(
+            proto, preserving_proto_field_name=True
+        )
+        return json_message.encode(_ENCODING)
+
+    def deserialize_proto(self, serialized: bytes | str, proto: _Proto) -> _Proto:
+        if not isinstance(serialized, (bytes, str)):
+            raise TypeError(
+                f"Parameter 'serialized' must be bytes or str, but got type: {type(serialized)}"
+            )
+        if isinstance(serialized, bytes):
+            serialized = serialized.decode(_ENCODING)
+        assert isinstance(serialized, str)
+        return google.protobuf.json_format.Parse(serialized, proto)
+
+
 # Register default serializers
 registry = _Registry()
 registry.register(_ProtobufSerializer())
 registry.register(_TextProtoSerializer())
+registry.register(_JsonSerializer())
