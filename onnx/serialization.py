c77397e046022faed614d43594bd13b7ec28fab7
Justin Chu <justinchuby@users.noreply.github.com>
2023-08-25 15:15:14 -0700

    Create the onnxtxt serialization format (#5524)
    Add support for serializing to and deserializing from the ONNX textual
    representation, and register it under the `onnxtxt` file extension and
    format.
    
    Users can now save a model to the textual representation with
    `onnx.save_model(model, "model.onnxtxt")`
    
    Note that the textual format has known limitations, including (1) it
    doesn’t serialize tensor protos or initializers and (2) bugs like #5102.
    We will call this out in a documentation in a separate PR.
    
    ---------
    
    Signed-off-by: Justin Chu <justinchu@microsoft.com>
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnx/serialization.py
+++ onnx/serialization.py
@@ -4,6 +4,8 @@
 
 from __future__ import annotations
 
+import warnings
+
 __all__ = [
     "registry",
 ]
@@ -165,8 +167,43 @@ class _JsonSerializer(ProtoSerializer):
         return google.protobuf.json_format.Parse(serialized, proto)
 
 
+class _TextualSerializer(ProtoSerializer):
+    """Serialize and deserialize the ONNX textual representation."""
+
+    supported_format = "onnxtxt"
+    file_extensions = frozenset({".onnxtxt"})
+
+    def serialize_proto(self, proto: _Proto) -> bytes:
+        text = onnx.printer.to_text(proto)  # type: ignore[arg-type]
+        return text.encode(_ENCODING)
+
+    def deserialize_proto(self, serialized: bytes | str, proto: _Proto) -> _Proto:
+        warnings.warn(
+            "The onnxtxt format is experimental. Please report any errors to the ONNX GitHub repository.",
+            stacklevel=2,
+        )
+        if not isinstance(serialized, (bytes, str)):
+            raise TypeError(
+                f"Parameter 'serialized' must be bytes or str, but got type: {type(serialized)}"
+            )
+        if isinstance(serialized, bytes):
+            text = serialized.decode(_ENCODING)
+        else:
+            text = serialized
+        if isinstance(proto, onnx.ModelProto):
+            return onnx.parser.parse_model(text)  # type: ignore[return-value]
+        if isinstance(proto, onnx.GraphProto):
+            return onnx.parser.parse_graph(text)  # type: ignore[return-value]
+        if isinstance(proto, onnx.FunctionProto):
+            return onnx.parser.parse_function(text)  # type: ignore[return-value]
+        if isinstance(proto, onnx.NodeProto):
+            return onnx.parser.parse_node(text)  # type: ignore[return-value]
+        raise ValueError(f"Unsupported proto type: {type(proto)}")
+
+
 # Register default serializers
 registry = _Registry()
 registry.register(_ProtobufSerializer())
 registry.register(_TextProtoSerializer())
 registry.register(_JsonSerializer())
+registry.register(_TextualSerializer())
