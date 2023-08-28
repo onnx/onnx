8d45697d45fc33b36a5a65d5ddcc332d2e6e1858
rajveer43 <rajveer.rathod1301@gmail.com>
2023-08-28 22:15:45 +0530

    update old.cc

--- onnx/defs/tensor/old.cc
+++ onnx/defs/tensor/old.cc
@@ -5015,6 +5015,24 @@ contain both a zero value and -1, as the value of the dimension corresponding
 to -1 cannot be determined uniquely.
 )DOC";
 
+static const char* Reshape_common_doc = R"DOC(
+Reshape the input tensor similar to numpy.reshape.
+First input is the data tensor, and the second input is a shape tensor that specifies the output shape. The function outputs the reshaped tensor.
+At most one dimension of the new shape can be -1. In this case, the value is inferred from the size of the tensor and the remaining dimensions. A dimension could also be 0, in which case the actual dimension value is unchanged (i.e., taken from the input tensor).
+Shape (second input) could be an empty shape, which means converting to a scalar.
+The input tensor's shape and the output tensor's shape must have the same number of elements.
+)DOC";
+
+static const char* Reshape_ver13_doc = R"DOC(
+%0
+)DOC";
+
+static const char* Reshape_ver14_doc = R"DOC(
+%0
+If 'allowzero' is set, and the new shape includes 0, the dimension will be set explicitly to zero (i.e., not taken from the input tensor).
+It is invalid for the specified shape to contain both a zero value and -1 when the 'allowzero' attribute is set, as the value of the dimension corresponding to -1 cannot be determined uniquely.
+)DOC";
+
 ONNX_OPERATOR_SET_SCHEMA(
     Reshape,
     14,
