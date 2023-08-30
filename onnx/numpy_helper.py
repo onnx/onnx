dcac68075e8a8a02b67c4707b9a294252b17f2d5
Clif Houck <me@clifhouck.com>
2023-08-30 11:30:46 -0500

    allow generic or ndarray
    Signed-off-by: Clif Houck <me@clifhouck.com>

--- onnx/numpy_helper.py
+++ onnx/numpy_helper.py
@@ -283,7 +283,7 @@ def from_array(
     Returns:
         TensorProto: the converted tensor def.
     """
-    if not isinstance(arr, np.generic):
+    if not isinstance(arr, np.ndarray) and not isinstance(arr, np.generic):
         raise TypeError(
             f"arr must be of type np.generic or np.ndarray, got {type(arr)}"
         )
