6396c13c120cc5078f129cb9da29e5ccf95066f0
Justin Chu <justinchu@microsoft.com>
2023-09-14 14:32:47 -0700

    Explain m
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- onnx/defs/math/defs.cc
+++ onnx/defs/math/defs.cc
@@ -2929,26 +2929,26 @@ ONNX_OPERATOR_SET_SCHEMA(
     OpSchema()
         .SetDoc(DFT_ver20_doc)
         .Attr(
-            // TODO(justinchuby): What is the m here?
             "onesided",
-            "If onesided is 1, only values for w in [0, 1, 2, ..., floor(n_fft/2) + 1] are returned because "
-            "the real-to-complex Fourier transform satisfies the conjugate symmetry, i.e., X[m, w] = X[m, n_fft-w]*. "
+            "If `onesided` is `1`, only values for `w` in `[0, 1, 2, ..., floor(n_fft/2) + 1]` are returned because "
+            "the real-to-complex Fourier transform satisfies the conjugate symmetry, i.e., `X[m, w] = X[m, n_fft-w]*`, "
+            "where `m` denotes \"all other dimensions\" DFT was not applied on. "
             "Note if the input or window tensors are complex, then onesided output is not possible. "
             "Enabling onesided with real inputs performs a Real-valued fast Fourier transform (RFFT). "
-            "When invoked with real or complex valued input, the default value is 0. "
-            "Values can be 0 or 1.",
+            "When invoked with real or complex valued input, the default value is `0`. "
+            "Value can be `0` or `1`.",
             AttributeProto::INT,
             static_cast<int64_t>(0))
         .Attr(
             "inverse",
-            "Whether to perform the inverse discrete fourier transform. By default this value is set to 0, which corresponds to false.",
+            "Whether to perform the inverse discrete fourier transform. By default this value is set to 0, which corresponds to `false`.",
             AttributeProto::INT,
             static_cast<int64_t>(0))
         .Input(
             0,
             "input",
-            "For real input, the following shape is expected: [signal_dim0][signal_dim1][signal_dim2]...[signal_dimN][1]. "
-            "For complex input, the following shape is expected: [signal_dim0][signal_dim1][signal_dim2]...[signal_dimN][2]. "
+            "For real input, the following shape is expected: `[signal_dim0][signal_dim1][signal_dim2]...[signal_dimN][1]`. "
+            "For complex input, the following shape is expected: `[signal_dim0][signal_dim1][signal_dim2]...[signal_dimN][2]`. "
             "The final dimension represents the real and imaginary parts of the value in that order.",
             "T1",
             OpSchema::Single,
@@ -2958,9 +2958,8 @@ ONNX_OPERATOR_SET_SCHEMA(
         .Input(
             1,
             "axis",
-            "The axis on which to perform the DFT. By default this value is set to `-1` (last axis). "
-            "Negative value means counting dimensions from the back. Accepted range is [-r, r-1] where r = rank(input). "
-            "The value should be a scalar.",
+            "The axis as a scalar on which to perform the DFT. By default this value is set to `-1` (last axis). "
+            "Negative value means counting dimensions from the back. Accepted range is `[-r, r-1]` where `r = rank(input)`. ",
             "tensor(int64)",
             OpSchema::Optional,
             true,
@@ -2970,8 +2969,8 @@ ONNX_OPERATOR_SET_SCHEMA(
             2,
             "dft_length",
             "The length of the signal as a scalar. "
-            "If greater than the axis dimension, the signal will be zero-padded up to dft_length. "
-            "If less than the axis dimension, only the first dft_length values will be used as the signal. "
+            "If greater than the axis dimension, the signal will be zero-padded up to `dft_length`. "
+            "If less than the axis dimension, only the first `dft_length` values will be used as the signal. "
             "It's an optional value. ",
             "T2",
             OpSchema::Optional,
@@ -2982,11 +2981,11 @@ ONNX_OPERATOR_SET_SCHEMA(
             0,
             "output",
             "The Fourier Transform of the input vector. "
-            "If onesided is 0, the following shape is expected: [signal_dim0][signal_dim1][signal_dim2]...[signal_dimN][2]. "
-            "If axis=0 and onesided is 1, the following shape is expected: [floor(signal_dim0/2)+1][signal_dim1][signal_dim2]...[signal_dimN][2]. "
-            "If axis=1 and onesided is 1, the following shape is expected: [signal_dim0][floor(signal_dim1/2)+1][signal_dim2]...[signal_dimN][2]. "
-            "If axis=N and onesided is 1, the following shape is expected: [signal_dim0][signal_dim1][signal_dim2]...[floor(signal_dimN/2)+1][2]. "
-            "The signal_dim at the specified axis is equal to the dft_length.",
+            "If `onesided` is `0`, the following shape is expected: `[signal_dim0][signal_dim1][signal_dim2]...[signal_dimN][2]`. "
+            "If `axis=0` and `onesided` is `1`, the following shape is expected: `[floor(signal_dim0/2)+1][signal_dim1][signal_dim2]...[signal_dimN][2]`. "
+            "If `axis=1` and `onesided` is `1`, the following shape is expected: `[signal_dim0][floor(signal_dim1/2)+1][signal_dim2]...[signal_dimN][2]`. "
+            "If `axis=N` and `onesided` is `1`, the following shape is expected: `[signal_dim0][signal_dim1][signal_dim2]...[floor(signal_dimN/2)+1][2]`. "
+            "The `signal_dim` at the specified `axis` is equal to the `dft_length`.",
             "T1")
         .TypeConstraint("T1", OpSchema::all_float_types_ir4(), "Constrain input and output types to float tensors.")
         .TypeConstraint("T2", {"tensor(int32)", "tensor(int64)"}, "Constrain scalar length types to integers.")
@@ -3021,6 +3020,7 @@ ONNX_OPERATOR_SET_SCHEMA(
             return;
           }
           // TODO(justinchuby): Create invariance checking functions to ensure shapes and sizes
+          // to abstrct the following logic out.
           if (axis_tensor->dims_size() != 0) {
             fail_shape_inference("axis input must be a scalar.");
           }
