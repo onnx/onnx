90a07e4c3431a7cbf566936847ff6f269e1e24e0
Justin Chu <justinchuby@users.noreply.github.com>
2023-09-15 10:31:12 -0700

    Simplify endian detenction logic
    Simplify endian detection logic by removing if branches and leveraging c++ syntax.
    
    Reference: https://github.com/pytorch/pytorch/blob/653c1564bf4c46d9758000ea70a610ee3699acf1/caffe2/operators/fused_rowwise_nbitfake_conversion_ops.h#L16-L19
    
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnx/common/platform_helpers.h
+++ onnx/common/platform_helpers.h
@@ -9,11 +9,9 @@
 namespace ONNX_NAMESPACE {
 
 // Determine if the processor is little endian or not
-inline bool is_processor_little_endian() {
-  int num = 1;
-  if (*(char*)&num == 1)
-    return true;
-  return false;
+inline bool is_processor_little_endian() { 
+  constexpr std::int32_t value = 1; 
+  return reinterpret_cast<const std::uint8_t*>(&value)[0] == 1; 
 }
 
 } // namespace ONNX_NAMESPACE
