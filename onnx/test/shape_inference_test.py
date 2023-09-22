f98137060aaa86dec28da8892188f2b83ead592c
Justin Chu <justinchu@microsoft.com>
2023-09-22 11:12:18 -0700

    Segfault
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- onnx/test/shape_inference_test.py
+++ onnx/test/shape_inference_test.py
@@ -8683,6 +8683,78 @@ class TestShapeInference(TestShapeInferenceHelper):
                 opset_imports=[helper.make_opsetid(ONNX_DOMAIN, 20)],
             )
 
+    @parameterized.expand(
+        [
+            ("real", (2, 5, 5, 1)),
+            ("complex", (2, 5, 5, 2)),
+        ]
+    )
+    def test_dft_dynamic_axis_opset20(self, _: str, shape: tuple[int, ...]) -> None:
+        graph = self._make_graph(
+            [("axis", TensorProto.INT64, ())],
+            [
+                make_node(
+                    "Constant",
+                    [],
+                    ["input"],
+                    value=make_tensor(
+                        "input",
+                        TensorProto.FLOAT,
+                        shape,
+                        np.ones(shape, dtype=np.float32).flatten(),
+                    ),
+                ),
+                make_node("DFT", ["input", "", "axis"], ["output"]),
+            ],
+            [],
+        )
+        self._assert_inferred(
+            graph,
+            [
+                make_tensor_value_info("input", TensorProto.FLOAT, shape),
+                make_tensor_value_info("output", TensorProto.FLOAT, (2, 5, 5, 2)),
+            ],
+            opset_imports=[helper.make_opsetid(ONNX_DOMAIN, 20)],
+        )
+
+    @parameterized.expand(
+        [
+            ("real", (2, 5, 5, 1)),
+            ("complex", (2, 5, 5, 2)),
+        ]
+    )
+    def test_dft_dynamic_axis_onesided_opset20(
+        self, _: str, shape: tuple[int, ...]
+    ) -> None:
+        graph = self._make_graph(
+            [("axis", TensorProto.INT64, ())],
+            [
+                make_node(
+                    "Constant",
+                    [],
+                    ["input"],
+                    value=make_tensor(
+                        "input",
+                        TensorProto.FLOAT,
+                        shape,
+                        np.ones(shape, dtype=np.float32).flatten(),
+                    ),
+                ),
+                make_node("DFT", ["input", "", "axis"], ["output"], onesided=1),
+            ],
+            [],
+        )
+        self._assert_inferred(
+            graph,
+            [
+                make_tensor_value_info("input", TensorProto.FLOAT, shape),
+                make_tensor_value_info(
+                    "output", TensorProto.FLOAT, (None, None, None, 2)
+                ),
+            ],
+            opset_imports=[helper.make_opsetid(ONNX_DOMAIN, 20)],
+        )
+
     def test_dft_onesided_default_axis_opset17(self) -> None:
         # Opset 17 sets default axis to be 1.
         graph = self._make_graph(
