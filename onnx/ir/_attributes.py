6a3093c5eb4adf89880971c8396cdc4d1b1fce1b
Justin Chu <justinchu@microsoft.com>
2023-10-05 00:41:34 +0000

    Protocols

--- onnx/ir/_attributes.py
+++ onnx/ir/_attributes.py
@@ -1,78 +1,29 @@
 from __future__ import annotations
 
-import enum
-from typing import (
-    Any,
-    Generic,
-    Protocol,
-    Sequence,
-    TypeVar,
-)
-
-
-
-S = TypeVar("S")
-T = TypeVar("T")
-AttrT = TypeVar("AttrT", bound="Attribute")
-AttrIterableT = TypeVar("AttrIterableT", bound="_AttrIterable")
-
-
-class AttributeType(enum.IntEnum):
-    """Enum for the types of ONNX attributes."""
-
-    # NOTE: Naming follows python conventions.
-    # C++ names can follow C++ conventions and rename when binding.
-
-    # TODO: Should we code gen this? We just need to get rid of protoc
-    # We can code gen with https://github.com/recap-build/proto-schema-parser/tree/main
-
-    # NOTE: We can assume the build tool chain has python, just not protoc, right?
-    # NOTE: We should alias OpSchema::AttrType as well
-    UNDEFINED = 0
-    FLOAT = 1
-    INT = 2
-    STRING = 3
-    TENSOR = 4
-    GRAPH = 5
-    FLOATS = 6
-    INTS = 7
-    STRINGS = 8
-    TENSORS = 9
-    GRAPHS = 10
-    SPARSE_TENSOR = 11
-    SPARSE_TENSORS = 12
-    TYPE_PROTO = 13
-    TYPE_PROTOS = 14
+from onnx.ir import _enums, _protocols
+from typing import Sequence
 
 
 # NOTE: None of these classes will have a "to_onnx" method because
 # We cannot assume that the build tool chain has protoc installed.
 
-class AttributeProtocol(Protocol):
-    """Protocol for ONNX attributes."""
 
-    name: str
-    type: AttributeType
-    value: Any
-    ref_attr_name: str
-    doc_string: str
-
-
-class Attribute(Generic[T], AttributeProtocol):
+class Attribute(_protocols.AttributeProtocol):
     """Base class for ONNX attributes."""
 
     # NOTE: We use primitive types for T
 
-    def __init__(self,
+    def __init__(
+        self,
         name: str,
-        type: AttributeType,
-        value: T,
+        typ: _enums.AttributeType,
+        value,
         *,
         ref_attr_name: str,
-        doc_string: str
+        doc_string: str,
     ):
         self.name = name
-        self.type = type
+        self.type = typ
         self.value = value
         self.ref_attr_name = ref_attr_name
         self.doc_string = doc_string
@@ -86,57 +37,212 @@ class Attribute(Generic[T], AttributeProtocol):
 # Arguably, they can also be functions that return Attribute objects.
 # TODO: We need something that is easy for pattern matchers, otherwise they
 # can just be make_attribute functions.
-class AttrFloat32(Attribute[float]):
-    def __init__(self, name: str, value: float, ref_attr_name: str = "", doc_string: str = ""):
-        super().__init__(name, AttributeType.FLOAT, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
+class AttrFloat32(Attribute):
+    def __init__(
+        self, name: str, value: float, ref_attr_name: str = "", doc_string: str = ""
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.FLOAT,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
 
-class AttrInt64(Attribute[int]):
-    def __init__(self, name: str, value: int, ref_attr_name: str = "", doc_string: str = ""):
-        super().__init__(name, AttributeType.INT, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
 
-class AttrString(Attribute[str]):
-    def __init__(self, name: str, value: str, ref_attr_name: str = "", doc_string: str = ""):
-        super().__init__(name, AttributeType.STRING, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
+class AttrInt64(Attribute):
+    def __init__(
+        self, name: str, value: int, ref_attr_name: str = "", doc_string: str = ""
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.INT,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
+
+
+class AttrString(Attribute):
+    def __init__(
+        self, name: str, value: str, ref_attr_name: str = "", doc_string: str = ""
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.STRING,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
+
 
 # NOTE: Tensor should be a tensor proto
-class AttrTensor(Attribute[Tensor]):
-    def __init__(self, name: str, value: Tensor, ref_attr_name: str = "", doc_string: str = ""):
-        super().__init__(name, AttributeType.TENSOR, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
+class AttrTensor(Attribute):
+    def __init__(
+        self,
+        name: str,
+        value: _protocols.TensorProtocol,
+        ref_attr_name: str = "",
+        doc_string: str = "",
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.TENSOR,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
 
-class AttrGraph(Attribute[Graph]):
-    def __init__(self, name: str, value: Graph, ref_attr_name: str = "", doc_string: str = ""):
-        super().__init__(name, AttributeType.GRAPH, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
 
-class AttrFloat32s(Attribute[Sequence[float]]):
-    def __init__(self, name: str, value: Sequence[float], ref_attr_name: str = "", doc_string: str = ""):
-        super().__init__(name, AttributeType.FLOATS, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
+class AttrGraph(Attribute):
+    def __init__(
+        self,
+        name: str,
+        value: _protocols.GraphProtocol,
+        ref_attr_name: str = "",
+        doc_string: str = "",
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.GRAPH,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
 
-class AttrInt64s(Attribute[Sequence[int]]):
-    def __init__(self, name: str, value: Sequence[int], ref_attr_name: str = "", doc_string: str = ""):
-        super().__init__(name, AttributeType.INTS, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
 
-class AttrStrings(Attribute[Sequence[str]]):
-    def __init__(self, name: str, value: Sequence[str], ref_attr_name: str = "", doc_string: str = ""):
-        super().__init__(name, AttributeType.STRINGS, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
+class AttrFloat32s(Attribute):
+    def __init__(
+        self,
+        name: str,
+        value: Sequence[float],
+        ref_attr_name: str = "",
+        doc_string: str = "",
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.FLOATS,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
 
-class AttrTensors(Attribute[Sequence[Tensor]]):
-    def __init__(self, name: str, value: Sequence[Tensor], ref_attr_name: str = "", doc_string: str = ""):
-        super().__init__(name, AttributeType.TENSORS, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
 
-class AttrGraphs(Attribute[Sequence[Graph]]):
-    def __init__(self, name: str, value: Sequence[Graph], ref_attr_name: str = "", doc_string: str = ""):
-        super().__init__(name, AttributeType.GRAPHS, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
+class AttrInt64s(Attribute):
+    def __init__(
+        self,
+        name: str,
+        value: Sequence[int],
+        ref_attr_name: str = "",
+        doc_string: str = "",
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.INTS,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
+
+
+class AttrStrings(Attribute):
+    def __init__(
+        self,
+        name: str,
+        value: Sequence[str],
+        ref_attr_name: str = "",
+        doc_string: str = "",
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.STRINGS,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
+
+
+class AttrTensors(Attribute):
+    def __init__(
+        self,
+        name: str,
+        value: Sequence[_protocols.TensorProtocol],
+        ref_attr_name: str = "",
+        doc_string: str = "",
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.TENSORS,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
+
+
+class AttrGraphs(Attribute):
+    def __init__(
+        self,
+        name: str,
+        value: Sequence[_protocols.GraphProtocol],
+        ref_attr_name: str = "",
+        doc_string: str = "",
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.GRAPHS,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
+
 
 # NOTE: SparseTensor should be a sparse tensor proto
-class AttrSparseTensor(Attribute[Sequence[SparseTensor]]):
-    def __init__(self, name: str, value: Sequence[SparseTensor], ref_attr_name: str = "", doc_string: str = ""):
-        super().__init__(name, AttributeType.SPARSE_TENSOR, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
+class AttrSparseTensor(Attribute):
+    def __init__(
+        self,
+        name: str,
+        value: Sequence[_protocols.SparseTensorProtocol],
+        ref_attr_name: str = "",
+        doc_string: str = "",
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.SPARSE_TENSOR,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
 
-class AttrSparseTensors(Attribute[Sequence[SparseTensor]]):
-    def __init__(self, name: str, value: Sequence[SparseTensor], ref_attr_name: str = "", doc_string: str = ""):
-        super().__init__(name, AttributeType.SPARSE_TENSORS, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
+
+class AttrSparseTensors(Attribute):
+    def __init__(
+        self,
+        name: str,
+        value: Sequence[_protocols.SparseTensorProtocol],
+        ref_attr_name: str = "",
+        doc_string: str = "",
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.SPARSE_TENSORS,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
 
 
-# class AttrTypeProto(Attribute[Sequence[TypeProto]]):
-#     def __init__(self, name: str, value: Sequence[TypeProto], ref_attr_name: str = "", doc_string: str = ""):
-#         super().__init__(name, AttributeType.TYPE_PROTO, value, ref_attr_name=ref_attr_name, doc_string=doc_string)
+class AttrTypeProto(Attribute):
+    def __init__(
+        self,
+        name: str,
+        value: _protocols.TypeProtocol,
+        ref_attr_name: str = "",
+        doc_string: str = "",
+    ):
+        super().__init__(
+            name,
+            _enums.AttributeType.TYPE_PROTO,
+            value,
+            ref_attr_name=ref_attr_name,
+            doc_string=doc_string,
+        )
