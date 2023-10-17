02d76e8b35a5684e948171c2b41ba6652bd50876
Justin Chu <justinchuby@users.noreply.github.com>
2023-10-17 14:29:28 -0700

    Update onnx/test/version_converter/automatic_upgrade_test.py
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnx/test/version_converter/automatic_upgrade_test.py
+++ onnx/test/version_converter/automatic_upgrade_test.py
@@ -1764,7 +1764,7 @@ class TestAutomaticUpgrade(automatic_conversion_test_base.TestAutomaticConversio
         expected_tested_ops = all_op_names - excluded_ops
 
         untested_ops = expected_tested_ops - set(self.tested_ops)
-        self.assertEqual(untested_ops, {})
+        self.assertEqual(untested_ops, set())
 
 
 if __name__ == "__main__":
