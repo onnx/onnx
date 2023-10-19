8a09fd39c4b7413d6b241900a0b158fcd910303c
Justin Chu <justinchuby@users.noreply.github.com>
2023-10-19 10:32:53 -0700

    Update onnx/defs/nn/defs.cc
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnx/defs/nn/defs.cc
+++ onnx/defs/nn/defs.cc
@@ -2517,8 +2517,8 @@ static const char* LayerNormalization_ver17_doc = R"DOC(
       If `X`'s shape is `[d[0], ..., d[axis-1], d[axis], ..., d[rank-1]]`,
       the shape of `Mean` and `InvStdDev` is `[d[0], ..., d[axis-1], 1, ..., 1]`.
       `Y` and `X` have the same shape. This operator supports unidirectional broadcasting
-      (tensors `Scale` and `B` should be unidirectional broadcastable to tensor `X`); 
-      for more details please check [the doc](Broadcasting.md). 
+      (tensors `Scale` and `B` should be unidirectional broadcastable to tensor `X`);
+      for more details please check [the doc](Broadcasting.md).
 )DOC";
 
 bool BuildContextDependentFunctionBodyLayerNormalization(
