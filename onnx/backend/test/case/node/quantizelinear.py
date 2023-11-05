58eab534670f70c56a87b7547f7ae471057cb997
adrianlizarraga <adlizarraga@microsoft.com>
2023-11-05 01:26:39 -0700

    Regenerate node models for Q/DQ ops
    Signed-off-by: adrianlizarraga <adlizarraga@microsoft.com>

--- onnx/backend/test/case/node/quantizelinear.py
+++ onnx/backend/test/case/node/quantizelinear.py
@@ -64,6 +64,50 @@ class QuantizeLinear(Base):
         )
 
     @staticmethod
+    def export_e4m3fn() -> None:
+        node = onnx.helper.make_node(
+            "QuantizeLinear",
+            inputs=["x", "y_scale", "y_zero_point"],
+            outputs=["y"],
+        )
+
+        x = np.array([0.0, 1.0, 2.0, 100000.0, 200.0]).astype(np.float32)
+        y_scale = np.float32(2)
+        y_zero_point = make_tensor("zero_point", TensorProto.FLOAT8E4M3FN, [1], [0])
+        y = make_tensor(
+            "zero_point", TensorProto.FLOAT8E4M3FN, [5], [0, 0.5, 1, 448, 96]
+        )
+
+        expect(
+            node,
+            inputs=[x, y_scale, y_zero_point],
+            outputs=[y],
+            name="test_quantizelinear_e4m3fn",
+        )
+
+    @staticmethod
+    def export_e5m2() -> None:
+        node = onnx.helper.make_node(
+            "QuantizeLinear",
+            inputs=["x", "y_scale", "y_zero_point"],
+            outputs=["y"],
+        )
+
+        x = np.array([0.0, 1.0, 2.0, 100000.0, 200.0]).astype(np.float32)
+        y_scale = np.float32(2)
+        y_zero_point = make_tensor("zero_point", TensorProto.FLOAT8E5M2, [1], [0.0])
+        y = make_tensor(
+            "zero_point", TensorProto.FLOAT8E5M2, [5], [0, 0.5, 1, 49152, 96]
+        )
+
+        expect(
+            node,
+            inputs=[x, y_scale, y_zero_point],
+            outputs=[y],
+            name="test_quantizelinear_e5m2",
+        )
+
+    @staticmethod
     def export_uint16() -> None:
         node = onnx.helper.make_node(
             "QuantizeLinear",
@@ -171,46 +215,3 @@ class QuantizeLinear(Base):
             name="test_quantizelinear_int16",
         )
 
-    @staticmethod
-    def export_e4m3fn() -> None:
-        node = onnx.helper.make_node(
-            "QuantizeLinear",
-            inputs=["x", "y_scale", "y_zero_point"],
-            outputs=["y"],
-        )
-
-        x = np.array([0.0, 1.0, 2.0, 100000.0, 200.0]).astype(np.float32)
-        y_scale = np.float32(2)
-        y_zero_point = make_tensor("zero_point", TensorProto.FLOAT8E4M3FN, [1], [0])
-        y = make_tensor(
-            "zero_point", TensorProto.FLOAT8E4M3FN, [5], [0, 0.5, 1, 448, 96]
-        )
-
-        expect(
-            node,
-            inputs=[x, y_scale, y_zero_point],
-            outputs=[y],
-            name="test_quantizelinear_e4m3fn",
-        )
-
-    @staticmethod
-    def export_e5m2() -> None:
-        node = onnx.helper.make_node(
-            "QuantizeLinear",
-            inputs=["x", "y_scale", "y_zero_point"],
-            outputs=["y"],
-        )
-
-        x = np.array([0.0, 1.0, 2.0, 100000.0, 200.0]).astype(np.float32)
-        y_scale = np.float32(2)
-        y_zero_point = make_tensor("zero_point", TensorProto.FLOAT8E5M2, [1], [0.0])
-        y = make_tensor(
-            "zero_point", TensorProto.FLOAT8E5M2, [5], [0, 0.5, 1, 49152, 96]
-        )
-
-        expect(
-            node,
-            inputs=[x, y_scale, y_zero_point],
-            outputs=[y],
-            name="test_quantizelinear_e5m2",
-        )
