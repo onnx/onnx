97f5d1c2a6db55de80f2cd325d02f56645395452
adrianlizarraga <adlizarraga@microsoft.com>
2023-11-06 08:43:03 -0800

    Fix type annotation again
    Signed-off-by: adrianlizarraga <adlizarraga@microsoft.com>

--- onnx/reference/ops/op_quantize_linear.py
+++ onnx/reference/ops/op_quantize_linear.py
@@ -28,7 +28,7 @@ from onnx.reference.op_run import OpRun
 class _CommonQuantizeLinear(OpRun):
     float32_to_float8e4m3 = np.vectorize(float32_to_float8e4m3)
     float32_to_float8e5m2 = np.vectorize(float32_to_float8e5m2)
-    quant_integer_ranges: ClassVar[dict[str, TensorProto.DataType]] = {
+    quant_integer_ranges: ClassVar[dict[TensorProto.DataType, tuple[int]]] = {
         TensorProto.UINT8: (0, 255),
         TensorProto.INT8: (-128, 127),
         TensorProto.UINT16: (0, 65535),
