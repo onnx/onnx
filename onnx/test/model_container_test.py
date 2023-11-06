ae34233a121bcc236e633cdfd0db130cfb0c4e3b
Xavier Dupre <xadupre@microsoft.com>
2023-11-06 10:54:15 +0100

    rename a variable
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>

--- onnx/test/model_container_test.py
+++ onnx/test/model_container_test.py
@@ -108,8 +108,8 @@ class TestLargeOnnx(unittest.TestCase):
             copy = onnx.model_container.ModelContainer()
             copy.load(filename)
             copy.check_model()
-            copy2 = onnx.load_model(filename, load_external_data=True)
-            onnx.checker.check_model(copy2)
+            loaded_model = onnx.load_model(filename, load_external_data=True)
+            onnx.checker.check_model(loaded_model)
 
     def test_large_multi_files(self):
         large_model = _large_linear_regression()
@@ -122,11 +122,14 @@ class TestLargeOnnx(unittest.TestCase):
             onnx.checker.check_model(copy)
             for tensor in ext_data._get_all_tensors(copy):
                 if ext_data.uses_external_data(tensor):
+                    tested = 0
                     for ext in tensor.external_data:
                         if ext.key == "location":  # type: ignore[attr-defined]
                             assert os.path.exists(ext.value)
-            copy2 = onnx.load_model(filename, load_external_data=True)
-            onnx.checker.check_model(copy2)
+                            tested += 1
+                    self.assertEqual(tested, 1)
+            loaded_model = onnx.load_model(filename, load_external_data=True)
+            onnx.checker.check_model(loaded_model)
 
 
 if __name__ == "__main__":
