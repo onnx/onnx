cbc6569d43438ec4753a7a0deb86b79d47b6e50e
Xavier Dupré <xadupre@users.noreply.github.com>
2023-11-13 18:06:22 +0100

    Update onnx/backend/test/runner/__init__.py
    Co-authored-by: Justin Chu <justinchuby@users.noreply.github.com>
    Signed-off-by: Xavier Dupré <xadupre@users.noreply.github.com>

--- onnx/backend/test/runner/__init__.py
+++ onnx/backend/test/runner/__init__.py
@@ -200,7 +200,7 @@ class Runner:
             if isinstance(outputs[i], (list, tuple)):
                 if not isinstance(ref_outputs[i], (list, tuple)):
                     raise AssertionError(
-                        f"Unexpected type {type(outputs[i])} for outputs[{i}] but expected "
+                        f"Unexpected type {type(outputs[i])} for outputs[{i}]. Expected "
                         f"type is {type(ref_outputs[i])}."
                     )
                 for j in range(len(outputs[i])):
