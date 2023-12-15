6231235bd3e482e57eafbe0b5711db4e3dde1802
Justin Chu <justinchuby@users.noreply.github.com>
2023-12-15 09:00:14 -0800

    Update checker_test.py
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnx/test/checker_test.py
+++ onnx/test/checker_test.py
@@ -1,7 +1,8 @@
 # Copyright (c) ONNX Project Contributors
+
+# SPDX-License-Identifier: Apache-2.0
 import os
 import tempfile
-# SPDX-License-Identifier: Apache-2.0
 import unittest
 from typing import Sequence
 
@@ -1059,18 +1060,17 @@ class TestChecker(unittest.TestCase):
         # Should not throw an error
         checker.check_model(model, full_check=True)
 
-    def test_check_model_unicode_onnx_name(self) -> None:
-        # create temp model file
-        input_tensor = helper.make_tensor_value_info('input', onnx.TensorProto.FLOAT, [1])
-        output_tensor = helper.make_tensor_value_info('output', onnx.TensorProto.FLOAT, [1])
-        node_def = helper.make_node('Identity', ['input'], ['output'])
-        graph_def = helper.make_graph([node_def], 'simple_model', [input_tensor], [output_tensor])
-        model_def = helper.make_model(graph_def, producer_name='simple_onnx_generator')
+    def test_check_model_supports_unicode_path(self):
+        input_tensor = helper.make_tensor_value_info("input", onnx.TensorProto.FLOAT, [1])
+        output_tensor = helper.make_tensor_value_info("output", onnx.TensorProto.FLOAT, [1])
+        node = helper.make_node("Identity", ["input"], ["output"])
+        graph = helper.make_graph([node], "test", [input_tensor], [output_tensor])
+        model = helper.make_model(graph, producer_name="test")
 
         with tempfile.TemporaryDirectory() as temp_dir:
             unicode_model_path = os.path.join(temp_dir, "模型モデル모델✨.onnx")
-            onnx.save(model_def, unicode_model_path)
-            checker.check_model(unicode_model_path, True, True)
+            onnx.save(model, unicode_model_path)
+            checker.check_model(unicode_model_path, full_check=True)
 
 
 if __name__ == "__main__":
