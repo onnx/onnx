d5121eb17b17d197f3cdb380160195ddefe1d31c
Yuan Yao <yuanyao@nvidia.com>
2023-12-29 22:03:09 +0000

    rename vbl names in adapter code
    Signed-off-by: Yuan Yao <yuanyao@nvidia.com>

--- onnx/version_converter/adapters/group_normalization_20_21.h
+++ onnx/version_converter/adapters/group_normalization_20_21.h
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <memory>
+
 #include "onnx/version_converter/adapters/adapter.h"
 
 namespace ONNX_NAMESPACE {
@@ -31,42 +33,42 @@ class GroupNormalization_20_21 final : public Adapter {
     shape->addInput(node->inputs()[input_id]);
     shape->insertBefore(node);
 
-    Tensor t0;
-    t0.elem_type() = TensorProto_DataType_INT64;
+    Tensor tensor_num_grp;
+    tensor_num_grp.elem_type() = TensorProto_DataType_INT64;
     int64_t num_groups = node->i(knum_groups);
-    t0.sizes() = {1};
-    t0.int64s() = {num_groups};
-    Node* constant0 = graph->create(kConstant);
-    constant0->t_(kvalue, t0);
-    constant0->insertBefore(node);
+    tensor_num_grp.sizes() = {1};
+    tensor_num_grp.int64s() = {num_groups};
+    Node* constant_num_grp = graph->create(kConstant);
+    constant_num_grp->t_(kvalue, tensor_num_grp);
+    constant_num_grp->insertBefore(node);
 
     Node* div = graph->create(kDiv);
     div->addInput(shape->output());
-    div->addInput(constant0->output());
+    div->addInput(constant_num_grp->output());
     div->insertBefore(node);
 
-    Tensor t1;
-    t1.elem_type() = TensorProto_DataType_INT64;
-    t1.sizes() = {2};
-    t1.int64s() = {-1, 1};
-    Node* constant1 = graph->create(kConstant);
-    constant1->t_(kvalue, t1);
-    constant1->insertBefore(node);
+    Tensor tensor_reshape0_shape;
+    tensor_reshape0_shape.elem_type() = TensorProto_DataType_INT64;
+    tensor_reshape0_shape.sizes() = {2};
+    tensor_reshape0_shape.int64s() = {-1, 1};
+    Node* constant_reshape0_shape = graph->create(kConstant);
+    constant_reshape0_shape->t_(kvalue, tensor_reshape0_shape);
+    constant_reshape0_shape->insertBefore(node);
     Node* reshape0 = graph->create(kReshape);
     reshape0->addInput(node->inputs()[1]);
-    reshape0->addInput(constant1->output());
+    reshape0->addInput(constant_reshape0_shape->output());
     reshape0->insertBefore(node);
 
-    Tensor t2;
-    t2.elem_type() = TensorProto_DataType_INT64;
-    t2.sizes() = {1};
-    t2.int64s() = {1};
-    Node* constant2 = graph->create(kConstant);
-    constant2->t_(kvalue, t2);
-    constant2->insertBefore(node);
+    Tensor tensor_one;
+    tensor_one.elem_type() = TensorProto_DataType_INT64;
+    tensor_one.sizes() = {1};
+    tensor_one.int64s() = {1};
+    Node* constant_one = graph->create(kConstant);
+    constant_one->t_(kvalue, tensor_one);
+    constant_one->insertBefore(node);
     Node* concat = graph->create(kConcat);
     concat->i_(kaxis, 0);
-    concat->addInput(constant2->output());
+    concat->addInput(constant_one->output());
     concat->addInput(div->output());
     concat->insertBefore(node);
     Node* expand = graph->create(kExpand);
@@ -74,16 +76,16 @@ class GroupNormalization_20_21 final : public Adapter {
     expand->addInput(concat->output());
     expand->insertBefore(node);
 
-    Tensor t3;
-    t3.elem_type() = TensorProto_DataType_INT64;
-    t3.sizes() = {1};
-    t3.int64s() = {-1};
-    Node* constant3 = graph->create(kConstant);
-    constant3->t_(kvalue, t3);
-    constant3->insertBefore(node);
+    Tensor tensor_reshape1_shape;
+    tensor_reshape1_shape.elem_type() = TensorProto_DataType_INT64;
+    tensor_reshape1_shape.sizes() = {1};
+    tensor_reshape1_shape.int64s() = {-1};
+    Node* constant_reshape1_shape = graph->create(kConstant);
+    constant_reshape1_shape->t_(kvalue, tensor_reshape1_shape);
+    constant_reshape1_shape->insertBefore(node);
     Node* reshape1 = graph->create(kReshape);
     reshape1->addInput(expand->output());
-    reshape1->addInput(constant3->output());
+    reshape1->addInput(constant_reshape1_shape->output());
     reshape1->insertBefore(node);
 
     node->replaceInput(input_id, reshape1->output());
