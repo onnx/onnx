9d3f8d55cd2a4ca61fe5987fcdcc43ddb1c3cef9
opluss <opluss@qq.com>
2024-03-03 10:55:42 +0800

    fix bug and add unittest
    Signed-off-by: opluss <opluss@qq.com>

--- onnx/defs/schema.cc
+++ onnx/defs/schema.cc
@@ -108,10 +108,28 @@ OpSchemaRegistry* OpSchemaRegistry::Instance() {
 void OpSchema::CheckInputOutputType(struct InferenceContext& ctx) const {
   std::unordered_map<std::string, std::string> type_constraints;
   if (inputs_.empty() && ctx.getNumInputs() > 0) {
-    fail_check("Node with type: \"", Name(), "\" need empty inputs, but got ", ctx.getNumInputs(), " in graph");
+    fail_check(
+        "Node (",
+        domain(),
+        "::",
+        Name(),
+        ":",
+        since_version(),
+        ") need empty inputs, but got ",
+        ctx.getNumInputs(),
+        " in graph");
   }
   if (outputs_.empty() && ctx.getNumOutputs() > 0) {
-    fail_check("Node with type: \"", Name(), "\" need empty outputs, but got ", ctx.getNumInputs(), " in graph");
+    fail_check(
+        "Node (",
+        domain(),
+        "::",
+        Name(),
+        ":",
+        since_version(),
+        ") need empty outputs, but got ",
+        ctx.getNumOutputs(),
+        " in graph");
   }
   // check all input types
   for (size_t in_idx = 0; in_idx < ctx.getNumInputs(); ++in_idx) {
