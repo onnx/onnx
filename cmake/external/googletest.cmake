5cb372f074964ff0a7ba6d513bba3f8e2b44f551
Justin Chu <justinchu@microsoft.com>
2024-04-10 20:46:09 -0700

    try again
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- cmake/external/googletest.cmake
+++ cmake/external/googletest.cmake
@@ -4,10 +4,24 @@
 
 include (ExternalProject)
 
-set(googletest_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/googletest/src/googletest/googletest/googletest/include)
+set(googletest_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/googletest/src/googletest/googletest/include)
 set(googletest_URL https://github.com/google/googletest.git)
 # https://github.com/google/googletest/commit/530d5c8c84abd2a46f38583ee817743c9b3a42b4
 set(googletest_TAG 530d5c8c84abd2a46f38583ee817743c9b3a42b4)
+
+if(WIN32 AND CMAKE_BUILD_TYPE MATCHES Debug)
+  set(googletest_STATIC_LIBRARIES
+      ${CMAKE_CURRENT_BINARY_DIR}/googletest/build/Debug/gtestd.lib)
+elseif(WIN32 AND NOT CMAKE_BUILD_TYPE MATCHES Debug)
+  set(googletest_STATIC_LIBRARIES
+      ${CMAKE_CURRENT_BINARY_DIR}/googletest/build/Release/gtest.lib)
+elseif(CMAKE_BUILD_TYPE MATCHES Debug)
+  set(googletest_STATIC_LIBRARIES
+      ${CMAKE_CURRENT_BINARY_DIR}/googletest/build/libgtest.a)
+else()
+  set(googletest_STATIC_LIBRARIES
+      ${CMAKE_CURRENT_BINARY_DIR}/googletest/build/libgtest.a)
+endif()
 
 if(ONNX_USE_MSVC_STATIC_RUNTIME)
   set(ONNX_USE_MSVC_SHARED_RUNTIME OFF)
@@ -18,7 +32,7 @@ ExternalProject_Add(googletest
     PREFIX googletest
     GIT_REPOSITORY ${googletest_URL}
     GIT_TAG ${googletest_TAG}
-    BUILD_IN_SOURCE 1
+    BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/googletest/build
     # https://cmake.org/cmake/help/latest/module/ExternalProject.html
     # SOURCE_DIR (default) = <prefix>/src/<name>
     BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE} --target gtest
