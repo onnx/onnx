def2af1f4ffa5c27b33b13823c21e48b3753246b
Justin Chu <justinchu@microsoft.com>
2024-04-10 20:47:32 -0700

    fetch
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- cmake/external/googletest.cmake
+++ cmake/external/googletest.cmake
@@ -2,44 +2,12 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-include (ExternalProject)
-
-set(googletest_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/googletest/src/googletest/googletest/include)
-set(googletest_URL https://github.com/google/googletest.git)
-# https://github.com/google/googletest/commit/530d5c8c84abd2a46f38583ee817743c9b3a42b4
-set(googletest_TAG 530d5c8c84abd2a46f38583ee817743c9b3a42b4)
-
-if(WIN32 AND CMAKE_BUILD_TYPE MATCHES Debug)
-  set(googletest_STATIC_LIBRARIES
-      ${CMAKE_CURRENT_BINARY_DIR}/googletest/build/Debug/gtestd.lib)
-elseif(WIN32 AND NOT CMAKE_BUILD_TYPE MATCHES Debug)
-  set(googletest_STATIC_LIBRARIES
-      ${CMAKE_CURRENT_BINARY_DIR}/googletest/build/Release/gtest.lib)
-elseif(CMAKE_BUILD_TYPE MATCHES Debug)
-  set(googletest_STATIC_LIBRARIES
-      ${CMAKE_CURRENT_BINARY_DIR}/googletest/build/libgtest.a)
-else()
-  set(googletest_STATIC_LIBRARIES
-      ${CMAKE_CURRENT_BINARY_DIR}/googletest/build/libgtest.a)
-endif()
-
-if(ONNX_USE_MSVC_STATIC_RUNTIME)
-  set(ONNX_USE_MSVC_SHARED_RUNTIME OFF)
-else()
-  set(ONNX_USE_MSVC_SHARED_RUNTIME ON)
-endif()
-ExternalProject_Add(googletest
-    PREFIX googletest
-    GIT_REPOSITORY ${googletest_URL}
-    GIT_TAG ${googletest_TAG}
-    BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/googletest/build
-    # https://cmake.org/cmake/help/latest/module/ExternalProject.html
-    # SOURCE_DIR (default) = <prefix>/src/<name>
-    BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE} --target gtest
-    INSTALL_COMMAND ""
-    CMAKE_CACHE_ARGS
-        -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-        -DBUILD_GMOCK:BOOL=OFF
-        -DBUILD_GTEST:BOOL=ON
-        -Dgtest_force_shared_crt:BOOL=${ONNX_USE_MSVC_SHARED_RUNTIME}
+include(FetchContent)
+FetchContent_Declare(
+  googletest
+  # Specify the commit you depend on and update it regularly.
+  URL https://github.com/google/googletest/archive/5376968f6948923e2411081fd9372e71a59d8e77.zip
 )
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
