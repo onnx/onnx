262ee31a355f1916d8ad45fcd185c3bd8c6c4a16
Justin Chu <justinchu@microsoft.com>
2024-04-10 19:53:40 -0700

    this
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- cmake/external/googletest.cmake
+++ cmake/external/googletest.cmake
@@ -18,10 +18,10 @@ elseif(WIN32 AND NOT CMAKE_BUILD_TYPE MATCHES Debug)
       ${CMAKE_CURRENT_BINARY_DIR}/googletest/src/googletest/googletest/Release/gtest.lib)
 elseif(CMAKE_BUILD_TYPE MATCHES Debug)
   set(googletest_STATIC_LIBRARIES
-      ${CMAKE_CURRENT_BINARY_DIR}/../lib/libgtest.a)
+      ${CMAKE_CURRENT_BINARY_DIR}/googletest/src/googletest/googletest/libgtestd.a)
 else()
   set(googletest_STATIC_LIBRARIES
-      ${CMAKE_CURRENT_BINARY_DIR}/../lib/libgtest.a)
+      ${CMAKE_CURRENT_BINARY_DIR}/googletest/src/googletest/googletest/libgtest.a)
 endif()
 
 if(ONNX_USE_MSVC_STATIC_RUNTIME)
@@ -34,7 +34,6 @@ ExternalProject_Add(googletest
     GIT_REPOSITORY ${googletest_URL}
     GIT_TAG ${googletest_TAG}
     DOWNLOAD_DIR "${DOWNLOAD_LOCATION}"
-    BUILD_IN_SOURCE 1
     BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE} --target gtest
     INSTALL_COMMAND ""
     CMAKE_CACHE_ARGS
@@ -42,5 +41,4 @@ ExternalProject_Add(googletest
         -DBUILD_GMOCK:BOOL=OFF
         -DBUILD_GTEST:BOOL=ON
         -Dgtest_force_shared_crt:BOOL=${ONNX_USE_MSVC_SHARED_RUNTIME}
-    BUILD_BYPRODUCTS ${googletest_STATIC_LIBRARIES}
 )
