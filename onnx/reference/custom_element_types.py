f4f9c26b6a3a1a71123976d9ba450a0897a870cf
Xavier Dupre <xadupre@microsoft.com>
2024-06-13 17:35:10 +0200

    yupdate
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>

--- onnx/reference/custom_element_types.py
+++ onnx/reference/custom_element_types.py
@@ -20,57 +20,49 @@ int4 = np.dtype((np.int8, {"int4": (np.int8, 0)}))
 
 
 def convert_from_ml_dtypes(array: np.ndarray) -> np.ndarray:
-    """If package *ml_dtypes* is installed,
-    this function detects the type and changes into one of the custom type
-    defined in this module.
-    The function returns tensor if numpy supports the type,
-    raises an exception if ml_dtypes is not installed.
-
+    """Detects the type and changes into one of the ONNX
+    defined custom types when ``ml_dtypes`` is installed.
+    
     Args:
-        tensor: Numpy array with a dtype from ml_dtypes.
+        array: Numpy array with a dtype from ml_dtypes.
 
     Returns:
         numpy array
     """
     if not ml_dtypes:
-        return tensor
-    dt = tensor.dtype
+        return array
+    dt = array.dtype
     new_dt = None
     if dt == ml_dtypes.bfloat16:
         new_dt = bfloat16
 
     if new_dt:
-        b = tensor.tobytes()
-        return np.frombuffer(b, dtype=new_dt).reshape(tensor.shape)
+        return array.view(dtype=new_dt).reshape(array.shape)
 
-    return tensor
+    return array
 
 
-def convert_to_ml_dtypes(tensor: np.ndarray) -> np.ndarray:
-    """If package *ml_dtypes* is installed,
-    this fonction changes dtype into the corresponding
-    dtype so that numpy can do simple operations.
-    The function returns tensor if numpy supports the type,
-    raises an exception if ml_dtypes is not installed.
-
+def convert_to_ml_dtypes(array: np.ndarray) -> np.ndarray:
+    """Detects the type and changes into one of the type
+    defined in ``ml_dtypes`` if installed.
+    
     Args:
-        tensor: numpy array
+        array: array
 
     Returns:
-        numpy array with a dtype from ml_dtypes
+        numpy Numpy array with a dtype from ml_dtypes.
     """
-    dt = tensor.dtype
+    dt = array.dtype
     new_dt = None
-    if dt == bfloat16 and tensor.dtype.descr[0][0] == "bfloat16":
+    if dt == bfloat16 and array.dtype.descr[0][0] == "bfloat16":
         assert ml_dtypes, (
             f"ml_dtypes is not installed and the tensor cannot "
-            f"be converted into ml_dtypes.{tensor.dtype.descr[0][0]}"
+            f"be converted into ml_dtypes.{array.dtype.descr[0][0]}"
         )
 
         new_dt = ml_dtypes.bfloat16
 
     if new_dt:
-        b = tensor.tobytes()
-        return np.frombuffer(b, dtype=new_dt).reshape(tensor.shape)
+        return array.view(dtype=new_dt).reshape(array.shape)
 
-    return tensor
+    return array
