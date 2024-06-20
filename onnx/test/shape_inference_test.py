bb7bd84b9dd248b5c48f5577b45e439f1328cb84
Xavier Dupre <xadupre@microsoft.com>
2024-06-20 13:29:18 +0200

    lint
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>

--- onnx/test/shape_inference_test.py
+++ onnx/test/shape_inference_test.py
@@ -9332,8 +9332,11 @@ class TestShapeInference(TestShapeInferenceHelper):
                         ),
                     ),
                     make_node(
-                        "DFT", ["input", "dft_length", ""], ["output"], **attributes
-                    ),  # type: ignore[arg-type]
+                        "DFT",
+                        ["input", "dft_length", ""],
+                        ["output"],
+                        **attributes,  # type: ignore[arg-type]
+                    ),
                 ]
                 value_infos = [
                     make_tensor_value_info("dft_length", TensorProto.INT64, ())
