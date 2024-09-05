ee791e084b6a605847147bed8456c7b34af14ea1
G. Ramalingam <grama@microsoft.com>
2024-09-05 16:44:12 -0700

    Extend printer and parser to support invalid identifiers (#6346)
    Many onnx models used in practice use tensor names that are not valid C
    identifiers (as required by the ONNX standard). This PR extends the
    printer and parser to support such identifiers.
    
    (It may be useful to relax the ONNX standard's requirements in this
    regard, but that's a separate issue, requiring some community
    discussion.)
    
    ---------
    
    Signed-off-by: Ganesan Ramalingam <grama@microsoft.com>
    Signed-off-by: Linsho Kaku <linsho@preferred.jp>

--- onnx/defs/parser.cc
+++ onnx/defs/parser.cc
@@ -150,12 +150,12 @@ bool ParserBase::NextIsValidFloatString() {
 Status OnnxParser::Parse(IdList& idlist) {
   idlist.Clear();
   std::string id;
-  ParseOptionalIdentifier(id);
+  ParseQuotableIdentifier(id);
   if (id.empty())
     return Status::OK(); // Treat as empty list of identifiers
   *idlist.Add() = id;
   while (Matches(',')) {
-    ParseOptionalIdentifier(id);
+    ParseQuotableIdentifier(id);
     *idlist.Add() = id;
   }
   return Status::OK();
@@ -175,7 +175,7 @@ Status OnnxParser::Parse(IdList& idlist, AttrList& attrlist) {
   attrlist.Clear();
   do {
     std::string id;
-    ParseIdentifier(id);
+    ParseQuotableIdentifier(id);
     auto next = NextChar();
     if (next == ':' || next == '=')
       Parse(*attrlist.Add(), id);
@@ -311,7 +311,7 @@ Status OnnxParser::Parse(ValueInfoProto& valueinfo) {
   if (NextIsType())
     PARSE(*valueinfo.mutable_type());
   std::string name;
-  CHECK_PARSER_STATUS(ParseIdentifier(name));
+  CHECK_PARSER_STATUS(ParseQuotableIdentifier(name));
   valueinfo.set_name(name);
   return Status::OK();
 }
@@ -351,7 +351,7 @@ Status OnnxParser::ParseFunctionInputOutput(IdList& idlist, ValueInfoList& vilis
         vi = vilist.Add();
         PARSE(*(vi->mutable_type()));
       }
-      CHECK_PARSER_STATUS(ParseIdentifier(*name));
+      CHECK_PARSER_STATUS(ParseQuotableIdentifier(*name));
       if (vi != nullptr)
         vi->set_name(*name);
     } while (Matches(','));
@@ -549,7 +549,7 @@ Status OnnxParser::ParseSingleAttributeValue(AttributeProto& attr, AttributeProt
       if ((next == '{') || (next == '=') || (NextIsIdentifier())) {
         attr.set_type(AttributeProto_AttributeType_TENSOR);
         auto& tensorProto = *attr.mutable_t();
-        ParseOptionalIdentifier(*tensorProto.mutable_name());
+        ParseQuotableIdentifier(*tensorProto.mutable_name());
         (void)Matches('='); // Optional, to unify handling of initializers
         Parse(tensorProto, typeProto);
       } else {
@@ -569,7 +569,7 @@ Status OnnxParser::ParseSingleAttributeValue(AttributeProto& attr, AttributeProt
     }
   } else if (Matches('@')) {
     std::string name;
-    CHECK_PARSER_STATUS(ParseIdentifier(name));
+    CHECK_PARSER_STATUS(ParseQuotableIdentifier(name));
     attr.set_ref_attr_name(name);
   } else {
     Literal literal;
@@ -753,7 +753,7 @@ Status OnnxParser::Parse(NodeList& nodelist) {
 
 Status OnnxParser::Parse(GraphProto& graph) {
   std::string id;
-  ParseIdentifier(id);
+  ParseQuotableIdentifier(id);
   return Parse(id, graph);
 }
 
@@ -799,7 +799,7 @@ Status OnnxParser::Parse(FunctionProto& fn) {
     MATCH('>');
   }
   std::string id;
-  ParseIdentifier(id);
+  ParseQuotableIdentifier(id);
   fn.set_name(id);
 
   PARSE('<', *fn.mutable_attribute(), *fn.mutable_attribute_proto(), '>');
