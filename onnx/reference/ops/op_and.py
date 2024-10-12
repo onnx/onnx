6cd7f3a94ef061e7b2299929527785cd7038f0cd
xadupre <xadupre@microsoft.com>
2024-10-12 15:58:47 +0200

    First try to support torch Tensor for the reference evaluator
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- onnx/reference/ops/op_and.py
+++ onnx/reference/ops/op_and.py
@@ -3,11 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 from __future__ import annotations
 
-import numpy as np
-
+from onnx.reference import apimod
 from onnx.reference.ops._op import OpRunBinary
 
 
 class And(OpRunBinary):
     def _run(self, x, y):  # type: ignore
-        return (np.logical_and(x, y),)
+        return (apimod(x).logical_and(x, y),)
