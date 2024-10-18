26a4b99f18555b464a02ae06afb3ce0703a86f0a
Po-Chu Hsu <tonypottera@gmail.com>
2024-10-17 21:25:57 -0700

    optimized collect_new_io_core
    Signed-off-by: Po-Chu Hsu <tonypottera@gmail.com>

--- onnx/utils.py
+++ onnx/utils.py
@@ -24,19 +24,16 @@ class Extractor:
         return {obj.name: obj for obj in objs}
 
     def _collect_new_io_core(self, original_io, io_names_to_extract):  # type: ignore
+        if len(io_names_to_extract) != len(set(io_names_to_extract)):
+            raise ValueError("Duplicate names found in the io_names_to_extract.")
         original_io_map = self._build_name2obj_dict(original_io)
-        original_io_names = set(original_io_map)
-        s_io_names_to_extract = set(io_names_to_extract)
-        io_names_to_keep = s_io_names_to_extract & original_io_names
-        new_io_names_to_add = s_io_names_to_extract - original_io_names
-
-        new_io_tensors = [original_io_map[name] for name in io_names_to_keep]
-        # activation become input or output
-        new_io_tensors.extend(self.vimap[name] for name in new_io_names_to_add)
-
-        # adjust sequence
-        new_io_tensors_map = self._build_name2obj_dict(new_io_tensors)
-        return [new_io_tensors_map[name] for name in io_names_to_extract]
+        new_io_tensors = []
+        for io_name_to_extract in io_names_to_extract:  # assume no duplicates
+            if io_name_to_extract in original_io_map:
+                new_io_tensors.append(original_io_map[io_name_to_extract])
+            else:
+                new_io_tensors.append(self.vimap[io_name_to_extract])
+        return new_io_tensors  # same order as io_names_to_extract
 
     def _collect_new_inputs(self, names: list[str]) -> list[ValueInfoProto]:
         return self._collect_new_io_core(self.graph.input, names)  # type: ignore
