8ddaefa30accaa3263efe191fcc3c50db0f02e66
Po-Chu Hsu <tonypottera@gmail.com>
2024-10-22 19:20:19 -0700

    add infer_shapes_path back
    Signed-off-by: Po-Chu Hsu <tonypottera@gmail.com>

--- onnx/shape_inference.py
+++ onnx/shape_inference.py
@@ -66,6 +66,44 @@ def infer_shapes(  # type: ignore[return]
         return onnx.load_from_string(inferred_model_str)
 
 
+def infer_shapes_path(
+    model_path: str | os.PathLike,
+    output_path: str | os.PathLike = "",
+    check_type: bool = False,
+    strict_mode: bool = False,
+    data_prop: bool = False,
+) -> None:
+    """Take model path for shape_inference.
+
+    This function is the same as :func:`infer_shape` but supports >2GB models.
+    The function outputs the inferred model to the `output_path`. The original model path
+    is used if not specified.
+    """
+    if isinstance(model_path, ModelProto):
+        raise TypeError(
+            "infer_shapes_path only accepts model Path (String),"
+            "you can use infer_shapes for the ModelProto."
+        )
+    try:
+        model_path = os.fspath(model_path)
+    except TypeError as exp:
+        raise TypeError(
+            "infer_shapes_path only accepts model path as a string or PathLike, "
+            f"incorrect model path type: {type(model_path)}"
+        ) from exp
+    try:
+        output_path = os.fspath(output_path)
+    except TypeError as exp:
+        raise TypeError(
+            "infer_shapes_path only accepts output path as a string or PathLike, "
+            f"incorrect output path type: {type(output_path)}"
+        ) from exp
+
+    if output_path == "":
+        output_path = model_path
+    C.infer_shapes_path(model_path, output_path, check_type, strict_mode, data_prop)
+
+
 def infer_node_outputs(
     schema: onnx.defs.OpSchema,
     node: onnx.NodeProto,
