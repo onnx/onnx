2ae8f377bc9779497e2f457349f9a3cef13071de
Andreas Fehlner <fehlner@arcor.de>
2024-11-15 07:08:05 +0100

    Introduction of the possibility to start devbuilds manually and to publish the artifacts to pypi/testpypi (#6540)
    ### Description
    * Introduction of the possibility to start devbuilds manually and to
    publish the artifacts to pypi/testpypi
    * Harmonization of code and wording in various places
    
    ### Motivation and Context
    Currently there is only one attempt per week to build a devbuild,
    currently called onnx-weekly via the pipeline, if there are now errors
    in the pipeline or at the interfaces, you only have one attempt per
    week...
    
    An error for which this functionality would have been helpful
    https://github.com/onnx/onnx/issues/6499
    
    ---------
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    Co-authored-by: Justin Chu <justinchuby@users.noreply.github.com>

--- .github/workflows/manylinux/entrypoint.sh
+++ .github/workflows/manylinux/entrypoint.sh
@@ -41,7 +41,7 @@ export CMAKE_ARGS="-DONNX_USE_LITE_PROTO=ON"
 $PIP_INSTALL_COMMAND -r requirements-release.txt || { echo "Installing Python requirements failed."; exit 1; }
 
 # Build wheels
-if [ "$GITHUB_EVENT_NAME" == "schedule" ]; then
+if [ "$GITHUB_EVENT_NAME" == "schedule" ] || [ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ]; then
     sed -i 's/name = "onnx"/name = "onnx-weekly"/' 'pyproject.toml'
     ONNX_PREVIEW_BUILD=1 $PYTHON_COMMAND -m build --wheel || { echo "Building wheels failed."; exit 1; }
 else
