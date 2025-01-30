73fe38a3acc45fe8c31085e5930e8b2909ab6bd6
Bilyana Indzheva <bilyana2002@gmail.com>
2025-01-30 17:05:32 +0200

    initial tests
    Signed-off-by: Bilyana Indzheva <bilyana2002@gmail.com>

--- onnx/backend/test/case/node/less.py
+++ onnx/backend/test/case/node/less.py
@@ -24,6 +24,36 @@ class Less(Base):
         z = np.less(x, y)
         expect(node, inputs=[x, y], outputs=[z], name="test_less")
 
+        x = np.random.randn(3, 4, 5).astype(np.int8)
+        y = np.random.randn(3, 4, 5).astype(np.int8)
+        z = np.less(x, y)
+        expect(node, inputs=[x, y], outputs=[z], name="test_less_int8")
+
+        x = np.random.randn(3, 4, 5).astype(np.int16)
+        y = np.random.randn(3, 4, 5).astype(np.int16)
+        z = np.less(x, y)
+        expect(node, inputs=[x, y], outputs=[z], name="test_less_int16")
+
+        x = np.random.randn(3, 4, 5).astype(np.uint8)
+        y = np.random.randn(3, 4, 5).astype(np.uint8)
+        z = np.less(x, y)
+        expect(node, inputs=[x, y], outputs=[z], name="test_less_uint8")
+
+        x = np.random.randn(3, 4, 5).astype(np.uint16)
+        y = np.random.randn(3, 4, 5).astype(np.uint16)
+        z = np.less(x, y)
+        expect(node, inputs=[x, y], outputs=[z], name="test_less_uint16")
+
+        x = np.random.randn(3, 4, 5).astype(np.uint32)
+        y = np.random.randn(3, 4, 5).astype(np.uint32)
+        z = np.less(x, y)
+        expect(node, inputs=[x, y], outputs=[z], name="test_less_uint32")
+
+        x = np.random.randn(3, 4, 5).astype(np.uint64)
+        y = np.random.randn(3, 4, 5).astype(np.uint64)
+        z = np.less(x, y)
+        expect(node, inputs=[x, y], outputs=[z], name="test_less_uint64")
+
     @staticmethod
     def export_less_broadcast() -> None:
         node = onnx.helper.make_node(
--- onnx/backend/test/case/node/less_equal.py
+++ onnx/backend/test/case/node/less_equal.py
@@ -24,6 +24,36 @@ class Less(Base):
         z = np.less_equal(x, y)
         expect(node, inputs=[x, y], outputs=[z], name="test_less_equal")
 
+        x = np.random.randn(3, 4, 5).astype(np.int8)
+        y = np.random.randn(3, 4, 5).astype(np.int8)
+        z = np.less_equal(x, y)
+        expect(node, inputs=[x, y], outputs=[z], name="test_less_equal_int8")
+
+        x = np.random.randn(3, 4, 5).astype(np.int16)
+        y = np.random.randn(3, 4, 5).astype(np.int16)
+        z = np.less_equal(x, y)
+        expect(node, inputs=[x, y], outputs=[z], name="test_less_equal_int16")
+
+        x = np.random.randn(3, 4, 5).astype(np.uint8)
+        y = np.random.randn(3, 4, 5).astype(np.uint8)
+        z = np.less_equal(x, y)
+        expect(node, inputs=[x, y], outputs=[z], name="test_less_equal_uint8")
+
+        x = np.random.randn(3, 4, 5).astype(np.uint16)
+        y = np.random.randn(3, 4, 5).astype(np.uint16)
+        z = np.less_equal(x, y)
+        expect(node, inputs=[x, y], outputs=[z], name="test_less_equal_uint16")
+
+        x = np.random.randn(3, 4, 5).astype(np.uint32)
+        y = np.random.randn(3, 4, 5).astype(np.uint32)
+        z = np.less_equal(x, y)
+        expect(node, inputs=[x, y], outputs=[z], name="test_less_equal_uint32")
+
+        x = np.random.randn(3, 4, 5).astype(np.uint64)
+        y = np.random.randn(3, 4, 5).astype(np.uint64)
+        z = np.less_equal(x, y)
+        expect(node, inputs=[x, y], outputs=[z], name="test_less_equal_uint64")
+
     @staticmethod
     def export_less_broadcast() -> None:
         node = onnx.helper.make_node(
