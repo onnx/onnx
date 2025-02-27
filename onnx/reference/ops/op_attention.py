b224eebc7e7a29d4cacb8dedf77287db4aee18b2
shubhambhokare1 <shubhambhokare@gmail.com>
2025-02-27 22:51:25 +0000

    try fix failing tests
    Signed-off-by: shubhambhokare1 <shubhambhokare@gmail.com>

--- onnx/reference/ops/op_attention.py
+++ onnx/reference/ops/op_attention.py
@@ -149,7 +149,10 @@ def _compute_attention(
     k_transpose = np.transpose(K, (0, 1, 3, 2))
     qk_matmul_output = np.matmul(Q * scale, k_transpose * scale)
     qk_with_bias = qk_matmul_output + attn_bias
-    if include_mask_in_qk_matmul_output == 1:
+    if (
+        include_mask_in_qk_matmul_output is not None
+        and include_mask_in_qk_matmul_output == 1
+    ):
         qk_matmul_output = qk_matmul_output + attn_bias
     qk_matmul_output = qk_matmul_output.astype(Q.dtype)
 
@@ -157,7 +160,9 @@ def _compute_attention(
     if softcap is not None:
         qk_with_bias = _softcap(qk_with_bias, softcap)
     if softmax_precision is not None:
-        qk_with_bias = qk_with_bias.astype(onnx.helper.tensor_dtype_to_np_dtype(softmax_precision))
+        qk_with_bias = qk_with_bias.astype(
+            onnx.helper.tensor_dtype_to_np_dtype(softmax_precision)
+        )
     qk_softmax = _softmax(qk_with_bias)
     output = np.matmul(qk_softmax, V).astype(Q.dtype)
     if input_shape_len == 3:
