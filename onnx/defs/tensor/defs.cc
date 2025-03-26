c0d7e2759a9c070fbba8cf2e298132dd49035050
Vlad Tytskiy <vlad75.76@mail.ru>
2025-03-26 11:43:22 +0300

    fix scatter nd defs

--- onnx/defs/tensor/defs.cc
+++ onnx/defs/tensor/defs.cc
@@ -1280,7 +1280,7 @@ When `reduction` is set to some reduction function `f`, `output` is calculated a
 output = np.copy(data)
 update_indices = indices.shape[:-1]
 for idx in np.ndindex(update_indices):
-    output[indices[idx]] = f(output[indices[idx]], updates[idx])
+    output[*indices[idx]] = f(output[*indices[idx]], updates[idx])
 ```
 
 where the `f` is `+`, `*`, `max` or `min` as specified.
