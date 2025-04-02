5fc860644b005adc2513f7abcc965b33cf440c30
ramkrishna2910 <ramkrishna2910@gmail.com>
2025-04-02 12:54:58 -0700

    update version number
    Signed-off-by: ramkrishna2910 <ramkrishna2910@gmail.com>

--- onnx/common/version.h
+++ onnx/common/version.h
@@ -9,6 +9,6 @@
 namespace ONNX_NAMESPACE {
 
 // Represents the most recent release version. Updated with every release.
-constexpr const char* LAST_RELEASE_VERSION = "1.17.0";
+constexpr const char* LAST_RELEASE_VERSION = "1.18.0";
 
 } // namespace ONNX_NAMESPACE
