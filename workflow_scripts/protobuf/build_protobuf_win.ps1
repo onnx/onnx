3599f1b09ce231d20f4bfa6daf014e8c8cc84445
Andreas Fehlner <fehlner@arcor.de>
2025-04-17 19:23:07 +0200

    update
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>

--- workflow_scripts/protobuf/build_protobuf_win.ps1
+++ workflow_scripts/protobuf/build_protobuf_win.ps1
@@ -24,8 +24,6 @@ mkdir build
 cd build
 $protobuf_root_dir = Get-Location
 
-
-
 cmake -G "Visual Studio 17 2022" -A $cmake_arch -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -DABSL_MSVC_STATIC_RUNTIME=OFF -DBUILD_SHARED_LIBS=OFF -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_BUILD_EXAMPLES=OFF -DABSL_ROOT_DIR="$protobuf_root_dir/../../abseil-cpp-20230802.2" -DCMAKE_CXX_STANDARD=17 -DABSL_PROPAGATE_CXX_STD=on -DCMAKE_INSTALL_PREFIX="$protobuf_install_dir" ..
 cmake --build . --config $build_type --target install
 echo "Protobuf installation complete."
