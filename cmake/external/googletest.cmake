7676f914e939e04be4417f5cbc9d4f07cd3025d7
Yuanyuan Chen <cyyever@outlook.com>
2025-05-20 04:05:34 +0800

    Update google test to v1.17.0 (#6977)
    ### Description
    <!-- - Describe your changes. -->
    And simplify test code.
    ### Motivation and Context
    Better tooling.
    <!-- - Why is this change required? What problem does it solve? -->
    <!-- - If it fixes an open issue, please link to the issue here. -->
    
    Signed-off-by: cyy <cyyever@outlook.com>
    Co-authored-by: Andreas Fehlner <fehlner@arcor.de>

--- cmake/external/googletest.cmake
+++ cmake/external/googletest.cmake
@@ -6,7 +6,7 @@ include(FetchContent)
 FetchContent_Declare(
   googletest
   # Specify the commit you depend on and update it regularly.
-  URL https://github.com/google/googletest/archive/0bdccf4aa2f5c67af967193caf31d42d5c49bde2.zip # Version from 8. March 2025, newer than v1.16.0
+  URL https://github.com/google/googletest/releases/download/v1.17.0/googletest-1.17.0.tar.gz
 )
 # For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
