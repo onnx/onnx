07fca4b431e7bdaa46546a0f43b8129ab39dc30a
G. Ramalingam <grama@microsoft.com>
2025-05-19 10:57:09 -0700

    Extend function inliner to support schema-defined functions (redo) (#6976)
    *Note*: This is a redo of the previous PR #6931 
    
    ### Description
    The previous version of inliner supported inlining only for model-local
    functions. This PR extends the support for schema-defined functions as
    well.
    
    ### Motivation and Context
    The increasing focus on extending the ONNX standard via new function-ops
    makes it useful for users to be able to inline these functions.
    
    ---------
    
    Signed-off-by: Ganesan Ramalingam <grama@microsoft.com>
    Signed-off-by: gramalingam <grama@microsoft.com>

--- onnx/inliner/inliner.h
+++ onnx/inliner/inliner.h
@@ -9,6 +9,7 @@
 #include <utility>
 #include <vector>
 
+#include "onnx/defs/schema.h"
 #include "onnx/onnx_pb.h"
 
 namespace ONNX_NAMESPACE {
@@ -38,9 +39,37 @@ class FunctionIdSet {
   static std::unique_ptr<FunctionIdSet> Create(FunctionIdVector&& function_ids, bool invert = false);
 };
 
-// Inlines the model-local functions in the given model that are in the given set.
-// The inlined functions are removed from the model's list of functions as well.
+/**
+ * @brief Inlines the schema-defined functions in the given model that are in the given set.
+ * @param model The model in which functions will be inlined.
+ * @param to_inline The set of functions to inline.
+ * @param schema_registry The schema registry used for function lookup. If nullptr,
+ *        the default schema registry is used.
+ * @note Only call-sites in the main graph are inlined.
+ */
+void InlineSelectedFunctions(ModelProto& model, const FunctionIdSet& to_inline, const ISchemaRegistry* schema_registry);
 
+/**
+ * @brief Inlines the model-local functions in the given model that are in the given set.
+ *
+ * This function processes the model and replaces all call-sites of the specified
+ * model-local functions with their inlined implementations. The inlined functions
+ * are also removed from the model's list of functions.
+ *
+ * @param model The model in which functions will be inlined.
+ * @param to_inline The set of functions to inline.
+ *
+ * @note This function does not perform schema-defined function inlining. For schema-defined
+ *       function inlining, use InlineSelectedFunctions instead.
+ */
+void InlineSelectedLocalFunctions(ModelProto& model, const FunctionIdSet& to_inline);
+
+/**
+ * @brief Inlines the model-local functions in the given model that are in the given set.
+ * @deprecated This function is deprecated. Use InlineSelectedLocalFunctions instead,
+ * to avoid confusion with the overloaded version of InlineSelectedFunctions that
+ * inlines schema-defined functions as well.
+ */
 void InlineSelectedFunctions(ModelProto& model, const FunctionIdSet& to_inline);
 
 // Inlines all model-local functions in the given model. This supports version
--- onnx/test/inliner_test.py
+++ onnx/test/inliner_test.py
@@ -111,6 +111,22 @@ class InlinerTest(unittest.TestCase):
         self.assertEqual(function_nodes[0].op_type, "Add")
         self.assertEqual(function_nodes[1].op_type, "Mul")
 
+    def test_schema_function_inlining(self):
+        model = parser.parse_model(
+            """
+            <ir_version: 8, opset_import: [ "" : 20]>
+            agraph (float[N] X) => (float[N] Y)
+            {
+                Y = Softsign (X)
+            }
+        """
+        )
+        inlined = inliner.inline_selected_functions(
+            model, [], exclude=True, inline_schema_functions=True
+        )
+        inlined_nodes = inlined.graph.node
+        self.assertIn("Abs", [n.op_type for n in inlined_nodes])
+
 
 if __name__ == "__main__":
     unittest.main()
