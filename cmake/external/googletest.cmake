450efe92245c1e7242bc136e3e38e156cc58b686
cyy <cyyever@outlook.com>
2025-05-20 15:14:39 +0800

    gtest_force_shared_crt is not necessary
    Signed-off-by: cyy <cyyever@outlook.com>

--- cmake/external/googletest.cmake
+++ cmake/external/googletest.cmake
@@ -8,6 +8,4 @@ FetchContent_Declare(
   # Specify the commit you depend on and update it regularly.
   URL https://github.com/google/googletest/releases/download/v1.17.0/googletest-1.17.0.tar.gz
 )
-# For Windows: Prevent overriding the parent project's compiler/linker settings
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
