a2b86bc285e5aa4c14e01250b2d6055ea04efe2e
Yuanyuan Chen <cyyever@outlook.com>
2025-05-22 22:12:34 +0800

    Use [[maybe_unused]] (#6988)
    ### Description
    <!-- - Describe your changes. -->
    For all C++17 compilers we support, it's possible to use this attribute.
    ### Motivation and Context
    Remove old code.
    <!-- - Why is this change required? What problem does it solve? -->
    <!-- - If it fixes an open issue, please link to the issue here. -->
    
    Signed-off-by: cyy <cyyever@outlook.com>

--- onnx/defs/schema.h
+++ onnx/defs/schema.h
@@ -1312,11 +1312,7 @@ class DbgOperatorSetTracker {
 // Helper function
 size_t ReplaceAll(std::string& s, const char* from, const char* to);
 
-#ifdef __GNUC__
-#define ONNX_UNUSED __attribute__((__unused__))
-#else
-#define ONNX_UNUSED
-#endif
+#define ONNX_UNUSED [[maybe_unused]]
 
 // Legacy macros to register schema at static initialization
 #define ONNX_OPERATOR_SCHEMA(name) ONNX_OPERATOR_SCHEMA_UNIQ_HELPER(__COUNTER__, name)
