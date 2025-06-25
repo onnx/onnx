799c8d3dfcef6f8b1199eed366a4b5f796d6be84
Clay Elmore <celmore25@gmail.com>
2025-06-24 21:01:54 -0700

    fix: update linting and type checking in cumprod node test cases
    Signed-off-by: Clay Elmore <celmore25@gmail.com>

--- onnx/backend/test/case/node/cumprod.py
+++ onnx/backend/test/case/node/cumprod.py
@@ -15,7 +15,7 @@ class CumProd(Base):
     def export_cumprod_1d() -> None:
         node = onnx.helper.make_node("CumProd", inputs=["x", "axis"], outputs=["y"])
         x = np.array([1.0, 2.0, 3.0, 4.0, 5.0]).astype(np.float64)
-        axis = np.int32(0)
+        axis = np.array(0, dtype=np.int32)
         y = np.array([1.0, 2.0, 6.0, 24.0, 120.0]).astype(np.float64)
         expect(node, inputs=[x, axis], outputs=[y], name="test_cumprod_1d")
 
@@ -25,7 +25,7 @@ class CumProd(Base):
             "CumProd", inputs=["x", "axis"], outputs=["y"], exclusive=1
         )
         x = np.array([1.0, 2.0, 3.0, 4.0, 5.0]).astype(np.float64)
-        axis = np.int32(0)
+        axis = np.array(0, dtype=np.int32)
         y = np.array([1.0, 1.0, 2.0, 6.0, 24.0]).astype(np.float64)
         expect(node, inputs=[x, axis], outputs=[y], name="test_cumprod_1d_exclusive")
 
@@ -35,7 +35,7 @@ class CumProd(Base):
             "CumProd", inputs=["x", "axis"], outputs=["y"], reverse=1
         )
         x = np.array([1.0, 2.0, 3.0, 4.0, 5.0]).astype(np.float64)
-        axis = np.int32(0)
+        axis = np.array(0, dtype=np.int32)
         y = np.array([120.0, 120.0, 60.0, 20.0, 5.0]).astype(np.float64)
         expect(node, inputs=[x, axis], outputs=[y], name="test_cumprod_1d_reverse")
 
@@ -45,10 +45,13 @@ class CumProd(Base):
             "CumProd", inputs=["x", "axis"], outputs=["y"], reverse=1, exclusive=1
         )
         x = np.array([1.0, 2.0, 3.0, 4.0, 5.0]).astype(np.float64)
-        axis = np.int32(0)
+        axis = np.array(0, dtype=np.int32)
         y = np.array([120.0, 60.0, 20.0, 5.0, 1.0]).astype(np.float64)
         expect(
-            node, inputs=[x, axis], outputs=[y], name="test_cumprod_1d_reverse_exclusive"
+            node,
+            inputs=[x, axis],
+            outputs=[y],
+            name="test_cumprod_1d_reverse_exclusive",
         )
 
     @staticmethod
@@ -59,8 +62,12 @@ class CumProd(Base):
             outputs=["y"],
         )
         x = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).astype(np.float64).reshape((2, 3))
-        axis = np.int32(0)
-        y = np.array([1.0, 2.0, 3.0, 4.0, 10.0, 18.0]).astype(np.float64).reshape((2, 3))
+        axis = np.array(0, dtype=np.int32)
+        y = (
+            np.array([1.0, 2.0, 3.0, 4.0, 10.0, 18.0])
+            .astype(np.float64)
+            .reshape((2, 3))
+        )
         expect(node, inputs=[x, axis], outputs=[y], name="test_cumprod_2d_axis_0")
 
     @staticmethod
@@ -71,8 +78,12 @@ class CumProd(Base):
             outputs=["y"],
         )
         x = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).astype(np.float64).reshape((2, 3))
-        axis = np.int32(1)
-        y = np.array([1.0, 2.0, 6.0, 4.0, 20.0, 120.0]).astype(np.float64).reshape((2, 3))
+        axis = np.array(1, dtype=np.int32)
+        y = (
+            np.array([1.0, 2.0, 6.0, 4.0, 20.0, 120.0])
+            .astype(np.float64)
+            .reshape((2, 3))
+        )
         expect(node, inputs=[x, axis], outputs=[y], name="test_cumprod_2d_axis_1")
 
     @staticmethod
@@ -83,9 +94,15 @@ class CumProd(Base):
             outputs=["y"],
         )
         x = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).astype(np.float64).reshape((2, 3))
-        axis = np.int32(-1)
-        y = np.array([1.0, 2.0, 6.0, 4.0, 20.0, 120.0]).astype(np.float64).reshape((2, 3))
-        expect(node, inputs=[x, axis], outputs=[y], name="test_cumprod_2d_negative_axis")
+        axis = np.array(-1, dtype=np.int32)
+        y = (
+            np.array([1.0, 2.0, 6.0, 4.0, 20.0, 120.0])
+            .astype(np.float64)
+            .reshape((2, 3))
+        )
+        expect(
+            node, inputs=[x, axis], outputs=[y], name="test_cumprod_2d_negative_axis"
+        )
 
     @staticmethod
     def export_cumprod_2d_int32() -> None:
@@ -95,7 +112,7 @@ class CumProd(Base):
             outputs=["y"],
         )
         x = np.array([1, 2, 3, 4, 5, 6]).astype(np.int32).reshape((2, 3))
-        axis = np.int32(0)
+        axis = np.array(0, dtype=np.int32)
         y = np.array([1, 2, 3, 4, 10, 18]).astype(np.int32).reshape((2, 3))
         expect(node, inputs=[x, axis], outputs=[y], name="test_cumprod_2d_int32")
 
@@ -105,7 +122,7 @@ class CumProd(Base):
             "CumProd", inputs=["x", "axis"], outputs=["y"], exclusive=1
         )
         x = np.array([1, 2, 3, 4, 5]).astype(np.int32)
-        axis = np.int32(0)
+        axis = np.array(0, dtype=np.int32)
         y = np.array([1, 1, 2, 6, 24]).astype(np.int32)
         expect(
             node, inputs=[x, axis], outputs=[y], name="test_cumprod_1d_int32_exclusive"
