9d3fb288ee90d8b41bf51088012be1db9e544194
Justin Chu <justinchu@microsoft.com>
2025-06-25 22:20:55 -0700

    _saturating_cast
    Signed-off-by: Justin Chu <justinchu@microsoft.com>

--- onnx/reference/ops/op_cast.py
+++ onnx/reference/ops/op_cast.py
@@ -3,20 +3,47 @@
 # SPDX-License-Identifier: Apache-2.0
 from __future__ import annotations
 
+import ml_dtypes
 import numpy as np
 
 import onnx
 from onnx.reference.op_run import OpRun
 
 
-def cast_to(x, to, saturate: bool):
-    # TODO(justinchuby): Implement support for saturate=False
-    if not saturate:
-        raise NotImplementedError("saturate=False is not supported yet.")
+def _saturating_cast(x: np.ndarray, dtype: np.dtype) -> np.ndarray:
+    """Saturating cast for float8 and float4 types.
+
+    This function ensures that values outside the representable range
+    of the target dtype are clamped to the maximum or minimum representable
+    value of that dtype.
+    """
+    max_val = ml_dtypes.finfo(dtype).max
+    min_val = ml_dtypes.finfo(dtype).min
+    max_mask = x > max_val
+    min_mask = x < min_val
+    x = x.astype(dtype)
+    x[max_mask] = max_val
+    x[min_mask] = min_val
+    return x
+
+
+def cast_to(x: np.ndarray, to: onnx.TensorProto.DataType, saturate: bool):
     if to == onnx.TensorProto.STRING:
         return x.astype(np.str_)
 
     dtype = onnx.helper.tensor_dtype_to_np_dtype(to)
+    if (
+        to
+        in {
+            onnx.TensorProto.FLOAT8E4M3FN,
+            onnx.TensorProto.FLOAT8E4M3FNUZ,
+            onnx.TensorProto.FLOAT8E5M2,
+            onnx.TensorProto.FLOAT8E5M2FNUZ,
+        }
+        and saturate
+    ):
+        return _saturating_cast(x, dtype)
+
     return x.astype(dtype)
 
 
