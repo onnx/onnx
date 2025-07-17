4597778547509b87eb6732c01b62c031ba749887
xadupre <xadupre@microsoft.com>
2025-07-17 17:43:35 +0200

    fix bracket
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- onnx/reference/ops/op_trilu.py
+++ onnx/reference/ops/op_trilu.py
@@ -10,7 +10,7 @@ from onnx.reference.op_run import OpRun
 
 class Trilu(OpRun):
     def _run(self, x, k=None, upper=None):
-        k = 0 if k is None else k.item())
+        k = 0 if k is None else k.item()
         if upper:
             return (np.triu(x, k),)
         return (np.tril(x, k).astype(x.dtype),)
