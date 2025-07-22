4498f4fd9b298f70b5b76d358b33467998c1913b
Justin Chu <justinchuby@users.noreply.github.com>
2025-07-22 10:26:31 -0700

    generate models
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnx/defs/reduction/utils.cc
+++ onnx/defs/reduction/utils.cc
@@ -63,7 +63,7 @@ to `False` instead of `True`.)DOC";
           "noop_with_empty_axes",
           "Defines behavior when axes is not provided or is empty. "
           "If false (default), reduction happens over all axes. "
-          "If true, no reduction is applied, but other operations may be performed. For example, ReduceSumSquare acts as a vanilla Square.",
+          "If true, no reduction is applied, but other operations will be performed. For example, ReduceSumSquare acts as a vanilla Square.",
           AttributeProto::INT,
           static_cast<int64_t>(0));
       schema.Input(
@@ -73,7 +73,7 @@ to `False` instead of `True`.)DOC";
           "The default is to reduce over empty axes. "
           "When axes is empty (either not provided or explicitly empty), behavior depends on 'noop_with_empty_axes': "
           "reduction over all axes if 'noop_with_empty_axes' is false, "
-          "or no reduction is applied if 'noop_with_empty_axes' is true (but other operations may be performed). "
+          "or no reduction is applied if 'noop_with_empty_axes' is true (but other operations will be performed). "
           "Accepted range is [-r, r-1] where r = rank(data).",
           "tensor(int64)",
           OpSchema::Optional,
