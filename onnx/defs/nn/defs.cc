ba7b9c1baeffee487680a9760e66f1efcef91552
Yuan Yao <yuanyao@nvidia.com>
2025-07-29 15:14:15 -0700

    more lint
    Signed-off-by: Yuan Yao <yuanyao@nvidia.com>

--- onnx/defs/nn/defs.cc
+++ onnx/defs/nn/defs.cc
@@ -3807,12 +3807,12 @@ ONNX_OPERATOR_SET_SCHEMA(
               builder.Add("KVSeqLen = Shape <start = -2, end = -1> (K)");
             }
             builder
-                .Add("KVSeqLenExpanded = Unsqueeze(nonpad_kv_seqlen, One1D)") // [batch_size, 1]
-                .Add("Range = Range(Zero1D, KVSeqLen, One1D)") // [KVSeqLen,]
-                .Add("PaddingMaskBool = Less(Range, KVSeqLenExpanded)") // [batch_size, KVSeqLen]
-                .Add("PaddingMaskFloat = Where(PaddingMaskBool, ScalarZero, FloatNegInf)") // [batch_size, KVSeqLen]
-                .Add("PaddingMask3D = Unsqueeze(PaddingMaskFloat, One1D)") // [batch_size, 1, KVSeqLen]
-                .Add("PaddingMask4D = Unsqueeze(PaddingMask3D, One1D)") // [batch_size, 1, 1, KVSeqLen]
+                .Add("KVSeqLenExpanded = Unsqueeze(nonpad_kv_seqlen, One1D)")  // [batch_size, 1]
+                .Add("Range = Range(Zero1D, KVSeqLen, One1D)")  // [KVSeqLen,]
+                .Add("PaddingMaskBool = Less(Range, KVSeqLenExpanded)")  // [batch_size, KVSeqLen]
+                .Add("PaddingMaskFloat = Where(PaddingMaskBool, ScalarZero, FloatNegInf)")  // [batch_size, KVSeqLen]
+                .Add("PaddingMask3D = Unsqueeze(PaddingMaskFloat, One1D)")  // [batch_size, 1, KVSeqLen]
+                .Add("PaddingMask4D = Unsqueeze(PaddingMask3D, One1D)")  // [batch_size, 1, 1, KVSeqLen]
                 .Add("AttnBiasCausalPad = Add(AttnBiasCausal, PaddingMask4D)");
           } else {
             builder.Add("AttnBiasCausalPad = Identity(AttnBiasCausal)");
