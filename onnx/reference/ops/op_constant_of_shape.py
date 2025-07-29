50e01a7cd7973fa5f9ecb6a4a112ed1a0d48fa1d
Justin Chu <justinchuby@users.noreply.github.com>
2025-07-29 10:06:26 -0700

    [Reference] Fix constant of shape when input value is 0d
    Update the logic to handle when the input value is 0d.
    
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnx/reference/ops/op_constant_of_shape.py
+++ onnx/reference/ops/op_constant_of_shape.py
@@ -9,19 +9,14 @@ from onnx.reference.op_run import OpRun
 
 
 class ConstantOfShape(OpRun):
-    def __init__(self, onnx_node, run_params):
-        OpRun.__init__(self, onnx_node, run_params)
-        self.cst = self.value[0] if isinstance(self.value, np.ndarray) else self.value
-        if isinstance(self.cst, int):
-            self.cst = np.int64(self.cst)
-        elif isinstance(self.cst, float):
-            self.cst = np.float64(self.cst)
-        elif self.cst is None:
-            self.cst = np.float32(0)
+    def _run(self, data, value: np.array | None = None):
+        if self.value is None:
+            value = np.array(0, dtype=np.float32)
+        else:
+            value = self.value.item()
 
-    def _run(self, data, value=None):
         try:
-            res = np.full(tuple(data), self.cst)
+            res = np.full(tuple(data), value)
         except TypeError as e:
             raise RuntimeError(
                 f"Unable to create a constant of shape {data!r} with value {self.cst!r} "
