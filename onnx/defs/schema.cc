c96447178359f9f21df077531d366d9941d41c67
take-cheeze <takechi101010@gmail.com>
2025-07-29 19:16:04 +0900

    ENFORCE node determinism in context dependent functions
    Signed-off-by: take-cheeze <takechi101010@gmail.com>

--- onnx/defs/schema.cc
+++ onnx/defs/schema.cc
@@ -1534,6 +1534,10 @@ void OpSchema::Finalize() {
   ParseAndSetTypes(&inputs_);
   ParseAndSetTypes(&outputs_);
 
+  if (HasContextDependentFunction()) {
+    ENFORCE(node_determinism_ != NodeDeterminism::Unknown);
+  }
+
   for (auto& func : opset_version_to_function_body_) {
     BuildFunction(*func.second);
   }
