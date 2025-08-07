4ab17b45055766538783c752b04f4414c2ace73e
xadupre <xadupre@microsoft.com>
2025-08-07 16:34:16 +0200

    fix annotation
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- onnx/test/cpp/test_onnx2_threads.cc
+++ onnx/test/cpp/test_onnx2_threads.cc
@@ -306,3 +306,132 @@ TEST(onnx2_threads, ParallelModelProcessing4_FileExternalData) {
   std::remove(temp_filename.c_str());
   std::remove(temp_weights.c_str());
 }
+
+TEST(onnx2_threads, ParallelModelProcessing4_FileExternalDataManyInitializers) {
+  ModelProto model;
+
+  GraphProto &graph = model.add_graph();
+  graph.set_name("test_graph");
+
+  for (uint8_t i = 0; i < 100; ++i) {
+    TensorProto &weights = graph.add_initializer();
+    weights.set_name("weights");
+    weights.set_data_type(TensorProto::DataType::FLOAT);
+    weights.ref_dims().push_back(1);
+    weights.ref_dims().push_back(1);
+    weights.ref_raw_data().push_back(i);
+    weights.ref_raw_data().push_back(i);
+    weights.ref_raw_data().push_back(i);
+    weights.ref_raw_data().push_back(i);
+  }
+
+  NodeProto &node = graph.add_node();
+  node.set_name("test_node");
+  node.set_op_type("Add");
+  AttributeProto &attr = node.add_attribute();
+  attr.set_name("bias");
+  TensorProto &biasw = attr.ref_t();
+  biasw.set_name("biasw");
+  biasw.set_data_type(TensorProto::DataType::FLOAT);
+  biasw.ref_dims().push_back(1);
+  biasw.ref_dims().push_back(1);
+  biasw.ref_raw_data().push_back(232);
+  biasw.ref_raw_data().push_back(232);
+  biasw.ref_raw_data().push_back(232);
+  biasw.ref_raw_data().push_back(232);
+
+  NodeProto &nodeg = graph.add_node();
+  nodeg.set_name("test_graph");
+  nodeg.set_op_type("If");
+  AttributeProto &attrg = nodeg.add_attribute();
+  attrg.set_name("bias");
+  GraphProto &nested = attrg.add_g();
+
+  for (uint8_t i = 0; i < 100; ++i) {
+    TensorProto &weights2 = nested.add_initializer();
+    weights2.set_name("weights2");
+    weights2.set_data_type(TensorProto::DataType::FLOAT);
+    weights2.ref_dims().push_back(1);
+    weights2.ref_dims().push_back(1);
+    weights2.ref_raw_data().push_back(105 + i);
+    weights2.ref_raw_data().push_back(105 + i);
+    weights2.ref_raw_data().push_back(105 + i);
+    weights2.ref_raw_data().push_back(105 + i);
+  }
+
+  NodeProto &node2 = nested.add_node();
+  node2.set_name("test_node");
+  node2.set_op_type("Add");
+  AttributeProto &attr2 = node2.add_attribute();
+  attr2.set_name("bias");
+  TensorProto &biasw2 = attr2.ref_t();
+  biasw.set_name("biasw2");
+  biasw2.set_data_type(TensorProto::DataType::FLOAT);
+  biasw2.ref_dims().push_back(1);
+  biasw2.ref_dims().push_back(1);
+  biasw2.ref_raw_data().push_back(244);
+  biasw2.ref_raw_data().push_back(244);
+  biasw2.ref_raw_data().push_back(244);
+  biasw2.ref_raw_data().push_back(244);
+
+  std::string temp_filename = "test_tensor_file_stream_read.big.tmp";
+  std::string temp_weights = "test_tensor_file_stream_read.weight.big.tmp";
+
+  {
+    utils::TwoFilesWriteStream wstream(temp_filename, temp_weights);
+    SerializeOptions wopts;
+    wopts.raw_data_threshold = 2;
+    SerializeProtoToStream(model, wstream, wopts);
+  }
+
+  {
+    utils::TwoFilesWriteStream wstream(temp_filename, temp_weights);
+    SerializeOptions wopts;
+    wopts.raw_data_threshold = 2;
+    SerializeProtoToStream(model, wstream, wopts);
+  }
+
+  int64_t length;
+  {
+    std::ifstream file(temp_weights, std::ios::binary | std::ios::ate);
+    length = static_cast<int64_t>(file.tellg());
+  }
+  EXT_ENFORCE(length, 100 + 2 * 4);
+  {
+    std::ifstream file(temp_weights, std::ios::binary);
+    std::vector<uint8_t> buffer(length);
+    file.read(reinterpret_cast<char *>(buffer.data()), length);
+    for (size_t i = 0; i < buffer.size(); ++i) {
+      if (i < 4)
+        EXPECT_EQ(buffer[i], static_cast<uint8_t>(232)) << " at index " << i;
+      else if (i < 8)
+        EXPECT_EQ(buffer[i], static_cast<uint8_t>(244)) << " at index " << i;
+      else if (i < 408)
+        EXPECT_EQ(buffer[i], static_cast<uint8_t>((i - 8) / 4 + 105)) << " at index " << i;
+      else
+        EXPECT_EQ(buffer[i], static_cast<uint8_t>((i - 408) / 4)) << " at index " << i;
+    }
+  }
+
+  ModelProto model2;
+  {
+    ParseOptions options;
+    options.parallel = true;
+    options.num_threads = 2;
+    utils::TwoFilesStream rstream(temp_filename, temp_weights);
+    rstream.StartThreadPool(2);
+    ParseProtoFromStream(model2, rstream, options);
+    rstream.WaitForDelayedBlock();
+  }
+
+  EXPECT_EQ(model.ref_graph().ref_initializer().size(), model2.ref_graph().ref_initializer().size());
+  for (size_t i = 0; i < model.ref_graph().ref_initializer().size(); ++i) {
+    EXPECT_EQ(model.ref_graph().ref_initializer()[i].ref_raw_data(),
+              model2.ref_graph().ref_initializer()[i].ref_raw_data());
+    EXPECT_EQ(model.ref_graph().ref_initializer()[i].ref_name().as_string(),
+              model2.ref_graph().ref_initializer()[i].ref_name().as_string());
+  }
+
+  std::remove(temp_filename.c_str());
+  std::remove(temp_weights.c_str());
+}
