8c1699280dc732ace13b2d101e4b5de07af22dea
Yuan Yao <yuanyao@nvidia.com>
2025-08-19 13:33:08 -0700

    use static_cast
    Signed-off-by: Yuan Yao <yuanyao@nvidia.com>

--- onnx/defs/nn/defs.cc
+++ onnx/defs/nn/defs.cc
@@ -3808,8 +3808,8 @@ ONNX_OPERATOR_SET_SCHEMA(
             builder
                 .Add("KVSeqLenExpanded = Unsqueeze(nonpad_kv_seqlen, One1D)") // [batch_size, 1]
                 .Add("KVSeqLen0D = Squeeze(KVSeqLen)")
-                .Const("Zero0D", (int64_t)(0))
-                .Const("One0D", (int64_t)(1))
+                .Const("Zero0D", static_cast<int64_t>(0))
+                .Const("One0D", static_cast<int64_t>(1))
                 .Add("Range = Range(Zero0D, KVSeqLen0D, One0D)") // [KVSeqLen,]
                 .Add("PaddingMaskBool = Less(Range, KVSeqLenExpanded)") // [batch_size, KVSeqLen]
                 .Add("PaddingMaskFloat = Where(PaddingMaskBool, ScalarZero, FloatNegInf)") // [batch_size, KVSeqLen]
