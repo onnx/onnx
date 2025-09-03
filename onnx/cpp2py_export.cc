9ecf8a810049f2e2e5b2c59ee978e838f42b99e4
Justin Chu <justinchuby@users.noreply.github.com>
2025-09-03 11:19:20 -0700

    support pairs
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnx/cpp2py_export.cc
+++ onnx/cpp2py_export.cc
@@ -5,6 +5,7 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/operators.h>
 #include <nanobind/stl/function.h>
+#include <nanobind/stl/pair.h>
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/tuple.h>
 #include <nanobind/stl/unordered_map.h>
