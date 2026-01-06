7bec4797ba42c1bdb5476fd7a8386e9aa7d8d512
Justin Chu <justinchuby@users.noreply.github.com>
2026-01-06 10:37:02 -0800

    Update op_sigmoid.py
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnx/reference/ops/op_sigmoid.py
+++ onnx/reference/ops/op_sigmoid.py
@@ -9,15 +9,13 @@ from onnx.reference.ops._op import OpRunUnaryNum
 
 
 def sigmoid(x: np.ndarray) -> np.ndarray:
-    """
-    Numerically stable sigmoid implementation that supports scalars
-    (0-dimensional arrays) and higher-dimensional arrays.
-    """
+    """Numerically stable sigmoid implementation that supports scalars and nd-arrays."""
     pos_mask = x > 0
+    exp_x = np.exp(x)
     return np.where(
         pos_mask,
         1.0 / (1.0 + np.exp(-x)),
-        np.exp(x) / (1.0 + np.exp(x)),
+        exp_x / (1.0 + exp_x),
     )
 
 
@@ -26,4 +24,4 @@ class Sigmoid(OpRunUnaryNum):
         OpRunUnaryNum.__init__(self, onnx_node, run_params)
 
     def _run(self, X):
-        return (sigmoid(X),)
+        return (sigmoid(X).astype(X.dtype),)
