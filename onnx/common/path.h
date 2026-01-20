2c801e7121c656857fbfd5c2e9407e68c386d611
Andreas Fehlner <fehlner@arcor.de>
2026-01-20 17:03:02 +0100

    update
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>

--- onnx/common/path.h
+++ onnx/common/path.h
@@ -1,8 +1,6 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// Copyright (c) ONNX Project Contributors.
+// Copyright (c) ONNX Project Contributors
+//
+// SPDX-License-Identifier: Apache-2.0
 
 #pragma once
 
