9d89e56cc18bac630cf3a3b95bd49937c28bf97f
Joshua Rothe <21348884+rothej@users.noreply.github.com>
2026-02-20 19:02:30 -0700

    Moved common function compute_output_dim_for_range() to utils.h
    Signed-off-by: Joshua Rothe <21348884+rothej@users.noreply.github.com>

--- onnx/defs/generator/old.cc
+++ onnx/defs/generator/old.cc
@@ -1009,24 +1009,6 @@ ONNX_OPERATOR_SET_SCHEMA(
           }
         }));
 
-template <typename T>
-static int64_t
-compute_output_dim_for_range(const TensorProto* start, const TensorProto* limit, const TensorProto* delta) {
-  if (!start->dims().empty() || !limit->dims().empty() || !delta->dims().empty()) {
-    fail_shape_inference("Input to 'Range' op should be scalars (Tensor with only one element and shape empty)");
-  }
-
-  const auto start_data = ParseData<T>(start);
-  const auto limit_data = ParseData<T>(limit);
-  const auto delta_data = ParseData<T>(delta);
-
-  int64_t n = static_cast<int64_t>(ceil((1.0 * (limit_data[0] - start_data[0])) / delta_data[0]));
-
-  n = std::max<int64_t>(n, 0);
-
-  return n;
-}
-
 static const char* const Range_ver11_doc = kDoc_Range_ver11;
 
 ONNX_OPERATOR_SET_SCHEMA(
