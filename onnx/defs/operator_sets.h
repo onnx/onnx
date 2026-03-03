384b0e28b1ba3720640f7130d9716a9cf631dc41
Sunny Anand <Sunnyanand.979@gmail.com>
2026-03-02 18:05:50 -0600

    lint changes
    Signed-off-by: Sunny Anand <Sunnyanand.979@gmail.com>

--- onnx/defs/operator_sets.h
+++ onnx/defs/operator_sets.h
@@ -1459,7 +1459,7 @@ class OpSet_Onnx_ver26 {
 // Iterate over schema from ai.onnx version 27
 class OpSet_Onnx_ver27 {
  public:
-  static void ForEachSchema(std::function<void(OpSchema&&)> fn) { 
+  static void ForEachSchema(std::function<void(OpSchema&&)> fn) {
     // TODO: Remove after introducing the first schema to opset 27
     (void)fn;
   }
