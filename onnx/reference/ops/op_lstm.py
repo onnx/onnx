718e572373c03681a3166e38017c9da15b907564
Maxwell Collins <mcollins@cs.wisc.edu>
2026-05-10 20:16:08 -0700

    ReferenceEvaluator implementation for LSTM direction attribute
    Signed-off-by: Maxwell Collins <mcollins@cs.wisc.edu>

--- onnx/reference/ops/op_lstm.py
+++ onnx/reference/ops/op_lstm.py
@@ -23,28 +23,27 @@ class CommonLSTM(OpRun):
     def h(self, x: np.ndarray) -> np.ndarray:
         return np.tanh(x)
 
-    def _step(
+    def _run_forward(
         self,
         X: np.ndarray,
+        W: np.ndarray,
         R: np.ndarray,
         B: np.ndarray,
-        W: np.ndarray,
+        P: np.ndarray,
         H_0: np.ndarray,
         C_0: np.ndarray,
-        P: np.ndarray,
-        num_directions: int,
     ) -> tuple[np.ndarray, np.ndarray]:
-        seq_length = X.shape[0]
-        hidden_size = H_0.shape[-1]
-        batch_size = X.shape[1]
+        """Run a forward pass of the LSTM.
 
-        Y = np.empty([seq_length, num_directions, batch_size, hidden_size])
+        Assumes that the num_directions axis has been squeezed out of the
+        inputs. (And returns Y, Yh without it.)
+        """
         h_list = []
 
         [p_i, p_o, p_f] = np.split(P, 3)
         H_t = H_0
         C_t = C_0
-        for x in np.split(X, X.shape[0], axis=0):
+        for x in X:
             gates = (
                 np.dot(x, np.transpose(W))
                 + np.dot(H_t, np.transpose(R))
@@ -61,15 +60,77 @@ class CommonLSTM(OpRun):
             H_t = H
             C_t = C
 
-        concatenated = np.concatenate(h_list)
-        if num_directions == 1:
-            Y[:, 0, :, :] = concatenated
+        Y = np.stack(h_list, axis=0)
+        Y_h = H_t
+        return Y, Y_h
 
-        if self.layout == 0:
-            Y_h = Y[-1]
+    def _step(
+        self,
+        X: np.ndarray,
+        R: np.ndarray,
+        B: np.ndarray,
+        W: np.ndarray,
+        H_0: np.ndarray,
+        C_0: np.ndarray,
+        P: np.ndarray,
+        num_directions: int,
+    ) -> tuple[np.ndarray, np.ndarray]:
+        if self.direction == "forward":
+            assert num_directions == 1
+            Y, Y_h = self._run_forward(
+                X,
+                W[0],
+                R[0],
+                B[0],
+                P[0],
+                H_0[0],
+                C_0[0],
+            )
+            # Add num_directions axis to outputs
+            Y = np.expand_dims(Y, 1)
+            Y_h = np.expand_dims(Y_h, 0)
+        elif self.direction == "reverse":
+            assert num_directions == 1
+            Y, Y_h = self._run_forward(
+                np.flip(X, axis=0),
+                W[0],
+                R[0],
+                B[0],
+                P[0],
+                H_0[0],
+                C_0[0],
+            )
+            Y = np.flip(Y, axis=0)
+            Y = np.expand_dims(Y, 1)
+            Y_h = np.expand_dims(Y_h, 0)
         else:
+            assert self.direction == "bidirectional"
+            assert num_directions == 2
+            Yf, Yf_h = self._run_forward(
+                X,
+                W[0],
+                R[0],
+                B[0],
+                P[0],
+                H_0[0],
+                C_0[0],
+            )
+            Yb, Yb_h = self._run_forward(
+                np.flip(X, axis=0),
+                W[1],
+                R[1],
+                B[1],
+                P[1],
+                H_0[1],
+                C_0[1],
+            )
+            Yb = np.flip(Yb, axis=0)
+            Y = np.stack([Yf, Yb], axis=1)
+            Y_h = np.stack([Yf_h, Yb_h], axis=0)
+
+        if self.layout:
             Y = np.transpose(Y, [2, 0, 1, 3])
-            Y_h = Y[:, :, -1, :]
+            Y_h = np.transpose(Y_h, [1, 0, 2])
 
         return Y, Y_h
 
@@ -79,7 +140,7 @@ class CommonLSTM(OpRun):
         W,
         R,
         B=None,
-        sequence_lens=None,
+        sequence_lens=None,  # noqa: ARG002
         initial_h=None,
         initial_c=None,
         P=None,
@@ -98,50 +159,19 @@ class CommonLSTM(OpRun):
 
         num_directions = W.shape[0]
 
-        if num_directions == 1:
-            R = np.squeeze(R, axis=0)
-            W = np.squeeze(W, axis=0)
-            if B is not None and len(B.shape) > 0 and B.shape[0] == 1:
-                B = np.squeeze(B, axis=0)
-            if (
-                sequence_lens is not None
-                and len(sequence_lens.shape) > 0
-                and sequence_lens.shape[0] == 1
-            ):
-                sequence_lens = np.squeeze(sequence_lens, axis=0)
-            if (
-                initial_h is not None
-                and len(initial_h.shape) > 0
-                and initial_h.shape[0] == 1
-            ):
-                initial_h = np.squeeze(initial_h, axis=0)
-            if (
-                initial_c is not None
-                and len(initial_c.shape) > 0
-                and initial_c.shape[0] == 1
-            ):
-                initial_c = np.squeeze(initial_c, axis=0)
-            if P is not None and len(P.shape) > 0 and P.shape[0] == 1:
-                P = np.squeeze(P, axis=0)
+        hidden_size = R.shape[-1]
 
-            hidden_size = R.shape[-1]
-            batch_size = X.shape[1]
-
-            if self.layout != 0:
-                X = np.swapaxes(X, 0, 1)
-            if B is None:
-                B = np.zeros(2 * n_gates * hidden_size, dtype=np.float32)
-            if P is None:
-                P = np.zeros(number_of_peepholes * hidden_size, dtype=np.float32)
-            if initial_h is None:
-                initial_h = np.zeros((batch_size, hidden_size), dtype=np.float32)
-            if initial_c is None:
-                initial_c = np.zeros((batch_size, hidden_size), dtype=np.float32)
-        else:
-            raise NotImplementedError(  # pragma: no cover
-                f"Unsupported value {num_directions!r} for num_directions "
-                f"and operator {self.__class__.__name__!r}."
-            )
+        if self.layout != 0:
+            X = np.swapaxes(X, 0, 1)
+        batch_size = X.shape[1]
+        if B is None:
+            B = np.zeros((num_directions, 2 * n_gates * hidden_size), dtype=X.dtype)
+        if P is None:
+            P = np.zeros((num_directions, number_of_peepholes * hidden_size), dtype=X.dtype)
+        if initial_h is None:
+            initial_h = np.zeros((num_directions, batch_size, hidden_size), dtype=X.dtype)
+        if initial_c is None:
+            initial_c = np.zeros((num_directions, batch_size, hidden_size), dtype=X.dtype)
 
         Y, Y_h = self._step(
             X, R, B, W, initial_h, initial_c, P, num_directions=num_directions
