18f93b455eccdef1ca30834329d2e4f5a5c5d36e
Maxwell Collins <mcollins@cs.wisc.edu>
2026-05-10 17:02:34 -0700

    Helper to run one direction (toward bidirectional support)
    Signed-off-by: Maxwell Collins <mcollins@cs.wisc.edu>

--- onnx/backend/test/case/node/gru.py
+++ onnx/backend/test/case/node/gru.py
@@ -32,35 +32,23 @@ class GRUHelper:
         self.num_directions = params[W].shape[0]
         self.direction = params.get(DIRECTION, "forward")
 
-        if self.num_directions == 1:
-            for k, v in params.items():
-                if k != X:
-                    params[k] = np.squeeze(v, axis=0)
+        hidden_size = params[R].shape[-1]
 
-            hidden_size = params[R].shape[-1]
+        layout = params.get(LAYOUT, 0)
+        x = params[X]
+        x = x if layout == 0 else np.swapaxes(x, 0, 1)
+        batch_size = x.shape[1]
+        b = params.get(B, np.zeros((self.num_directions, 2 * number_of_gates * hidden_size)))
+        h_0 = params.get(H_0,  np.zeros((self.num_directions, batch_size, hidden_size)))
+        lbr = params.get(LBR, 0)
 
-            layout = params.get(LAYOUT, 0)
-            x = params[X]
-            x = x if layout == 0 else np.swapaxes(x, 0, 1)
-            batch_size = x.shape[1]
-            b = (
-                params[B]
-                if B in params
-                else np.zeros(2 * number_of_gates * hidden_size)
-            )
-            h_0 = params[H_0] if H_0 in params else np.zeros((batch_size, hidden_size))
-            lbr = params.get(LBR, 0)
-
-            self.X = x
-            self.W = params[W]
-            self.R = params[R]
-            self.B = b
-            self.H_0 = h_0
-            self.LBR = lbr
-            self.LAYOUT = layout
-
-        else:
-            raise NotImplementedError
+        self.X = x
+        self.W = params[W]
+        self.R = params[R]
+        self.B = b
+        self.H_0 = h_0
+        self.LBR = lbr
+        self.LAYOUT = layout
 
     def f(self, x: np.ndarray) -> np.ndarray:
         return 1 / (1 + np.exp(-x))
@@ -68,25 +56,29 @@ class GRUHelper:
     def g(self, x: np.ndarray) -> np.ndarray:
         return np.tanh(x)
 
-    def step(self) -> tuple[np.ndarray, np.ndarray]:
-        seq_length = self.X.shape[0]
-        hidden_size = self.H_0.shape[-1]
-        batch_size = self.X.shape[1]
+    def _run_forward(self, X, R, B, W, H_0):
+        """Run a single forward pass of the GRU.
 
-        Y = np.empty([seq_length, self.num_directions, batch_size, hidden_size])
+        Assumes that the num_directions axis has been squeezed out of the
+        inputs. (And returns Y, Yh without it.)
+        """
+        seq_length = X.shape[0]
+        hidden_size = H_0.shape[-1]
+        batch_size = X.shape[1]
+
+        Y = np.empty([seq_length, batch_size, hidden_size])
         h_list = []
 
-        [w_z, w_r, w_h] = np.split(self.W, 3)
-        [r_z, r_r, r_h] = np.split(self.R, 3)
-        [w_bz, w_br, w_bh, r_bz, r_br, r_bh] = np.split(self.B, 6)
+        [w_z, w_r, w_h] = np.split(W, 3)
+        [r_z, r_r, r_h] = np.split(R, 3)
+        [w_bz, w_br, w_bh, r_bz, r_br, r_bh] = np.split(B, 6)
+
         gates_w = np.transpose(np.concatenate((w_z, w_r)))
         gates_r = np.transpose(np.concatenate((r_z, r_r)))
         gates_b = np.add(np.concatenate((w_bz, w_br)), np.concatenate((r_bz, r_br)))
 
-        H_t = self.H_0
-        assert self.direction in {"forward", "reverse"}
-        X_iter = self.X if self.direction == "forward" else np.flip(self.X, axis=0)
-        for x in np.split(X_iter, seq_length, axis=0):
+        H_t = H_0
+        for x in X:
             gates = np.dot(x, gates_w) + np.dot(H_t, gates_r) + gates_b
             z, r = np.split(gates, 2, -1)
             z = self.f(z)
@@ -107,13 +99,31 @@ class GRUHelper:
             h_list.append(H)
             H_t = H
 
-        if self.direction == "reverse":
-            h_list.reverse()
-        concatenated = np.concatenate(h_list)
-        if self.num_directions == 1:
-            Y[:, 0, :, :] = concatenated
-
+        Y = np.stack(h_list, axis=0)
         Y_h = H_t
+        return Y, Y_h
+
+    def step(self) -> tuple[np.ndarray, np.ndarray]:
+        if self.direction == "forward":
+            Y, Y_h = self._run_forward(
+                self.X,
+                np.squeeze(self.R, axis=0),
+                np.squeeze(self.B, axis=0),
+                np.squeeze(self.W, axis=0),
+                np.squeeze(self.H_0, axis=0),
+            )
+        elif self.direction == "reverse":
+            Y, Y_h = self._run_forward(
+                np.flip(self.X, axis=0),
+                np.squeeze(self.R, axis=0),
+                np.squeeze(self.B, axis=0),
+                np.squeeze(self.W, axis=0),
+                np.squeeze(self.H_0, axis=0),
+            )
+            Y = np.flip(Y, axis=0)
+        Y = np.expand_dims(Y, 1)
+        Y_h = np.expand_dims(Y_h, 0)
+
         if self.LAYOUT:
             Y = np.transpose(Y, [2, 0, 1, 3])
             Y_h = np.transpose(Y_h, [1, 0, 2])
