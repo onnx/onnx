d62e873e4a7017596d8a3e2b2b1c5c9217195bbe
pratishtha <182654574+Pratishtha5@users.noreply.github.com>
2026-05-15 22:44:32 +0530

    minor fixes
    Signed-off-by: pratishtha <182654574+Pratishtha5@users.noreply.github.com>

--- onnx/test/utils_test.py
+++ onnx/test/utils_test.py
@@ -79,66 +79,65 @@ class TestUtilityFunctions(unittest.TestCase):
             ):
                 onnx.utils._tar_members_filter(tar, base)
 
+    def test_get_model_initializers_size(self) -> None:
+        # Empty
+        graph_empty = helper.make_graph([], "test_empty", [], [])
+        model_empty = helper.make_model(graph_empty)
+        self.assertEqual(utils.get_model_initializers_size(model_empty), 0)
 
-def test_get_model_initializers_size(self) -> None:
-    # Empty
-    graph_empty = helper.make_graph([], "test_empty", [], [])
-    model_empty = helper.make_model(graph_empty)
-    self.assertEqual(utils.get_model_initializers_size(model_empty), 0)
+        # Standard (FLOAT=4 bytes, INT64=8 bytes)
+        t1 = helper.make_tensor(
+            "t1", TensorProto.FLOAT, [2, 2], [1.0, 2.0, 3.0, 4.0]
+        )  # 16 bytes
+        t2 = helper.make_tensor("t2", TensorProto.INT64, [3], [1, 2, 3])  # 24 bytes
+        graph_std = helper.make_graph([], "test_std", [], [], initializer=[t1, t2])
+        model_std = helper.make_model(graph_std)
+        self.assertEqual(utils.get_model_initializers_size(model_std), 40)
 
-    # Standard (FLOAT=4 bytes, INT64=8 bytes)
-    t1 = helper.make_tensor(
-        "t1", TensorProto.FLOAT, [2, 2], [1.0, 2.0, 3.0, 4.0]
-    )  # 16 bytes
-    t2 = helper.make_tensor("t2", TensorProto.INT64, [3], [1, 2, 3])  # 24 bytes
-    graph_std = helper.make_graph([], "test_std", [], [], initializer=[t1, t2])
-    model_std = helper.make_model(graph_std)
-    self.assertEqual(utils.get_model_initializers_size(model_std), 40)
+        # Raw Data
+        t3 = helper.make_tensor("t3", TensorProto.FLOAT, [2], b"12345678", raw=True)
+        graph_raw = helper.make_graph([], "test_raw", [], [], initializer=[t3])
+        model_raw = helper.make_model(graph_raw)
+        self.assertEqual(utils.get_model_initializers_size(model_raw), 8)
 
-    # Raw Data
-    t3 = helper.make_tensor("t3", TensorProto.FLOAT, [2], b"12345678", raw=True)
-    graph_raw = helper.make_graph([], "test_raw", [], [], initializer=[t3])
-    model_raw = helper.make_model(graph_raw)
-    self.assertEqual(utils.get_model_initializers_size(model_raw), 8)
+        # INT4 Packed Tensor
+        t4 = helper.make_tensor(
+            "int4_tensor",
+            TensorProto.INT4,
+            [5],
+            [1, 2, 3, 4, 5],
+        )
+        graph_int4 = helper.make_graph([], "test_int4", [], [], initializer=[t4])
+        model_int4 = helper.make_model(graph_int4)
 
-    # INT4 Packed Tensor
-    t4 = helper.make_tensor(
-        "int4_tensor",
-        TensorProto.INT4,
-        [5],
-        [1, 2, 3, 4, 5],
-    )
-    graph_int4 = helper.make_graph([], "test_int4", [], [], initializer=[t4])
-    model_int4 = helper.make_model(graph_int4)
+        # ceil(5 * 0.5) = 3 bytes
+        self.assertEqual(utils.get_model_initializers_size(model_int4), 3)
 
-    # ceil(5 * 0.5) = 3 bytes
-    self.assertEqual(utils.get_model_initializers_size(model_int4), 3)
+        # UINT2 Packed Tensor
+        t5 = helper.make_tensor(
+            "uint2_tensor",
+            TensorProto.UINT2,
+            [7],
+            [0, 1, 2, 3, 0, 1, 2],
+        )
+        graph_uint2 = helper.make_graph([], "test_uint2", [], [], initializer=[t5])
+        model_uint2 = helper.make_model(graph_uint2)
 
-    # UINT2 Packed Tensor
-    t5 = helper.make_tensor(
-        "uint2_tensor",
-        TensorProto.UINT2,
-        [7],
-        [0, 1, 2, 3, 0, 1, 2],
-    )
-    graph_uint2 = helper.make_graph([], "test_uint2", [], [], initializer=[t5])
-    model_uint2 = helper.make_model(graph_uint2)
+        # ceil(7 * 0.25) = 2 bytes
+        self.assertEqual(utils.get_model_initializers_size(model_uint2), 2)
 
-    # ceil(7 * 0.25) = 2 bytes
-    self.assertEqual(utils.get_model_initializers_size(model_uint2), 2)
+        # STRING Tensor
+        t6 = helper.make_tensor(
+            "str_tensor",
+            TensorProto.STRING,
+            [3],
+            [b"abc", b"de", b"f"],
+        )
+        graph_string = helper.make_graph([], "test_string", [], [], initializer=[t6])
+        model_string = helper.make_model(graph_string)
 
-    # STRING Tensor
-    t6 = helper.make_tensor(
-        "str_tensor",
-        TensorProto.STRING,
-        [3],
-        [b"abc", b"de", b"f"],
-    )
-    graph_string = helper.make_graph([], "test_string", [], [], initializer=[t6])
-    model_string = helper.make_model(graph_string)
-
-    # 3 + 2 + 1 = 6 bytes
-    self.assertEqual(utils.get_model_initializers_size(model_string), 6)
+        # 3 + 2 + 1 = 6 bytes
+        self.assertEqual(utils.get_model_initializers_size(model_string), 6)
 
 
 if __name__ == "__main__":
