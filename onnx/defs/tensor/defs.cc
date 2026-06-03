440a6a646a32bae2d8c2f89dc089710dea1ee0ae
joshua <joshua.abraham@multicorewareinc.com>
2026-06-03 10:43:24 +0530

    docs: clarify antialias behavior for sizes input
    Signed-off-by: joshua <joshua.abraham@multicorewareinc.com>

--- onnx/defs/tensor/defs.cc
+++ onnx/defs/tensor/defs.cc
@@ -2233,7 +2233,7 @@ ONNX_OPERATOR_SET_SCHEMA(
         .Attr(
             "antialias",
             "If set to 1, \"linear\" and \"cubic\" interpolation modes will use an antialiasing filter when downscaling. "
-            "Antialiasing is achieved by stretching the resampling filter by a factor max(1, 1 / scale), where the scale may be provided explicitly through the 'scales' input or implicitly derived from the ratio of output size to input size when the 'sizes' input is used. This means that when downsampling, more input pixels contribute to an output pixel.",
+            "Antialiasing is achieved by stretching the resampling filter by a factor max(1, 1 / scale), where the scale may be provided explicitly through the \"scales\" input or implicitly derived from the ratio of output size to input size when the \"sizes\" input is used. This means that when downsampling, more input pixels contribute to an output pixel.",
             AttributeProto::INT,
             static_cast<int64_t>(0))
         .Attr(
