fix(reference): stabilize log-softmax evaluation
Signed-off-by: kadyrbekovhamit-cyber <288885044+kadyrbekovhamit-cyber@users.noreply.github.com>

onnx/reference/ops/op_log_softmax.py
@@ -10,6 +10,9 @@ from onnx.reference.ops.op_softmax import Softmax
 
 class LogSoftmax(Softmax):
     def _run(self, X):
-        Y = Softmax._run(self, X)[0]
-        np.log(Y, out=Y)
+        if X.size == 0:
+            return (X,)
+        tmp = X - X.max(axis=self.axis, keepdims=True)
+        Y = tmp - np.log(np.exp(tmp).sum(axis=self.axis, keepdims=True))
+        Y = Y.astype(X.dtype)
         return (Y,)

onnx/reference/ops/op_softmax_cross_entropy_loss.py
@@ -21,9 +21,10 @@ def softmaxcrossentropy(
 
     # compute log_softmax
     max_x = np.max(x, axis=1, keepdims=True)
-    exp_x = np.exp(x - max_x)
-    p = exp_x / np.sum(exp_x, axis=1, keepdims=True)
-    inp = np.log(p)
+    shifted_x = x - max_x
+    inp = shifted_x - np.log(
+        np.sum(np.exp(shifted_x), axis=1, keepdims=True)
+    )
     log_prob = None
     if get_log_prob is True:
         log_prob = np.copy(inp)