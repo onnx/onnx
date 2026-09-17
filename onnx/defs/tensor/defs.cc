58dacbbccfbd30e1c740d4c2b356cab40538e3e5
Christian Bourjau <cbourjau@users.noreply.github.com>
2026-09-17 13:22:49 +0200

    Reject invalid `batch_dim` attribute in GatherND (#8465)
    Currently, a negative (i.e., invalid) `batch_dim` causes an
    out-of-bounds read in the shape inference function of the GatherND
    operator.
    
    ### Motivation and Context
    
    I am currently running a brief audit of our shape inference function,
    and this one came up pretty easily.
    
    ---------
    
    Signed-off-by: Christian Bourjau <christian.bourjau@quantco.com>

--- onnx/defs/tensor/defs.cc
+++ onnx/defs/tensor/defs.cc
@@ -2801,6 +2801,9 @@ ONNX_OPERATOR_SET_SCHEMA(
                 "Both `data` and `indices` input tensors in GatherND op "
                 "need to have rank larger than 0.");
           }
+          if (batch_dims_data < 0) {
+            fail_shape_inference("attribute 'batch_dims' of 'GatherND' must not be negative");
+          }
 
           // cannot ascertain if the input shapes are valid if shape of
           // `indices` is missing last dimension value so return at this point
@@ -2821,7 +2824,7 @@ ONNX_OPERATOR_SET_SCHEMA(
             *ctx.getOutputType(0)->mutable_tensor_type()->mutable_shape()->add_dim() = indices_shape.dim(i);
           }
 
-          for (int i = static_cast<int>(last_index_dimension); i < data_rank; ++i) {
+          for (auto i = last_index_dimension; i < data_rank; ++i) {
             *ctx.getOutputType(0)->mutable_tensor_type()->mutable_shape()->add_dim() = data_shape.dim(i);
           }
         }));
